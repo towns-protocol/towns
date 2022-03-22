1e65ddc821701f53ff75ba417d83de6c1cc68bfc
Theo Tillberg <ttillberg@gmail.com>
2022-03-22 10:45:18 +0100

    use runtime overlay in dev mode

--- clients/web/app/src/main.tsx
+++ clients/web/app/src/main.tsx
@@ -1,6 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { App } from "./App";
+const isDev = import.meta.env.DEV;
+
+if (isDev) {
+  // Register runtime-error overlay
+  // From: https://github.com/vitejs/vite/issues/2076
+  const showErrorOverlay = (event: ErrorEvent) => {
+    // must be within function call because that's when the element is defined for sure.
+    const ErrorOverlay = customElements.get("vite-error-overlay");
+    // don't open outside vite environment
+    if (!ErrorOverlay) {
+      return;
+    }
+    console.log(event.error);
+    const overlay = new ErrorOverlay(event.error);
+    document.body.appendChild(overlay);
+  };
+
+  window.addEventListener("error", showErrorOverlay);
+  window.addEventListener("unhandledrejection", ({ reason }) =>
+    showErrorOverlay(reason)
+  );
+}
 
 ReactDOM.render(
   <React.StrictMode>
