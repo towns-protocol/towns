68ef25c036b4acfb4240d2bd091930b0f8e1badc
Brian Meek <brian@here.video>
2022-03-31 11:40:50 -0700

    Simple test for NodeManager, learning how to use fuzzing

--- contracts/test/NodeManager.t.sol
+++ contracts/test/NodeManager.t.sol
@@ -18,10 +18,12 @@
 pragma solidity ^0.8.0;
 
 import "ds-test/test.sol";
+import "forge-std/Vm.sol";
 import "src/NodeManager.sol";
 
 contract NodeManagerTest is DSTest {
     NodeManager public hello;
+    Vm vm = Vm(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D);
 
     function setUp() public {
         hello = new NodeManager();
@@ -38,7 +40,26 @@ contract NodeManagerTest is DSTest {
         );
     }
 
+    function testsupportsInterfaceFuzzed(bytes4 _interfaceId) public {
+        vm.assume(
+            _interfaceId != type(IERC1822ProxiableUpgradeable).interfaceId
+        );
+        vm.assume(_interfaceId != type(IAccessControlUpgradeable).interfaceId);
+
+        assertTrue(!hello.supportsInterface(_interfaceId));
+    }
+
     function testgetContractVersion() public {
         assertEq(hello.getContractVersion(), 1);
     }
+
+    /*
+    Simple test to play around with warp and assume cheat codes
+    function testWarp(uint8 _timestamp) public {
+        vm.assume(_timestamp == 0);
+        assertEq(block.timestamp, _timestamp);
+        vm.warp(100);
+        assertEq(block.timestamp, _timestamp + 100);
+    }
+    */
 }
