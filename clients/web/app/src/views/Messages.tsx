d8cdf7d446dac7ee4166d73d4ca6ed218c0e0cc6
Theo Tillberg <ttillberg@gmail.com>
2022-04-08 11:53:13 +0200

    Refactor messages

--- clients/web/app/src/views/Messages.tsx
+++ clients/web/app/src/views/Messages.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-import { ChatWindow } from "@components/ChatWindow";
-import { MessageList } from "@components/MessageList";
-import { Box } from "@ui";
+import { AvatarStack } from "@components/AvatarStack";
 import { MainNav, NavContainer } from "@components/MainNav/MainNav";
+import { MessageList } from "@components/MessageList";
+import { Avatar, Box, Heading, Icon, Paragraph } from "@ui";
 
 export const Messages = () => (
   <>
@@ -11,7 +11,91 @@ export const Messages = () => (
       <MessageList />
     </NavContainer>
     <Box grow="x9">
-      <ChatWindow />
+      <Box grow>
+        <Box
+          borderBottom
+          direction="row"
+          paddingX="sm"
+          height="md"
+          gap="xs"
+          alignItems="center"
+        >
+          <Avatar nft src="/placeholders/nft_4.png" />
+          <Paragraph size="lg" color="muted">
+            iamblue
+          </Paragraph>
+        </Box>
+        <Box grow justifyContent="end" padding="sm" gap="sm">
+          <Box borderBottom gap="sm" paddingBottom="sm">
+            <Avatar nft src="/placeholders/nft_4.png" size="xl" />
+            <Heading level={1}>iamblue</Heading>
+
+            <Box direction="row" alignItems="center" gap="xs">
+              <Paragraph size="lg" fontWeight="strong">
+                50
+              </Paragraph>
+              <Icon type="token" />
+              <Paragraph size="lg" color="muted1">
+                &#8226; 3 Servers in common
+              </Paragraph>
+
+              <AvatarStack />
+            </Box>
+
+            <Paragraph size="sm" color="muted1">
+              This is the beginning of your direct messa with iamblue
+            </Paragraph>
+          </Box>
+          <Box gap="sm">
+            <ChatMessage
+              name="You"
+              date="Today at 11:01 AM"
+              avatar={<Avatar nft src="/placeholders/nft_1.png" />}
+            >
+              gm
+            </ChatMessage>
+
+            <ChatMessage
+              name="iamblue"
+              date="Today at 11:01 AM"
+              avatar={<Avatar nft src="/placeholders/nft_4.png" />}
+            >
+              gm you
+            </ChatMessage>
+            <ChatMessage
+              name="You"
+              date="Today at 11:01 AM"
+              avatar={<Avatar nft src="/placeholders/nft_1.png" />}
+            >
+              gm gm
+            </ChatMessage>
+          </Box>
+        </Box>
+      </Box>
     </Box>
   </>
+);
+
+const ChatMessage = (props: {
+  avatar: React.ReactNode;
+  name: string;
+  date: string;
+  children: React.ReactText;
+}) => (
+  <Box direction="row" gap="xs">
+    {props.avatar}
+    <Box gap="xxs">
+      <Box direction="row" gap="xxs" alignItems="baseline">
+        <Paragraph size="lg" color="muted">
+          {props.name}
+        </Paragraph>
+        <Paragraph size="sm" color="muted1">
+          {props.date}
+        </Paragraph>
+      </Box>
+      <Box>
+        <Paragraph size="md">{props.children}</Paragraph>
+      </Box>
+    </Box>
+  </Box>
 );
