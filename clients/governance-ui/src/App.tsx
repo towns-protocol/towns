3e568750c2cae0def14368b0f491aeaefde45cb0
Pat Fives <pfives@gmail.com>
2022-04-19 23:01:03 -0500

    Add new governance client directory

---
+++ clients/governance-ui/src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Hello Human</p>
+      </header>
+    </div>
+  )
+}
+
+export default App
