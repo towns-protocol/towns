826b75dda4fac1db14e6dd8f6d1a15cb5fba4652
Theo Tillberg <ttillberg@gmail.com>
2022-05-11 11:20:09 +0200

    Add TS error checker + adapt overlay

--- clients/web/app/vite.config.ts
+++ clients/web/app/vite.config.ts
@@ -1,7 +1,7 @@
 import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-// import checker from "vite-plugin-checker";
+import checker from "vite-plugin-checker";
 import tsconfigPaths from "vite-tsconfig-paths";
 import eslintPlugin from "vite-plugin-eslint";
 
@@ -10,8 +10,13 @@ export default defineConfig({
   plugins: [
     react(),
     tsconfigPaths(),
-    // checker({ typescript: true }),
+    checker({ typescript: true }),
     eslintPlugin({ fix: true }),
     vanillaExtractPlugin(),
   ],
+  server: {
+    hmr: {
+      overlay: false,
+    },
+  },
 });
