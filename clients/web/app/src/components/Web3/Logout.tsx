9fa53a9649a3446d9e8093a0ca13a68f72fba22c
texuf <austin@hntlabs.com>
2022-05-10 21:24:42 -0700

    Add Logout to the Web3Bar

---
+++ clients/web/app/src/components/Web3/Logout.tsx
@@ -0,0 +1,17 @@
+import { useMatrixClient, useMatrixStore } from "use-matrix-client";
+import React, { useCallback } from "react";
+import { Button } from "@ui";
+
+export function Logout(): JSX.Element | null {
+  const { isAuthenticated } = useMatrixStore();
+  const { logout } = useMatrixClient();
+
+  const onLogout = useCallback(
+    async function () {
+      await logout();
+    },
+    [logout]
+  );
+
+  return isAuthenticated ? <Button onClick={onLogout}>Logout</Button> : null;
+}
--- clients/web/app/src/components/Web3/Web3Bar.tsx
+++ clients/web/app/src/components/Web3/Web3Bar.tsx
@@ -1,25 +1,40 @@
-import React from "react";
-import { useMatrixStore } from "use-matrix-client";
+import React, { useMemo } from "react";
+import { getShortUsername, useMatrixStore } from "use-matrix-client";
 import { Stack } from "@ui";
 import { Login } from "@components/Web3/Login";
+import { Logout } from "@components/Web3/Logout";
 
 export const Web3Bar = () => {
-  const { isAuthenticated } = useMatrixStore();
-  return (
-    <Stack
-      borderBottom
-      direction="row"
-      shrink={false}
-      height="height_xl"
-      paddingX="md"
-      background="level1"
-      alignItems="center"
-      gap="md"
-      color="gray2"
-      position="sticky"
-      style={{ top: 0, zIndex: 10 }}
-    >
-      {isAuthenticated ? "You're Authenticated to Web3!" : <Login />}
-    </Stack>
-  );
+  const { isAuthenticated, username } = useMatrixStore();
+
+  const myWalletAddress = useMemo(() => {
+    if (username) {
+      return getShortUsername(username);
+    }
+  }, [username]);
+
+    return (
+        <Stack
+            borderBottom
+            direction="row"
+            shrink={false}
+            height="height_xl"
+            paddingX="md"
+            background="level1"
+            alignItems="center"
+            gap="md"
+            color="gray2"
+            position="sticky"
+            style={{ top: 0, zIndex: 10 }}
+        >
+            {isAuthenticated ? (
+        <Stack direction="row" paddingX="md" gap="md" alignItems="center">
+          <p>"You're Authenticated to Web3! ({myWalletAddress})" </p>
+          <Logout />
+        </Stack>
+            ) : (
+                <Login />
+            )}
+        </Stack>
+    );
 };
