8f5dc8b99c59775762f9b5980302383cd02258af
Tak Wai Wong <takwaiw@gmail.com>
2022-05-23 14:18:07 -0700

    clean install

---
+++ clean.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+function yarn-clean-fn() {
+    pushd "$(git rev-parse --show-toplevel)"
+    echo "cleaning node"
+    find . -name "node_modules" -type d -exec rm -r "{}" \;
+    find . -name "dist" -type d -exec rm -r "{}" \;
+    find . -name "bin" -type d -exec rm -r "{}" \;
+    yarn cache clean
+    popd
+}
+
+yarn-clean-fn
