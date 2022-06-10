5a0b069630779da1e75f6eea1fe9a3e3e8607cd8
Brian Meek <brian@hntlabs.com>
2022-06-10 11:46:33 -0700

    Revert "Update clean.sh to really clean to git's view"
    This reverts commit 7ab8b604c229b1c9ad78a9f9613faebecf6e187c.

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 pushd "$(git rev-parse --show-toplevel)"
-echo "running git clean"
-git clean -f -x -d -i 
+echo "cleaning node"
+find . -name "node_modules" -type d -exec rm -r "{}" \;
+find . -name "dist" -type d -exec rm -r "{}" \;
+find . -name "bin" -type d -exec rm -r "{}" \;
 yarn cache clean
 popd
