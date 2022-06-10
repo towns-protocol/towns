f97d91264b587be8ab98b232e60acfae9ba49e35
Brian Meek <brian@hntlabs.com>
2022-06-10 10:29:26 -0700

    Update clean.sh to really clean to git's view

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 pushd "$(git rev-parse --show-toplevel)"
-echo "cleaning node"
-find . -name "node_modules" -type d -exec rm -r "{}" \;
-find . -name "dist" -type d -exec rm -r "{}" \;
-find . -name "bin" -type d -exec rm -r "{}" \;
+echo "running git clean"
+git clean -f -x -d -i 
 yarn cache clean
 popd
