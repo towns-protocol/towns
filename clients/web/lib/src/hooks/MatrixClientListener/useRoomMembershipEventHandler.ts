3e6238031b845237fe74afb3563d6487007353f4
texuf <austin@hntlabs.com>
2022-06-24 17:54:34 -0700

    Remove unnecessary sync membership effect
    Issue #115

--- clients/web/lib/src/hooks/MatrixClientListener/useRoomMembershipEventHandler.ts
+++ clients/web/lib/src/hooks/MatrixClientListener/useRoomMembershipEventHandler.ts
@@ -3,32 +3,10 @@ import { MutableRefObject, useCallback, useEffect, useState } from "react";
 import { Membership } from "../../types/matrix-types";
 import { useMatrixStore } from "../../store/use-matrix-store";
 
-interface SyncMembership {
-  roomId: string;
-  userId: string;
-  membership: Membership;
-}
-
 export const useRoomMembershipEventHandler = (
   matrixClientRef: MutableRefObject<MatrixClient | undefined>,
 ) => {
   const { joinRoom, leaveRoom, setRoom, updateMembership } = useMatrixStore();
-  const [syncInfo, setSyncInfo] = useState<SyncMembership>();
-
-  useEffect(() => {
-    if (matrixClientRef.current && syncInfo) {
-      const room = matrixClientRef.current.getRoom(syncInfo.roomId);
-      if (room) {
-        setRoom(room);
-      }
-      updateMembership(
-        syncInfo.roomId,
-        syncInfo.userId,
-        syncInfo.membership,
-        syncInfo.userId === matrixClientRef.current.getUserId(),
-      );
-    }
-  }, [matrixClientRef, setRoom, syncInfo, updateMembership]);
 
   const handleRoomMembershipEvent = useCallback(
     (event: MatrixEvent, member: RoomMember) => {
@@ -44,11 +22,16 @@ export const useRoomMembershipEventHandler = (
       }
       switch (member.membership) {
         case Membership.Invite: {
-          setSyncInfo({
-            roomId: member.roomId,
-            userId: member.userId,
-            membership: member.membership as Membership,
-          });
+          const room = matrixClientRef.current.getRoom(member.roomId);
+          if (room) {
+            setRoom(room);
+          }
+          updateMembership(
+            member.roomId,
+            member.userId,
+            member.membership as Membership,
+            member.userId === matrixClientRef.current.getUserId(),
+          );
           break;
         }
         case Membership.Join: {
@@ -72,7 +55,7 @@ export const useRoomMembershipEventHandler = (
           break;
       }
     },
-    [joinRoom, leaveRoom, matrixClientRef],
+    [joinRoom, leaveRoom, matrixClientRef, setRoom, updateMembership],
   );
 
   return handleRoomMembershipEvent;
