24ea4f4e74470d05168d3807975687f12e0c7ce0
texuf <austin@hntlabs.com>
2022-06-24 17:54:34 -0700

    Project updates for testing
    Issue #115
    
    This enables us to write tests with react components using the zion hooks
    
    Use jsdom
    Use react-jsx for rendering tests instead of react
    Add jest setup
    Add eth-testing for mocking metamask extension wallets

--- clients/web/lib/jest.config.ts
+++ clients/web/lib/jest.config.ts
@@ -1,8 +1,15 @@
 import type { Config } from "@jest/types";
-
 // Sync object
 const config: Config.InitialOptions = {
   verbose: true,
   preset: "ts-jest",
+  setupFilesAfterEnv: ["<rootDir>/jest-setup.ts"],
+  testEnvironment: "jsdom",
+  testEnvironmentOptions: { browsers: ["chrome", "firefox", "safari"] },
+  globals: {
+    "ts-jest": {
+      tsconfig: "<rootDir>/tsconfig.test.json", // the tests are failing with a react error otherwise, see https://stackoverflow.com/questions/70721310/react-is-undefind-in-jest-tests
+    },
+  },
 };
 export default config;
