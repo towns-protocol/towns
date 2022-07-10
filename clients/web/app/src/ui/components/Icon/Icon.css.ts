874277671fdab4b5c9438048b2f67ba736f295d9
Theo Tillberg <ttillberg@gmail.com>
2022-07-10 21:41:19 +0200

    Use round borders

--- clients/web/app/src/ui/components/Icon/Icon.css.ts
+++ clients/web/app/src/ui/components/Icon/Icon.css.ts
@@ -7,8 +7,8 @@ export const iconAtoms = createSprinkles(iconProperties);
 
 export const iconBaseStyle = style({
   borderRadius: vars.borderRadius.sm,
-  WebkitMaskImage: `url(/squircle.svg)`,
-  WebkitMaskSize: `cover`,
+  // WebkitMaskImage: `url(/squircle.svg)`,
+  // WebkitMaskSize: `cover`,
 });
 
 export type IconAtoms = Parameters<typeof iconAtoms>[0];
