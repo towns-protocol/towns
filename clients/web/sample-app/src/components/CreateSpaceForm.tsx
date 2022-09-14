78e62bf8169b2fafa31b8a4996d2a2162f064d7d
texuf <austin@hntlabs.com>
2022-09-14 16:59:50 -0700

    If on local chain, Add a fund wallet button to the sample app create space page (#428)

--- clients/web/sample-app/src/components/CreateSpaceForm.tsx
+++ clients/web/sample-app/src/components/CreateSpaceForm.tsx
@@ -16,16 +16,19 @@ import {
   RoomIdentifier,
   RoomVisibility,
   useIntegratedSpaceManagement,
+  useWeb3Context,
 } from "use-zion-client";
 import { useCallback, useMemo, useState } from "react";
 
 import { useAsyncButtonCallback } from "../hooks/use-async-button-callback";
+import { ethers } from "ethers";
 
 interface Props {
   onClick: (roomId: RoomIdentifier, membership: Membership) => void;
 }
 
 export const CreateSpaceForm = (props: Props) => {
+  const { chainId, accounts, getProvider } = useWeb3Context();
   const [spaceName, setSpaceName] = useState<string>("");
   const [visibility, setVisibility] = useState<RoomVisibility>(
     RoomVisibility.Private,
@@ -44,6 +47,32 @@ export const CreateSpaceForm = (props: Props) => {
       setSpaceName(event.target.value);
     },
     [],
+  );
+
+  const onClickFundWallet = useCallback(
+    (account: string) => {
+      const afunc = async () => {
+        const provider = getProvider();
+        if (!provider) {
+          throw new Error("No provider");
+        }
+        // anvil default funded address #1
+        const privateKey =
+          "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
+        const wallet = new ethers.Wallet(privateKey, provider);
+        const amount = 0.1;
+        const tx = {
+          from: wallet.address,
+          to: account,
+          value: ethers.utils.parseEther(amount.toString()),
+          gasLimit: 1000000,
+        };
+        const result = await wallet.sendTransaction(tx);
+        console.log(result);
+      };
+      void afunc();
+    },
+    [getProvider],
   );
 
   const onChangeVisibility = useCallback((event: SelectChangeEvent) => {
@@ -73,6 +102,10 @@ export const CreateSpaceForm = (props: Props) => {
     >
       <Typography variant="h6" noWrap component="div" sx={spacingStyle}>
         CREATE SPACE
+      </Typography>
+
+      <Typography variant="body1" noWrap component="div" sx={spacingStyle}>
+        ChainId: {chainId}
       </Typography>
       <Box display="grid" gridTemplateRows="repeat(5, 1fr)">
         <Box
@@ -131,6 +164,24 @@ export const CreateSpaceForm = (props: Props) => {
             Create
           </Button>
         </Box>
+        {chainId === "0x539" &&
+          accounts.map((accountId) => (
+            <Box
+              display="flex"
+              flexDirection="column"
+              alignItems="center"
+              key={accountId}
+            >
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={() => onClickFundWallet(accountId)}
+                disabled={false}
+              >
+                Fund Wallet (debug) ({accountId})
+              </Button>
+            </Box>
+          ))}
       </Box>
     </Box>
   );
