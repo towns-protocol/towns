9da5fa416a38d537de6fa15bfb3f0ae9208aed97
Brian Meek <brian@here.video>
2022-09-29 08:31:10 -0700

    Update dependencies (#543)
    Updates all dependencies across the yarn packages to the latest released
    versions.
    Switches to newer version of OLM, fixing security issues as well as
    adding better typescript support.
    Switches to newer version of jest and ts-jest, allowing for simpler
    tsconfig.json handling for tests.
    
    Tested with Tests, Sample App, and Web client, but there may be subtle bugs in the UI I didn't test.
    
    Signed-off-by: Brian Meek <brian@hntlabs.com>

--- clients/web/lib/jest-setup.ts
+++ clients/web/lib/jest-setup.ts
@@ -4,8 +4,8 @@ import "@testing-library/jest-dom/extend-expect";
 import { request as matrixRequest } from "matrix-js-sdk";
 import { ZionTestClient } from "./tests/integration/helpers/ZionTestClient";
 import { webcrypto } from "node:crypto";
-import * as Olm from "olm";
-import * as request from "request";
+import Olm from "@matrix-org/olm";
+import request from "request";
 import { configure } from "@testing-library/dom";
 
 process.env.HOMESERVER = "http://localhost:8008"; // OR "https://node1.zion.xyz";
@@ -13,13 +13,6 @@ process.env.DISABLE_ENCRYPTION = "false";
 process.env.ETHERS_NETWORK = "http://localhost:8545"; // OR "rinkeby"
 process.env.FUNDED_WALLET_PRIVATE_KEY_0 =
   "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"; // we need a wallet with assets to fund our test clients
-
-// This is here to extend the globalThis interface for loading Olm, should be in global.d.ts but
-// that wasn't working
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-interface globalThis {
-  Olm: typeof import("olm");
-}
 
 beforeAll(async () => {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
--- clients/web/lib/jest.config.ts
+++ clients/web/lib/jest.config.ts
@@ -6,10 +6,5 @@ const config: Config.InitialOptions = {
   setupFilesAfterEnv: ["<rootDir>/jest-setup.ts"],
   testEnvironment: "jsdom",
   testEnvironmentOptions: { browsers: ["chrome", "firefox", "safari"] },
-  globals: {
-    "ts-jest": {
-      tsconfig: "<rootDir>/tsconfig.test.json", // the tests are failing with a react error otherwise, see https://stackoverflow.com/questions/70721310/react-is-undefind-in-jest-tests
-    },
-  },
 };
 export default config;
