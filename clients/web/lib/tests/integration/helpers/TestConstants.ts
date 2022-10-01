b7903bbbe133ad93097639b1a0a095a9e704eeef
texuf <austin@hntlabs.com>
2022-10-01 09:36:41 -0700

    Fix NONCE_EXPIRED follow up (#585)
    I overlooked this on the previous commit, it only works if the wallet is
    a singleton.

--- clients/web/lib/tests/integration/helpers/TestConstants.ts
+++ clients/web/lib/tests/integration/helpers/TestConstants.ts
@@ -2,10 +2,14 @@
 import { ethers } from "ethers";
 
 export class TestConstants {
+  private static _funded_wallet_0: ethers.Wallet | undefined;
   public static get FUNDED_WALLET_0(): ethers.Wallet {
-    const network = process.env.ETHERS_NETWORK!;
-    const privateKey = process.env.FUNDED_WALLET_PRIVATE_KEY_0!;
-    const provider = new ethers.providers.JsonRpcProvider(network);
-    return new ethers.Wallet(privateKey, provider);
+    if (!this._funded_wallet_0) {
+      const network = process.env.ETHERS_NETWORK!;
+      const privateKey = process.env.FUNDED_WALLET_PRIVATE_KEY_0!;
+      const provider = new ethers.providers.JsonRpcProvider(network);
+      this._funded_wallet_0 = new ethers.Wallet(privateKey, provider);
+    }
+    return this._funded_wallet_0;
   }
 }
