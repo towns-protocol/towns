559b686d56073114297bd35de2720de400d44c8c
sergekh2 <104387024+sergekh2@users.noreply.github.com>
2022-10-19 09:40:22 -0700

    Remove sudo for docker requirement for start scripts (#668)

--- casablanca/scripts/launch.sh
+++ casablanca/scripts/launch.sh
@@ -1,8 +1,7 @@
 #!/bin/bash -ue
 cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")"
 
-echo "I can't figure out how to run Docker on Mac OS without sudo, so here you go:"
-sudo docker compose up --detach --wait
+docker compose up --detach --wait
 
 echo 
 echo "To test instance run tests with: yarn workspace @zion/server run test-remote"
--- casablanca/scripts/stop.sh
+++ casablanca/scripts/stop.sh
@@ -1,5 +1,4 @@
 #!/bin/bash -ue
 cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")"
 
-echo "I can't figure out how to run Docker on Mac OS without sudo, so here you go:"
-sudo docker compose down
+docker compose down
