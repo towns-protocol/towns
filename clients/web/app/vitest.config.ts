12e7a5c7d63c304a27d1e4c2591e69c2298b78ed
Evan Salzbrenner <evan@hntlabs.com>
2022-10-23 14:32:31 -0700

    configure app/vitest to work with use-zion-client (#717)

--- clients/web/app/vitest.config.ts
+++ clients/web/app/vitest.config.ts
@@ -5,9 +5,16 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
     test: {
+        setupFiles: ['./vitest.setup.ts'],
         environment: 'happy-dom',
         deps: {
             fallbackCJS: true,
+            inline: [
+                // workaround to allow tests to work with `use-zion-client`
+                // "Vite will process inlined modules. This could be helpful to handle packages that ship .js in ESM format (that Node can't handle)."
+                /..\/lib\/dist\/(.*\.js)$/,
+                'vitest-canvas-mock',
+            ],
         },
     },
     plugins: [tsconfigPaths(), vanillaExtractPlugin()],
