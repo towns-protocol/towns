35b3d230012a2e33ddf1607c60f4eabb2bd5dab9
texuf <austin@hntlabs.com>
2022-10-24 10:15:15 -0700

    Delete unused code (#715)
    I refactored how we instanciate zion client conracts, we no longer need
    to lookup the abi per chain.
    
    ref: https://github.com/HereNotThere/harmony/pull/711

--- clients/web/lib/src/client/web3/ZionContracts.ts
+++ clients/web/lib/src/client/web3/ZionContracts.ts
@@ -1,22 +1,13 @@
 /* eslint-disable no-restricted-imports */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import Goerli_SpaceManagerAddresses from '@harmony/contracts/goerli/addresses/space-manager.json'
-import Goerli_ZionSpaceManagerArtifact from '@harmony/contracts/goerli/abis/ZionSpaceManager.json'
-
-import Goerli_CouncilNFTArtifact from '@harmony/contracts/goerli/abis/CouncilNFT.json'
 import Goerli_CouncilAddresses from '@harmony/contracts/goerli/addresses/council.json'
 
 import Localhost_SpaceManagerAddresses from '@harmony/contracts/localhost/addresses/space-manager.json'
-import Localhost_ZionSpaceManagerArtifact from '@harmony/contracts/localhost/abis/ZionSpaceManager.json'
-
 import Localhost_CouncilAddresses from '@harmony/contracts/localhost/addresses/council.json'
-import Localhost_CouncilNFTArtifact from '@harmony/contracts/localhost/abis/CouncilNFT.json'
-
-import { ethers } from 'ethers'
 
 export interface IZionContractsInfo {
     spaceManager: {
-        abi: ethers.ContractInterface
         addresses: {
             spacemanager: string
             usergranted: string
@@ -24,7 +15,6 @@ export interface IZionContractsInfo {
         }
     }
     council: {
-        abi: ethers.ContractInterface
         addresses: { councilnft: string }
     }
 }
@@ -35,11 +25,9 @@ export function getContractInfo(chainId: number): IZionContractsInfo {
         case 5:
             return {
                 spaceManager: {
-                    abi: Goerli_ZionSpaceManagerArtifact.abi,
                     addresses: Goerli_SpaceManagerAddresses,
                 },
                 council: {
-                    abi: Goerli_CouncilNFTArtifact.abi,
                     addresses: Goerli_CouncilAddresses,
                 },
             }
@@ -47,11 +35,9 @@ export function getContractInfo(chainId: number): IZionContractsInfo {
         case 31337:
             return {
                 spaceManager: {
-                    abi: Localhost_ZionSpaceManagerArtifact.abi,
                     addresses: Localhost_SpaceManagerAddresses,
                 },
                 council: {
-                    abi: Localhost_CouncilNFTArtifact.abi,
                     addresses: Localhost_CouncilAddresses,
                 },
             }
@@ -64,11 +50,9 @@ export function getContractInfo(chainId: number): IZionContractsInfo {
             // return localhost, won't matter
             return {
                 spaceManager: {
-                    abi: Localhost_ZionSpaceManagerArtifact.abi,
                     addresses: { spacemanager: '', usergranted: '', tokengranted: '' },
                 },
                 council: {
-                    abi: Localhost_CouncilNFTArtifact.abi,
                     addresses: { councilnft: '' },
                 },
             }
