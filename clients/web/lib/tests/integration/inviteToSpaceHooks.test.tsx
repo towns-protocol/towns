bebf4da9ea653bc8fef89168264ba8f36a70c0be
Tak Wai Wong <64229756+tak-hntlabs@users.noreply.github.com>
2022-10-26 18:51:55 -0700

    Add act() in invite test to update DOM before assertions (#745)
    Test is printing lots of warning. Wrap it inside act()

--- clients/web/lib/tests/integration/inviteToSpaceHooks.test.tsx
+++ clients/web/lib/tests/integration/inviteToSpaceHooks.test.tsx
@@ -2,23 +2,27 @@
  * @jest-environment jsdom
  */
 /* eslint-disable @typescript-eslint/no-non-null-assertion, @typescript-eslint/no-explicit-any */
-import React, { useCallback } from 'react'
-import { useZionClient } from '../../src/hooks/use-zion-client'
-import { fireEvent, render, screen, waitFor } from '@testing-library/react'
-import { ZionTestApp } from './helpers/ZionTestApp'
 import { Membership, RoomVisibility, SpaceItem } from '../../src/types/matrix-types'
-import { registerAndStartClients } from './helpers/TestUtils'
+import React, { useCallback } from 'react'
+import { fireEvent, render, screen, waitFor } from '@testing-library/react'
+
 import { LoginWithWallet } from './helpers/TestComponents'
-import { useZionContext } from '../../src/components/ZionContextProvider'
+import { ZionTestApp } from './helpers/ZionTestApp'
+import { act } from 'react-dom/test-utils'
+import { registerAndStartClients } from './helpers/TestUtils'
 import { useMyMembership } from '../../src/hooks/use-my-membership'
+import { useZionClient } from '../../src/hooks/use-zion-client'
+import { useZionContext } from '../../src/components/ZionContextProvider'
 
 // TODO Zustand https://docs.pmnd.rs/zustand/testing
 // https://linear.app/hnt-labs/issue/HNT-38/invites-might-not-sync-show-properly-in-some-instances
 describe('inviteToSpace', () => {
     jest.setTimeout(60000)
-    test('user can receive an invite, display it, accept it', async () => {
+
+    test('user can receive an invite, display it, and accept it', async () => {
         // create clients
         const { jane, bob } = await registerAndStartClients(['jane', 'bob'])
+
         // create a wallet for bob
         const bobProvider = bob.provider
         const bobUserId = bob.matrixUserId!
@@ -48,50 +52,59 @@ describe('inviteToSpace', () => {
         // send an invite to bob while he's logged out (regression)
         await jane.inviteUser(janesSpaceId_1, bobUserId)
 
-        // create a veiw for bob
-        const TestSpaceInvitesComponent = () => {
-            const { joinRoom } = useZionClient()
-            const { spaces, invitedToIds } = useZionContext()
-            const myMembership1 = useMyMembership(janesSpaceId_1)
-            const myMembership2 = useMyMembership(janesSpaceId_2)
-            const myMembership3 = useMyMembership(janesChannelId_2)
+        // Wrap inside act() to makes sure all updates related to these “components”
+        // have been processed and applied to the DOM before making any assertions.
+        // Avoids warnings.
+        act(() => {
+            // create a veiw for bob
+            const TestSpaceInvitesComponent = () => {
+                const { joinRoom } = useZionClient()
+                const { spaces, invitedToIds } = useZionContext()
 
-            // accept the invites
-            const onClickAcceptInvite1 = useCallback(() => {
-                void joinRoom(janesSpaceId_1)
-            }, [joinRoom])
-            const onClickAcceptInvite2 = useCallback(() => {
-                void joinRoom(janesSpaceId_2)
-            }, [joinRoom])
-            const onClickAcceptInvite3 = useCallback(() => {
-                void joinRoom(janesChannelId_2)
-            }, [joinRoom])
-            // format for easy reading
-            function formatSpace(s: SpaceItem) {
-                return `${s.id.matrixRoomId}: ${s.name}`
+                const myMembership1 = useMyMembership(janesSpaceId_1)
+                const myMembership2 = useMyMembership(janesSpaceId_2)
+                const myMembership3 = useMyMembership(janesChannelId_2)
+
+                // accept the invites
+                const onClickAcceptInvite1 = useCallback(() => {
+                    void joinRoom(janesSpaceId_1)
+                }, [joinRoom])
+                const onClickAcceptInvite2 = useCallback(() => {
+                    void joinRoom(janesSpaceId_2)
+                }, [joinRoom])
+                const onClickAcceptInvite3 = useCallback(() => {
+                    void joinRoom(janesChannelId_2)
+                }, [joinRoom])
+                // format for easy reading
+                function formatSpace(s: SpaceItem) {
+                    return `${s.id.matrixRoomId}: ${s.name}`
+                }
+                return (
+                    <>
+                        <LoginWithWallet />
+                        <button onClick={onClickAcceptInvite1}>Accept Invite 1</button>
+                        <button onClick={onClickAcceptInvite2}>Accept Invite 2</button>
+                        <button onClick={onClickAcceptInvite3}>Accept Invite 3</button>
+                        <div data-testid="allSpaces">
+                            {spaces.map((space) => formatSpace(space)).join('\n')}
+                        </div>
+                        <div data-testid="allInvites">
+                            {invitedToIds.map((id) => id).join('\n')}
+                        </div>
+                        <div data-testid="myMembership1">{myMembership1}</div>
+                        <div data-testid="myMembership2">{myMembership2}</div>
+                        <div data-testid="myMembership3">{myMembership3}</div>
+                    </>
+                )
             }
-            return (
-                <>
-                    <LoginWithWallet />
-                    <button onClick={onClickAcceptInvite1}>Accept Invite 1</button>
-                    <button onClick={onClickAcceptInvite2}>Accept Invite 2</button>
-                    <button onClick={onClickAcceptInvite3}>Accept Invite 3</button>
-                    <div data-testid="allSpaces">
-                        {spaces.map((space) => formatSpace(space)).join('\n')}
-                    </div>
-                    <div data-testid="allInvites">{invitedToIds.map((id) => id).join('\n')}</div>
-                    <div data-testid="myMembership1">{myMembership1}</div>
-                    <div data-testid="myMembership2">{myMembership2}</div>
-                    <div data-testid="myMembership3">{myMembership3}</div>
-                </>
+            // render it
+            render(
+                <ZionTestApp provider={bobProvider}>
+                    <TestSpaceInvitesComponent />
+                </ZionTestApp>,
             )
-        }
-        // render it
-        render(
-            <ZionTestApp provider={bobProvider}>
-                <TestSpaceInvitesComponent />
-            </ZionTestApp>,
-        )
+        })
+
         // get our test elements
         const myMembership1 = screen.getByTestId('myMembership1')
         const myMembership2 = screen.getByTestId('myMembership2')
@@ -107,30 +120,42 @@ describe('inviteToSpace', () => {
         const acceptInviteButton3 = screen.getByRole('button', {
             name: 'Accept Invite 3',
         })
+
         // wait the invite to show up
         await waitFor(() => expect(allInvites).toHaveTextContent(janesSpaceId_1.matrixRoomId))
         await waitFor(() => expect(myMembership1).toHaveTextContent(Membership.Invite))
+
         // accept the invite
         fireEvent.click(acceptInviteButton1)
+
         // expect our sidebar to show the space
         await waitFor(() => expect(allSpaces).toHaveTextContent('janes_space_1'))
         await waitFor(() => expect(myMembership1).toHaveTextContent(Membership.Join))
-        // have jane invite bob to the second space and channel
-        await jane.inviteUser(janesSpaceId_2, bobUserId)
-        await jane.inviteUser(janesChannelId_2, bobUserId)
+
+        // This call triggers react re-rendering. Wrap inside act() to
+        // avoid warnings.
+        await act(async () => {
+            // have jane invite bob to the second space and channel
+            await jane.inviteUser(janesSpaceId_2, bobUserId)
+            await jane.inviteUser(janesChannelId_2, bobUserId)
+        })
+
         // wait for the invite to show up
         await waitFor(() => expect(allInvites).toHaveTextContent(janesSpaceId_2.matrixRoomId))
-        await waitFor(() => expect(allInvites).toHaveTextContent(janesChannelId_2.matrixRoomId))
-        // expect the space to still render (regression)
+        // expect the space to still render
         await waitFor(() => expect(allSpaces).toHaveTextContent('janes_space_1'))
-        // accept the invite
+
+        // accept the invites
         fireEvent.click(acceptInviteButton2)
+        fireEvent.click(acceptInviteButton3)
+
         // wait for the space to show up
         await waitFor(() => expect(allSpaces).toHaveTextContent('janes_space_2'))
         await waitFor(() => expect(myMembership2).toHaveTextContent(Membership.Join))
-        // accept the channel invite
-        fireEvent.click(acceptInviteButton3)
         await waitFor(() => expect(myMembership3).toHaveTextContent(Membership.Join))
+        // expect the space to still render (regression)
+        await waitFor(() => expect(allSpaces).toHaveTextContent('janes_space_1'))
+
         // and the invite to go away
         await waitFor(() => expect(allInvites).toBeEmptyDOMElement()) // regression, the channel invite was not removed
     })
