7fe06de8109bf12cb9f1fc66196be157d38ddd0d
sergekh2 <104387024+sergekh2@users.noreply.github.com>
2022-11-01 13:52:40 -0700

    Apply Austin's fix to run jsdom on @zion/core tests (#786)

--- casablanca/packages/core/jest.config.jsdom.ts
+++ casablanca/packages/core/jest.config.jsdom.ts
@@ -1,12 +1,17 @@
 import type { Config } from '@jest/types'
 
+const esModules = ['nanoid'].join('|')
+
 const config: Config.InitialOptions = {
     preset: 'ts-jest',
     testEnvironment: 'jsdom',
     testEnvironmentOptions: { browsers: ['chrome', 'firefox', 'safari'] },
     verbose: true,
     modulePathIgnorePatterns: ['/dist/'],
-    testPathIgnorePatterns: ['/dist/', '/node_modules/', 'util.test.ts'],
-    setupFilesAfterEnv: ['jest-extended/all'],
+    transformIgnorePatterns: [`/node_modules/(?!${esModules})`],
+    moduleNameMapper: {
+        '^nanoid(/(.*)|$)': 'nanoid$1',
+    },
 }
+
 export default config
