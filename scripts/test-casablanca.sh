b96fc99afe5514d1dd5f130bce76e8a1bf937bcd
texuf <austin@hntlabs.com>
2022-11-03 11:35:19 -0700

    Add comment to test-casablanca.sh (#812)
    I'd like that this scripts folder is a self documenting description of
    all that can and should be done in the repo, this script was missing any
    comment

--- scripts/test-casablanca.sh
+++ scripts/test-casablanca.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
 
+#
+# runs integration tests in casablanca
+# makes actual api calls against locally running services 
+#
+# prerequisites:
+# yarn install
+# ./scripts/start-local-casablanca.sh
+#
+
 yarn workspace harmony run test-csb
