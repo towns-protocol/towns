9e92cb5868ee38c9e3fb88d2cba663d9ac7b5c4f
Tak Wai Wong <64229756+tak-hntlabs@users.noreply.github.com>
2022-11-04 15:50:19 -0700

    Add ZionRoleManagerShim (#838)

---
+++ clients/web/lib/src/client/web3/shims/ZionRoleManagerShim.ts
@@ -0,0 +1,32 @@
+/* eslint-disable no-restricted-imports */
+
+import { BaseContractShim } from './BaseContractShim'
+import Goerli_SpaceManagerAddresses from '@harmony/contracts/goerli/addresses/space-manager.json'
+import { ZionRoleManager as Goerli_ZionRoleManager } from '@harmony/contracts/goerli/typings/ZionRoleManager'
+import Goerli_ZionRoleManagerArtifact from '@harmony/contracts/goerli/abis/ZionRoleManager.json'
+import Localhost_SpaceManagerAddresses from '@harmony/contracts/localhost/addresses/space-manager.json'
+import type { ZionRoleManager as Localhost_ZionRoleManager } from '@harmony/contracts/localhost/typings/ZionRoleManager'
+import Localhost_ZionRoleManagerArtifact from '@harmony/contracts/localhost/abis/ZionRoleManager.json'
+import { ethers } from 'ethers'
+
+export class ZionSpaceManagerShim extends BaseContractShim<
+    Localhost_ZionRoleManager,
+    Goerli_ZionRoleManager
+> {
+    constructor(
+        provider: ethers.providers.Provider | undefined,
+        signer: ethers.Signer | undefined,
+        chainId: number,
+    ) {
+        super(chainId, provider, signer, {
+            localhost: {
+                address: Localhost_SpaceManagerAddresses.rolemanager,
+                abi: Localhost_ZionRoleManagerArtifact.abi,
+            },
+            goerli: {
+                address: Goerli_SpaceManagerAddresses.rolemanager,
+                abi: Goerli_ZionRoleManagerArtifact.abi,
+            },
+        })
+    }
+}
