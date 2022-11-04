870c83f39ab834a34e63e2987ce0fdbbf2f46181
texuf <austin@hntlabs.com>
2022-11-03 21:23:57 -0700

    Fix casablanca port in wait-for helper script (#831)
    !!!7104 == “ZION”!!!

--- scripts/wait-for-casablanca.sh
+++ scripts/wait-for-casablanca.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-echo "Waiting Casablanca to launch on 80..."
+echo "Waiting Casablanca to launch on 7104..."
 
-while ! nc -z localhost 80; do   
+while ! nc -z localhost 7104; do   
   sleep 0.1 # wait for 1/10 of the second before check again
 done
 
