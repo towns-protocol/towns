84404cbe6494047a1c5723a79cbbe3d556af98cd
Evan Salzbrenner <evan@hntlabs.com>
2022-11-07 15:35:36 -0800

    remove get-urls (#864)

--- clients/web/app/src/components/RichText/utils/textParsers.ts
+++ clients/web/app/src/components/RichText/utils/textParsers.ts
@@ -1,11 +1,7 @@
-import getUrlsLib from 'get-urls'
 import { MessageType, ZionTextMessageContent } from 'use-zion-client/dist/types/matrix-types'
 
-export const getUrls = (text: string) =>
-    getUrlsLib(text, {
-        requireSchemeOrWww: true,
-        forceHttps: true,
-    })
+// TODO: get-urls broke stuff b/c of deps
+export const getUrls = (_text: string) => ''
 
 export const contentWithUrlsAttached = (text: string) => {
     const attachments = Array.from(getUrls(text)).map((url) => ({ url }))
