2d324eba77185a397212216d0c80e5244220d5f6
Tak Wai Wong <64229756+tak-hntlabs@users.noreply.github.com>
2022-11-07 11:53:26 -0800

    fix copy path of the deploy script (#856)

--- scripts/deploy-local-zion-governance-contracts.sh
+++ scripts/deploy-local-zion-governance-contracts.sh
@@ -4,4 +4,4 @@ yarn clean
 source .env.localhost
 make deployLocalSpaceManager
 make deployLocalCouncilNFT
-cp -r ../../packages/contracts/localhost/addresses/ ../../servers/dendrite/zion/contracts/localhost/addresses/
+cp -r ../../packages/contracts/localhost/addresses/ ../../servers/dendrite/zion/contracts/zion_localhost/
