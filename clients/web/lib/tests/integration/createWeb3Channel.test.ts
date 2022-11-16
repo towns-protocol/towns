490c4935e9c0e432d9ddcd2cc6a5c4afd8626a34
Tak Wai Wong <64229756+tak-hntlabs@users.noreply.github.com>
2022-11-16 15:16:15 -0800

    remove test that checks if Owner role throws (#951)
    https://linear.app/hnt-labs/issue/HNT-427/create-channel-with-owner-role-should-be-rejected
    
    Removing test because the behavior as-is is fine.

--- clients/web/lib/tests/integration/createWeb3Channel.test.ts
+++ clients/web/lib/tests/integration/createWeb3Channel.test.ts
@@ -72,48 +72,6 @@ describe('On-chain channel creation tests', () => {
         expect(channel).toBeDefined()
     })
 
-    // https://linear.app/hnt-labs/issue/HNT-427/create-channel-with-owner-role-should-be-rejected
-    test.skip('reject create channel with Owner role', async () => {
-        /* Arrange */
-        const { alice } = await registerAndStartClients(['alice'])
-        await alice.fundWallet()
-
-        // create a space with token entitlement to write
-        const readPermission: DataTypes.PermissionStruct = { name: Permission.Read }
-        const writePermission: DataTypes.PermissionStruct = { name: Permission.Write }
-
-        const roomId = (await createTestSpaceWithEntitlement(
-            alice,
-            [readPermission, writePermission],
-            [],
-        )) as RoomIdentifier
-
-        /* Act */
-        // create a channel on-chain with Owner role
-        const roleIds: number[] = []
-        const allowedRoles = await getRolesFromSpace(alice, roomId.matrixRoomId)
-        for (const r of allowedRoles) {
-            if (r.name === 'Owner') {
-                roleIds.push(r.roleId.toNumber())
-            }
-        }
-
-        /* Assert */
-        const error = await getError<Error>(async function () {
-            await alice.createWeb3Channel({
-                name: 'test_channel',
-                visibility: RoomVisibility.Public,
-                parentSpaceId: roomId,
-                roleIds,
-            })
-        })
-
-        /* Assert */
-        // check that the returned error wasn't that no error was thrown.
-        expect(error).not.toBeInstanceOf(NoThrownError)
-        expect(error).toHaveProperty('name', CONTRACT_ERROR.AddRoleFailed)
-    })
-
     test('reject create channel with duplicate roles', async () => {
         /* Arrange */
         const { alice } = await registerAndStartClients(['alice'])
