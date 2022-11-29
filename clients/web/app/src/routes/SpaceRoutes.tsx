d7e85c95a8295cd0cd0a24e8c7e7aaf6e84ff7ef
Theo Tillberg <ttillberg@gmail.com>
2022-11-29 17:59:35 +0100

    HNT-468 Fix closing threads not working fixes (#997)

--- clients/web/app/src/routes/SpaceRoutes.tsx
+++ clients/web/app/src/routes/SpaceRoutes.tsx
@@ -50,7 +50,10 @@ export const SpaceRoutes = () => (
             <Route path="invite" element={<SpacesInvite />} />
             <Route path="channels/new" element={<SpacesNewChannel />} />
             <Route path="channels/:channelSlug" element={<SpacesChannel />}>
-                <Route path="replies/:messageId" element={<SpacesChannelReplies />} />
+                <Route
+                    path="replies/:messageId"
+                    element={<SpacesChannelReplies parentRoute=".." />}
+                />
             </Route>
             <Route element={<SpacesChannelRoute />}>
                 <Route path="channels/:channelSlug/settings" element={<ChannelSettings />} />
