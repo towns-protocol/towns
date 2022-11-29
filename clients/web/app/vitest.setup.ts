f6dad8c7cf815fbb92fc68e5a5654b4142648f0c
Evan Salzbrenner <evan@hntlabs.com>
2022-11-28 19:46:44 -0800

    reset zustand stores between tests (#993)

--- clients/web/app/vitest.setup.ts
+++ clients/web/app/vitest.setup.ts
@@ -1,16 +1,37 @@
 import 'vitest-canvas-mock'
 import { server } from './mocks/server'
 import '@testing-library/jest-dom/extend-expect' // dont delete this line its needed for extending vitest expect matchers
+import { vi } from 'vitest'
+import { act } from 'react-test-renderer'
+import zustandCreate from 'zustand'
+
+const storeResetFns = new Set<() => void>()
+
+// resetting zustand stores between tests
+// issues mocking zustand with this example https://docs.pmnd.rs/zustand/guides/testing
+// wasn't picking up any files in __mocks__ folder
+vi.mock('zustand', async () => {
+    const actual = (await vi.importActual('zustand')) as {
+        default: typeof zustandCreate
+    }
+    return {
+        default: (createStatee) => {
+            const store = actual.default(createStatee)
+            const initialState = store.getState()
+            storeResetFns.add(() => store.setState(initialState, true))
+            return store
+        },
+    }
+})
 
 beforeAll(() => {
     server.listen()
 })
 
-beforeAll(() => {
+beforeEach(() => {
     server.resetHandlers()
+    act(() => storeResetFns.forEach((resetFn) => resetFn()))
 })
-
-afterEach(() => server.resetHandlers())
 
 afterAll(() => {
     server.close()
