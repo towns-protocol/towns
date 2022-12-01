627f886c620e7f9f683ac7113c315f2719abf785
texuf <austin@hntlabs.com>
2022-11-30 19:17:09 -0800

    Add casablanca specific files to the yarn-clean.sh script (#1013)

--- scripts/yarn-clean.sh
+++ scripts/yarn-clean.sh
@@ -5,5 +5,6 @@ echo "cleaning node"
 find . -name "node_modules" -type d -exec rm -r "{}" \;
 find . -name "dist" -type d -exec rm -r "{}" \;
 find . -name "bin" -type d -exec rm -r "{}" \;
+find . -name "tsconfig.tsbuildinfo" -type f -exec rm -r "{}" \;
 yarn cache clean
 popd
