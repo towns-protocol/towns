caa1e7de97b04ad20d332509689143cd671c6c92
Tak Wai Wong <64229756+tak-hntlabs@users.noreply.github.com>
2022-12-09 15:10:03 -0800

    Update zion space_id (#1067)
    version 1.0.32 roomId is !YtwhtUR6ghEUoc7H

--- clients/web/app/src/App.tsx
+++ clients/web/app/src/App.tsx
@@ -22,7 +22,7 @@ const Playground = React.lazy(() => import('@components/Playground'))
 FontLoader.init()
 
 const MATRIX_HOMESERVER_URL = import.meta.env.VITE_MATRIX_HOMESERVER_URL
-const ZION_SPACE_ID = '!PXXlmYR9kl1kCgGG:node1.zion.xyz'
+const ZION_SPACE_ID = '!YtwhtUR6ghEUoc7H:node1.zion.xyz'
 const ZION_SPACE_NAME = 'Zion Preview' // name is temporary until peek() is implemented https://github.com/HereNotThere/harmony/issues/188
 const ZION_SPACE_AVATAR_SRC = '/placeholders/nft_10.png' // avatar is temporary until peek() is implemented https://github.com/HereNotThere/harmony/issues/188
 
