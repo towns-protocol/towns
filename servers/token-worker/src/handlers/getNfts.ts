2be32f0af9f0c591488e6b2542f384857bd84b48
Evan Salzbrenner <evan@hntlabs.com>
2022-12-13 09:46:10 -0800

    token-worker: add query params for pageKey and contractMetadata (#1088)
    and add alchemy-sdk for its types

--- servers/token-worker/src/handlers/getNfts.ts
+++ servers/token-worker/src/handlers/getNfts.ts
@@ -1,15 +1,42 @@
 import { withCorsHeaders } from '../cors'
-import { RequestWithAlchemyConfig } from '../types'
+import { AccurateNftResponse, ContractMetadataResponse, RequestWithAlchemyConfig } from '../types'
 
 export const getNftsForOwner = async (request: RequestWithAlchemyConfig) => {
-    const { rpcUrl, params } = request
+    const { rpcUrl, params, query } = request
 
     const { wallet } = params || {}
 
-    const response = await fetch(`${rpcUrl}/getNFTs?owner=${wallet}`)
+    const pageKey = query?.pageKey || ''
+    const response = await fetch(`${rpcUrl}/getNFTs?owner=${wallet}&pageKey=${pageKey}`)
+    let json = await response.json()
 
-    const body = JSON.stringify(await response.json())
+    if (query && 'contractMetadata' in query) {
+        json = getContractMetadata(json as AccurateNftResponse)
+    }
+
+    const body = JSON.stringify(json)
 
     const headers = { 'Content-type': 'application/json', ...withCorsHeaders(request) }
     return new Response(body, { headers })
+}
+
+function getContractMetadata(response: AccurateNftResponse): ContractMetadataResponse {
+    const { ownedNfts, ...rest } = response
+    return {
+        ...rest,
+        ownedNftsContract: ownedNfts
+            .map((nft) => {
+                const { name, symbol, tokenType, openSea } = nft.contractMetadata
+                return {
+                    name,
+                    address: nft.contract.address,
+                    symbol,
+                    tokenType,
+                    imageUrl: openSea?.imageUrl,
+                }
+            })
+            .filter((data, index, self) => {
+                return index === self.findIndex((contract) => data.address === contract.address)
+            }),
+    }
 }
--- servers/token-worker/src/types.ts
+++ servers/token-worker/src/types.ts
@@ -1,4 +1,5 @@
 import { Request as IttyRequest } from 'itty-router'
+import { OwnedNftsResponse, NftContract, OwnedNft } from 'alchemy-sdk'
 
 export interface Env {
     ALCHEMY_API_KEY: string
@@ -7,8 +8,29 @@ export interface Env {
 export interface RequestWithAlchemyConfig extends Request {
     rpcUrl: string
     params: IttyRequest['params']
+    query: IttyRequest['query']
 }
 
 export interface IError extends Error {
     status?: number
+}
+
+interface AccurateOwnedNft extends OwnedNft {
+    contractMetadata: NftContract
+}
+export interface AccurateNftResponse extends OwnedNftsResponse {
+    blockHash: string
+    ownedNfts: AccurateOwnedNft[]
+}
+
+export type ContractMetadata = {
+    address?: string
+    name?: string
+    symbol?: string
+    tokenType?: string
+    imageUrl?: string
+}
+
+export interface ContractMetadataResponse extends Omit<AccurateNftResponse, 'ownedNfts'> {
+    ownedNftsContract: ContractMetadata[]
 }
