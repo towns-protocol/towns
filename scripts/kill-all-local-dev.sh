90a62df2594810b365ffebf3d4334b1fe7499d15
texuf <austin@hntlabs.com>
2022-12-19 16:57:17 -0800

    Add a kill-all-local-dev.sh script to clean up after vscode crashes (#1129)
    this can also happen when running dendrite directly from vscod, and I
    think it can happen using the terminal if your terminal crashes

---
+++ scripts/kill-all-local-dev.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env zsh
+
+##
+## If visual studio crashes after running ~start local dev~ 
+## it will leave lots of things running in the background
+## this script will kill all the processes that are running on your local machine.
+##
+
+
+function do_killl() {
+    echo ""
+    echo "finding processes containing $1"
+    echo ""
+    param="$1"
+    first="${param:0:1}"
+    rest="${param:1}"
+    term="[${first}]${rest}"
+    if [[ $(ps -ax | grep "$term") ]]
+    then
+        ps -ax | grep "$term"
+        echo ""
+        read b_continue'?Kill these processes?:y/n '
+        if [[ "$b_continue" == "y" ]]
+        then
+            kill $(ps -ax | grep "$term" | awk '{print $1}')
+        fi
+    else
+        echo "no results found"
+    fi
+}
+
+
+do_killl dendrite
+do_killl yarn
+do_killl anvil
