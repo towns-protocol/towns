048ae2ba4689db4926cacc78a98d6c63a0639f36
texuf <austin@hntlabs.com>
2023-01-30 11:30:00 -0800

    Fix infinite loop/rerender when depending on useTimelineThread (#1356)
    I was returning [] and [] !=== [].
    Also hand memoized the dummy thread stats, since that will cause a
    re-render as well

--- clients/web/lib/src/hooks/use-timeline-thread.ts
+++ clients/web/lib/src/hooks/use-timeline-thread.ts
@@ -1,36 +1,48 @@
 import { RoomIdentifier } from '../types/room-identifier'
 import { ThreadStats, TimelineEvent, ZTEvent } from '../types/timeline-types'
 import { TimelineStoreStates, useTimelineStore } from '../store/use-timeline-store'
+import { useRef } from 'react'
+
+const EMPTY_TIMELINE: TimelineEvent[] = []
 
 export function useTimelineThread(
     roomId: RoomIdentifier,
     eventId?: string,
 ): { messages: TimelineEvent[]; parent: ThreadStats | undefined } {
+    const dummyThreadStatCache = useRef<Record<string, ThreadStats>>({})
     return useTimelineStore((state: TimelineStoreStates) =>
         eventId
             ? {
                   parent:
                       state.threadsStats[roomId.networkId]?.[eventId] ??
                       toDummyThreadStats(
+                          dummyThreadStatCache,
                           state.timelines[roomId.networkId]?.find((e) => e.eventId === eventId),
                       ),
-                  messages: state.threads[roomId.networkId]?.[eventId] ?? [],
+                  messages: state.threads[roomId.networkId]?.[eventId] ?? EMPTY_TIMELINE,
               }
-            : { parent: undefined, messages: [] },
+            : { parent: undefined, messages: EMPTY_TIMELINE },
     )
 }
 
-function toDummyThreadStats(event?: TimelineEvent) {
+function toDummyThreadStats(
+    cache: React.MutableRefObject<Record<string, ThreadStats>>,
+    event?: TimelineEvent,
+): ThreadStats | undefined {
     const content = event?.content?.kind === ZTEvent.RoomMessage ? event.content : undefined
     if (!event || !content) {
         return undefined
     }
-    return {
-        replyCount: 0,
-        userIds: new Set<string>(event.sender.id ? [event.sender.id] : []),
-        latestTs: event.originServerTs,
-        parentId: event.eventId,
-        parentEvent: event,
-        parentMessageContent: content,
+    if (!cache.current[event.eventId]) {
+        cache.current[event.eventId] = {
+            replyCount: 0,
+            userIds: new Set<string>(event.sender.id ? [event.sender.id] : []),
+            latestTs: event.originServerTs,
+            parentId: event.eventId,
+            parentEvent: event,
+            parentMessageContent: content,
+            isParticipating: false,
+        }
     }
+    return cache.current[event.eventId]
 }
