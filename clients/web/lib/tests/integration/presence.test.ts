67693e6cae6528835f3e9a118bf3f3f492fb983f
texuf <austin@hntlabs.com>
2023-02-17 14:40:57 -0800

    Add a presence test (#1520)
    So that we can implement this in casablanca

---
+++ clients/web/lib/tests/integration/presence.test.ts
@@ -0,0 +1,55 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import {
+    createTestChannelWithSpaceRoles,
+    createTestSpaceWithEveryoneRole,
+    getTestPrimaryProtocol,
+    registerAndStartClients,
+} from './helpers/TestUtils'
+import { Permission } from '../../src/client/web3/ContractTypes'
+import { RoomVisibility } from '../../src/types/zion-types'
+import { waitFor } from '@testing-library/dom'
+
+describe('presence', () => {
+    // test:
+    test('can we see other users as "currentlyActive?"', async () => {
+        // create clients
+        const { bob, alice } = await registerAndStartClients(['bob', 'alice'])
+        // bob needs funds to create a space
+        await bob.fundWallet()
+        // create a space
+        const spaceId = (await createTestSpaceWithEveryoneRole(
+            bob,
+            [Permission.Read, Permission.Write],
+            {
+                name: bob.makeUniqueName(),
+                visibility: RoomVisibility.Public,
+                spaceProtocol: getTestPrimaryProtocol(),
+            },
+        ))!
+        // create a channel
+        const channelId = (await createTestChannelWithSpaceRoles(bob, {
+            name: 'bobs channel',
+            parentSpaceId: spaceId,
+            visibility: RoomVisibility.Public,
+            roleIds: [],
+        }))!
+
+        console.log("bob's spaceId", { spaceId, channelId })
+
+        await alice.joinRoom(channelId)
+
+        await alice.sendMessage(channelId, 'Hi @bob')
+
+        await waitFor(() =>
+            expect(bob.getEvents_TypedRoomMessage(channelId).at(-1)?.content?.body).toEqual(
+                'Hi @bob',
+            ),
+        )
+        // expect bob to see alice as online
+        await waitFor(() => {
+            const bobsViewOfAlice = bob.getUser(alice.matrixUserId!)
+            expect(bobsViewOfAlice).toBeDefined()
+            expect(bobsViewOfAlice?.currentlyActive).toEqual(true)
+        })
+    }) // end test
+}) // end describe
