2a7ea49e574d91ae88229393f9d12fe95edd6543
texuf <austin@hntlabs.com>
2023-02-21 11:56:41 -0800

    fix useUpdateChannelTransaction.test: Add room listeners to useChannelData so that it responds ot name changes (#1547)
    The useChannelData returns a simplified version of the channel that is
    all we need for rendering the left hand nav. When first built, I didn’t
    consider channel name changes to be possible, much less account for
    them.
    
    Here is the channel data:
    ```
    export interface Channel {
        id: RoomIdentifier
        label: string
        private?: boolean
        highlight?: boolean
    }
    ```
    
    It was previously populated from the result of the sync space
    hierarchies call, which is run in useSyncSpaceHierarchies
    
    This call is made once per space, and again whenever we detect a space
    has new children via a EventType.SpaceChild event
    
    This change adds a second stream of data to the useChannelData, and
    pulls the name from the updated matrix room before returning it.
    
    This fixes the useUpdateChannelTransaction, but leaves the list of
    spaces stale. Ideally we would rebuild that list on any name change, in
    the meantime, users can refresh their browser.

--- clients/web/lib/src/hooks/use-channel-data.ts
+++ clients/web/lib/src/hooks/use-channel-data.ts
@@ -1,11 +1,14 @@
-import { ChannelData } from '../types/zion-types'
+import { Channel, ChannelData } from '../types/zion-types'
 import { useChannelContext } from '../components/ChannelContextProvider'
 import { useMemo } from 'react'
 import { useSpaceData } from './use-space-data'
+import { useRoom } from './use-room'
 
 export function useChannelData(): ChannelData {
     const { channelId, spaceId } = useChannelContext()
     const space = useSpaceData()
+
+    const room = useRoom(channelId)
 
     const channelGroup = useMemo(
         () =>
@@ -13,10 +16,19 @@ export function useChannelData(): ChannelData {
         [space?.channelGroups, channelId.slug],
     )
 
-    const channel = useMemo(
-        () => channelGroup?.channels.find((c) => c.id.slug === channelId.slug),
-        [channelGroup?.channels, channelId.slug],
-    )
+    const channel = useMemo(() => {
+        const channelChild = channelGroup?.channels.find((c) => c.id.slug === channelId.slug)
+        if (!channelChild) {
+            return undefined
+        }
+        const channel: Channel = {
+            id: channelChild.id,
+            label: room?.name ?? channelChild.label,
+            private: channelChild.private,
+            highlight: channelChild.highlight,
+        }
+        return channel
+    }, [channelGroup?.channels, channelId.slug, room?.name])
 
     return useMemo(() => {
         return {
