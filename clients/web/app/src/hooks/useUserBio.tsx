3e489a8691ec00e96dd559088f11003ef59247cb
John Terzis <john@hntlabs.com>
2023-03-16 09:13:11 -0700

    user bio query / mutation hook (#1786)
    Closes HNT-1061 creating hook similar to `useSpaceTopic` pointing to a
    different route on gateway-worker that gets / sets user bio remotely.

---
+++ clients/web/app/src/hooks/useUserBio.tsx
@@ -0,0 +1,79 @@
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
+import { useCallback } from 'react'
+import { z } from 'zod'
+import { env } from 'utils'
+import { axiosClient } from '../api/apiClient'
+
+const queryKey = 'userBio'
+
+const zBioReadData: z.ZodType<{
+    bio: string
+}> = z.object({
+    bio: z.string(),
+})
+
+export async function getUserBio(walletAddress: string): Promise<string> {
+    const GATEWAY_SERVER_URL = env.VITE_GATEWAY_URL
+    const url = `${GATEWAY_SERVER_URL}/user/${walletAddress}/bio`
+
+    const userBio = await axiosClient.get(url)
+    const parseResult = zBioReadData.safeParse(userBio.data)
+
+    if (!parseResult.success) {
+        throw new Error("Couldn't parse user bio:: ", parseResult.error)
+    }
+
+    return parseResult.data.bio
+}
+
+export async function setUserBio(walletAddress: string, bio: string): Promise<void> {
+    const GATEWAY_SERVER_URL = env.VITE_GATEWAY_URL
+    const url = `${GATEWAY_SERVER_URL}/user/${walletAddress}/bio`
+    await axiosClient.post(url, JSON.stringify({ bio: bio }), {
+        withCredentials: true,
+    })
+}
+
+export const useGetUserBio = (walletAddress: string | undefined) => {
+    const _getUserBio = useCallback(async () => {
+        if (!walletAddress) {
+            return
+        }
+        return getUserBio(walletAddress as string)
+    }, [walletAddress])
+
+    return useQuery([queryKey, walletAddress], _getUserBio, {
+        enabled: !!walletAddress,
+        retry: 3,
+        retryDelay: 1000,
+        refetchOnMount: false,
+        refetchOnWindowFocus: false,
+        refetchOnReconnect: false,
+        staleTime: 1000 * 60 * 60 * 24,
+        cacheTime: 1000 * 60 * 60 * 24,
+    })
+}
+
+export const useSetUserBio = (walletAddress: string | undefined) => {
+    const queryClient = useQueryClient()
+
+    const _setUserBio = useCallback(
+        (bio: string) => {
+            if (!walletAddress) {
+                return Promise.reject('No wallet address')
+            }
+
+            return setUserBio(walletAddress, bio)
+        },
+        [walletAddress],
+    )
+
+    return useMutation(_setUserBio, {
+        onSuccess: async () => {
+            return queryClient.invalidateQueries([queryKey, walletAddress])
+        },
+        onError: (error) => {
+            console.error('[useSetUserBio] error', error)
+        },
+    })
+}
