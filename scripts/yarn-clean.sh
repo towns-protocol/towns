f0b897e34cce411cc70714e29388d6611e93d28c
texuf <austin@hntlabs.com>
2023-03-20 10:57:43 -0700

    Update the yarn-clean script to include turbo and eslint caches (#1828)
    I got into a weird spot where stopping and nuking everything didn’t fix
    a vite cache issue, not sure if this fixes it because i can no longer
    repo, but seems like these files should go.

--- scripts/yarn-clean.sh
+++ scripts/yarn-clean.sh
@@ -5,6 +5,8 @@ echo "cleaning node"
 find . -name "node_modules" -type d -exec rm -r "{}" \;
 find . -name "dist" -type d -exec rm -r "{}" \;
 find . -name "bin" -type d -exec rm -r "{}" \;
+find . -name ".turbo" -type d -exec rm -r "{}" \;
 find . -name "tsconfig.tsbuildinfo" -type f -exec rm -r "{}" \;
+find . -name ".eslintcache" -type f -exec rm -r "{}" \;
 yarn cache clean
 popd
