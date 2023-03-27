0eb43d1a92709feeb517c5c026679c1a35f3810b
texuf <austin@hntlabs.com>
2023-03-27 10:35:16 -0700

    update the yarn-clean script (#1906)
    new folders for casablanca, better description

--- scripts/yarn-clean.sh
+++ scripts/yarn-clean.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
+# very similar to running git clean -xd however, we don't want to remove
+# .env.local files, dendrite keys (if you're running from vs), etc
+# if you want to clean everything temporary created by the build, but 
+# don't want a full reset on your dev environment, this is the script for you
+
 pushd "$(git rev-parse --show-toplevel)"
 echo "cleaning node"
 find . -name "node_modules" -type d -exec rm -r "{}" \;
 find . -name "dist" -type d -exec rm -r "{}" \;
 find . -name "bin" -type d -exec rm -r "{}" \;
+find . -name "coverage" -type d -exec rm -r "{}" \;
+find . -name "out" -type d -exec rm -r "{}" \;
+find . -name "gen" -type d -exec rm -r "{}" \;
 find . -name ".turbo" -type d -exec rm -r "{}" \;
 find . -name "tsconfig.tsbuildinfo" -type f -exec rm -r "{}" \;
 find . -name ".eslintcache" -type f -exec rm -r "{}" \;
