2b594513370d30b8117aed837428a60fac7cbccc
texuf <austin@hntlabs.com>
2023-04-03 10:47:01 -0700

    Add casablanca debug-app to Start Local Dev (#2003)

--- casablanca/debug-app/vite.config.ts
+++ casablanca/debug-app/vite.config.ts
@@ -29,7 +29,7 @@ export default defineConfig({
         visualizer({ filename: 'dist/stats.html' }),
     ],
     server: {
-        port: 3001,
+        port: 3002,
         hmr: {
             overlay: false,
         },
