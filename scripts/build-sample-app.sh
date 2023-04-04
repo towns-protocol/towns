eddd9b8b55cdc51335b88a3ca6cb5046df4ff8cd
texuf <austin@hntlabs.com>
2023-04-04 11:24:34 -0700

    Hopefully fix sample app OOM on render.com (#2033)
    Looks like brian did something similar here:
    
    https://github.com/HereNotThere/harmony/commit/f8a61cb50768ad451d59ba203e8bb70ecea590e3
    
    The sample app build is failling with this error
    ```
    
    Apr 3 05:23:43 PM  <--- JS stacktrace --->
    Apr 3 05:23:43 PM
    Apr 3 05:23:43 PM  FATAL ERROR: Reached heap limit Allocation failed - JavaScript heap out of memory
    Apr 3 05:23:43 PM   1: 0xb57b90 node::Abort() [/opt/render/project/nodes/node-18.0.0/bin/node]
    Apr 3 05:23:43 PM   2: 0xa701b3 node::FatalError(char const*, char const*) [/opt/render/project/nodes/node-18.0.0/bin/node]
    Apr 3 05:23:43 PM   3: 0xd43b9e v8::Utils::ReportOOMFailure(v8::internal::Isolate*, char const*, bool) [/opt/render/project/nodes/node-18.0.0/bin/node]
    Apr 3 05:23:43 PM   4: 0xd43f17 v8::internal::V8::FatalProcessOutOfMemory(v8::internal::Isolate*, char const*, bool) [/opt/render/project/nodes/node-18.0.0/bin/node]
    ```

--- scripts/build-sample-app.sh
+++ scripts/build-sample-app.sh
@@ -6,6 +6,8 @@ set -v
 # Build script for render build job "sample-app" https://dashboard.render.com/
 #
 
+export VITE_APP_RELEASE_VERSION=$RENDER_GIT_COMMIT
+export NODE_OPTIONS=--max_old_space_size=32768
 yarn install
 yarn workspace use-zion-client build
 yarn workspace sample-app build
