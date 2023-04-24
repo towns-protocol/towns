8371603716c5d7c0ed8180a776215570be8d3d34
Erik Olsson <valross2@gmail.com>
2023-04-24 13:12:40 +0200

    app: add "Copy" tooltip when hovering a user's wallet address (hnt-923) (#2094)
    This PR adds a Copy/Copied tooltip when hovering a user's wallet
    address:
    
    <img width="369" alt="image"
    src="https://user-images.githubusercontent.com/352982/233378964-343ebabc-8e3c-4506-aff9-1bd056276af9.png">

--- clients/web/app/src/components/ClipboardCopy/ClipboardCopy.tsx
+++ clients/web/app/src/components/ClipboardCopy/ClipboardCopy.tsx
@@ -1,6 +1,7 @@
 import React, { forwardRef, useEffect, useState } from 'react'
+import { motion } from 'framer-motion'
 import { useEvent } from 'react-use-event-hook'
-import { Icon, Stack, Text } from '@ui'
+import { Box, Icon, Stack, Text, TooltipRenderer } from '@ui'
 import useCopyToClipboard from 'hooks/useCopyToClipboard'
 
 type Props = {
@@ -32,22 +33,74 @@ export const ClipboardCopy = forwardRef<HTMLDivElement, Props>((props, ref) => {
     })
 
     return (
-        <Stack
-            horizontal
-            gap="sm"
-            alignItems="center"
-            cursor={!copied ? 'pointer' : 'default'}
-            ref={ref}
-            onClick={onCopy}
+        <TooltipRenderer
+            keepOpenOnTriggerRefClick
+            trigger="hover"
+            distance="sm"
+            placement="vertical-top"
+            render={<CopyTooltip copied={copied} />}
         >
-            <Text truncate size="md" color="gray2">
-                {props.label}
-            </Text>
-            {!copied ? (
-                <Icon type="copy" color="gray2" size="square_xs" insetTop="xxs" />
-            ) : (
-                <Icon type="check" color="positive" size="square_xs" insetTop="xxs" />
-            )}
-        </Stack>
+            {({ triggerProps }) => {
+                return (
+                    <Box {...triggerProps}>
+                        <Stack
+                            {...triggerProps}
+                            horizontal
+                            gap="sm"
+                            alignItems="center"
+                            cursor={!copied ? 'pointer' : 'default'}
+                            ref={ref}
+                            onClick={onCopy}
+                        >
+                            <Text truncate size="md" color="gray2">
+                                {props.label}
+                            </Text>
+                            {!copied ? (
+                                <Icon type="copy" color="gray2" size="square_xs" insetTop="xxs" />
+                            ) : (
+                                <Icon
+                                    type="check"
+                                    color="positive"
+                                    size="square_xs"
+                                    insetTop="xxs"
+                                />
+                            )}
+                        </Stack>
+                    </Box>
+                )
+            }}
+        </TooltipRenderer>
     )
 })
+
+type CopiedProps = {
+    copied: boolean
+}
+
+const CopyTooltip = (props: CopiedProps) => {
+    return (
+        <MotionBox
+            layout
+            border
+            gap
+            padding="md"
+            background="level2"
+            rounded="sm"
+            boxShadow="avatar"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{
+                duration: 0.1,
+                opacity: { delay: 0.2 },
+            }}
+        >
+            <MotionBox layout="position">
+                <Box horizontal centerContent gap="md">
+                    <Text>{props.copied ? 'Copied' : 'Copy'}</Text>
+                </Box>
+            </MotionBox>
+        </MotionBox>
+    )
+}
+
+const MotionBox = motion(Box)
