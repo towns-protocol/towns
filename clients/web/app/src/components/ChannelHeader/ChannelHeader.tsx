286dce4f545279953e0a59240af6f43752622969
Erik Olsson <valross2@gmail.com>
2023-04-26 16:41:52 +0200

    app: update ChannelHeader (hnt-1366) (#2137)
    This PR updates the Channel Header:
    
    <img width="726" alt="image"
    src="https://user-images.githubusercontent.com/352982/234535599-ff08969f-bf1c-4494-bfe6-604a89c38f72.png">

--- clients/web/app/src/components/ChannelHeader/ChannelHeader.tsx
+++ clients/web/app/src/components/ChannelHeader/ChannelHeader.tsx
@@ -17,8 +17,8 @@ export const ChannelHeader = (props: Props) => {
     return (
         <Stack
             horizontal
-            gap
             borderBottom
+            gap
             paddingX="lg"
             background="level1"
             height="x8"
@@ -27,13 +27,24 @@ export const ChannelHeader = (props: Props) => {
             overflow="hidden"
             shrink={false}
         >
-            <Icon type="tag" background="level2" size="square_lg" />
-            <Stack horizontal gap alignItems="end">
-                <Paragraph strong size="lg">
-                    <Link to="info?channel">{channel.label}</Link>
-                </Paragraph>
-                {topic && <Paragraph color="gray2">{topic}</Paragraph>}
-            </Stack>
+            <Link to="info?channel">
+                <Stack
+                    horizontal
+                    border
+                    paddingX
+                    gap="sm"
+                    paddingY="sm"
+                    background="level2"
+                    alignItems="center"
+                    rounded="sm"
+                >
+                    <Icon type="tag" size="square_sm" color="gray2" />
+                    <Paragraph fontWeight="strong" color="default">
+                        {channel.label}
+                    </Paragraph>
+                </Stack>
+            </Link>
+            {topic && <Paragraph color="gray2">{topic}</Paragraph>}
             <Stack grow />
             <ChannelUsersPill channelId={channel.id} spaceId={spaceId} />
         </Stack>
