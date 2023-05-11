5fcd7aff9f0a96092cf2404d3aa5c21ee3b109cc
Erik Olsson <valross2@gmail.com>
2023-05-11 09:47:07 +0200

    app: add support for serving app over https on local network (#2199)
    This PR adds support for hosting the website on a .local domain over a
    local network, which is needed to run the app on a mobile device.
    
    Everything should hopefully work exactly as before, but setting the vars
    `VITE_USE_LOCAL_NETWORK_HTTPS=true` and
    `VITE_LOCAL_HOSTNAME=your-machine.local` will make the app accessible
    over the local network.

--- clients/web/app/vite.config.ts
+++ clients/web/app/vite.config.ts
@@ -1,54 +1,67 @@
 import sourcemaps from 'rollup-plugin-sourcemaps'
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin'
 import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv, UserConfigExport } from 'vite'
 import checker from 'vite-plugin-checker'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import eslintPlugin from 'vite-plugin-eslint'
 import { visualizer } from 'rollup-plugin-visualizer'
 import polyfillNode from 'rollup-plugin-polyfill-node'
+import mkcert from 'vite-plugin-mkcert'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    build: {
-        target: 'esnext',
-        sourcemap: true,
-        rollupOptions: {
-            output: {
-                manualChunks: (id) => {
-                    // This works around a circular dependency issue with the @wagmi package
-                    if (id.includes('@wagmi')) {
-                        return 'wagmi'
-                        // Lodash is used in many chunks, split it into one bundle
-                    } else if (id.includes('lodash')) {
-                        return 'lodash'
-                    } else if (id.includes('matrix-sdk-crypto')) {
-                        return 'matrix-sdk-crypto'
-                    } else if (id.includes('matrix')) {
-                        return 'matrix-rest'
-                    }
+export default ({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), '')
+    let config: UserConfigExport = {
+        build: {
+            target: 'esnext',
+            sourcemap: true,
+            rollupOptions: {
+                output: {
+                    manualChunks: (id) => {
+                        // This works around a circular dependency issue with the @wagmi package
+                        if (id.includes('@wagmi')) {
+                            return 'wagmi'
+                            // Lodash is used in many chunks, split it into one bundle
+                        } else if (id.includes('lodash')) {
+                            return 'lodash'
+                        } else if (id.includes('matrix-sdk-crypto')) {
+                            return 'matrix-sdk-crypto'
+                        } else if (id.includes('matrix')) {
+                            return 'matrix-rest'
+                        }
+                    },
                 },
             },
         },
-    },
-    define: {
-        APP_VERSION: JSON.stringify(process.env.npm_package_version),
-    },
-    assetsInclude: ['**/*.png', '**/*.svg'],
-    plugins: [
-        polyfillNode(),
-        react(),
-        tsconfigPaths(),
-        checker({ typescript: true }),
-        eslintPlugin(),
-        vanillaExtractPlugin(),
-        visualizer({ filename: 'dist/stats.html', template: 'treemap' }),
-        sourcemaps({ exclude: '**/@sentry/**/*.js' }),
-    ],
-    server: {
-        port: 3000,
-        hmr: {
-            overlay: false,
+        define: {
+            APP_VERSION: JSON.stringify(process.env.npm_package_version),
         },
-    },
-})
+        assetsInclude: ['**/*.png', '**/*.svg'],
+        plugins: [
+            polyfillNode(),
+            react(),
+            tsconfigPaths(),
+            checker({ typescript: true }),
+            eslintPlugin(),
+            vanillaExtractPlugin(),
+            visualizer({ filename: 'dist/stats.html', template: 'treemap' }),
+            sourcemaps({ exclude: '**/@sentry/**/*.js' }),
+        ],
+        server: {
+            port: 3000,
+            hmr: {
+                overlay: false,
+            },
+        },
+    }
+
+    if (env.VITE_USE_LOCAL_NETWORK_HTTPS === 'true') {
+        console.log('Using local network HTTPS')
+        config.server!.host = '0.0.0.0'
+        config.server!.https = true
+        config.plugins!.push(mkcert({ hosts: [env.VITE_LOCAL_HOSTNAME] }))
+    }
+
+    return defineConfig(config)
+}
