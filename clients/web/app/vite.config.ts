4dd8ab761e1b9d4b36843b1ac2d44cafd02039f1
Theo Tillberg <ttillberg@gmail.com>
2023-05-17 07:54:14 +0200

    app: remove eslint and checker plugins from prod build (#2265)
    - remove eslint + checker from build process
    - ~~introduce an eslint error (not TS) on purpose~~
    - make eslint error show up in `checker-plugin` overlay
    
    | Before | After |
    | --- | --- |
    | <img width="379" alt="image"
    src="https://github.com/HereNotThere/harmony/assets/557990/daf3bdc9-f225-46dc-8c26-b21fea9b3b8d">
    | <img width="382" alt="image"
    src="https://github.com/HereNotThere/harmony/assets/557990/179af2f1-a049-4db1-aed6-41eed9cd6b3f">
    |
    |  (5.8Gb)| (1.1Gb)|
    
    not sure I can trust what it means in terms of memory since I'm testing
    this on a laptop but Render.com build time (build only! not yarn etc.)
    goest from approx 3m30 to 1m10 which proofs something.

--- clients/web/app/vite.config.ts
+++ clients/web/app/vite.config.ts
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 import { defineConfig, loadEnv, UserConfigExport } from 'vite'
 import checker from 'vite-plugin-checker'
 import tsconfigPaths from 'vite-tsconfig-paths'
-import eslintPlugin from 'vite-plugin-eslint'
 import { visualizer } from 'rollup-plugin-visualizer'
 import polyfillNode from 'rollup-plugin-polyfill-node'
 import mkcert from 'vite-plugin-mkcert'
@@ -13,6 +12,18 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default ({ mode }) => {
     const env = loadEnv(mode, process.cwd(), '')
+
+    const devPlugins = [
+        checker({
+            typescript: true,
+            eslint: {
+                lintCommand: 'eslint "./src/**/*.{ts,tsx}"',
+            },
+        }),
+        visualizer({ filename: 'dist/stats.html', template: 'treemap' }),
+    ]
+    const prodPlugins = []
+
     let config: UserConfigExport = {
         build: {
             target: 'esnext',
@@ -48,12 +59,10 @@ export default ({ mode }) => {
             polyfillNode(),
             react(),
             tsconfigPaths(),
-            checker({ typescript: true }),
-            eslintPlugin(),
             vanillaExtractPlugin(),
-            visualizer({ filename: 'dist/stats.html', template: 'treemap' }),
+            ,
             sourcemaps({ exclude: '**/@sentry/**/*.js' }),
-        ],
+        ].concat(mode === 'development' ? devPlugins : prodPlugins),
         server: {
             port: 3000,
             hmr: {
