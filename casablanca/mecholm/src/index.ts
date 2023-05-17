368bef7b71b958c95ab9fb50de5f05b276dcadf5
John Terzis <john@hntlabs.com>
2023-05-16 21:01:10 -0700

    Jt/hnt 1417 migrate megolm (#2269)
    This PR builds off
    [#2222](https://github.com/HereNotThere/harmony/pull/2222) by bringing
    in matrix-org/olm wasm pre-built bundle used by matrix-js-sdk, version
    3.2.14 and testing initialization + account creation.
    
    Follow-on PR will implement TypedEventEmitter class in `mecholm` package
    injecting the olm wasm library using a delegate pattern and implement
    jest unit tests that cover objects from this library we will be using in
    our port of the Crypto module (see list below).
    
    1. PkSigning -
    https://github.com/matrix-org/matrix-js-sdk/blob/43160094019adb6f62d4eb16f8d16069e89524c1/src/crypto/CrossSigning.ts#L21
    2. Utility, SAS -
    https://github.com/matrix-org/matrix-js-sdk/blob/43160094019adb6f62d4eb16f8d16069e89524c1/src/crypto/verification/SAS.ts#L22
    3. PkDecryption, PkSigning -
    https://github.com/matrix-org/matrix-js-sdk/blob/43160094019adb6f62d4eb16f8d16069e89524c1/src/crypto/index.ts#L24
    4. Account, InboundGroupSession, OutboundGroupSession, Session, Utility
    -
    https://github.com/matrix-org/matrix-js-sdk/blob/43160094019adb6f62d4eb16f8d16069e89524c1/src/crypto/OlmDevice.ts#L17
    5. PkSigning -
    https://github.com/matrix-org/matrix-js-sdk/blob/43160094019adb6f62d4eb16f8d16069e89524c1/src/crypto/olmlib.ts#L23
    
    ---------
    
    Co-authored-by: texuf <austin@hntlabs.com>

--- casablanca/mecholm/src/index.ts
+++ casablanca/mecholm/src/index.ts
@@ -1,3 +1,8 @@
-export function foo() {
-    console.log('foo')
+import Olm from '@matrix-org/olm'
+
+export async function createOlmAccount(): Promise<Olm.Account> {
+    // Olm library must be init prior to creating an account
+    await Olm.init()
+    const account = new Olm.Account()
+    return account
 }
--- casablanca/mecholm/tests/index.test.ts
+++ casablanca/mecholm/tests/index.test.ts
@@ -1,5 +1,13 @@
+import { createOlmAccount } from '../src/index'
+
 describe('Megolm Encryption Protocol', () => {
-    test('Empty test for first ci commit', () => {
+    test('emptyInitialTest', () => {
         expect(true).toBe(true)
+    })
+
+    test('testOlmAccountCreate', async () => {
+        const account = await createOlmAccount()
+        expect(account).toBeDefined()
+        expect(Object.entries(account).length).toEqual(2)
     })
 })
