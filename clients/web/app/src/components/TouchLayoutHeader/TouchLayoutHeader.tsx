d7d83d5194b22171e92aac203af1441219044180
Erik Olsson <valross2@gmail.com>
2023-06-01 23:21:16 +0200

    app: show badge in TouchLayoutHeader if another town has activity (#2368)

--- clients/web/app/src/components/TouchLayoutHeader/TouchLayoutHeader.tsx
+++ clients/web/app/src/components/TouchLayoutHeader/TouchLayoutHeader.tsx
@@ -1,8 +1,8 @@
-import React, { useCallback, useState } from 'react'
-import { useMyProfile, useSpaceData } from 'use-zion-client'
+import React, { useCallback, useMemo, useState } from 'react'
+import { useMyProfile, useSpaceData, useZionContext } from 'use-zion-client'
 import { AnimatePresence } from 'framer-motion'
+import { Avatar, Box, Dot, Stack, Text } from '@ui'
 import { ImageVariants } from '@components/UploadImage/useImageSource'
-import { Avatar, Box, Stack, Text } from '@ui'
 import { SpaceIcon } from '@components/SpaceIcon'
 import { TouchHomeOverlay } from '@components/TouchHomeOverlay/TouchHomeOverlay'
 import { useNavigateToCurrentSpaceInfo } from 'hooks/useNavigateToCurrentSpaceInfo'
@@ -10,8 +10,19 @@ import { useNavigateToCurrentSpaceInfo } from 'hooks/useNavigateToCurrentSpaceIn
 export const TouchLayoutHeader = () => {
     const userId = useMyProfile()?.userId
     const space = useSpaceData()
+    const currentSpaceId = space?.id
     const [showOverlay, setShowOverlay] = useState(false)
     const { navigateToCurrentSpace } = useNavigateToCurrentSpaceInfo()
+    const { spaces, spaceUnreads, spaceMentions } = useZionContext()
+    const hasUnread = useMemo(() => {
+        const unreadCount = spaces.some((space) => {
+            if (space.id === currentSpaceId) {
+                return false
+            }
+            return spaceUnreads[space.id.networkId] || spaceMentions[space.id.networkId]
+        }, 0)
+        return unreadCount
+    }, [spaceUnreads, spaceMentions, currentSpaceId, spaces])
 
     const onTokenClick = useCallback(() => {
         navigateToCurrentSpace()
@@ -28,7 +39,10 @@ export const TouchLayoutHeader = () => {
                 zIndex="tooltips"
                 paddingTop="safeAreaInsetTop"
             >
-                <Avatar size="avatar_x4" userId={userId} onClick={() => setShowOverlay(true)} />
+                <Box position="relative">
+                    <Avatar size="avatar_x4" userId={userId} onClick={() => setShowOverlay(true)} />
+                    {hasUnread && <Dot />}
+                </Box>
                 <Stack grow />
                 {space && (
                     <Stack
