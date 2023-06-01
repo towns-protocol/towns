eed10ca409d0735aec8fa145d75b5f28b1ae5b5e
Brian Meek <brian@hntlabs.com>
2023-06-01 12:37:48 -0700

    Add memo around flatmap in useSpaceChannels (#2372)
    Found during inspection, minor change.
    
    Signed-off-by: Brian Meek <brian@hntlabs.com>

--- clients/web/app/src/hooks/useSpaceChannels.ts
+++ clients/web/app/src/hooks/useSpaceChannels.ts
@@ -1,6 +1,11 @@
+import { useMemo } from 'react'
 import { Channel, useSpaceData } from 'use-zion-client'
 
 export const useSpaceChannels = (): Channel[] => {
     const spaceData = useSpaceData()
-    return spaceData ? spaceData?.channelGroups.flatMap((cg) => cg.channels) : []
+    return useMemo(
+        () =>
+            spaceData?.channelGroups ? spaceData?.channelGroups.flatMap((cg) => cg.channels) : [],
+        [spaceData?.channelGroups],
+    )
 }
