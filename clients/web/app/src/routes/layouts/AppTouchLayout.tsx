0ea6bb3e1a736d32a39fb26ed18aea4b62060bba
Erik Olsson <valross2@gmail.com>
2023-06-05 13:41:55 +0200

    app: show shimmer while loading space (#2404)

--- clients/web/app/src/routes/layouts/AppTouchLayout.tsx
+++ clients/web/app/src/routes/layouts/AppTouchLayout.tsx
@@ -2,10 +2,14 @@ import React from 'react'
 import { Outlet } from 'react-router'
 import { Stack } from '@ui'
 import { SuspenseLoader } from '@components/Loaders/SuspenseLoader'
+import { useContractAndServerSpaceData } from 'hooks/useContractAndServerSpaceData'
+import { TimelineShimmer } from '@components/Shimmer'
 
 export const AppTouchLayout = () => {
+    const { serverSpace: space } = useContractAndServerSpaceData()
     return (
         <Stack height="100%">
+            {!space && <TimelineShimmer />}
             <SuspenseLoader>
                 <Outlet />
             </SuspenseLoader>
