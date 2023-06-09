8043371566b6289a82ffef2147e2a8921b5c0463
Tak Wai Wong <64229756+tak-hntlabs@users.noreply.github.com>
2023-06-09 13:46:52 -0700

    update PushSubscription schema (#2439)

--- servers/workers/push-notification-worker/sql/create-schema-local.sql
+++ servers/workers/push-notification-worker/sql/create-schema-local.sql
@@ -2,24 +2,39 @@
 DROP TABLE IF EXISTS PushSubscription;
 
 CREATE TABLE IF NOT EXISTS PushSubscription (
-  UserID VARCHAR(255) NOT NULL, -- wallet account 
-  PushSubscription VARCHAR(512), -- push subscription object from the web-push library
+  SpaceId VARCHAR(255) NOT NULL, -- space id
+  ChannelId VARCHAR(255), -- channel id
   PushType VARCHAR(64) NOT NULL DEFAULT 'web-push', -- push type [ 'web-push' | 'mobile' ]. Currently only 'web-push' is supported
+  UserId VARCHAR(255) NOT NULL, -- wallet account
+  PushSubscription VARCHAR(512), -- push subscription object from the web-push library
   CONSTRAINT PushSubscription_PK PRIMARY KEY (PushSubscription)
 );
 
+CREATE INDEX idx_user_id
+ON PushSubscription (UserId);
+
+CREATE INDEX idx_space_id
+ON PushSubscription (SpaceId);
+
+CREATE INDEX idx_channel_id
+ON PushSubscription (ChannelId);
+
 -- insert a test subscription for verification
 INSERT INTO PushSubscription (
-  UserID,
+  UserId,
+  SpaceId,
   PushSubscription
 ) VALUES (
   '0x1111',
+  '!space-id_1:towns.com',
   '{"endpoint": "https://some-push-service.com/unique-0x1111", "keys": { "p256dh": "1111", "auth":"1111" }}'
 );
 INSERT INTO PushSubscription (
-  UserID,
+  UserId,
+  SpaceId,
   PushSubscription
 ) VALUES (
   '0x2222',
+  '!space-id_2:towns.com',
   '{"endpoint": "https://some-push-service.com/unique-0x2222", "keys": { "p256dh": "2222", "auth":"2222" }}'
 );
