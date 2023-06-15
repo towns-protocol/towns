62311cdd44447262632a71d2f79f113f05e11e84
Evan Salzbrenner <evan@hntlabs.com>
2023-06-15 07:19:43 -0700

    app: config change for useIsHolderOfPioneerNFT (#2468)
    ensure it's called once per session

--- clients/web/app/src/api/lib/isHolderOfToken.ts
+++ clients/web/app/src/api/lib/isHolderOfToken.ts
@@ -18,7 +18,13 @@ export function useIsHolderOfPioneerNFT() {
         },
         {
             select: (isHolder) => isHolder,
+            // set the staleTime to 24 hours so that the data is never refetched while this query has an observer
             staleTime: 1000 * 60 * 60 * 24,
+            // cacheTime default is 5 minutes, so also need to set it to ensure the entry is not garbage collected. otherwise, if the observer went away for 5 minutes, the data would be refetched
+            cacheTime: 1000 * 60 * 60 * 24,
+            refetchOnWindowFocus: false,
+            refetchOnReconnect: false,
+            refetchOnMount: false,
             enabled: Boolean(wallet) && Boolean(client),
         },
     )
