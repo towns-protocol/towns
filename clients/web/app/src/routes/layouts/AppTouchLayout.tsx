f20e99f4e6011e4880d342310f5ae782b2ae5fa5
Erik Olsson <valross2@gmail.com>
2023-07-06 15:20:24 +0200

    app: remove timelineshimmer from touch layout (#2610)
    this PR fixes an issue where we were sometimes incorrectly rendering two
    shimmers on top of each other.
    
    I tried a few different solutions, but I think this is the best one,
    simply because the logic for showing/hiding in `AppPanelLayout` is a bit
    orthogonal with the logic in `SpacesChannel`
    
    
    
    ![image](https://github.com/HereNotThere/harmony/assets/352982/63d990d6-4d48-4d66-8731-7d1c67526c4a)

--- clients/web/app/src/routes/layouts/AppTouchLayout.tsx
+++ clients/web/app/src/routes/layouts/AppTouchLayout.tsx
@@ -1,18 +1,11 @@
 import React from 'react'
-import { Outlet, useMatch } from 'react-router'
+import { Outlet } from 'react-router'
 import { Stack } from '@ui'
 import { SuspenseLoader } from '@components/Loaders/SuspenseLoader'
-import { useContractAndServerSpaceData } from 'hooks/useContractAndServerSpaceData'
-import { TimelineShimmer } from '@components/Shimmer'
-import { PATHS } from 'routes'
 
 export const AppTouchLayout = () => {
-    const { serverSpace: space } = useContractAndServerSpaceData()
-    const messageRoute = useMatch({ path: `/${PATHS.MESSAGES}`, end: false })
-    const isMessagesRoute = !!messageRoute
     return (
         <Stack height="100%">
-            {!isMessagesRoute && !space && <TimelineShimmer />}
             <SuspenseLoader>
                 <Outlet />
             </SuspenseLoader>
