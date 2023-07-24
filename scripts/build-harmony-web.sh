f335690c7b7f02f515908df3be44d63ff1f442ee
Brian Meek <brian@hntlabs.com>
2023-07-24 16:13:47 -0700

    Limit build to 4gb
    Signed-off-by: Brian Meek <brian@hntlabs.com>

--- scripts/build-harmony-web.sh
+++ scripts/build-harmony-web.sh
@@ -6,7 +6,7 @@ set -v
 # Build script for render build job "harmony-web" https://dashboard.render.com/
 #
 # Render limits us to 8gb of memory for build jobs, so we need to set node limit
-# to 6gb to avoid OOM errors.
+# to 4gb to avoid OOM errors.
 export NODE_OPTIONS="--max-old-space-size=4096"
 export VITE_APP_RELEASE_VERSION=$RENDER_GIT_COMMIT
 yarn install
