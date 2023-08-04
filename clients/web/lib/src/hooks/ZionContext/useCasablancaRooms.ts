db8ee9438394d8ba2b24b6466ff410a8b1c236bc
artem-hnt <133605745+artem-hnt@users.noreply.github.com>
2023-08-04 10:43:56 +0400

    useCasablancaRooms for proper mentions (#2878)
    Connecting the dots:
    Implementation of useCasablancaRooms which relies on
    [use-casablanca-store.ts](https://github.com/HereNotThere/harmony/blob/77065a88e7c144c936fd89d24f4ff5200c3998fc/clients/web/lib/src/store/use-casablanca-store.ts#L4)
    changes implemented
    [here](https://github.com/HereNotThere/harmony/pull/2875)
    
    We use data provided by
    [use-casabanca-store.ts](https://github.com/HereNotThere/harmony/blob/77065a88e7c144c936fd89d24f4ff5200c3998fc/clients/web/lib/src/store/use-casablanca-store.ts#L4)
    for mentions support, its autocomplete and counters of users in the
    channels and towns in the UI
    
    TODO:
    -proper testing code
    -review events model

--- clients/web/lib/src/hooks/ZionContext/useCasablancaRooms.ts
+++ clients/web/lib/src/hooks/ZionContext/useCasablancaRooms.ts
@@ -1,6 +1,8 @@
-import { Client as CasablancaClient } from '@towns/sdk'
+import { Client as CasablancaClient, isSpaceStreamId, isChannelStreamId } from '@towns/sdk'
 import { useEffect, useState } from 'react'
+import { toZionCasablancaRoom } from '../../store/use-casablanca-store'
 import { Room } from '../../types/zion-types'
+import isEqual from 'lodash/isEqual'
 
 export function useCasablancaRooms(client?: CasablancaClient): Record<string, Room | undefined> {
     const [rooms, setRooms] = useState<Record<string, Room | undefined>>({})
@@ -12,20 +14,50 @@ export function useCasablancaRooms(client?: CasablancaClient): Record<string, Ro
         }
 
         // helpers
-        //const updateState = (roomId: string) => {
-        //
-        //}
+        const updateState = (streamId: string) => {
+            const newRoom = streamId ? toZionCasablancaRoom(streamId, client) : undefined
+            setRooms((prev) =>
+                isEqual(prev[streamId], newRoom) ? prev : { ...prev, [streamId]: newRoom },
+            )
+        }
 
         const setInitialState = () => {
             setRooms({})
+            const allChannelsAndSpaces = Array.from(client.streams.keys())
+                .filter((stream) => {
+                    return isSpaceStreamId(stream) || isChannelStreamId(stream)
+                })
+                .reduce((acc: Record<string, Room | undefined>, stream: string) => {
+                    acc[stream] = toZionCasablancaRoom(stream, client)
+                    return acc
+                }, {})
+            setRooms(allChannelsAndSpaces)
         }
 
         // initial state
         setInitialState()
 
         // subscribe to changes
+        const onStreamNewUserJoined = (streamId: string) => {
+            updateState(streamId)
+        }
+
+        const onStreamUserLeft = (streamId: string) => {
+            updateState(streamId)
+        }
+
+        const onUserLeftStream = (streamId: string) => {
+            updateState(streamId)
+        }
+
+        client.on('streamNewUserJoined', onStreamNewUserJoined)
+        client.on('streamUserLeft', onStreamUserLeft)
+        client.on('userLeftStream', onUserLeftStream)
 
         return () => {
+            client.off('streamNewUserJoined', onStreamNewUserJoined)
+            client.off('streamUserLeft', onStreamUserLeft)
+            client.off('userLeftStream', onUserLeftStream)
             setRooms({})
         }
     }, [client])
