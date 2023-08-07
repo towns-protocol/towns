20872e77bab5103b617d6e9a58b7151754d0bba1
Erik Olsson <valross2@gmail.com>
2023-08-07 22:23:04 +0200

    add wireframe tab bar

---
+++ clients/web/app/src/components/TouchTabBar/TouchTabBar.tsx
@@ -0,0 +1,76 @@
+import React from 'react'
+import { useMyProfile, useSpaceData } from 'use-zion-client'
+import { Avatar, Box, Icon, Stack, Text } from '@ui'
+import { SpaceIcon } from '@components/SpaceIcon'
+import { ImageVariants } from '@components/UploadImage/useImageSource'
+
+export const TouchTabBar = () => {
+    const space = useSpaceData()
+    const userId = useMyProfile()?.userId
+    if (!space) {
+        return null
+    }
+
+    return (
+        <Box background="level2" insetBottom="safeArea">
+            <Stack horizontal width="100%" background="level2" display="flex" paddingY="sm">
+                <TabBarItem
+                    highlighted
+                    title="Home"
+                    icon={
+                        <SpaceIcon
+                            width="x3"
+                            height="x3"
+                            spaceId={space?.id.slug}
+                            firstLetterOfSpaceName={space?.name[0]}
+                            overrideBorderRadius="sm"
+                            variant={ImageVariants.thumbnail50}
+                            fadeIn={false}
+                        />
+                    }
+                />
+                <TabBarItem
+                    title="Threads"
+                    icon={<Icon type="message" size="square_md" />}
+                    highlighted={false}
+                />
+                <TabBarItem
+                    title="Mentions"
+                    icon={<Icon type="at" size="square_md" />}
+                    highlighted={false}
+                />
+                <TabBarItem
+                    title="You"
+                    icon={<Avatar size="avatar_sm" userId={userId} />}
+                    highlighted={false}
+                />
+            </Stack>
+        </Box>
+    )
+}
+
+type TabBarItemProps = {
+    title: string
+    icon: React.ReactNode
+    highlighted: boolean
+    onClick?: () => void
+}
+
+const TabBarItem = (props: TabBarItemProps) => {
+    const { title, icon, highlighted } = props
+
+    return (
+        <Stack
+            flexGrow="x1"
+            alignItems="center"
+            color={highlighted ? 'default' : 'gray2'}
+            gap="xs"
+            onClick={() => {
+                props.onClick?.()
+            }}
+        >
+            {icon}
+            <Text fontSize="xs">{title}</Text>
+        </Stack>
+    )
+}
--- clients/web/app/src/routes/layouts/AppTouchLayout.tsx
+++ clients/web/app/src/routes/layouts/AppTouchLayout.tsx
@@ -3,6 +3,7 @@ import { Outlet } from 'react-router'
 import { Stack } from '@ui'
 import { SuspenseLoader } from '@components/Loaders/SuspenseLoader'
 import { TouchLayoutHeader } from '@components/TouchLayoutHeader/TouchLayoutHeader'
+import { TouchTabBar } from '@components/TouchTabBar/TouchTabBar'
 
 export const AppTouchLayout = () => {
     return (
@@ -11,6 +12,7 @@ export const AppTouchLayout = () => {
             <SuspenseLoader>
                 <Outlet />
             </SuspenseLoader>
+            <TouchTabBar />
         </Stack>
     )
 }
