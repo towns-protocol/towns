f0f56ff830ea89d18d424c6f25f85e2ad6676bf3
Erik Olsson <valross2@gmail.com>
2023-08-08 18:42:45 +0200

    app: clean up overlay, add title (#2928)
    This PR cleans up the overlay for the new navigation.
    
    | Before | After |
    | :-- | :-- |
    |
    ![IMG_6837](https://github.com/HereNotThere/harmony/assets/352982/6331f809-6a71-4e45-8cad-d5b414e025b0)
    |
    ![IMG_6836](https://github.com/HereNotThere/harmony/assets/352982/d8ff1faf-36ec-4321-8889-5f289ab60ef1)
    |

--- clients/web/app/src/components/TouchHomeOverlay/TouchHomeOverlay.tsx
+++ clients/web/app/src/components/TouchHomeOverlay/TouchHomeOverlay.tsx
@@ -1,17 +1,10 @@
-import React, { useCallback, useState } from 'react'
-import {
-    createUserIdFromString,
-    useMyProfile,
-    useSpaceContext,
-    useZionContext,
-} from 'use-zion-client'
-import { useNavigate } from 'react-router'
+import React, { useState } from 'react'
+import { useSpaceContext, useZionContext } from 'use-zion-client'
+
 import { useEvent } from 'react-use-event-hook'
-import { Avatar, Box, Icon, IconName, MotionBox, MotionStack, Stack, Text } from '@ui'
-import { shortAddress } from 'ui/utils/utils'
+import { Box, Icon, IconButton, IconName, MotionBox, MotionStack, Stack, Text } from '@ui'
+
 import { SpaceNavItem } from '@components/NavItem/SpaceNavItem'
-import { useCreateLink } from 'hooks/useCreateLink'
-import { useAuth } from 'hooks/useAuth'
 import { ModalContainer } from '@components/Modals/ModalContainer'
 import { SentryErrorReportForm } from '@components/SentryErrorReport/SentryErrorReport'
 import { transitions } from 'ui/transitions/transitions'
@@ -23,25 +16,13 @@ type Props = {
 
 export const TouchHomeOverlay = (props: Props) => {
     const { onClose } = props
-    const { logout } = useAuth()
-    const user = useMyProfile()
-    const userAddress = user ? createUserIdFromString(user.userId)?.accountAddress : undefined
+
     const { spaces } = useZionContext()
     const { spaceId } = useSpaceContext()
-
-    const { createLink: createProfileLink } = useCreateLink()
-    const link = createProfileLink({ profileId: 'me' })
-    const navigate = useNavigate()
 
     const [isSentryModalVisible, setIsSentryModalVisible] = useState(false)
     const showSentryModal = useEvent(() => setIsSentryModalVisible(true))
     const hideSentryModal = useEvent(() => setIsSentryModalVisible(false))
-
-    const onViewProfileClicked = useCallback(() => {
-        if (link) {
-            navigate(link)
-        }
-    }, [link, navigate])
 
     return (
         <Box absoluteFill height="100dvh">
@@ -72,59 +53,40 @@ export const TouchHomeOverlay = (props: Props) => {
                     background="level1"
                     style={{ position: 'absolute', left: -100, top: 0, bottom: 0, width: 100 }}
                 />
-                {user && (
-                    <Stack
-                        horizontal
-                        paddingX="lg"
-                        paddingTop="lg"
-                        paddingBottom="sm"
-                        alignItems="center"
-                        gap="sm"
-                    >
-                        <Avatar
-                            size="avatar_x4"
-                            userId={user.userId}
-                            onClick={onViewProfileClicked}
-                        />
-                        <Stack gap="sm" onClick={onViewProfileClicked}>
-                            <Text>{user.displayName}</Text>
-                            {userAddress && <Text color="gray2">{shortAddress(userAddress)}</Text>}
-                        </Stack>
 
-                        <Stack grow />
-                        <Icon type="close" color="gray2" onClick={onClose} />
-                    </Stack>
-                )}
                 <Box grow>
-                    <Stack scroll paddingX="sm">
-                        {spaces.map((s) => (
-                            <SpaceNavItem
-                                key={s.id.slug}
-                                exact={false}
-                                forceMatch={s.id.networkId === spaceId?.networkId}
-                                id={s.id}
-                                name={s.name}
-                                avatar={s.avatarSrc}
-                                pinned={false}
-                                onClick={onClose}
-                            />
-                        ))}
+                    <Stack horizontal padding="md" paddingRight="sm" alignItems="center">
+                        <Text fontSize="lg" fontWeight="strong">
+                            Towns
+                        </Text>
+                        <Box grow />
+                        <IconButton icon="close" onClick={onClose} />
                     </Stack>
+                    <Box grow scroll>
+                        <Box minHeight="forceScroll">
+                            {spaces.map((s) => (
+                                <SpaceNavItem
+                                    key={s.id.slug}
+                                    exact={false}
+                                    forceMatch={s.id.networkId === spaceId?.networkId}
+                                    id={s.id}
+                                    name={s.name}
+                                    avatar={s.avatarSrc}
+                                    pinned={false}
+                                    onClick={onClose}
+                                />
+                            ))}
+                        </Box>
+                    </Box>
                 </Box>
+
                 <Stack borderTop padding="sm">
-                    <BottomSectionButton
-                        signout={false}
-                        label="View profile"
-                        icon="profile"
-                        onClick={onViewProfileClicked}
-                    />
                     <BottomSectionButton
                         signout={false}
                         label="Report a bug"
                         icon="help"
                         onClick={showSentryModal}
                     />
-                    <BottomSectionButton signout label="Log out" icon="logout" onClick={logout} />
                     <Box paddingTop="md" paddingBottom="lg">
                         <Text textAlign="center" color="gray2" fontSize="sm">
                             Towns {APP_VERSION} ({APP_COMMIT_HASH})
