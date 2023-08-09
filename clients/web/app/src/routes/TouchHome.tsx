837c3e3fb33f9589f0a502521b85f571e4ce2450
Erik Olsson <valross2@gmail.com>
2023-08-09 11:43:37 +0200

    app: add browse channels from Home (hnt-1963) (#2940)
    this PR adds a "Browse Channels" button in the home view:
    
    <img width="374" alt="image"
    src="https://github.com/HereNotThere/harmony/assets/352982/ec716290-ca90-494d-a1ab-636b719dd0e9">

--- clients/web/app/src/routes/TouchHome.tsx
+++ clients/web/app/src/routes/TouchHome.tsx
@@ -1,27 +1,51 @@
-import React from 'react'
+import React, { useCallback, useState } from 'react'
 import { useSpaceData, useSpaceMentions } from 'use-zion-client'
-import { Stack } from '@ui'
+import { Box, Stack } from '@ui'
 import { SyncedChannelList } from '@components/SideBars/SpaceSideBar/SyncedChannelList'
 import { TouchLayoutHeader } from '@components/TouchLayoutHeader/TouchLayoutHeader'
+import { ActionNavItem } from '@components/NavItem/ActionNavItem'
+import { ModalContainer } from '@components/Modals/ModalContainer'
 import { CentralPanelLayout } from './layouts/CentralPanelLayout'
+import { AllChannelsList } from './AllChannelsList/AllChannelsList'
 
 export const TouchHome = () => {
     const space = useSpaceData()
     const mentions = useSpaceMentions()
 
+    const [visibleModal, setVisibleModal] = useState<'browse' | undefined>(undefined)
+    const onShowBrowseChannels = useCallback(() => setVisibleModal('browse'), [setVisibleModal])
+    const onHideBrowseChannels = useCallback(() => setVisibleModal(undefined), [setVisibleModal])
+
     return (
         <CentralPanelLayout>
             <Stack height="100%">
                 <TouchLayoutHeader />
-                {space && (
-                    <SyncedChannelList
-                        space={space}
-                        mentions={mentions}
-                        canCreateChannel={false}
-                        onShowCreateChannel={() => {
-                            // noop
-                        }}
-                    />
+                <Box scroll grow>
+                    <Box minHeight="forceScroll">
+                        {space && (
+                            <>
+                                <SyncedChannelList
+                                    space={space}
+                                    mentions={mentions}
+                                    canCreateChannel={false}
+                                />
+
+                                {!space.isLoadingChannels && (
+                                    <ActionNavItem
+                                        icon="search"
+                                        id="browseChannels"
+                                        label="Browse channels"
+                                        onClick={onShowBrowseChannels}
+                                    />
+                                )}
+                            </>
+                        )}
+                    </Box>
+                </Box>
+                {visibleModal === 'browse' && (
+                    <ModalContainer touchTitle="Browse channels" onHide={onHideBrowseChannels}>
+                        <AllChannelsList onHideBrowseChannels={onHideBrowseChannels} />
+                    </ModalContainer>
                 )}
             </Stack>
         </CentralPanelLayout>
