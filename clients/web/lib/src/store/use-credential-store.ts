de018f7a83dd332c9b0917949d0414b7a12daed0
Evan Salzbrenner <evan@hntlabs.com>
2023-08-10 11:25:47 -0700

    lib: pass stable deviceId on login (#2964)
    Alternative to https://github.com/HereNotThere/harmony/pull/2961, to
    remedy the issue of multiple indexedDB instances.
    
    Sends a previously local storage stored device_id when logging in, if
    one exists. We can't send it on register, because the indexDB/device_id
    doesn't exist yet. Though we could create one by some means during the
    register flow, if that is preferred, I suppose using some sort of uuid
    lib.
    
    On register and/or next login:
    - saves the device_id from the matrix auth response to local storage
    - on subsequent logins, uses the same device_id

--- clients/web/lib/src/store/use-credential-store.ts
+++ clients/web/lib/src/store/use-credential-store.ts
@@ -21,6 +21,9 @@ export type CasablancaCredentials = {
 
 export type CredentialStoreStates = {
     matrixCredentialsMap: Record<string, MatrixCredentials | null>
+    // the device id generated on first login/register is saved and used for all subsequent logins
+    // stored in a separate map because of logic around `matrixCredentialsMap` in our login/client listener flow
+    matrixDeviceIdMap?: Record<string, string | null>
     setMatrixCredentials: (
         homeServerUrl: string,
         matrixCredentials: MatrixCredentials | null,
@@ -45,17 +48,34 @@ export const useCredentialStore = create<CredentialStoreStates>(
     (persist as unknown as MyPersist)(
         (set) => ({
             matrixCredentialsMap: {},
+            // setMatrixCredentials is called when logging in, to set the credentials
+            // and when logging out, to clear them
             setMatrixCredentials: (
                 homeServerUrl: string,
                 matrixCredentials: MatrixCredentials | null,
             ) =>
-                set((state) => ({
-                    ...state,
-                    matrixCredentialsMap: {
-                        ...state.matrixCredentialsMap,
-                        [homeServerUrl]: matrixCredentials,
-                    },
-                })),
+                set((state) => {
+                    if (matrixCredentials === null) {
+                        return {
+                            ...state,
+                            matrixCredentialsMap: {
+                                ...state.matrixCredentialsMap,
+                                [homeServerUrl]: null,
+                            },
+                        }
+                    }
+                    return {
+                        ...state,
+                        matrixCredentialsMap: {
+                            ...state.matrixCredentialsMap,
+                            [homeServerUrl]: matrixCredentials,
+                        },
+                        matrixDeviceIdMap: {
+                            ...state.matrixDeviceIdMap,
+                            [homeServerUrl]: matrixCredentials.deviceId,
+                        },
+                    }
+                }),
             casablancaCredentialsMap: {},
             setCasablancaCredentials: (
                 homeServerUrl: string,
