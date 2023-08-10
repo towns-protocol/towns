e1699f94122dd0f776afd8af121f796629eb3d0d
texuf <austin@hntlabs.com>
2023-08-10 12:07:42 -0700

    Add nil guards when updating snapshots (#2982)
    Turns out proto.Clone won’t clone an empty map, it becomes nil. That’s
    fine i guess. This code will be easier to update.

--- casablanca/node/events/snapshot_init.go
+++ casablanca/node/events/snapshot_init.go
@@ -24,60 +24,35 @@ func Make_SnapshotContent(iPayload IsInceptionPayload) (IsSnapshot_Content, erro
 	switch payload := iPayload.(type) {
 	case *SpacePayload_Inception:
 		return &Snapshot_SpaceContent{
-			SpaceContent: make_Space_Snapshot(payload),
+			SpaceContent: &SpacePayload_Snapshot{
+				Inception: payload,
+			},
 		}, nil
 	case *ChannelPayload_Inception:
 		return &Snapshot_ChannelContent{
-			ChannelContent: make_Channel_Snapshot(payload),
+			ChannelContent: &ChannelPayload_Snapshot{
+				Inception: payload,
+			},
 		}, nil
 	case *UserPayload_Inception:
 		return &Snapshot_UserContent{
-			UserContent: make_User_Snapshot(payload),
+			UserContent: &UserPayload_Snapshot{
+				Inception: payload,
+			},
 		}, nil
 	case *UserSettingsPayload_Inception:
 		return &Snapshot_UserSettingsContent{
-			UserSettingsContent: make_UserSettings_Snapshot(payload),
+			UserSettingsContent: &UserSettingsPayload_Snapshot{
+				Inception: payload,
+			},
 		}, nil
 	case *UserDeviceKeyPayload_Inception:
 		return &Snapshot_UserDeviceKeyContent{
-			UserDeviceKeyContent: make_UserDeviceKey_Snapshot(payload),
+			UserDeviceKeyContent: &UserDeviceKeyPayload_Snapshot{
+				Inception: payload,
+			},
 		}, nil
 	default:
 		return nil, fmt.Errorf("unknown inception payload type %T", iPayload)
-	}
-}
-
-func make_Space_Snapshot(inception *SpacePayload_Inception) *SpacePayload_Snapshot {
-	return &SpacePayload_Snapshot{
-		Inception:   inception,
-		Channels:    make(map[string]*SpacePayload_Channel),
-		Memberships: make(map[string]*Membership),
-	}
-}
-
-func make_Channel_Snapshot(inception *ChannelPayload_Inception) *ChannelPayload_Snapshot {
-	return &ChannelPayload_Snapshot{
-		Inception:   inception,
-		Memberships: make(map[string]*Membership),
-	}
-}
-
-func make_User_Snapshot(inception *UserPayload_Inception) *UserPayload_Snapshot {
-	return &UserPayload_Snapshot{
-		Inception:   inception,
-		Memberships: make(map[string]*UserPayload_UserMembership),
-	}
-}
-
-func make_UserSettings_Snapshot(inception *UserSettingsPayload_Inception) *UserSettingsPayload_Snapshot {
-	return &UserSettingsPayload_Snapshot{
-		Inception:        inception,
-		FullyReadMarkers: make(map[string]*UserSettingsPayload_FullyReadMarkers),
-	}
-}
-
-func make_UserDeviceKey_Snapshot(inception *UserDeviceKeyPayload_Inception) *UserDeviceKeyPayload_Snapshot {
-	return &UserDeviceKeyPayload_Snapshot{
-		Inception: inception,
 	}
 }
