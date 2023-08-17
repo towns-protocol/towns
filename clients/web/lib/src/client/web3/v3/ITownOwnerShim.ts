dbfc9856ac18dba64abb75cd37e8306754b303c2
Tak Wai Wong <64229756+tak-hntlabs@users.noreply.github.com>
2023-08-17 17:52:33 -0400

    add ITownOwnerShim (#3139)

---
+++ clients/web/lib/src/client/web3/v3/ITownOwnerShim.ts
@@ -0,0 +1,41 @@
+import {
+    ITownOwner as GoerliContract,
+    ITownOwnerInterface as GoerliInterface,
+} from '@towns/generated/goerli/v3/typings/ITownOwner'
+import {
+    ITownOwner as LocalhostContract,
+    ITownOwnerBase as LocalhostITownOwnerBase,
+    ITownOwnerInterface as LocalhostInterface,
+} from '@towns/generated/localhost/v3/typings/ITownOwner'
+import {
+    ITownOwner as SepoliaContract,
+    ITownOwnerInterface as SepoliaInterface,
+} from '@towns/generated/sepolia/v3/typings/ITownOwner'
+
+import { BaseContractShimV3 } from './BaseContractShimV3'
+
+import GoerliAbi from '@towns/generated/goerli/v3/abis/TownOwner.abi.json' assert { type: 'json' }
+
+import LocalhostAbi from '@towns/generated/localhost/v3/abis/TownOwner.abi.json' assert { type: 'json' }
+
+import SepoliaAbi from '@towns/generated/sepolia/v3/abis/TownOwner.abi.json' assert { type: 'json' }
+import { ethers } from 'ethers'
+
+export type { LocalhostITownOwnerBase as ITownOwnerBase }
+
+export class ITownOwnerShim extends BaseContractShimV3<
+    LocalhostContract,
+    LocalhostInterface,
+    GoerliContract,
+    GoerliInterface,
+    SepoliaContract,
+    SepoliaInterface
+> {
+    constructor(address: string, chainId: number, provider: ethers.providers.Provider | undefined) {
+        super(address, chainId, provider, {
+            localhostAbi: LocalhostAbi,
+            goerliAbi: GoerliAbi,
+            sepoliaAbi: SepoliaAbi,
+        })
+    }
+}
