f177a84aacbe37fae6dd75a0b9e616905af9cdd4
Theo Tillberg <ttillberg@gmail.com>
2023-08-21 20:18:24 +0200

    app: fix tooltips on safari (#3180)

--- clients/web/app/src/ui/components/Tooltip/Tooltip.css.ts
+++ clients/web/app/src/ui/components/Tooltip/Tooltip.css.ts
@@ -3,7 +3,7 @@ import { lightTheme } from 'ui/styles/vars.css'
 
 export const tooltip = style({
     filter: `
-        drop-shadow(0px 0px 0.5px rgba(255, 255, 255, 0.5))
+        drop-shadow(0px 0px 1px rgba(255, 255, 255, 0.33))
         drop-shadow(0px 0px 2px rgba(0, 0, 0, 0.5))
         drop-shadow(2px 4px 16px rgba(0, 0, 0, 0.5))
     `,
@@ -20,6 +20,7 @@ globalStyle(`${lightTheme} ${tooltip} `, {
 })
 
 export const arrow = style({
+    position: `relative`,
     selectors: {
         '&:after': {
             content: '',
