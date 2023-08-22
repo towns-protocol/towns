b68069ff94acd37ece429bffd1ecedb5d37ca1af
Theo Tillberg <ttillberg@gmail.com>
2023-08-22 22:35:02 +0200

    app: dismiss keyboard when swiping down (hnt-1939) (#3198)
    it kind of fakes it pretty well, you don't get that nice native
    interaction but the expected outcome is similar I reckon. especially if
    the gesture is performed swiftly.
    
    
    https://github.com/HereNotThere/harmony/assets/557990/8cd138c7-a625-4b69-99e0-54a31b8f3fde

--- clients/web/app/src/components/VisualViewportContext/VisualViewportContext.tsx
+++ clients/web/app/src/components/VisualViewportContext/VisualViewportContext.tsx
@@ -1,33 +1,35 @@
 import React, { useContext, useEffect } from 'react'
+import { useDevice } from 'hooks/useDevice'
 
-const VisualKeyboardContext = React.createContext<{
+const VisualViewportContext = React.createContext<{
     visualViewportScrolled: boolean
     offset?: number
     height?: number
 }>({ visualViewportScrolled: false, offset: 0 })
 
 export const useVisualViewportContext = () => {
-    const context = useContext(VisualKeyboardContext)
+    const context = useContext(VisualViewportContext)
     return context
 }
 
 const { visualViewport } = window
 
 export const VisualViewportContextProvider = ({ children }: { children: React.ReactNode }) => {
-    const [tabBarHidden, setTabBarHidden] = React.useState(false)
+    const { isTouch } = useDevice()
+    const [setIsKeyboardPresent, isKeyboardPresent] = React.useState(false)
     const [offset, setOffset] = React.useState(visualViewport?.pageTop)
     const [height, setHeight] = React.useState(visualViewport?.height)
 
     useEffect(() => {
         const onScroll = () => {
-            setTabBarHidden((visualViewport?.pageTop ?? 0) > 0)
+            isKeyboardPresent((visualViewport?.pageTop ?? 0) > 0)
             setOffset(visualViewport?.pageTop ?? 0)
         }
         visualViewport?.addEventListener('scroll', onScroll)
         return () => {
             visualViewport?.removeEventListener('scroll', onScroll)
         }
-    }, [setTabBarHidden])
+    }, [isKeyboardPresent])
 
     useEffect(() => {
         const onResize = () => {
@@ -37,13 +39,56 @@ export const VisualViewportContextProvider = ({ children }: { children: React.Re
         return () => {
             visualViewport?.removeEventListener('resize', onResize)
         }
-    }, [setTabBarHidden])
+    }, [isKeyboardPresent])
+
+    /**
+     * dismiss keyboard when (fake) swiping down on iOS
+     */
+    useEffect(() => {
+        const viewportHeight = visualViewport?.height
+
+        if (!isTouch || !setIsKeyboardPresent || !viewportHeight) {
+            return
+        }
+
+        const activeElement = document.activeElement as HTMLElement
+
+        if (!activeElement) {
+            return
+        }
+
+        const touchStart = (e: TouchEvent) => {
+            window.addEventListener('touchmove', touchMove, { passive: true })
+            window.addEventListener('touchend', touchEnd, { passive: true })
+        }
+
+        const touchMove = (e: TouchEvent) => {
+            const y = e.touches[0].clientY
+            if (y > viewportHeight) {
+                if (activeElement) {
+                    activeElement.blur()
+                }
+            }
+        }
+        const touchEnd = (e: TouchEvent) => {
+            window.removeEventListener('touchmove', touchMove)
+            window.removeEventListener('touchend', touchEnd)
+        }
+
+        window.addEventListener('touchstart', touchStart, { passive: true })
+
+        return () => {
+            window.removeEventListener('touchstart', touchStart)
+            window.removeEventListener('touchmove', touchMove)
+            window.removeEventListener('touchend', touchEnd)
+        }
+    }, [isTouch, setIsKeyboardPresent])
 
     return (
-        <VisualKeyboardContext.Provider
-            value={{ visualViewportScrolled: tabBarHidden, offset, height }}
+        <VisualViewportContext.Provider
+            value={{ visualViewportScrolled: setIsKeyboardPresent, offset, height }}
         >
             {children}
-        </VisualKeyboardContext.Provider>
+        </VisualViewportContext.Provider>
     )
 }
