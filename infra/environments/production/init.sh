bd7d3e1e56628de75b4029bea27c9e7cc481a069
John Terzis <john@hntlabs.com>
2023-08-23 16:01:53 -0700

    Partition backends between alpha and beta along with workspaces (#3204)
    Closes HNT-2186: Partitions our backend (s3+dynamo for lock file) in aws
    for Terraform state between `alpha` and `beta`.
    Updates aws provider module to latest to remove errors thrown on
    `./init.sh`.
    
    This PR sets us up to run `tf plan` for our new beta infrastructure
    (ecs, nlb, rds) after we prune away modules specific to alpha (resources
    with dendrite* labels). Still using the same aws account and IAM
    profile, which we may want to change in the future as well.
    
    To test run:
    
    ```
    # navigate to test environment folder
    ./init.sh --beta
    terraform plan
    ```
    
    ![Screenshot 2023-08-22 at 11 57 21
    AM](https://github.com/HereNotThere/harmony/assets/1355694/b3f8ec65-c8ba-4345-aeca-69e0aea8520d)

--- infra/environments/production/init.sh
+++ infra/environments/production/init.sh
@@ -1,5 +1,33 @@
 #!/bin/sh
 
-terraform init -backend-config=../../backend.conf
-terraform workspace new production
-terraform workspace select production
+# Default values
+backend_folder="../../backends"
+backend_config="$backend_folder/backend.conf"
+workspace_name="production"
+
+# Function to display usage instructions
+usage() {
+    echo "Usage: $0 [--beta]"
+    exit 1
+}
+
+# Parse command line arguments
+while [ "$#" -gt 0 ]; do
+    case "$1" in
+        --beta)
+            backend_config="$backend_folder/backend-beta.conf"
+            workspace_name="$workspace_name-beta"
+            shift
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+
+# Initialize Terraform
+terraform init -backend-config="$backend_config"
+
+# Create and select workspace
+terraform workspace new "$workspace_name"
+terraform workspace select "$workspace_name"
