6ac00b5c1233816542e6d8bb1de49087fb3e138b
Giuseppe Rodriguez <5714678+giuseppecrj@users.noreply.github.com>
2023-08-24 12:00:30 -0700

    Deploy sepolia contracts (#3235)

--- contracts/scripts/deployments/DeployTownOwner.s.sol
+++ contracts/scripts/deployments/DeployTownOwner.s.sol
@@ -14,13 +14,14 @@ import {TownOwner} from "contracts/src/towns/facets/owner/TownOwner.sol";
 import {DiamondCutFacet} from "contracts/src/diamond/facets/cut/DiamondCutFacet.sol";
 import {DiamondLoupeFacet} from "contracts/src/diamond/facets/loupe/DiamondLoupeFacet.sol";
 import {OwnableFacet} from "contracts/src/diamond/facets/ownable/OwnableFacet.sol";
+import {IntrospectionFacet} from "contracts/src/diamond/facets/introspection/IntrospectionFacet.sol";
 
 // helpers
 import {DiamondCutHelper} from "contracts/test/diamond/cut/DiamondCutSetup.sol";
 import {DiamondLoupeHelper} from "contracts/test/diamond/loupe/DiamondLoupeSetup.sol";
 import {OwnableHelper} from "contracts/test/diamond/ownable/OwnableSetup.sol";
-
 import {TownOwnerHelper} from "contracts/test/towns/owner/TownOwnerSetup.sol";
+import {IntrospectionHelper} from "contracts/test/diamond/introspection/IntrospectionSetup.sol";
 
 import {MultiInit} from "contracts/src/diamond/initializers/MultiInit.sol";
 
@@ -28,16 +29,18 @@ contract DeployTownOwner is Deployer {
   DiamondCutHelper diamondCutHelper = new DiamondCutHelper();
   DiamondLoupeHelper diamondLoupeHelper = new DiamondLoupeHelper();
   OwnableHelper ownableHelper = new OwnableHelper();
+  IntrospectionHelper introspectionHelper = new IntrospectionHelper();
 
   TownOwnerHelper townOwnerHelper = new TownOwnerHelper();
 
-  address[] addresses = new address[](4);
-  bytes[] payloads = new bytes[](4);
+  address[] addresses = new address[](5);
+  bytes[] payloads = new bytes[](5);
 
   address diamondCut;
   address diamondLoupe;
   address ownable;
   address townOwner;
+  address introspection;
   address multiInit;
 
   function versionName() public pure override returns (string memory) {
@@ -53,10 +56,11 @@ contract DeployTownOwner is Deployer {
     diamondLoupe = address(new DiamondLoupeFacet());
     ownable = address(new OwnableFacet());
     townOwner = address(new TownOwner());
+    introspection = address(new IntrospectionFacet());
     multiInit = address(new MultiInit());
     vm.stopBroadcast();
 
-    IDiamond.FacetCut[] memory cuts = new IDiamond.FacetCut[](4);
+    IDiamond.FacetCut[] memory cuts = new IDiamond.FacetCut[](5);
     uint256 index;
 
     cuts[index++] = diamondCutHelper.makeCut(
@@ -68,6 +72,12 @@ contract DeployTownOwner is Deployer {
       IDiamond.FacetCutAction.Add
     );
     cuts[index++] = ownableHelper.makeCut(ownable, IDiamond.FacetCutAction.Add);
+
+    cuts[index++] = introspectionHelper.makeCut(
+      introspection,
+      IDiamond.FacetCutAction.Add
+    );
+
     cuts[index++] = townOwnerHelper.makeCut(
       townOwner,
       IDiamond.FacetCutAction.Add
@@ -78,6 +88,7 @@ contract DeployTownOwner is Deployer {
     addresses[index++] = diamondCut;
     addresses[index++] = diamondLoupe;
     addresses[index++] = ownable;
+    addresses[index++] = introspection;
     addresses[index++] = townOwner;
 
     index = 0;
@@ -85,6 +96,7 @@ contract DeployTownOwner is Deployer {
     payloads[index++] = diamondCutHelper.makeInitData("");
     payloads[index++] = diamondLoupeHelper.makeInitData("");
     payloads[index++] = ownableHelper.makeInitData(abi.encode(deployer));
+    payloads[index++] = introspectionHelper.makeInitData("");
     payloads[index++] = abi.encodeWithSelector(
       townOwnerHelper.initializer(),
       "Town Owner",
