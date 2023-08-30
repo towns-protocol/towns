bac560e425105a4feb9ccf96bea1af21743c4782
Tak Wai Wong <64229756+tak-hntlabs@users.noreply.github.com>
2023-08-29 20:26:04 -0400

    remove smart contract v3 flag from the sample-app (#3307)

--- clients/web/sample-app/src/App.tsx
+++ clients/web/sample-app/src/App.tsx
@@ -26,7 +26,6 @@ import { Channels } from './routes/Channels'
 import { AuthenticatedContent } from './routes/AuthenticatedContent'
 
 const ALCHEMY_KEY = import.meta.env.VITE_ALCHEMY_API_KEY ?? ''
-const SMART_CONTRACT_VERSION = 'v3' /*import.meta.env.VITE_SMART_CONTRACT_VERSION*/
 
 export const App = () => {
     const { matrixUrl, casablancaUrl, chainId } = useEnvironment()
@@ -42,7 +41,6 @@ export const App = () => {
                     chainId={chainId}
                     onboardingOpts={{ skipAvatar: true, showWelcomeSpash: false }}
                     initialSyncLimit={100}
-                    smartContractVersion={SMART_CONTRACT_VERSION}
                 >
                     <Routes>
                         <Route path="/alpha-access" element={<AlphaAccessMainPage />} />
