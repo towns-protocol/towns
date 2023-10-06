693000d163f233f8f4654dbe9e969d0b96743ba5
Evan Salzbrenner <evan@hntlabs.com>
2023-10-06 14:41:16 -0700

    app: delete the old create space form (#3674)

--- clients/web/app/src/components/Web3/index.tsx
+++ clients/web/app/src/components/Web3/index.tsx
@@ -1,4 +1,3 @@
-export { CreateSpaceForm } from './CreateSpaceForm'
 export { CreateChannelForm } from './CreateChannelForm'
 export { InviteUserToRoomForm } from './InviteUserToRoomForm'
 export { AcceptRoomInviteForm } from './AcceptRoomInviteForm'
