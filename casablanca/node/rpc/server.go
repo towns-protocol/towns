0fe8a00e5f07eaf7b1f50b7d064cf0fe8ac0c6e3
Brian Meek <brian@hntlabs.com>
2023-10-13 14:53:36 -0700

    Add stacks page to help us detect leaking goroutines and hung nodes (#3744)
    <img width="2147" alt="image"
    src="https://github.com/HereNotThere/harmony/assets/55990082/7a722074-1078-4cda-85ac-1fd49857e03b">
    
    Signed-off-by: Brian Meek <brian@hntlabs.com>

--- casablanca/node/rpc/server.go
+++ casablanca/node/rpc/server.go
@@ -9,6 +9,9 @@ import (
 	"casablanca/node/nodes"
 	"casablanca/node/protocol/protocolconnect"
 	"casablanca/node/storage"
+	"html"
+	"runtime"
+	"strings"
 
 	"context"
 	"fmt"
@@ -25,6 +28,40 @@ import (
 )
 
 type Cleanup func(context.Context) error
+
+// GenerateStackHTML generates an HTML table from runtime.Stack
+func generateStackHTML() string {
+	buf := make([]byte, 1024*1024)
+	stackSize := runtime.Stack(buf, true)
+	stacks := string(buf[:stackSize])
+
+	// Escape HTML-specific characters for safety
+	stacks = html.EscapeString(stacks)
+
+	lines := strings.Split(stacks, "\n")
+	var sb strings.Builder
+	sb.WriteString("<table border='1'>")
+	sb.WriteString("<tr><th>Goroutine</th><th>Stack</th></tr>")
+	var goroutine, stack string
+	for _, line := range lines {
+		if strings.HasPrefix(line, "goroutine ") {
+			if goroutine != "" {
+				sb.WriteString(fmt.Sprintf("<tr><td>%s</td><td><pre>%s</pre></td></tr>", goroutine, stack))
+			}
+			goroutine = line
+			stack = ""
+		} else if strings.HasPrefix(line, "\t") || line == "" {
+			stack += line + "\n"
+		} else {
+			stack += "\t" + line + "\n"
+		}
+	}
+	if goroutine != "" {
+		sb.WriteString(fmt.Sprintf("<tr><td>%s</td><td><pre>%s</pre></td></tr>", goroutine, stack))
+	}
+	sb.WriteString("</table>")
+	return sb.String()
+}
 
 func loadNodeRegistry(ctx context.Context, nodeRegistryPath string, localNodeAddress string) (nodes.NodeRegistry, error) {
 	log := dlog.CtxLog(ctx)
@@ -159,6 +196,17 @@ func StartServer(ctx context.Context, cfg *config.Config, wallet *crypto.Wallet)
 		w.Header().Set("Content-Type", "text/plain")
 		w.WriteHeader(http.StatusOK)
 		_, err := w.Write([]byte("All good in the Towns land!\n"))
+		if err != nil {
+			log.Warn("Failed to write response", "error", err, "request", *r)
+		}
+	})
+
+	mux.HandleFunc("/stacks", func(w http.ResponseWriter, r *http.Request) {
+		// TODO: reply with graffiti from config and with node version
+		log.Debug("Got request for /monitor", "request", *r)
+		w.Header().Set("Content-Type", "text/html")
+		w.WriteHeader(http.StatusOK)
+		_, err := w.Write([]byte(generateStackHTML()))
 		if err != nil {
 			log.Warn("Failed to write response", "error", err, "request", *r)
 		}
