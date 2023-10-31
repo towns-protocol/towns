26d952230f3e83b10f4bae0e669b057d06d0203f
texuf <austinellis+git@gmail.com>
2023-10-31 10:53:02 -0700

    Remove unnecessary code in go - getMembers (#3840)
    What role is the filterBy array playing in this... seems like it can
    only cause bugs...
    
    Each stream only contains 1 type of snapshot, and 1 stream type of
    membership. There’s never a case where you would need to filter out
    anything else.
    
    If you want the members of a stream, you don’t care what kind of stream
    it is.
    
    1) if you put the right value in your array, it works like you'd expect,
    i.e. ever instance of slices.Contains always returns true
    
    2) if you forget to put the right value in your slice, this function
    returns incorrect data.
    
    Just remove it

--- casablanca/node/events/stream_viewstate_joinable.go
+++ casablanca/node/events/stream_viewstate_joinable.go
@@ -1,11 +1,9 @@
 package events
 
 import (
-	"casablanca/node/common"
 	"casablanca/node/protocol"
 
 	mapset "github.com/deckarep/golang-set/v2"
-	"golang.org/x/exp/slices"
 )
 
 type JoinableStreamView interface {
@@ -15,7 +13,7 @@ type JoinableStreamView interface {
 }
 
 func (r *streamViewImpl) IsUserJoined(userId string) (bool, error) {
-	members, err := r.getMembers([]common.StreamType{common.Space, common.Channel, common.DMChannel})
+	members, err := r.getMembers()
 	if err != nil {
 		return false, err
 	}
@@ -24,35 +22,29 @@ func (r *streamViewImpl) IsUserJoined(userId string) (bool, error) {
 }
 
 func (r *streamViewImpl) GetChannelMembers() (*mapset.Set[string], error) {
-	return r.getMembers([]common.StreamType{common.Channel})
+	return r.getMembers()
 }
 
-func (r *streamViewImpl) getMembers(filterBy []common.StreamType) (*mapset.Set[string], error) {
+func (r *streamViewImpl) getMembers() (*mapset.Set[string], error) {
 	members := mapset.NewSet[string]()
 
 	switch snapshotContent := r.snapshot.Content.(type) {
 	case *protocol.Snapshot_SpaceContent:
-		if slices.Contains(filterBy, common.Space) {
-			for _, user := range snapshotContent.SpaceContent.GetMemberships() {
-				if user.GetOp() == protocol.MembershipOp_SO_JOIN {
-					members.Add(user.UserId)
-				}
+		for _, user := range snapshotContent.SpaceContent.GetMemberships() {
+			if user.GetOp() == protocol.MembershipOp_SO_JOIN {
+				members.Add(user.UserId)
 			}
 		}
 	case *protocol.Snapshot_ChannelContent:
-		if slices.Contains(filterBy, common.Channel) {
-			for _, user := range snapshotContent.ChannelContent.GetMemberships() {
-				if user.GetOp() == protocol.MembershipOp_SO_JOIN {
-					members.Add(user.UserId)
-				}
+		for _, user := range snapshotContent.ChannelContent.GetMemberships() {
+			if user.GetOp() == protocol.MembershipOp_SO_JOIN {
+				members.Add(user.UserId)
 			}
 		}
 	case *protocol.Snapshot_DmChannelContent:
-		if slices.Contains(filterBy, common.DMChannel) {
-			for _, user := range snapshotContent.DmChannelContent.GetMemberships() {
-				if user.GetOp() == protocol.MembershipOp_SO_JOIN {
-					members.Add(user.UserId)
-				}
+		for _, user := range snapshotContent.DmChannelContent.GetMemberships() {
+			if user.GetOp() == protocol.MembershipOp_SO_JOIN {
+				members.Add(user.UserId)
 			}
 		}
 	default:
@@ -64,13 +56,11 @@ func (r *streamViewImpl) getMembers(filterBy []common.StreamType) (*mapset.Set[s
 		case *protocol.StreamEvent_SpacePayload:
 			switch spacePayload := payload.SpacePayload.Content.(type) {
 			case *protocol.SpacePayload_Membership:
-				if slices.Contains(filterBy, common.Space) {
-					user := spacePayload.Membership.UserId
-					if spacePayload.Membership.GetOp() == protocol.MembershipOp_SO_JOIN {
-						members.Add(user)
-					} else if spacePayload.Membership.GetOp() == protocol.MembershipOp_SO_LEAVE {
-						members.Remove(user)
-					}
+				user := spacePayload.Membership.UserId
+				if spacePayload.Membership.GetOp() == protocol.MembershipOp_SO_JOIN {
+					members.Add(user)
+				} else if spacePayload.Membership.GetOp() == protocol.MembershipOp_SO_LEAVE {
+					members.Remove(user)
 				}
 			default:
 				break
@@ -78,13 +68,11 @@ func (r *streamViewImpl) getMembers(filterBy []common.StreamType) (*mapset.Set[s
 		case *protocol.StreamEvent_ChannelPayload:
 			switch channelPayload := payload.ChannelPayload.Content.(type) {
 			case *protocol.ChannelPayload_Membership:
-				if slices.Contains(filterBy, common.Channel) {
-					user := channelPayload.Membership.UserId
-					if channelPayload.Membership.GetOp() == protocol.MembershipOp_SO_JOIN {
-						members.Add(user)
-					} else if channelPayload.Membership.GetOp() == protocol.MembershipOp_SO_LEAVE {
-						members.Remove(user)
-					}
+				user := channelPayload.Membership.UserId
+				if channelPayload.Membership.GetOp() == protocol.MembershipOp_SO_JOIN {
+					members.Add(user)
+				} else if channelPayload.Membership.GetOp() == protocol.MembershipOp_SO_LEAVE {
+					members.Remove(user)
 				}
 			default:
 				break
@@ -92,13 +80,11 @@ func (r *streamViewImpl) getMembers(filterBy []common.StreamType) (*mapset.Set[s
 		case *protocol.StreamEvent_DmChannelPayload:
 			switch dmChannelPayload := payload.DmChannelPayload.Content.(type) {
 			case *protocol.DmChannelPayload_Membership:
-				if slices.Contains(filterBy, common.DMChannel) {
-					user := dmChannelPayload.Membership.UserId
-					if dmChannelPayload.Membership.GetOp() == protocol.MembershipOp_SO_JOIN {
-						members.Add(user)
-					} else if dmChannelPayload.Membership.GetOp() == protocol.MembershipOp_SO_LEAVE {
-						members.Remove(user)
-					}
+				user := dmChannelPayload.Membership.UserId
+				if dmChannelPayload.Membership.GetOp() == protocol.MembershipOp_SO_JOIN {
+					members.Add(user)
+				} else if dmChannelPayload.Membership.GetOp() == protocol.MembershipOp_SO_LEAVE {
+					members.Remove(user)
 				}
 			default:
 				break
