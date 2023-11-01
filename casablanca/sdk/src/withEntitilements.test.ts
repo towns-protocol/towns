17264ab92762d78e294bf43a7cbeeb2dcfbb37c8
texuf <austinellis+git@gmail.com>
2023-11-01 11:48:28 -0700

    Update entitlements test to join a second person to the space (#3844)
    This test was “functional” but pretty inadequate for using as a template
    to do more complicated stuff…

--- casablanca/sdk/src/withEntitilements.test.ts
+++ casablanca/sdk/src/withEntitilements.test.ts
@@ -2,15 +2,9 @@
  * @group with-entitilements
  */
 
-import { makeEvent, SignerContext, unpackEnvelopes } from './sign'
-import { MembershipOp } from '@river/proto'
 import { LocalhostWeb3Provider, ITownArchitectBase, Permission, SpaceDappV3 } from '@river/web3'
 import { dlog } from './dlog'
-import {
-    lastEventFiltered,
-    makeRandomUserContextWithOldDelegate,
-    TEST_URL_WITH_ENTITILEMENTS,
-} from './util.test'
+import { makeRandomUserContextWithOldDelegate, TEST_URL_WITH_ENTITILEMENTS } from './util.test'
 import {
     genId,
     makeChannelStreamId,
@@ -18,65 +12,42 @@ import {
     makeUserStreamId,
     userIdFromAddress,
 } from './id'
-import {
-    getUserPayload_Membership,
-    make_ChannelPayload_Inception,
-    make_ChannelPayload_Membership,
-    make_SpacePayload_Inception,
-    make_SpacePayload_Membership,
-    make_UserPayload_Inception,
-} from './types'
 import { makeStreamRpcClient } from './makeStreamRpcClient'
-import { ethers, Wallet } from 'ethers'
+import { ethers } from 'ethers'
+import { Client } from './client'
 
 const base_log = dlog('csb:test:withEntitlements')
 
-const makeTestRpcClient = () => makeStreamRpcClient(TEST_URL_WITH_ENTITILEMENTS)
+const makeStreamRpcClientWithEntitlements = () => makeStreamRpcClient(TEST_URL_WITH_ENTITILEMENTS)
 const ETH_ADDRESS = '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE'
 
 describe('withEntitlements', () => {
-    let bobsWallet: Wallet
-    let bobsContext: SignerContext
-
-    beforeEach(async () => {
-        bobsWallet = ethers.Wallet.createRandom()
-        bobsContext = await makeRandomUserContextWithOldDelegate(bobsWallet)
-    })
-
     test('createSpaceAndChannel', async () => {
         const log = base_log.extend('createSpaceAndChannel')
 
         log('start')
 
         // set up the web3 provider and spacedap
-        const provider = new LocalhostWeb3Provider(bobsWallet)
-        const chainId = (await provider.getNetwork()).chainId
-        await provider.fundWallet()
-        const mintReceipt = await provider.mintMockNFT()
+        const bobsWallet = ethers.Wallet.createRandom()
+        const bobsContext = await makeRandomUserContextWithOldDelegate(bobsWallet)
+        const bobProvider = new LocalhostWeb3Provider(bobsWallet)
+        const chainId = (await bobProvider.getNetwork()).chainId
+        await bobProvider.fundWallet()
+        const mintReceipt = await bobProvider.mintMockNFT()
         log('mintReceipt', mintReceipt)
-        const spaceDapp = new SpaceDappV3(chainId, provider)
+        const spaceDapp = new SpaceDappV3(chainId, bobProvider)
 
         // create a user stream
-        const bob = makeTestRpcClient()
+        const bob = new Client(bobsContext, makeStreamRpcClientWithEntitlements())
         const bobsUserId = userIdFromAddress(bobsContext.creatorAddress)
         const bobsUserStreamId = makeUserStreamId(bobsUserId)
-        await bob.createStream({
-            events: [
-                await makeEvent(
-                    bobsContext,
-                    make_UserPayload_Inception({
-                        streamId: bobsUserStreamId,
-                    }),
-                    [],
-                ),
-            ],
-            streamId: bobsUserStreamId,
-        })
+        await bob.createNewUser()
+        bob.startSync()
 
         // create a space stream,
-        const spacedStreamId = makeSpaceStreamId('bobs-space-' + genId())
+        const spaceId = makeSpaceStreamId('bobs-space-' + genId())
         const channelId = makeChannelStreamId('bobs-channel-' + genId())
-        log('Bob created user, about to create space', { spacedStreamId, channelId })
+        log('Bob created user, about to create space', { spaceId, channelId })
         // first on the blockchain
         const membershipInfo: ITownArchitectBase.MembershipStruct = {
             settings: {
@@ -97,87 +68,68 @@ describe('withEntitlements', () => {
         }
         const transaction = await spaceDapp.createSpace(
             {
-                spaceId: spacedStreamId,
-                spaceName: spacedStreamId,
+                spaceId: spaceId,
+                spaceName: spaceId,
                 spaceMetadata: 'bobs-space-metadata',
                 channelId: channelId,
                 channelName: 'general', // default channel name
                 membership: membershipInfo,
             },
-            provider.wallet,
+            bobProvider.wallet,
         )
         const receipt = await transaction.wait()
         log('receipt', receipt)
         expect(receipt.status).toEqual(1)
         // then on the river node
-        const spaceInceptionEvent = await makeEvent(
-            bobsContext,
-            make_SpacePayload_Inception({
-                streamId: spacedStreamId,
-            }),
-            [],
-        )
-        await bob.createStream({
-            events: [
-                spaceInceptionEvent,
-                await makeEvent(
-                    bobsContext,
-                    make_SpacePayload_Membership({
-                        userId: bobsUserId,
-                        op: MembershipOp.SO_JOIN,
-                    }),
-                    [spaceInceptionEvent.hash],
-                ),
-            ],
-            streamId: spacedStreamId,
-        })
+        const returnVal = await bob.createSpace(spaceId)
+        expect(returnVal.streamId).toEqual(spaceId)
         // Now there must be "joined space" event in the user stream.
-        let userResponse = await bob.getStream({ streamId: bobsUserStreamId })
-        expect(userResponse.stream).toBeDefined()
-        let joinPayload = lastEventFiltered(
-            unpackEnvelopes(userResponse.stream!.events, 0n),
-            getUserPayload_Membership,
-        )
-        expect(joinPayload).toBeDefined()
-        expect(joinPayload?.op).toEqual(MembershipOp.SO_JOIN)
-        expect(joinPayload?.streamId).toEqual(spacedStreamId)
+        let bobUserStreamView = await bob.getStream(bobsUserStreamId)
+        expect(bobUserStreamView).toBeDefined()
+        expect(bobUserStreamView.userContent.userJoinedStreams).toContain(spaceId)
         // create the channel
         log('Bob created space, about to create channel')
         const channelProperties = 'Bobs channel properties'
-        const channelInceptionEvent = await makeEvent(
-            bobsContext,
-            make_ChannelPayload_Inception({
-                streamId: channelId,
-                spaceId: spacedStreamId,
-                channelProperties: { text: channelProperties },
-            }),
-            [],
+        const channelReturnVal = await bob.createChannel(
+            spaceId,
+            "Bob's channel",
+            channelProperties,
+            channelId,
         )
-        const channelJoinEvent = await makeEvent(
-            bobsContext,
-            make_ChannelPayload_Membership({
-                userId: bobsUserId,
-                op: MembershipOp.SO_JOIN,
-            }),
-            [channelInceptionEvent.hash],
-        )
-        await bob.createStream({
-            events: [channelInceptionEvent, channelJoinEvent],
-            streamId: channelId,
-        })
+        expect(channelReturnVal.streamId).toEqual(channelId)
 
         // Now there must be "joined channel" event in the user stream.
-        userResponse = await bob.getStream({ streamId: bobsUserStreamId })
-        expect(userResponse.stream).toBeDefined()
-        joinPayload = lastEventFiltered(
-            unpackEnvelopes(userResponse.stream!.events, 0n),
-            getUserPayload_Membership,
+        bobUserStreamView = await bob.getStream(bobsUserStreamId)
+        expect(bobUserStreamView).toBeDefined()
+        expect(bobUserStreamView.userContent.userJoinedStreams).toContain(channelId)
+
+        // join alice
+        const alicesWallet = ethers.Wallet.createRandom()
+        const alicesContext = await makeRandomUserContextWithOldDelegate(alicesWallet)
+        const aliceProvider = new LocalhostWeb3Provider(alicesWallet)
+        await aliceProvider.fundWallet()
+        const alice = new Client(alicesContext, makeStreamRpcClientWithEntitlements())
+        await alice.createNewUser()
+        alice.startSync()
+        log('Alice created user, about to join space', { alicesUserId: alice.userId })
+
+        // first join the space on chain
+        const aliceSpaceDapp = new SpaceDappV3(chainId, aliceProvider)
+        const transaction2 = await aliceSpaceDapp.joinTown(
+            spaceId,
+            alicesWallet.address,
+            aliceProvider.wallet,
         )
+        const receipt2 = await transaction2.wait()
+        log('receipt for alice joining town', receipt2)
 
-        expect(joinPayload).toBeDefined()
-        expect(joinPayload?.op).toEqual(MembershipOp.SO_JOIN)
-        expect(joinPayload?.streamId).toEqual(channelId)
+        const aliceSpaceResponse = await alice.joinStream(spaceId)
+        expect(aliceSpaceResponse).toBeDefined()
+        log('response', aliceSpaceResponse)
 
+        // kill the clients
+        bob.stopSync()
+        alice.stopSync()
         log('Done')
     })
 })
