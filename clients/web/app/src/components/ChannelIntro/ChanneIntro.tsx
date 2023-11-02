aa3a2429cdafa3ff82ea7c5ec0ef861b20f98a4c
Erik Olsson <valross2@gmail.com>
2023-11-02 15:56:27 +0100

    add hoverable names in channel intro

--- clients/web/app/src/components/ChannelIntro/ChanneIntro.tsx
+++ clients/web/app/src/components/ChannelIntro/ChanneIntro.tsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import { RoomIdentifier, useDMData } from 'use-zion-client'
+import { Link } from 'react-router-dom'
 import { Box, Icon, Paragraph, Stack, Text } from '@ui'
 import { useChannelType } from 'hooks/useChannelType'
 import { shortAddress } from 'ui/utils/utils'
+import { ProfileHoverCard } from '@components/ProfileHoverCard/ProfileHoverCard'
 
 type Props = {
     roomIdentifier: RoomIdentifier
@@ -17,6 +19,8 @@ export const ChannelIntro = (props: Props) => {
         return <RegularChannelIntro {...props} />
     } else if (channelType === 'dm') {
         return <ChannelDMIntro roomIdentifier={roomIdentifier} />
+    } else if (channelType === 'gdm') {
+        return <ChannelGDMIntro roomIdentifier={roomIdentifier} />
     }
 }
 
@@ -59,6 +63,48 @@ export const ChannelDMIntro = (props: { roomIdentifier: RoomIdentifier }) => {
                         This end-to-end encrypted chat is just between{' '}
                         <Text as="span" color="default" fontWeight="medium" display="inline">
                             {shortAddress(counterParty)}
+                        </Text>{' '}
+                        and you
+                    </Paragraph>
+                </Stack>
+            </Stack>
+        </Stack>
+    )
+}
+
+export const ChannelGDMIntro = (props: { roomIdentifier: RoomIdentifier }) => {
+    const { data } = useDMData(props.roomIdentifier)
+
+    const memberLinks = useMemo(() => {
+        if (!data?.userIds) {
+            return []
+        }
+        return data.userIds.map((userId) => {
+            return (
+                <Box display="inline" key={userId} tooltip={<ProfileHoverCard userId={userId} />}>
+                    <Link to={`profile/${userId}`}>{shortAddress(userId)}</Link>
+                </Box>
+            )
+        })
+    }, [data?.userIds])
+
+    return (
+        <Stack gap="md" paddingX="lg" paddingY="sm">
+            <Stack horizontal gap>
+                <Box centerContent rounded="sm" background="level2" aspectRatio="1/1" height="x7">
+                    <Icon type="tag" color="gray2" size="square_lg" />
+                </Box>
+                <Stack justifyContent="spaceBetween" paddingY="sm" overflow="hidden">
+                    <Paragraph color="gray2">
+                        <Text as="span" color="gray1" fontWeight="medium" display="inline">
+                            {memberLinks.map((link, index) => [index > 0 && ', ', link])}
+                        </Text>{' '}
+                        and you
+                    </Paragraph>
+                    <Paragraph color="gray2">
+                        This end-to-end encrypted chat is just between{' '}
+                        <Text as="span" color="default" fontWeight="medium" display="inline">
+                            {memberLinks.map((link, index) => [index > 0 && ', ', link])}
                         </Text>{' '}
                         and you
                     </Paragraph>
