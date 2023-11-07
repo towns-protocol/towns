520ddaf2158e37f9444308d9958650f475c6b10a
Erik Olsson <valross2@gmail.com>
2023-11-07 13:14:11 +0100

    index DM channels

--- clients/web/app/src/hooks/useIndexMessages.ts
+++ clients/web/app/src/hooks/useIndexMessages.ts
@@ -5,12 +5,15 @@ import { isRoomMessage } from '@components/MessageTimeline/util/getEventsByDate'
 import { useSpaceChannels } from 'hooks/useSpaceChannels'
 import { useThrottledValue } from 'hooks/useThrottledValue'
 import { notUndefined } from 'ui/utils/utils'
+import { useDmChannels } from './useDMChannels'
 
 const log = debug('app:useMessageIndex')
 log.enabled = true
 
 export const useIndexMessages = () => {
     const channels = useSpaceChannels()
+    const dmChannels = useDmChannels()
+
     const timelines = useThrottledValue(
         useDeferredValue(useTimelineStore((s) => s.timelines)),
         1000,
@@ -34,5 +37,22 @@ export const useIndexMessages = () => {
             .filter(notUndefined)
     }, [channels, timelines])
 
-    return { messages }
+    const dmMessages = useMemo(() => {
+        return dmChannels
+            .flatMap((channel) => {
+                return timelines[channel.id.networkId]
+                    ?.filter(isRoomMessage)
+                    .filter((e) => e.content?.body)
+                    .filter((e) => e.content?.msgType !== 'm.bad.encrypted')
+                    .map((e) => ({
+                        key: e.eventId,
+                        type: 'dmMessage' as const,
+                        channelId: channel.id.networkId,
+                        body: e.content?.body ?? '',
+                        source: e,
+                    }))
+            })
+            .filter(notUndefined)
+    }, [dmChannels, timelines])
+    return { messages, dmMessages }
 }
