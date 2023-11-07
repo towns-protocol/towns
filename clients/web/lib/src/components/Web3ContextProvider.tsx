30d391d232fd6af693a90e44b67528ac1eb3d4c0
texuf <austinellis+git@gmail.com>
2023-11-07 11:45:21 -0800

    Consolidate Web3Context code (#3942)
    This has been through a few iterations, in this pr I deleted a helper
    function and moved the useWeb3 hook into the context provider, so that
    all the code is in the same place

--- clients/web/lib/src/components/Web3ContextProvider.tsx
+++ clients/web/lib/src/components/Web3ContextProvider.tsx
@@ -1,8 +1,9 @@
-import { Address, Chain, usePublicClient } from 'wagmi'
-import React, { createContext, useContext } from 'react'
-import { TProvider, WalletStatus } from '../types/web3-types'
+import React, { createContext, useCallback, useContext, useEffect, useMemo, useState } from 'react'
+import { Address, Chain, useAccount, useNetwork, usePublicClient } from 'wagmi'
 import { ethers } from 'ethers'
-import { useWeb3 } from '../hooks/Web3Context/useWeb3'
+import { TProvider, WalletStatus } from '../types/web3-types'
+import { useEthersSigner } from '../hooks/Web3Context/useEthersSigner'
+import { useEthersProvider } from '../hooks/Web3Context/useEthersProvider'
 
 export interface IWeb3Context {
     provider?: TProvider
@@ -35,10 +36,6 @@ interface Props {
 }
 
 export function Web3ContextProvider(props: Props): JSX.Element {
-    return <ContextImpl {...props} />
-}
-
-export function ContextImpl(props: Props): JSX.Element {
     const { chainId, web3Signer } = props
     const publicClient = usePublicClient({ chainId })
     // wagmiConfig.chanins is not populated unless you are connected!
@@ -54,4 +51,95 @@ export function ContextImpl(props: Props): JSX.Element {
         chain,
     })
     return <Web3Context.Provider value={web3}>{props.children}</Web3Context.Provider>
+}
+
+type Web3ContextOptions = {
+    chain?: Chain
+    web3Signer?: ethers.Signer
+    chainId: number
+}
+
+/// web3 components, pass chain to lock to a specific chain, pass signer to override the default signer (usefull for tests)
+function useWeb3({ chain, chainId, web3Signer }: Web3ContextOptions): IWeb3Context {
+    const { address: activeWalletAddress, connector, isConnected, status } = useAccount()
+    const { chain: walletChain, chains } = useNetwork()
+    const accounts = useMemo(
+        () => (activeWalletAddress ? [activeWalletAddress] : []),
+        [activeWalletAddress],
+    )
+    const { signer, setSigner } = useAppSigner(chainId, web3Signer)
+
+    // allowing app to pass in chain allows to load on correct chain per env regardless of user wallet settings
+    // they are able to login w/out swapping networks
+    // we still need guards for transactions
+    const activeChain = useMemo(() => chain || walletChain, [chain, walletChain])
+    const provider = useEthersProvider({ chainId: activeChain?.id })
+
+    useEffect(() => {
+        console.log('use web3 ##', {
+            connector,
+            activeWalletAddress,
+            activeChain,
+            chains,
+            rpc: activeChain?.rpcUrls,
+            def: activeChain?.rpcUrls?.default,
+            status,
+            signer,
+        })
+    }, [activeChain, activeWalletAddress, chains, connector, status, signer])
+
+    return useMemo(
+        () => ({
+            provider,
+            signer,
+            activeWalletAddress,
+            accounts,
+            chain: activeChain,
+            chains: chains,
+            isConnected: isConnected,
+            walletStatus: status as WalletStatus,
+            setSigner,
+        }),
+        [
+            accounts,
+            activeChain,
+            activeWalletAddress,
+            chains,
+            isConnected,
+            provider,
+            setSigner,
+            signer,
+            status,
+        ],
+    )
+}
+
+// we need to set a signer only once, which should be automatic,
+// but in case it's not, we allow the app to set it manually
+function useAppSigner(appChainId: number, web3Signer?: ethers.Signer) {
+    // default to passed web3 signer, if any
+    const [signer, _setSigner] = useState<ethers.Signer | undefined>(web3Signer)
+    const wagmiSigner = useEthersSigner({
+        chainId: appChainId,
+    })
+
+    useEffect(() => {
+        if (signer) {
+            return
+        }
+        // web3Signer should be the same as wagmiSigner, so it shouldn't matter which one comes first, but we can add an equality check if needed
+        if (web3Signer) {
+            _setSigner(web3Signer)
+        } else if (wagmiSigner) {
+            _setSigner(wagmiSigner)
+        }
+    }, [signer, web3Signer, wagmiSigner])
+
+    // but there might be cases where a wallet is set after this hook is called, and for some unknown reason the wagmiSigner is not updated properly
+    // in this case, the app can set the signer manually
+    const setSigner = useCallback((signerInput: ethers.Signer) => {
+        _setSigner(signerInput)
+    }, [])
+
+    return { signer, setSigner }
 }
