towns-protocol/towns

commit c8208b0236e66ea06c98a166c1253d68a794a69d

Stop sending key solicitations for streams you’re not a member of (#3978)
You can’t append to the stream if you haven’t joined.

I'm seeing lots of failures in the
unreadMessageCountEdgeCaseHooks.test.tsx, I think it's just grabbing a
stream before you join, the encrypted events are firing, but you're not
a member yet... Pretty weird that it got this far.


The error:
```
  ● unreadMessageCountHooks › user can join a room, see messages, and send messages

    ConnectError: [permission_denied] AddEvent: localAddEvent: (7:PERMISSION_DENIED) user is not a member of channel
        user = 0xA4E27F2db74487A6CB2cc62E9D2292782b1Bb63A
        streamId = 22-5ydSs0IWDHwx-NnNjvD1L

      36 |     try {
      37 |         const res: UnaryResponse<AnyMessage, AnyMessage> | StreamResponse<AnyMessage, AnyMessage> =
    > 38 |             await next(localReq)
         |             ^
      39 |
      40 |         if (res.stream) {
      41 |             // to intercept streaming response messages, we wrap

      at errorFromJson (../../../node_modules/@connectrpc/connect/dist/cjs/protocol-connect/error-json.js:57:19)
      at next (../../../node_modules/@connectrpc/connect-web/dist/cjs/connect-transport.js:87:68)
      at ../../../casablanca/sdk/src/makeStreamRpcClient.ts:38:13
      at Object.unary (../../../node_modules/@connectrpc/connect-web/dist/cjs/connect-transport.js:53:20)
      at Object.addEvent (../../../node_modules/@connectrpc/connect/dist/cjs/promise-client.js:70:26)
      at Client.makeEventAndAddToStream (../../../casablanca/sdk/src/client.ts:1649:9)
      at RiverDecryptionExtension._askStreamForKeys (src/client/casablanca/RiverDecryptionExtensions.ts:662:13)
      at RiverDecryptionExtension.startLookingForKeys (src/client/casablanca/RiverDecryptionExtensions.ts:507:13)
```

clients/web/lib/src/client/casablanca/RiverDecryptionExtensions.ts
@@ -29,6 +29,7 @@ import {
     OlmMessage,
     KeySolicitation,
     StreamEvent,
+    MembershipOp,
 } from '@river/proto'
 import { Room } from '../../types/zion-types'
 import { RoomIdentifier } from '../../types/room-identifier'
@@ -594,12 +595,22 @@ export class RiverDecryptionExtension {
         // for each room, ask for all missing sessionIds sequentially to stream
         for (const unknownSessionAndSender of unknownSessionIds) {
             const unknownSessionRequested = unknownSessionAndSender[1]
+            const stream = this.client.streams.get(streamId)
+            if (!stream) {
+                console.log("CDE::_askRoomForKeys - stream doesn't exist")
+                return
+            }
+            if (!stream?.view.getMemberships().isMember(MembershipOp.SO_JOIN)) {
+                console.log(
+                    'CDE::_askForRoomKeys - user is not a memeber of the room and cannot request keys',
+                )
+                return
+            }
             if (isChannelStreamId(streamId)) {
-                const keySolicitations =
-                    this.client.streams.get(streamId)?.view.channelContent.keySolicitations
+                const keySolicitations = stream.view.channelContent.keySolicitations
                 // todo jterzis: add restart criteria based on eventNum returned from miniblock in the case that
                 // a fulfillment didn't return the correct session key
-                if (keySolicitations?.hasKeySolicitation(ourDeviceKey, unknownSessionRequested)) {
+                if (keySolicitations.hasKeySolicitation(ourDeviceKey, unknownSessionRequested)) {
                     console.log(
                         `CDE::_askRoomForKeys - not requesting for same senderKey and streamId ${streamId} sessionId ${unknownSessionRequested}`,
                     )
@@ -614,9 +625,8 @@ export class RiverDecryptionExtension {
                         knownSessions.length < MAX_EVENTS_PER_REQUEST ? knownSessions : [],
                 })
             } else if (isDMChannelStreamId(streamId)) {
-                const keySolicitations =
-                    this.client.streams.get(streamId)?.view.dmChannelContent.keySolicitations
-                if (keySolicitations?.hasKeySolicitation(ourDeviceKey, unknownSessionRequested)) {
+                const keySolicitations = stream.view.dmChannelContent.keySolicitations
+                if (keySolicitations.hasKeySolicitation(ourDeviceKey, unknownSessionRequested)) {
                     console.log(
                         `CDE::_askRoomForKeys - not requesting for same senderKey and streamId ${streamId} sessionId ${unknownSessionRequested}`,
                     )
@@ -630,9 +640,8 @@ export class RiverDecryptionExtension {
                         knownSessions.length < MAX_EVENTS_PER_REQUEST ? knownSessions : [],
                 })
             } else if (isGDMChannelStreamId(streamId)) {
-                const keySolicitations =
-                    this.client.streams.get(streamId)?.view.gdmChannelContent.keySolicitations
-                if (keySolicitations?.hasKeySolicitation(ourDeviceKey, unknownSessionRequested)) {
+                const keySolicitations = stream.view.gdmChannelContent.keySolicitations
+                if (keySolicitations.hasKeySolicitation(ourDeviceKey, unknownSessionRequested)) {
                     console.log(
                         `CDE::_askRoomForKeys - not requesting for same senderKey and streamId ${streamId} sessionId ${unknownSessionRequested}`,
                     )

commit 92fa6a231ed4ced3dfa7cf48c3263605e6ff2b9c

centralize key request entitlement logic + cleanup (#3997)
this PR fixes a small regression from #3978. will add some tests for
these checks when things calm down a bit.
[added a
ticket](https://linear.app/hnt-labs/issue/HNT-3536/add-key-entitlement-tests-for-dmsgdms)

casablanca/sdk/src/streamStateView.ts
@@ -7,6 +7,7 @@ import {
     Snapshot,
     Miniblock,
     MiniblockHeader,
+    MembershipOp,
 } from '@river/proto'
 import TypedEmitter from 'typed-emitter'
 import { check, logNever, isDefined, throwWithCode } from './check'
@@ -527,6 +528,25 @@ export class StreamStateView {
             default:
                 logNever(this.contentKind)
                 return new StreamStateView_UnknownContent()
+        }
+    }
+
+    /**
+     * Streams behave slightly differently.
+     * Regular channels: the user needs to be an active member. SO_JOIN
+     * DMs: always open for key exchange for any of the two participants
+     * GDMs: open for key exchange while the membership state is either SO_JOIN or SO_INVITE
+     */
+    userIsEntitledToKeyExchange(userId: string): boolean {
+        switch (this.contentKind) {
+            case 'channelContent':
+                return this.getMemberships().isMember(MembershipOp.SO_JOIN, userId)
+            case 'dmChannelContent':
+                return this.dmChannelContent.participants().has(userId)
+            case 'gdmChannelContent':
+                return this.gdmChannelContent.joinedOrInvitedParticipants().has(userId)
+            default:
+                throw new Error('Stream does not support key exchange')
         }
     }
 }

clients/web/lib/src/client/casablanca/RiverDecryptionExtensions.ts
@@ -29,7 +29,6 @@ import {
     OlmMessage,
     KeySolicitation,
     StreamEvent,
-    MembershipOp,
 } from '@river/proto'
 import { Room } from '../../types/zion-types'
 import { RoomIdentifier } from '../../types/room-identifier'
@@ -600,12 +599,13 @@ export class RiverDecryptionExtension {
                 console.log("CDE::_askRoomForKeys - stream doesn't exist")
                 return
             }
-            if (!stream?.view.getMemberships().isMember(MembershipOp.SO_JOIN)) {
+            if (!stream?.view.userIsEntitledToKeyExchange(this.client.userId)) {
                 console.log(
                     'CDE::_askForRoomKeys - user is not a memeber of the room and cannot request keys',
                 )
                 return
             }
+
             if (isChannelStreamId(streamId)) {
                 const keySolicitations = stream.view.channelContent.keySolicitations
                 // todo jterzis: add restart criteria based on eventNum returned from miniblock in the case that
@@ -716,44 +716,15 @@ export class RiverDecryptionExtension {
         if (!room) {
             throw new Error('CDE::_onKeySolicitation - room not found')
         }
-        if (isChannelStreamId(streamId)) {
-            const roomMembers = room.members.filter(
-                (member) =>
-                    // filter out non members of the stream
-                    member.userId === fromUserId,
-            )
-            if (!roomMembers.length) {
-                console.log(`CDE::_onKeySolicitation - not a room member asking for keys`, {
-                    streamId,
-                    fromUserId,
-                })
-                return
-            }
-        } else if (isDMChannelStreamId(streamId)) {
-            const participants = stream.view.dmChannelContent.participants()
-            if (!participants.has(fromUserId)) {
-                console.log(`CDE::_onKeySolicitation - not a DM member asking for keys`, {
-                    streamId,
-                    fromUserId,
-                })
-                return
-            }
-        } else if (isGDMChannelStreamId(streamId)) {
-            const participants = stream.view.gdmChannelContent.joinedOrInvitedParticipants()
-            if (!participants.has(fromUserId)) {
-                console.log(`CDE::_onKeySolicitation - not a GDM member asking for keys`, {
-                    streamId,
-                    fromUserId,
-                })
-                return
-            }
-        } else {
-            console.log(`CDE::_onKeySolicitation - key request for unknown stream type`, {
+
+        if (!stream?.view.userIsEntitledToKeyExchange(fromUserId)) {
+            console.log(`CDE::_onKeySolicitation - not a stream member asking for keys`, {
                 streamId,
                 fromUserId,
             })
             return
         }
+
         // check with the space contract to see if this user is entitled
         const isEntitled = await this.delegate.isEntitled(
             spaceId,