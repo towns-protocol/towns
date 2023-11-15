f3f9e2780bac9050fba0c99d388ade9b73642fcb
texuf <austinellis+git@gmail.com>
2023-11-14 16:33:11 -0800

    simplify the UnauthenticatedClient (#4088)
    It doesn’t dispatch any events, so we don’t need to wire it up as an
    event emitter

--- casablanca/sdk/src/unauthenticatedClient.ts
+++ casablanca/sdk/src/unauthenticatedClient.ts
@@ -1,17 +1,11 @@
 import debug from 'debug'
-import EventEmitter from 'events'
-import TypedEmitter from 'typed-emitter'
 import { check, hasElements, isDefined } from './check'
 import { DLogger, dlog } from './dlog'
-import { RiverEventsV2 } from './eventV2'
 import { StreamRpcClientType } from './makeStreamRpcClient'
 import { unpackStreamResponse } from './sign'
-import { StreamEvents } from './streamEvents'
 import { StreamStateView } from './streamStateView'
 
-export type EmittedEvents = StreamEvents & RiverEventsV2
-
-export class UnauthenticatedClient extends (EventEmitter as new () => TypedEmitter<EmittedEvents>) {
+export class UnauthenticatedClient {
     readonly rpcClient: StreamRpcClientType
 
     private readonly logCall: DLogger
@@ -20,7 +14,6 @@ export class UnauthenticatedClient extends (EventEmitter as new () => TypedEmitt
     private readonly userId = 'unauthenticatedClientUser'
 
     constructor(rpcClient: StreamRpcClientType, logNamespaceFilter?: string) {
-        super()
         if (logNamespaceFilter) {
             debug.enable(logNamespaceFilter)
         }
@@ -55,10 +48,5 @@ export class UnauthenticatedClient extends (EventEmitter as new () => TypedEmitt
             this.logCall('getStream', streamId, 'ERROR', err)
             throw err
         }
-    }
-
-    emit<E extends keyof EmittedEvents>(event: E, ...args: Parameters<EmittedEvents[E]>): boolean {
-        this.logEmitFromClient(event, ...args)
-        return super.emit(event, ...args)
     }
 }
