f26938e581b011cf43ea6ffaab92fb1ba8b98f11
Kerem Kazan <kerem.kazan@gmail.com>
2023-11-15 10:44:29 -0500

    updating the rpc proxy version for aggressive logging and better debugs (#4111)
    I just updated the proxy worker version. This one has a lot more console
    logs. I also pulled the provider settings to a more visible spot so it's
    easy for everyone to change them at will.
    
    If you want to read about the exact changes in this patch, here's the
    changelog:
    https://github.com/whats-good/nexus/blob/main/packages/nexus/CHANGELOG.md
    
    You can take a look at this more detailed documentation page if you want
    to learn about the inner workings of the proxy cloudflare worker:
    https://nexus.whatsgood.dog/recipes/cloudflare-worker

--- servers/workers/nexus-rpc-worker/src/index.ts
+++ servers/workers/nexus-rpc-worker/src/index.ts
@@ -1,14 +1,28 @@
-import { Config, RpcProxyResponseHandler } from '@whatsgood/nexus'
+import { Nexus } from '@whatsgood/nexus'
+
+// Leaving this here for now, so that we can
+// disable any provider we don't want to use,
+// even if their keys are in the env.
+const server = Nexus.createServer({
+	providers: {
+		alchemy: {
+			disabled: true,
+		},
+		infura: {
+			disabled: true,
+		},
+		ankr: {
+			disabled: true,
+		},
+		// base: {
+		// 	disabled: true,
+		// },
+	},
+})
 
 export const worker = {
 	async fetch(request: Request, env: Record<string, string>): Promise<Response> {
-		const config = new Config({
-			env,
-		})
-		const responseHandler = new RpcProxyResponseHandler({
-			config,
-		})
-		return responseHandler.handle(request)
+		return server.fetch(request, env)
 	},
 }
 
