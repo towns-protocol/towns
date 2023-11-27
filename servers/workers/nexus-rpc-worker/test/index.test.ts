0cf8ebf95ba3144958ffdb56d633d87e29c46910
Kerem Kazan <kerem.kazan@gmail.com>
2023-11-27 12:13:18 -0500

    Kerem/hnt 3685 nexus rpc worker test seems to be broken (#4168)
    @tak-hntlabs logged this yesterday. proxy worker did not have tests, and
    the tests were not a part of ci. this pr adds the tests, and makes it
    part of ci. i'm also added the `test:unit` script to the worker template
    so new workers we'll add will automatically fail on ci unless there's
    some minimal tests added.
    
    I want to draw your attention to a specific point in this PR, where I
    had to change the rpc worker's `jest.config`.testEnvironment from
    "miniflare" to "node". This is because I need to mock outbound fetch
    requests to alchemy, and i wasn't able to make mocking work with
    miniflare. i tried `msw`, which allows mocking the web fetch api, but
    that didn't work. i then tried `fetch-mock`, which again did not work
    unless I ran jest in `node` mode.
    
    I would personally prefer keeping jest in miniflare mode, in alignment
    with the rest of our workers and test suites. I'm open to suggestions
    here. @texuf @jterzis what do you think?

--- servers/workers/nexus-rpc-worker/test/index.test.ts
+++ servers/workers/nexus-rpc-worker/test/index.test.ts
@@ -1,9 +1,70 @@
 import { worker } from '../src/index'
 
-test('GET /', async () => {
-	const result = await worker.fetch(new Request('http://falcon', { method: 'GET' }))
-	expect(result.status).toBe(200)
+const blockNumberMockResponse = {
+	jsonrpc: '2.0',
+	id: 1,
+	result: '0x4b7', // 1207
+}
 
-	const text = await result.text()
-	expect(text).toBe('Hello World from GET!')
+describe('rpc worker tests', () => {
+	const NEXUS_GLOBAL_ACCESS_KEY = 'my-dev-key'
+	const NEXUS_ALCHEMY_KEY = 'my-alchemy-key'
+	const ETH_MAINNET_ENDPOINT = `https://falcon.com/eth/mainnet?key=${NEXUS_GLOBAL_ACCESS_KEY}`
+
+	beforeEach(() => {
+		const fetchMock = getMiniflareFetchMock()
+		fetchMock.disableNetConnect()
+
+		const origin = fetchMock.get('https://eth-mainnet.alchemyapi.io')
+		origin.intercept({ method: 'POST', path: () => true }).reply(200, blockNumberMockResponse)
+	})
+
+	describe('alchemy is up', () => {
+		it('GET /eth/mainnet', async () => {
+			const request = new Request(ETH_MAINNET_ENDPOINT, {
+				method: 'GET',
+			})
+			const result = await worker.fetch(request, {
+				NEXUS_GLOBAL_ACCESS_KEY,
+				NEXUS_ALCHEMY_KEY,
+			})
+
+			const data = await result.json()
+
+			expect(result.status).toBe(200)
+			expect(data).toMatchObject({
+				success: true,
+				message: 'Provider is up and running.',
+				access: 'authorized',
+				code: 200,
+				chain: {
+					chainId: 1,
+					networkName: 'ethereum',
+					chainName: 'mainnet',
+				},
+			})
+		})
+
+		it('POST /eth/mainnet', async () => {
+			const request = new Request(ETH_MAINNET_ENDPOINT, {
+				method: 'POST',
+				body: JSON.stringify({
+					jsonrpc: '2.0',
+					id: 1,
+					method: 'eth_getBlockByNumber',
+					params: ['latest', false],
+				}),
+			})
+			const result = await worker.fetch(request, {
+				NEXUS_GLOBAL_ACCESS_KEY,
+				NEXUS_ALCHEMY_KEY,
+			})
+
+			const data = await result.json()
+
+			expect(result.status).toBe(200)
+
+			expect(data).toMatchObject(blockNumberMockResponse)
+		})
+	})
 })
