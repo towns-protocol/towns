c2b2be898678e77ae6d7ea570c38c2843256ee6f
Theo Tillberg <ttillberg@gmail.com>
2023-11-28 18:25:15 +0100

    app: enable global display name suggestions (hnt-3775) (#4277)

--- clients/web/lib/src/hooks/use-space-members.ts
+++ clients/web/lib/src/hooks/use-space-members.ts
@@ -1,10 +1,21 @@
 import { useMembers } from './use-members'
 import { useSpaceContext } from '../components/SpaceContextProvider'
+import { useAllKnownUsers } from './use-all-known-users'
+import { useMemo } from 'react'
 
 /**
  * Returns all members from the space in the current space context
  */
 export function useSpaceMembers() {
+    const allUsers = useAllKnownUsers()
+    const allMembers = useMemo(
+        () => ({
+            members: allUsers.users,
+            membersMap: allUsers.usersMap,
+        }),
+        [],
+    )
     const { spaceId } = useSpaceContext()
-    return useMembers(spaceId)
+    const spaceMembers = useMembers(spaceId)
+    return spaceId ? spaceMembers : allMembers
 }
