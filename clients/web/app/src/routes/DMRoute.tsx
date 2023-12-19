1658fe1d987671e0ec1eaea124deb32eb48e328c
Theo Tillberg <ttillberg@gmail.com>
2023-12-19 17:14:45 +0100

    app: redirect message route from desktop to mobile (hnt-3927) (#4662)

--- clients/web/app/src/routes/DMRoute.tsx
+++ clients/web/app/src/routes/DMRoute.tsx
@@ -1,13 +1,24 @@
-import React from 'react'
-import { Outlet, useOutlet } from 'react-router'
+import React, { useEffect, useRef } from 'react'
+import { Outlet, useNavigate, useOutlet, useParams } from 'react-router'
+import { useZionContext } from 'use-zion-client'
 import { GlobalContextUserLookupProvider } from 'use-zion-client/dist/components/UserLookupContext'
 import { DirectMessagesPanel } from '@components/DirectMessages/DirectMessages'
 import { Box, Heading, Icon, Paragraph, Stack } from '@ui'
 import { useDevice } from 'hooks/useDevice'
+import { PATHS } from 'routes'
+import { WelcomeLayout } from './layouts/WelcomeLayout'
 
 export const DirectMessages = () => {
     const { isTouch } = useDevice()
     const outlet = useOutlet()
+
+    const { spaceSlug } = useParams()
+    useTouchRedirect({ isTouch })
+
+    if (isTouch && !spaceSlug) {
+        return <WelcomeLayout debugText="TOUCH MESSAGE REDIRECT" />
+    }
+
     return isTouch ? (
         <GlobalContextUserLookupProvider>
             <Box absoluteFill background="level1">
@@ -31,4 +42,36 @@ export const DirectMessages = () => {
             </Stack>
         )
     )
+}
+
+/**
+ * on a touch device landing on  a desktop message route (i.e. outside of /t/:townId)
+ * will redirect to the first space
+ */
+const useTouchRedirect = (params: { isTouch: boolean }) => {
+    const { isTouch } = params
+
+    const navigate = useNavigate()
+    const spaceId = useParams().spaceId
+    const channelId = useParams().channelSlug
+    const replyId = useParams().messageId
+
+    const isDesktopRoute = !spaceId
+    const hasRedirectedRef = useRef(false)
+    const { spaces } = useZionContext()
+    const firstSpaceStreamId = spaces[0]?.id.streamId
+
+    const needsRedirect = isTouch && isDesktopRoute
+    const canRedirect = !hasRedirectedRef.current && !!firstSpaceStreamId
+
+    useEffect(() => {
+        if (needsRedirect && canRedirect) {
+            hasRedirectedRef.current = true
+            const messageSegment = channelId ? `${channelId}/` : ''
+            const threadSegment = replyId ? `${PATHS.REPLIES}/${replyId}` : ''
+            navigate(
+                `/${PATHS.SPACES}/${firstSpaceStreamId}/${PATHS.MESSAGES}/${messageSegment}${threadSegment}`,
+            )
+        }
+    }, [canRedirect, firstSpaceStreamId, channelId, navigate, needsRedirect, replyId])
 }
