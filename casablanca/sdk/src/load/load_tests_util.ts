8953514d4944f3430487f773e2b5d5f5b0d9ed4c
zhao <eclipsegst@users.noreply.github.com>
2023-12-19 20:10:29 -0800

    Add load test scenario 3 (#4427)

--- casablanca/sdk/src/load/load_tests_util.ts
+++ casablanca/sdk/src/load/load_tests_util.ts
@@ -6,7 +6,7 @@ import { Client } from '../client'
 import { RiverSDK } from '../testSdk'
 import { RiverDbManager } from '../riverDbManager'
 import { MockEntitlementsDelegate } from '../utils'
-import { createSpaceDapp } from '@river/web3'
+import { ISpaceDapp, createSpaceDapp } from '@river/web3'
 
 type ClientWalletInfo = {
     client: Client
@@ -96,4 +96,172 @@ export async function multipleClientsJoinSpaceAndChannel(
     })
 
     await Promise.all(clientPromises)
+}
+
+export type ClientSpaceChannelInfo = {
+    client: Client
+    spaceDapp: ISpaceDapp
+    spaceId: string
+    channelId: string
+}
+
+export async function createClientSpaceAndChannel(
+    account: {
+        address: string
+        privateKey: string
+    },
+    jsonRpcProviderUrl: string,
+    nodeRpcURL: string,
+): Promise<ClientSpaceChannelInfo> {
+    const clientWalletInfo = await createAndStartClient(account, jsonRpcProviderUrl, nodeRpcURL)
+    const client = clientWalletInfo.client
+    const provider = clientWalletInfo.provider
+    const walletWithProvider = clientWalletInfo.walletWithProvider
+    const network = await provider.getNetwork()
+    const chainId = network.chainId
+    const spaceDapp = createSpaceDapp(chainId, provider)
+
+    const balance = await walletWithProvider.getBalance()
+    const minBalanceRequired = ethers.utils.parseEther('0.01')
+    expect(balance.gte(minBalanceRequired)).toBe(true)
+    const riverSDK = new RiverSDK(spaceDapp, client, walletWithProvider)
+
+    // create space
+    const createTownReturnVal = await riverSDK.createTownWithDefaultChannel('load-tests', '')
+    const spaceStreamId = createTownReturnVal.spaceStreamId
+
+    // create channel
+    const channelStreamId = await riverSDK.createChannel(
+        spaceStreamId,
+        'load-tests',
+        'load-tests topic',
+    )
+
+    const spaceId = spaceStreamId
+    const channelId = channelStreamId
+
+    return {
+        client: client,
+        spaceDapp: spaceDapp,
+        spaceId: spaceId,
+        channelId: channelId,
+    }
+}
+
+export const startMessageSendingWindow = (
+    contentKind: string,
+    windowIndex: number,
+    clients: Client[],
+    channelId: string,
+    messagesSentPerUserMap: Map<string, Set<string>>,
+    windownDuration: number,
+): void => {
+    for (let i = 0; i < clients.length; i++) {
+        const client = clients[i]
+        const recipients = getRecipients(client.userId, clients)
+        sendMessageAfterRandomDelay(
+            contentKind,
+            client,
+            recipients,
+            channelId,
+            windowIndex.toString(),
+            messagesSentPerUserMap,
+            windownDuration,
+        )
+    }
+}
+
+export const sendMessageAfterRandomDelay = (
+    contentKind: string,
+    client: Client,
+    recipients: string[],
+    channelId: string,
+    windowIndex: string,
+    messagesSentPerUserMap: Map<string, Set<string>>,
+    windownDuration: number,
+): void => {
+    const randomDelay: number = Math.random() * windownDuration
+    setTimeout(() => {
+        void sendMessageAsync(
+            contentKind,
+            client,
+            recipients,
+            channelId,
+            windowIndex,
+            messagesSentPerUserMap,
+            randomDelay,
+        )
+    }, randomDelay)
+}
+
+const sendMessageAsync = async (
+    contentKind: string,
+    client: Client,
+    recipients: string[],
+    streamId: string,
+    windowIndex: string,
+    messagesSentPerUserMap: Map<string, Set<string>>,
+    randomDelay: number,
+) => {
+    const randomDelayInSec = (randomDelay / 1000).toFixed(3)
+    const prefix = `${streamId}:${Date.now()}`
+    // streamId:startTimestamp:messageBody
+    const newMessage = `${prefix}:Message<${contentKind}> from client<${
+        client.userId
+    }>, window<${windowIndex}>, ${getCurrentTime()} with delay ${randomDelayInSec}s`
+
+    for (const recipientUserId of recipients) {
+        const userStreamKey = getUserStreamKey(recipientUserId, streamId)
+        let messagesSet = messagesSentPerUserMap.get(userStreamKey)
+        if (!messagesSet) {
+            messagesSet = new Set()
+            messagesSentPerUserMap.set(userStreamKey, messagesSet)
+        }
+        messagesSet.add(newMessage)
+    }
+
+    await client.sendMessage(streamId, newMessage)
+}
+
+export function getRecipients(excludedUserId: string, clients: Client[]): string[] {
+    return clients
+        .filter((client) => client.userId !== excludedUserId)
+        .map((client) => client.userId)
+}
+
+export function getCurrentTime(): string {
+    const currentDate = new Date()
+    const isoFormattedTime = currentDate.toISOString()
+    return isoFormattedTime
+}
+
+export function wait(durationMS: number): Promise<void> {
+    return new Promise((resolve) => {
+        setTimeout(resolve, durationMS)
+    })
+}
+
+export function getUserStreamKey(userId: string, streamId: string): string {
+    return `${userId}_${streamId}`
+}
+
+// inputString starts with 'streamId:startTimestamp:messageBody'
+export function extractComponents(inputString: string): {
+    streamId: string
+    startTimestamp: number
+    messageBody: string
+} {
+    const firstColon = inputString.indexOf(':')
+    const secondColon = inputString.indexOf(':', firstColon + 1)
+
+    if (firstColon === -1 || secondColon === -1) {
+        throw new Error('Invalid input format')
+    }
+
+    const streamId = inputString.substring(0, firstColon)
+    const startTimestampStr = inputString.substring(firstColon + 1, secondColon)
+    const startTimestamp = Number(startTimestampStr)
+    const messageBody = inputString.substring(secondColon + 1, secondColon)
+
+    return { streamId, startTimestamp, messageBody }
 }
