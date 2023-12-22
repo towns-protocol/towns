d3c51bc767ce5a103d28285ce4c483864c781f76
Theo Tillberg <ttillberg@gmail.com>
2023-12-22 16:48:43 +0100

    app: mobile - fix backbutton on deeplinks (hnt-4230) (#4733)
    having trouble to try this on localhost since I won't get notifications
    but the idea is to use relative links unless the channel was openened
    from within the app with a `rel=` link

--- clients/web/app/src/components/DirectMessages/DirectMessageThread.tsx
+++ clients/web/app/src/components/DirectMessages/DirectMessageThread.tsx
@@ -1,32 +1,16 @@
-import React, { useCallback } from 'react'
-import { useNavigate, useParams } from 'react-router'
+import React from 'react'
+import { useParams } from 'react-router'
 import { SpaceContextProvider } from 'use-zion-client'
-import { useSearchParams } from 'react-router-dom'
 import { DMChannelContextUserLookupProvider } from 'use-zion-client/dist/components/UserLookupContext'
 import { Stack } from '@ui'
-import { SpacesChannel } from 'routes/SpacesChannel'
-import { useCreateLink } from 'hooks/useCreateLink'
-import { SpacesChannelAnimated } from 'routes/SpacesChannelAnimated'
 import { useDevice } from 'hooks/useDevice'
+import { SpacesChannel } from 'routes/SpacesChannel'
+import { SpacesChannelAnimated } from 'routes/SpacesChannelAnimated'
 
 export const DirectMessageThread = () => {
-    const { createLink } = useCreateLink()
     const { isTouch } = useDevice()
-    const navigate = useNavigate()
-    const [search] = useSearchParams()
+
     const { channelSlug } = useParams()
-    const onBack = useCallback(() => {
-        if (search.get('ref')) {
-            // if panel has been referred from another place in the app, go back
-            // to that place rather than parent. (e.g. from profile panel)
-            navigate(-1)
-        } else {
-            const link = createLink({ route: 'messages' })
-            if (link) {
-                navigate(link)
-            }
-        }
-    }, [createLink, navigate, search])
 
     return (
         <Stack height="100%" width="100%">
@@ -35,7 +19,7 @@ export const DirectMessageThread = () => {
                     fallbackToParentContext
                     channelId={channelSlug ?? ''}
                 >
-                    {isTouch ? <SpacesChannelAnimated /> : <SpacesChannel onTouchClose={onBack} />}
+                    {isTouch ? <SpacesChannelAnimated /> : <SpacesChannel />}
                 </DMChannelContextUserLookupProvider>
             </SpaceContextProvider>
         </Stack>
--- clients/web/app/src/routes/SpacesChannelAnimated.tsx
+++ clients/web/app/src/routes/SpacesChannelAnimated.tsx
@@ -1,6 +1,7 @@
 import { AnimatePresence } from 'framer-motion'
 import React, { useCallback, useEffect, useState } from 'react'
 import { useNavigate } from 'react-router'
+import { useSearchParams } from 'react-router-dom'
 import { ZLayerBox } from '@components/ZLayer/ZLayerContext'
 import { transitions } from 'ui/transitions/transitions'
 import { SpacesChannel } from './SpacesChannel'
@@ -13,13 +14,20 @@ export const SpacesChannelAnimated = () => {
         setChannelPresented(true)
     }, [])
 
+    const [search] = useSearchParams()
+
     const closePanel = useCallback(() => {
         setChannelPresented(false)
         setTimeout(() => {
-            // const to = `/${PATHS.SPACES}/${spaceId}/`
-            navigate(-1)
+            if (search.get('ref')) {
+                // if panel has been referred from another place in the app, go back
+                // to that place rather than parent. (e.g. from profile panel)
+                navigate(-1)
+            } else {
+                navigate('../')
+            }
         }, transitions.panelAnimationDuration * 1000)
-    }, [setChannelPresented, navigate])
+    }, [navigate, search])
 
     return (
         <AnimatePresence>
