1b8fb18f9dbb3e2048105bd4fbd6d43a06579985
texuf <austinellis+git@gmail.com>
2024-01-10 14:23:05 -0800

    Default to baseSepolia if we don’t have a custom base sepolia domain in the .env (#4897)
    So we don’t have to add this to config to run locally against test-beta

--- clients/web/app/src/customChains.ts
+++ clients/web/app/src/customChains.ts
@@ -15,10 +15,12 @@ if (env.VITE_PROVIDER_WS_URL) {
     baseSepoliaRpcUrls.webSocket = [env.VITE_PROVIDER_WS_URL]
 }
 
-export const baseSepoliaClone: Chain = {
-    ...baseSepolia,
-    rpcUrls: {
-        default: baseSepoliaRpcUrls,
-        public: baseSepoliaRpcUrls,
-    },
-}
+export const baseSepoliaClone: Chain = env.VITE_PROVIDER_HTTP_URL
+    ? {
+          ...baseSepolia,
+          rpcUrls: {
+              default: baseSepoliaRpcUrls,
+              public: baseSepoliaRpcUrls,
+          },
+      }
+    : baseSepolia
