f94245be8a5f1017ccd54325f20dd226ceeab3ef
Theo Tillberg <ttillberg@gmail.com>
2024-01-16 16:54:20 +0100

    app: fix long TLD links + add tests (hnt-4375) (#4954)
    - for prefixed links we can allow more than 6 chars TLDs (e.g.
    `.technology`)
    - adding `.technology` to the non prefixed list
    - adding some basic tests, should have done that from start...

---
+++ clients/web/app/src/components/RichText/plugins/AutoLinkMatcherPlugin.test.ts
@@ -0,0 +1,37 @@
+import { NON_PREFIXED_MATCHER, URL_MATCHER } from './AutoLinkMatcherPlugin'
+describe('AutoLinkMatcherPlugin', () => {
+    const links = [
+        'www.test.com',
+        'https://polygon.technology/developers',
+        'https://subdomain.subdomain.test.com',
+        'test.com',
+        'test.com?test=1',
+        'test.shop/test',
+        'test.technology/test?abc=123',
+        'test-ok.technology/',
+        'https://test.com',
+        'http://test.com',
+        'https://test.com/',
+        'https://test.com/m-ore',
+        'https://test.com/m-ore/123/234',
+        'https://test.com/m-ore/123/234#test',
+        'https://test.com/m-ore/a/b/c/b/c/d/e/f/g/h/i/j/k/l/m/n/o/p/q/r/s/t/u/v/w/x/y/z',
+        'https://test.com/m-ore/a/b/?c=d&e=f&g=h&i=j&k=l&m=n&o=p&q=r&s=t&u=v&w=x&y=z#tes[1-2]t',
+    ]
+
+    test.each(links)(`matching "%s" should resove to a link`, (link) => {
+        expect(link.match(URL_MATCHER) || link.match(NON_PREFIXED_MATCHER)).toBeTruthy()
+    })
+
+    const nonLinks = [
+        'www.test .com',
+        'some links.come over here',
+        'this linked.in my head',
+        'test.techologyssssssss/link',
+        'subdomain.test',
+    ]
+
+    test.each(nonLinks)(`matching "%s" should not resolve to a link`, (link) => {
+        expect(link.match(URL_MATCHER) || link.match(NON_PREFIXED_MATCHER)).toBeFalsy()
+    })
+})
--- clients/web/app/src/components/RichText/plugins/AutoLinkMatcherPlugin.tsx
+++ clients/web/app/src/components/RichText/plugins/AutoLinkMatcherPlugin.tsx
@@ -9,18 +9,18 @@
 import { AutoLinkPlugin } from '@lexical/react/LexicalAutoLinkPlugin'
 import * as React from 'react'
 
-const URL_MATCHER =
-    /((https?:\/\/(www\.)?)|(www\.))[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()[\],@:%!_+.~#?&//=]*)/
+export const URL_MATCHER =
+    /((https?:\/\/(www\.)?)|(www\.))[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9]{2,10}(?:\/(?:[-a-zA-Z0-9()[\],@:%!_+.~#?&//=]*)|)/
 
 // matches without https?:// prefix (e.g. `app.towns.com/?invite`). needs to be more strict than the above
 // since it could easily mess up a normal sentence.
 // 1/ domain (pattern repeated once for potential subdomain)
 // 2/ tld - any 2 char is ok, otherwise pick from a list of known tlds
 // 3/ path - optional
-const NON_PREFIXED_MATCHER =
-    /^([-a-z0-9@:%._+~#=]{1,256}\.){1,2}(?:com|net|org|co|dev|app|inc|online|info|live|us|tech|pro|world|space|life|shop|art|[a-z]{2})\b(?:[-a-zA-Z0-9()[\],@:%!_+.~#?&//=]*)$/
+export const NON_PREFIXED_MATCHER =
+    /^([-a-z0-9@:%._+~#=]{1,256}\.){1,2}(?:com|net|org|co|dev|app|inc|online|info|live|us|technology|tech|pro|world|space|life|shop|art|[a-z]{2})\b([-a-zA-Z0-9()[\],@:%!_+.~#?&//=]*)$/
 
-const EMAIL_MATCHER =
+export const EMAIL_MATCHER =
     /(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))/
 
 const MATCHERS = [
