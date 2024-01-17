3398bb7112c2442085ccf14fbcb86925d0f61cec
Tak Wai Wong <64229756+tak-hntlabs@users.noreply.github.com>
2024-01-17 13:33:42 -0800

    Tak/close notification when visibility changed (#4960)
    add a visibility listener to the app's notification hook. If the
    document is visible, clear the notifications.
    
    closed hnt-4525

--- clients/web/app/src/hooks/useAppNotifications.ts
+++ clients/web/app/src/hooks/useAppNotifications.ts
@@ -2,8 +2,43 @@ import { useEffect } from 'react'
 import { useNavigate } from 'react-router'
 import { WEB_PUSH_NAVIGATION_CHANNEL } from 'workers/types.d'
 
+const log = console.debug
+
 export const useAppNotifications = () => {
     const navigate = useNavigate()
+
+    useEffect(() => {
+        async function handleVisibilityChange() {
+            if (isDocumentDefined() && document.visibilityState !== 'visible') {
+                return // do nothing if the document is not visible
+            }
+            if (isNavigatorDefined()) {
+                const registration = await navigator.serviceWorker.getRegistration()
+                if (registration) {
+                    const notifications = await registration.getNotifications()
+                    if (notifications.length === 0) {
+                        return
+                    }
+                    // double check that the document is visible before closing notifications
+                    if (isDocumentDefined() && document.visibilityState === 'visible') {
+                        for (const n of notifications) {
+                            log('useAppNotifications:push: closing notification', n.tag)
+                            n.close()
+                        }
+                    }
+                }
+            }
+        }
+
+        if (isDocumentDefined()) {
+            document.onvisibilitychange = handleVisibilityChange
+        }
+        return () => {
+            if (isDocumentDefined()) {
+                document.onvisibilitychange = null
+            }
+        }
+    }, [])
 
     useEffect(() => {
         const broadcastChannel = new BroadcastChannel(WEB_PUSH_NAVIGATION_CHANNEL)
@@ -15,4 +50,12 @@ export const useAppNotifications = () => {
             broadcastChannel.close()
         }
     }, [navigate])
+}
+
+function isNavigatorDefined(): boolean {
+    return typeof navigator !== 'undefined'
+}
+
+function isDocumentDefined(): boolean {
+    return typeof document !== 'undefined'
 }
