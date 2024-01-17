3b3f665c846d8884e61705f96985388fbc7ff633
Artem <133605745+artem-hnt@users.noreply.github.com>
2024-01-17 15:40:16 +0300

    yml for local kafka with zookeper for stress test (#4940)
    As we agreed to use Kafka for load testing purposes adding docker file
    for local run. It has:
    -ZooKeeper and Kafka
    -Schema Registry for Kafka REST proxy
    -Kafka REST proxy as from Jest and Browser based environment it is the
    only way to interact with Kafka
    -BullMQ which will be used for sending events happening in the River for
    further reconciliation

---
+++ casablanca/sdk/src/load/coordinator.test.ts
@@ -0,0 +1,9 @@
+/**
+ * @group stress-test
+ */
+
+describe('Stress test', () => {
+    test('stress test', async () => {
+        //Placeholder
+    })
+})
---
+++ casablanca/sdk/src/load/follower.test.ts
@@ -0,0 +1,20 @@
+import { dlog } from '@river/mecholm'
+import axios from 'axios'
+
+const kafkaRestProxyUrl = 'http://127.0.0.1:8082' // Replace with your Kafka REST Proxy URL
+
+describe('Stress test', () => {
+    test('stress test', async () => {
+        async function consumeFromKafka() {
+            //Placeholder to see how it will work with Kafka
+            try {
+                const consumer = await axios.get(`${kafkaRestProxyUrl}/topics/`)
+                dlog('Consumer:', consumer.data)
+            } catch (error) {
+                dlog('Error consuming messages from Kafka')
+            }
+        }
+
+        await consumeFromKafka()
+    })
+})
