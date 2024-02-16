7332d181fb958cba86dc7618f478c6abf1ae8986
Theo Tillberg <ttillberg@gmail.com>
2024-02-16 14:30:18 +0100

    app: fix space hierarchies stuck in memo (#5498)
    Problem: Channel lists in `spaceHierarchies` weren't updating and were
    left at the initial state (sometimes empty sometimes populated depending
    on the startup sequence).
    
    This has probably been leading to various inconsistencies throughout the
    app. In my case `unreads` not showing up because we are using
    spaceHierarchies to map unreads against channels.
    
    The issue has been around since beginning of January. Before we used to
    depend on `spaces` instead of memo `spaceIds`. The dependency check yet
    passed the linter since the `casablancaClient` is stable.
    
    TODO: The hook doesn't get called with a high-frequency and we could
    probably get rid off `SpaceChild`, `SpaceHierarchiy` all together which
    I believe is a remainder of Martrix room hierarchies.

--- clients/web/lib/src/hooks/ZionContext/useCasablancaSpaceHierarchies.ts
+++ clients/web/lib/src/hooks/ZionContext/useCasablancaSpaceHierarchies.ts
@@ -1,59 +1,47 @@
 import { useMemo, useRef } from 'react'
 import { useCasablancaSpaces } from '../CasablancClient/useCasablancaSpaces'
 import { Client as CasablancaClient } from '@river/sdk'
-import { SpaceHierarchies } from 'types/zion-types'
-import { SpaceChild } from 'types/zion-types'
+import { SpaceChild, SpaceHierarchies } from 'types/zion-types'
 import isEqual from 'lodash/isEqual'
 
 export function useCasablancaSpaceHierarchies(
     casablancaClient?: CasablancaClient,
 ): SpaceHierarchies {
     const spaces = useCasablancaSpaces(casablancaClient)
-
-    const previousSpaceIdsRef = useRef<string[]>([])
-
-    const spaceIds = useMemo(() => {
-        const newSpaceIds = spaces.map((space) => space.id)
-        if (isEqual(previousSpaceIdsRef.current, newSpaceIds)) {
-            return previousSpaceIdsRef.current
-        } else {
-            previousSpaceIdsRef.current = newSpaceIds
-            return newSpaceIds
-        }
-    }, [spaces])
-
     const previousSpaceHierarchiesRef = useRef<SpaceHierarchies>({})
 
-    // todo austin - seems like this should be a useEffect that listens to add/remove channel events
+    // todo austin - seems like this should be a useEffect that listens to
+    // add/remove channel events
+
     return useMemo(() => {
         if (!casablancaClient) {
-            return previousSpaceHierarchiesRef.current
+            return {}
         }
+        const rootHierarchy = spaces.reduce((rootHierarchy, space) => {
+            const spaceId = space.id
+            const spaceHierarchy = toSpaceHierarchy(casablancaClient, spaceId)
+            const prevChannels = previousSpaceHierarchiesRef.current[spaceId]
+            return {
+                ...rootHierarchy,
+                [spaceId]: isEqual(prevChannels, spaceHierarchy) ? prevChannels : spaceHierarchy,
+            }
+        }, {})
 
-        const result: SpaceHierarchies = {}
-        spaceIds.forEach((spaceId) => {
-            //TODO:
-            //1.Add proper support for worldReadable, guestCanJoin, numjoinedMembers
-            // (not critical for Unreads bur definitely required for consistency and absence of surprises)
-            //2.Consider refactoring code using reduce (if it will be more readable)
-            result[spaceId] = toSpaceHierarchy(casablancaClient, spaceId)
-        })
-        if (isEqual(previousSpaceHierarchiesRef.current, result)) {
+        if (isEqual(previousSpaceHierarchiesRef.current, rootHierarchy)) {
             return previousSpaceHierarchiesRef.current
         } else {
-            previousSpaceHierarchiesRef.current = result
-            return result
+            previousSpaceHierarchiesRef.current = rootHierarchy
+            return rootHierarchy
         }
-    }, [spaceIds, casablancaClient])
+    }, [casablancaClient, spaces])
 }
 
 export function toSpaceHierarchy(casablancaClient: CasablancaClient, spaceId: string) {
-    const children: SpaceChild[] = []
     const spaceChannels = Array.from(
         casablancaClient.stream(spaceId)?.view.spaceContent.spaceChannelsMetadata.keys() || [],
     )
+    const children: SpaceChild[] = []
     spaceChannels.forEach((channel) => {
-        console.log(channel)
         children.push({
             id: channel,
             name: '',
@@ -62,8 +50,9 @@ export function toSpaceHierarchy(casablancaClient: CasablancaClient, spaceId: st
             worldReadable: false,
             guestCanJoin: false,
             numjoinedMembers: 0,
-        })
+        } satisfies SpaceChild)
     })
+
     return {
         root: {
             id: spaceId,
@@ -74,6 +63,7 @@ export function toSpaceHierarchy(casablancaClient: CasablancaClient, spaceId: st
             guestCanJoin: false,
             numjoinedMembers: 0,
         } satisfies SpaceChild,
+
         children: children,
     }
 }
