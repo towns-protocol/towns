e95a45928c496dc17826ca3d768d257e7d5194d8
Erik Olsson <valross2@gmail.com>
2024-02-19 16:00:11 +0100

    app: fix flashing "Browse channels" in Threads and Mentions (#5526)

--- clients/web/app/src/hooks/useHasJoinedChannels.ts
+++ clients/web/app/src/hooks/useHasJoinedChannels.ts
@@ -5,7 +5,7 @@ export const useHasJoinedChannels = () => {
     const spaceData = useSpaceData()
     return useMemo(() => {
         return {
-            loadingChannels: spaceData?.isLoadingChannels,
+            loadingChannels: spaceData?.isLoadingChannels ?? true,
             hasJoinedChannels: Boolean(
                 spaceData?.channelGroups?.flatMap((g) => g.channels).length ?? 0,
             ),
