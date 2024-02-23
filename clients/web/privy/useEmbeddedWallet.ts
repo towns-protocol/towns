1c3d0608aba049c7f42b56729500861f5c388cfd
Evan Salzbrenner <evan@hntlabs.com>
2024-02-23 09:02:27 -0800

    useEmbeddedWallet: use ready and log if privy not ready (#5651)
    I don't think this is going to solve the random issue of the embedded
    wallet missing while still authenticated to privy, but let's see

--- clients/web/privy/useEmbeddedWallet.ts
+++ clients/web/privy/useEmbeddedWallet.ts
@@ -1,9 +1,15 @@
 import { useWallets } from '@privy-io/react-auth'
 
-// https://docs.privy.io/guide/guides/wagmi#4-set-the-users-active-wallet
-// we only have an embedded wallet for now, so useWalletClient should be returning that one
-// however once wallet linking is established, we may need to be setting user's active wallet appropriately
 export function useEmbeddedWallet() {
-    const { wallets } = useWallets()
-    return wallets.find((wallet) => wallet.walletClientType === 'privy')
+    const { wallets, ready } = useWallets()
+    if (!ready) {
+        console.log('Privy wallet not ready')
+        return
+    }
+    const privyWallet = wallets.find((wallet) => wallet.walletClientType === 'privy')
+    if (!privyWallet) {
+        console.log('Privy wallet not found')
+        return
+    }
+    return privyWallet
 }
