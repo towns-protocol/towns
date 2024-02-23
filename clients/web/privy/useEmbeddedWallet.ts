4d4b8a1b8d44c077427c5ec241c25e1ff7608613
Evan Salzbrenner <evan@hntlabs.com>
2024-02-23 12:59:55 -0800

    useEmbeddedWallet: more logging (#5654)

--- clients/web/privy/useEmbeddedWallet.ts
+++ clients/web/privy/useEmbeddedWallet.ts
@@ -1,15 +1,17 @@
-import { useWallets } from '@privy-io/react-auth'
+import { usePrivy, useWallets } from '@privy-io/react-auth'
 
 export function useEmbeddedWallet() {
+    const { ready: privyReady, authenticated } = usePrivy()
     const { wallets, ready } = useWallets()
+    if (!privyReady) {
+        console.log('Privy not ready. Authentication status: ', authenticated)
+    }
     if (!ready) {
-        console.log('Privy wallet not ready')
-        return
+        console.log('Privy wallet not ready. Authentication status: ', authenticated)
     }
     const privyWallet = wallets.find((wallet) => wallet.walletClientType === 'privy')
     if (!privyWallet) {
-        console.log('Privy wallet not found')
-        return
+        console.log('Privy wallet not found. Authentication status: ', authenticated)
     }
     return privyWallet
 }
