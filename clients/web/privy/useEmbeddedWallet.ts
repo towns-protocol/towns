638e5d5d4331ba7ba0d50b3f743f763c4af6d735
Evan Salzbrenner <evan@hntlabs.com>
2024-02-27 20:06:47 -0800

    app: reduce noise from privy logs (#5728)

--- clients/web/privy/useEmbeddedWallet.ts
+++ clients/web/privy/useEmbeddedWallet.ts
@@ -1,19 +1,25 @@
 import { usePrivy, useWallets } from '@privy-io/react-auth'
+import { useEffect } from 'react'
 
 export function useEmbeddedWallet() {
     const { ready: privyReady, authenticated } = usePrivy()
     const { wallets, ready } = useWallets()
-    if (!privyReady) {
-        console.log('Privy not ready. Authentication status: ', authenticated)
-    }
-    if (!ready) {
-        console.log('Privy wallet not ready. Authentication status: ', authenticated)
-    } else {
-        console.log('Privy wallet ready. Authentication status: ', authenticated)
-    }
     const privyWallet = wallets.find((wallet) => wallet.walletClientType === 'privy')
-    if (!privyWallet) {
-        console.log('Privy wallet not found. Authentication status: ', authenticated)
-    }
+
+    useEffect(() => {
+        if (!privyReady) {
+            console.log('Privy not ready. Authentication status: ', authenticated)
+        }
+        if (!ready) {
+            console.log('Privy wallet not ready. Authentication status: ', authenticated)
+        } else {
+            console.log('Privy wallet ready. Authentication status: ', authenticated)
+        }
+
+        if (!privyWallet) {
+            console.log('Privy wallet not found. Authentication status: ', authenticated)
+        }
+    }, [authenticated, privyReady, privyWallet, ready])
+
     return privyWallet
 }
