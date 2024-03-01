2068da220b2b57c6f3359613bf58e5264dec288a
John Terzis <john@hntlabs.com>
2024-03-01 10:39:21 -0700

    remove notifications service from servers

--- servers/notification-service/src/application/routes/notificationRouter.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express'
-import { validateSchema } from '../middleware/validation'
-import { notifyUsersSchema } from '../schema/notificationSchema'
-import { notifyUsersHandler } from '../controller/notificationHandler'
-
-export const notificationRouter = Router()
-
-notificationRouter.post('/notify-users', validateSchema(notifyUsersSchema), notifyUsersHandler)
