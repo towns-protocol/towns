2677393a0c49f192d49200ed8285909ef759fd21
John Terzis <john@hntlabs.com>
2024-03-01 10:36:59 -0700

    remove servers/workers dir

--- servers/workers/token-worker/test/global.d.ts
+++
@@ -1,7 +0,0 @@
-import { Env } from '../src/types'
-import { MockAgent } from 'undici'
-
-declare global {
-    function getMiniflareBindings(): Env
-    function getMiniflareFetchMock(): MockAgent
-}
--- servers/workers/unfurl-worker/test/mocks/index.ts
+++
@@ -1,8 +0,0 @@
-import giphy from './giphy'
-import tweet from './tweet'
-import imgur from './imgur'
-import imgurJSON from './imgurJSON'
-import giphyJSON from './giphyJSON'
-import noContent from './noContent'
-
-export { giphy, imgur, tweet, imgurJSON, giphyJSON, noContent }
