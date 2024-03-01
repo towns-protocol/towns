be07116412f69878396759c87313bc7e8adec107
Brian Meek <brian@hntlabs.com>
2024-03-01 15:00:23 -0800

    cleanup copy
    Signed-off-by: Brian Meek <brian@hntlabs.com>

---
+++ core/xchain/cmd/run_cmd.go
@@ -0,0 +1,84 @@
+package cmd
+
+import (
+	"context"
+	"core/xchain/config"
+	"core/xchain/server"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+
+	"github.com/river-build/river/core/node/dlog"
+	"github.com/river-build/river/core/node/infra"
+
+	"github.com/spf13/cobra"
+)
+
+func run() error {
+	cfg := config.GetConfig()
+	ctx := context.Background()
+
+	pid := os.Getpid()
+
+	log := dlog.FromCtx(ctx).With("pid", pid)
+
+	ctx = dlog.CtxWithLog(ctx, log)
+
+	if cfg.Metrics.Enabled {
+		go infra.StartMetricsService(ctx, cfg.Metrics)
+	}
+
+	shutdown := make(chan struct{})
+	var once sync.Once
+	closeShutdown := func() {
+		once.Do(func() {
+			close(shutdown)
+			log.Info("Channel shutdown closed")
+		})
+	}
+	wgDone := make(chan struct{})
+	// Start the worker goroutines
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	go func() {
+		wg.Wait()
+		close(wgDone)
+	}()
+
+	go func() {
+		defer wg.Done()
+		server.RunServer(ctx, 1, shutdown)
+	}()
+
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+out:
+
+	for {
+		select {
+		case <-interrupt:
+			log.Info("Interrupted")
+			closeShutdown()
+		case <-wgDone:
+			log.Info("Done")
+			break out
+		}
+	}
+	return nil
+
+}
+
+func init() {
+	cmd := &cobra.Command{
+		Use:   "run",
+		Short: "Runs the node",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return run()
+		},
+	}
+
+	rootCmd.AddCommand(cmd)
+}
