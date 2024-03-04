9f8c7c236c38bcc9986ec6ff9832e8f563fd1c3f
g <5714678+giuseppecrj@users.noreply.github.com>
2024-03-04 14:36:58 -0800

    fix multicall import

--- core/web3/src/v3/IMulticallShim.ts
+++ core/web3/src/v3/IMulticallShim.ts
@@ -7,8 +7,8 @@ import {
     IMulticallInterface as BaseSepoliaInterface,
 } from '@river/generated/v3/typings/IMulticall'
 
-import LocalhostAbi from '@river/generated/dev/abis/Multicall.abi.json' assert { type: 'json' }
-import BaseSepoliaAbi from '@river/generated/v3/abis/Multicall.abi.json' assert { type: 'json' }
+import LocalhostAbi from '@river/generated/dev/abis/IMulticall.abi.json' assert { type: 'json' }
+import BaseSepoliaAbi from '@river/generated/v3/abis/IMulticall.abi.json' assert { type: 'json' }
 
 import { ethers } from 'ethers'
 import { BaseContractShim } from './BaseContractShim'
