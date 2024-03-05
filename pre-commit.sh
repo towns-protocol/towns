49a14e1cf76d34aa7e8a95b08fea30d9eb8290f1
Brian Meek <brian@hntlabs.com>
2024-03-05 10:18:56 -0800

    Add override env variable to pre-commit hook (#5816)
    Signed-off-by: Brian Meek <brian@hntlabs.com>

--- pre-commit.sh
+++ pre-commit.sh
@@ -3,6 +3,12 @@
 # Redirect output to stderr.
 exec 1>&2
 
+# Check if RIVER_ALLOW_COMMIT is set to true; if so, bypass the check.
+if [ "$RIVER_ALLOW_COMMIT" = "true" ]; then
+  echo "RIVER_ALLOW_COMMIT is set to true. Bypassing the check for changes in the 'river' directory."
+  exit 0
+fi
+
 # Check for staged files in the 'river' directory.
 if git diff --cached --name-only | grep '^river/'; then
   echo "Error: Attempt to commit changes in the 'river' subtree is blocked."
