25430b43a09cca0809fc8b767206885fe13f1164
Abhinav <154382495+abhi-bc@users.noreply.github.com>
2024-03-07 04:01:03 -0800

    fix: placeholder alignment and color (HNT-5171) (#5849)
    Signed-off-by: Abhinav Pandey <abhinav@hntlabs.com>
    Co-authored-by: Theo Tillberg <ttillberg@gmail.com>

--- clients/web/app/src/components/RichTextPlate/components/RichTextEditorPlaceholder.tsx
+++ clients/web/app/src/components/RichTextPlate/components/RichTextEditorPlaceholder.tsx
@@ -1,16 +1,22 @@
 import React from 'react'
 import { RenderPlaceholderProps } from 'slate-react'
-import { Box, Paragraph } from '@ui'
+import { Box } from '@ui'
 
-export const RichTextPlaceholder = ({ children, attributes }: RenderPlaceholderProps) => (
-    <Box
-        absoluteFill
-        pointerEvents="none"
-        color="gray2"
-        justifyContent="center"
-        paddingY="md"
-        {...attributes}
-    >
-        <Paragraph truncate>{children}</Paragraph>
-    </Box>
-)
+export const RichTextPlaceholder = ({ children, attributes }: RenderPlaceholderProps) => {
+    return (
+        <Box
+            absoluteFill
+            pointerEvents="none"
+            color="gray2"
+            justifyContent="center"
+            paddingY="md"
+            {...attributes}
+            style={{
+                ...attributes.style,
+                opacity: 1,
+            }}
+        >
+            <Box as="p">{children}</Box>
+        </Box>
+    )
+}
