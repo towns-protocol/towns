120e75709f0dfc5178e859cdb7e5e0f048999658
Erik Olsson <valross2@gmail.com>
2024-03-14 13:04:19 +0100

    app: DM route fix for channel browse (hnt-5327) (#5992)
    temp fix until we get panels in order

--- clients/web/app/src/routes/DMInfoPanel.tsx
+++ clients/web/app/src/routes/DMInfoPanel.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import { useSearchParams } from 'react-router-dom'
+import { SpaceContextProvider } from 'use-towns-client'
 import { CHANNEL_INFO_PARAMS } from 'routes'
+import { BrowseChannelsPanel } from '@components/BrowseChannelsPanel/BrowseChannelsPanel'
+import { useSpaceIdFromPathname } from 'hooks/useSpaceInfoFromPathname'
 import { ChannelDirectoryPanel } from './SpaceChannelDirectoryPanel'
 import { DMChannelInfoPanel } from './DMChannelInfoPanel'
 import { GDMChannelPermissionsPanel } from './GDMChannelPermissions'
 import { ChannelInvitePanel } from './ChannelInvitePanel'
 
 export const DMInfoPanelWrapper = () => {
+    const spaceId = useSpaceIdFromPathname()
     const [search] = useSearchParams()
     if (
         search.get(CHANNEL_INFO_PARAMS.DM_CHANNEL) !== null ||
@@ -19,6 +23,12 @@ export const DMInfoPanelWrapper = () => {
         return <GDMChannelPermissionsPanel />
     } else if (search.get(CHANNEL_INFO_PARAMS.INVITE) !== null) {
         return <ChannelInvitePanel />
+    } else if (search.get(CHANNEL_INFO_PARAMS.BROWSE_CHANNELS) !== null) {
+        return (
+            <SpaceContextProvider spaceId={spaceId}>
+                <BrowseChannelsPanel />
+            </SpaceContextProvider>
+        )
     }
     return undefined
 }
