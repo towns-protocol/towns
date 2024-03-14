834b287c9d5a9133b2d3a0b888ebf90668b71998
zhao <eclipsegst@users.noreply.github.com>
2024-03-14 09:06:18 -0700

    Clean up unused code (#5906)
    Saw those multiple times, maybe not worth to keep and update them.
    Some work is done here #5941

--- clients/web/app/src/components/_archived/Logout.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { Button } from '@ui'
-import { useAuth } from 'hooks/useAuth'
-
-export const Logout = () => {
-    const { logout, isAuthenticated } = useAuth()
-
-    return isAuthenticated ? <Button onClick={logout}>Logout</Button> : null
-}
