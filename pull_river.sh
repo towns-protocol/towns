2274a84b72498e1d77504cb22b93eec23ece2389
texuf <austinellis+git@gmail.com>
2024-03-14 14:05:18 -0700

    Update pull_river.sh to check for build breakages locally (#5991)
    check output of yarn install and yarn lint before pushing

--- pull_river.sh
+++ pull_river.sh
@@ -5,9 +5,10 @@ print_usage() {
     echo "Prerequisites: 1) have gh github cli installed, and"
     echo "               2) run: gh auth login && gh repo set-default"
     echo "  -h: Display this help message."
-    echo "  -i: Interactive mode. Will prompt for confirmation before creating a pull request."
-    echo "  -u: Enable USER_MODE. Will wait if there are ci errors and allow you to fix them."
-    echo "  -c: Enable CI_MODE. Will fail on ci errors and not prompt for user input."
+    echo "  -u: USER_MODE. Will wait if there are CI errors and allow you to fix them."
+    echo "  -x: USER_MODE_X. includes -u, and runs yarn lint, build, and test:unit."
+    echo "  -i: INTERACTIVE. Runs -x and prompts for confirmation before creating a pull request."
+    echo "  -c: CI_MODE. Will fail on any errors and not prompt for user input."
 }
 
 # Check if at least one argument is passed
@@ -16,18 +17,23 @@ if [ $# -eq 0 ]; then
     exit 1
 fi
 
-while getopts "hiuc" arg; do
+while getopts "huxic" arg; do
   case $arg in
     h)
         print_usage
         exit 0
         ;;
-    i)
-        INTERACTIVE=1
-        USER_MODE=1
-        ;;
     u)
         USER_MODE=1
+        ;;
+    x)
+        USER_MODE=1
+        USER_MODE_X=1
+        ;;
+    i)
+        USER_MODE=1
+        USER_MODE_X=1
+        INTERACTIVE=1
         ;;
     c)
         CI_MODE=1
@@ -46,6 +52,71 @@ function parse_git_branch() {
 function make_pr_description() {
     # Use git log to list commit messages not present on origin/main
     git log origin/main..HEAD
+}
+
+function remove_river_yarn_files() {
+    # these files shouldn't be checked into the harmony repo
+    git rm "${SUBTREE_PREFIX}/package.json" 2>/dev/null || echo "${SUBTREE_PREFIX}/package.json not found, skipping"
+    git rm "${SUBTREE_PREFIX}/yarn.lock" 2>/dev/null || echo "${SUBTREE_PREFIX}/yarn.lock not found, skipping"
+}
+
+function yarn_install_and_check() {
+    # clean yarn artifacts so we know that yarn link will actually run and tell is if the build is broken
+    rm -rf node_modules/@river
+    rm -rf node_modules/@towns
+    rm -rf node_modules/use-towns-client
+    rm -rf node_modules/harmonyweb
+    rm -rf node_modules/sample-app
+    yarn cache clean
+
+    # run yarn, give user a chance to fix issues
+    while true; do
+        # Run yarn to check for build breakages and update the lockfile
+        echo "Running yarn install..."
+        yarn install
+        exit_status_yarn=$?
+        if [ $exit_status_yarn -eq 0 ]; then
+            if [[ $USER_MODE_X -ne 1 ]]; then
+                break
+            else
+                echo "Running yarn lint..."
+                yarn lint
+                exit_status_yarn=$?
+                if [ $exit_status_yarn -eq 0 ]; then
+                    echo "Running yarn build..."
+                    yarn build
+                    exit_status_yarn=$?
+                    if [ $exit_status_yarn -eq 0 ]; then
+                        echo "Running yarn test:unit..."
+                        yarn test:unit
+                        exit_status_yarn=$?
+                        if [ $exit_status_yarn -eq 0 ]; then
+                            break
+                        fi
+                    fi
+                fi
+            fi
+        fi
+
+        echo "Yarn process failed."
+        if [[ $USER_MODE -eq 1 ]]; then
+            echo ""
+            read -n 1 -s -r -p "Please fix issues and commit your changes. Press any key to continue"
+            echo ""
+        else
+            exit $exit_status_yarn
+        fi
+    done
+
+    # if you run on a fresh repo, the yarn lock file can get re-created
+    remove_river_yarn_files
+
+    # check for changes after running yarn
+    if [[ "$(git status --porcelain)" != "" ]]; then
+        echo "Commiting yarn changes."
+        git add .
+        git commit -m "Yarn Install Fix-Ups" -m "$(git commit --dry-run --short)"
+    fi
 }
 
 if [[ "$(git status --porcelain)" != "" ]]; then
@@ -82,11 +153,10 @@ PR_BODY_DESC="This merges the latest changes from the ${SUBTREE_PREFIX} reposito
 git checkout -b "${BRANCH_NAME}"
 
 # Pull the latest changes from the subtree, preserving history
-git subtree pull --prefix="${SUBTREE_PREFIX}" "${SUBTREE_REPO}" "${SUBTREE_BRANCH}" --squash || echo "Subtree pull expected to fail due to conflicts"
+git subtree pull --prefix="${SUBTREE_PREFIX}" "${SUBTREE_REPO}" "${SUBTREE_BRANCH}" --squash -m "git subtree pull ${SUBTREE_PREFIX} at ${SHORT_HASH}"
 
 # Remove specific files that should not be merged
-git rm "${SUBTREE_PREFIX}/package.json" 2>/dev/null || echo "package.json not found, skipping"
-git rm "${SUBTREE_PREFIX}/yarn.lock" 2>/dev/null || echo "yarn.lock not found, skipping"
+remove_river_yarn_files
 
 # Check for unresolved conflicts
 if git ls-files -u | grep -q '^[^ ]'; then
@@ -101,17 +171,9 @@ if ! git diff main --quiet --cached; then
     RIVER_ALLOW_COMMIT=true git commit -m "git subtree pull --prefix=${SUBTREE_PREFIX} ${SUBTREE_REPO} ${SUBTREE_BRANCH} --squash" -m "$SUBTREE_MERGE_MESSAGE"
     echo "Subtree changes committed."
 
-    # Run yarn to update the lockfile
-    yarn
-
-    # check for changes after running yarn
-    if [[ "$(git status --porcelain)" != "" ]]; then
-        echo "Commiting yarn lockfile."
-        git add yarn.lock
-        git commit -m "Update yarn.lock"
-    fi
+    # Run yarn, commit new yarn.lock, and check for build breakages
+    yarn_install_and_check
     
-
     if [[ $INTERACTIVE -eq 1 ]]; then
         read -p "Do you want to continue and create a pull request? (y/n) " -n 1 -r
         echo
@@ -122,15 +184,20 @@ if ! git diff main --quiet --cached; then
     fi
 
     # Push the branch to origin
-    git push origin "${BRANCH_NAME}"
+    git push -u origin "${BRANCH_NAME}"
 
     # Create a pull request
     PR_BODY="${PR_BODY_DESC}"$'\n\n'"$(make_pr_description)"
     gh pr create --base main --head "${BRANCH_NAME}" --title "${PR_TITLE}" --body "${PR_BODY}"
     echo "Pull request created."
 
-    while true; do
+    # Open the pull request in the browser
+    if [[ $USER_MODE_X -eq 1 ]]; then
+        gh pr view --web
+    fi
 
+    # wait for the checks to complete
+    while true; do
       WAIT_TIME=5
       while true; do
         OUTPUT=$(gh pr checks "${BRANCH_NAME}" 2>&1)
@@ -150,7 +217,7 @@ if ! git diff main --quiet --cached; then
           echo "Failure detected in PR checks."
           if [[ $USER_MODE -eq 1 ]]; then
               read -p "Have you fixed the issue and pushed your changes yet? (y/n) " -n 1 -r
-              echo
+              echo ""
               if [[ ! $REPLY =~ ^[Yy]$ ]]; then
                   echo "Pull request creation aborted."
                   exit $exit_status
