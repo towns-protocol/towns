499d4982067e45baaaf49bff32b5b249418fd514
texuf <austinellis+git@gmail.com>
2024-03-18 13:15:58 -0700

    Update kill-all-local-dev to kill stray river and xchain processes (#6058)
    Noticed some node and river_node processes hanging out in my activity
    monitor over the weekend, spinning at 100% cpu. This kills them.

--- scripts/kill-all-local-dev.sh
+++ scripts/kill-all-local-dev.sh
@@ -74,11 +74,17 @@ if prompt 'Stop Casbablanca?:y/n '
 then
     ./river/core/scripts/stop_node.sh 
     ./river/core/node/stop_multi.sh
+
+    # just in case
+    do_killl './bin/river_node run'
 fi
 
 if prompt 'Stop XChain?:y/n '
 then
     ./river/core/xchain/stop_multi.sh 
+
+    # that script doesn't always work
+    do_killl './bin/xchain_node run'
 fi
 
 do_killl yarn "$1"
