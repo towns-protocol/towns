9e54aea1a8048b02d20cb610a82a2ba178f8618a
texuf <austinellis+git@gmail.com>
2024-03-25 12:17:17 -0700

    Break up user contexts to not use and set state in the same hook (#6193)

--- clients/web/lib/src/components/UserLookupContextProviders.tsx
+++ clients/web/lib/src/components/UserLookupContextProviders.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useRef } from 'react'
+import React, { useMemo, useRef, useEffect } from 'react'
 import { useAllKnownUsers } from '../hooks/use-all-known-users'
 import { useRoomWithStreamId } from '../hooks/use-room'
 import { useSpaceContext } from './SpaceContextProvider'
@@ -35,30 +35,42 @@ export const SpaceContextUserLookupProvider = (props: { children: React.ReactNod
 
     const usersCache = useRef<LookupUserMap>({})
 
+    const users = useMemo(() => {
+        if (!spaceId) {
+            return []
+        }
+        return allSpaceUsers.map((member) => {
+            const user = {
+                ...member,
+                memberOf: parentContextUsersMap?.[member.userId]?.memberOf,
+            }
+            const prev = usersCache.current[user.userId]
+            if (prev && isEqual(prev, user)) {
+                return prev
+            } else {
+                usersCache.current[user.userId] = user
+                return user
+            }
+        })
+    }, [allSpaceUsers, parentContextUsersMap, spaceId])
+
+    useEffect(() => {
+        if (!spaceId) {
+            return
+        }
+        users.forEach((user) => {
+            const offlineUserKey = generateOfflineUserKey(spaceId, user.userId)
+            setOfflineUser(offlineUserKey, {
+                userId: user.userId,
+                username: user.username,
+                displayName: user.displayName,
+            } as OfflineUser)
+        })
+    }, [setOfflineUser, spaceId, users])
+
     const value = useMemo(() => {
         // avoid overriding parent context if spaceId isn't set (e.g DM channels)
         if (spaceId) {
-            const users = allSpaceUsers.map((member) => {
-                const user = {
-                    ...member,
-                    memberOf: parentContextUsersMap?.[member.userId]?.memberOf,
-                }
-
-                const offlineUserKey = generateOfflineUserKey(spaceId, user.userId)
-                setOfflineUser(offlineUserKey, {
-                    userId: user.userId,
-                    username: user.username,
-                    displayName: user.displayName,
-                } as OfflineUser)
-
-                const prev = usersCache.current[user.userId]
-                if (prev && isEqual(prev, user)) {
-                    return prev
-                } else {
-                    usersCache.current[user.userId] = user
-                    return user
-                }
-            })
             const usersMap = users.reduce((acc, user) => {
                 acc[user.userId] = user
                 return acc
@@ -77,7 +89,7 @@ export const SpaceContextUserLookupProvider = (props: { children: React.ReactNod
                 usersMap: parentContextUsersMap,
             } satisfies UserLookupContextType
         }
-    }, [allSpaceUsers, parentContextUsers, parentContextUsersMap, setOfflineUser, spaceId])
+    }, [parentContextUsers, parentContextUsersMap, spaceId, users])
 
     return <UserLookupContext.Provider value={value}>{props.children}</UserLookupContext.Provider>
 }
@@ -94,23 +106,27 @@ export const DMChannelContextUserLookupProvider = (props: {
     const spaceId = parentContext?.spaceId
     const { offlineUserMap, setOfflineUser } = useOfflineStore()
 
-    const value = useMemo(() => {
+    const users = useMemo(() => {
         if (!room) {
-            return {
-                users: [],
-                usersMap: {},
-            }
+            return []
         }
-        const users = room.members.map((member) => ({
+        return room.members.map((member) => ({
             ...member,
             memberOf: parentContext?.usersMap[member.userId]?.memberOf,
         }))
+    }, [room, parentContext?.usersMap])
 
-        const allUsers = Object.values(room.membersMap).map((member) => ({
+    const allUsers = useMemo(() => {
+        if (!room) {
+            return []
+        }
+        return Object.values(room.membersMap).map((member) => ({
             ...member,
             memberOf: parentContext?.usersMap[member.userId]?.memberOf,
         }))
+    }, [room, parentContext?.usersMap])
 
+    useEffect(() => {
         if (spaceId) {
             users.forEach((user) => {
                 const offlineUserKey = generateOfflineUserKey(spaceId, user.userId)
@@ -120,6 +136,15 @@ export const DMChannelContextUserLookupProvider = (props: {
                     displayName: user.displayName,
                 } as OfflineUser)
             })
+        }
+    }, [spaceId, setOfflineUser, users])
+
+    const value = useMemo(() => {
+        if (!room) {
+            return {
+                users: [],
+                usersMap: {},
+            }
         }
 
         // opportunistically fill in displayName from parent context if
@@ -157,15 +182,7 @@ export const DMChannelContextUserLookupProvider = (props: {
             users,
             usersMap,
         } satisfies UserLookupContextType
-    }, [
-        room,
-        spaceId,
-        props.fallbackToParentContext,
-        offlineUserMap,
-        channelId,
-        parentContext?.usersMap,
-        setOfflineUser,
-    ])
+    }, [room, props.fallbackToParentContext, allUsers, offlineUserMap, channelId, spaceId, users])
 
     return <UserLookupContext.Provider value={value}>{props.children}</UserLookupContext.Provider>
 }
