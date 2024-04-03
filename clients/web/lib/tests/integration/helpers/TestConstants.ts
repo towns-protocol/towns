91dada7b164e5c37cf48116ed39203c341cf58d7
texuf <austinellis+git@gmail.com>
2024-04-03 11:51:20 -0700

    Remove global provider in test constants (#6303)
    kinda wonder if this was the cause of some of our nonce errors in tests

--- clients/web/lib/tests/integration/helpers/TestConstants.ts
+++ clients/web/lib/tests/integration/helpers/TestConstants.ts
@@ -6,7 +6,6 @@ import { getJsonProvider, parseOptInt } from './TestUtils'
 import { TestGatingNFT } from '@river-build/web3'
 
 const amount = ethers.BigNumber.from(10).pow(18).toHexString()
-const provider = getJsonProvider()
 
 export class TestConstants {
     public static readonly EveryoneAddress = '0x0000000000000000000000000000000000000001'
@@ -34,26 +33,29 @@ export class TestConstants {
     }
 
     public static async getWalletWithoutNft(): Promise<ethers.Wallet> {
+        const provider = getJsonProvider()
+        await provider.ready
         const tempWallet = ethers.Wallet.createRandom()
         const fund = provider.send('anvil_setBalance', [tempWallet.address, amount])
-
         const wallet = tempWallet.connect(provider)
-
         await fund
 
         return wallet
     }
 
-    public static getUnfundedWallet(): Promise<ethers.Wallet> {
+    public static async getUnfundedWallet(): Promise<ethers.Wallet> {
         const tempWallet = ethers.Wallet.createRandom()
 
         const provider = getJsonProvider()
+        await provider.ready
         const wallet = tempWallet.connect(provider)
 
         return Promise.resolve(wallet)
     }
 
-    public static getWalletFromPrivateKey(privateKey: string): Promise<ethers.Wallet> {
+    public static async getWalletFromPrivateKey(privateKey: string): Promise<ethers.Wallet> {
+        const provider = getJsonProvider()
+        await provider.ready
         return Promise.resolve(new ethers.Wallet(privateKey, provider))
     }
 }
