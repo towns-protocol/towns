d6dcfe6b888e683378897ddb87ffa15fa9063701
Wil <157021769+wil-hnt@users.noreply.github.com>
2024-05-08 13:32:47 -0300

    prevent direct commits to the main branch (#6655)

--- pre-commit.sh
+++ pre-commit.sh
@@ -15,3 +15,10 @@ if git diff --cached --name-only | grep '^river/'; then
   echo "Please remove changes to the 'river' directory from the staging area to proceed."
   exit 1
 fi
+
+branch="$(git rev-parse --abbrev-ref HEAD)"
+
+if [ "$branch" = "main" ]; then
+  echo "You can't commit directly to main branch"
+  exit 1
+fi
