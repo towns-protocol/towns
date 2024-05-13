e3b732bc3c52e9eb6319a23b306f1e33269455e0
Abhinav <154382495+abhi-bc@users.noreply.github.com>
2024-05-13 08:27:55 -0700

    editor: unescape HTML entities in inlineCode [HNT-6330] (#6691)
    Before
    
    ![image](https://github.com/HereNotThere/harmony/assets/154382495/45ce7893-320f-4432-83c4-50591d1657e6)
    
    
    After
    ![Screenshot 2024-05-10 at 10 54
    01 AM](https://github.com/HereNotThere/harmony/assets/154382495/86cd91de-b340-4e85-841a-4b924cd1104d)
    
    Signed-off-by: Abhinav Pandey <abhinav@hntlabs.com>

--- clients/web/app/src/components/RichTextPlate/utils/remark/remarkDecodeHTMLCodeBlocks.ts
+++ clients/web/app/src/components/RichTextPlate/utils/remark/remarkDecodeHTMLCodeBlocks.ts
@@ -3,15 +3,18 @@ import { Transformer } from 'unified'
 import unescape from 'lodash/unescape'
 import { MdastNode } from './ast-types'
 
+const unescapeHTML = (node: MdastNode, _index: number) => {
+    node.value = node.value ? unescape(node.value) : node.value
+}
+
 /**
  * This transformers decodes HTML entities in code blocks.
  * &gt; becomes >
  */
 const remarkDecodeHTMLCodeBlocks = () => {
     const transformer: Transformer = (tree, _file) => {
-        visit(tree, 'code', (node: MdastNode, _index) => {
-            node.value = node.value ? unescape(node.value) : node.value
-        })
+        visit(tree, 'code', unescapeHTML)
+        visit(tree, 'inlineCode', unescapeHTML)
     }
 
     return transformer
