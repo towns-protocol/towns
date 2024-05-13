e45f45ceb1d88b523dca8a74f371a225de363066
Evan Salzbrenner <evan@hntlabs.com>
2024-05-13 14:15:02 -0700

    app: wrap PrivyNotAuthenticated in PrivyWrapper (#6711)

--- clients/web/app/src/components/Notifications/utils.tsx
+++ clients/web/app/src/components/Notifications/utils.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import { toast } from 'react-hot-toast/headless'
 import { PrivyNotAuthenticatedNotification } from '@components/Notifications/PrivyNotAuthenticatedNotification'
+import { PrivyWrapper } from 'privy/PrivyProvider'
 
 export function createPrivyNotAuthenticatedNotification() {
     return toast.custom(
         (t) => {
-            return <PrivyNotAuthenticatedNotification toast={t} />
+            return (
+                <PrivyWrapper>
+                    <PrivyNotAuthenticatedNotification toast={t} />
+                </PrivyWrapper>
+            )
         },
         {
             duration: Infinity,
