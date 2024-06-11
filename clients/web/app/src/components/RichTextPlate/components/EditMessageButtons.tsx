485204c1858a1f1108d56880617c6e7702dd66c4
Abhinav <154382495+abhi-bc@users.noreply.github.com>
2024-06-11 09:33:00 -0700

    editor: :) doesn't appear after sending on PWA [HNT-6415] (#7067)
    Signed-off-by: Abhinav Pandey <abhinav@hntlabs.com>

--- clients/web/app/src/components/RichTextPlate/components/EditMessageButtons.tsx
+++ clients/web/app/src/components/RichTextPlate/components/EditMessageButtons.tsx
@@ -3,6 +3,22 @@ import { Button, Stack } from '@ui'
 import { MotionIconButton } from 'ui/components/Motion/MotionComponents'
 import { useDevice } from 'hooks/useDevice'
 
+/**
+ * Dispatches a mock Enter event to the active element (editor).
+ * This is used in ExitComboboxPlugin.ts plugin to convert any unselected combobox item to a paragraph node.
+ * E.g. :) or @ me
+ *
+ * Only required for touch devices, because on touch, we don't use Enter key to send the message.
+ */
+const dispatchMockEnterEvent = () => {
+    const mockEnterEvent = new KeyboardEvent('keydown', {
+        bubbles: true,
+        key: 'Enter',
+        keyCode: 13,
+    })
+    document.activeElement?.dispatchEvent(mockEnterEvent)
+}
+
 export const EditMessageButtons = (props: {
     onSave?: () => void
     onCancel?: () => void
@@ -43,9 +59,12 @@ export const EditMessageButtons = (props: {
         (event: React.MouseEvent) => {
             event.preventDefault()
             event.stopPropagation()
+            if (isTouch) {
+                dispatchMockEnterEvent()
+            }
             onSave?.()
         },
-        [onSave],
+        [onSave, isTouch],
     )
 
     const disabled = props.disabled || (isEditorEmpty && !hasImage)
