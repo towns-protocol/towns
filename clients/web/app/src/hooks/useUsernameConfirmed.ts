a1025f044b7024c7ec564734ef4fc85aa1dca2c5
Theo Tillberg <ttillberg@gmail.com>
2024-06-18 12:36:02 +0200

    app: adding logs around confirmed user state (#7174)
    looking at the logs here I see there is a user and that it has the
    confirmed state `false`
    
    
    https://linear.app/hnt-labs/issue/HNT-7144/%5Bgamma%5D%5Buser-feedback%5D-i-get-constantly-prompted-to-enter-username

--- clients/web/app/src/hooks/useUsernameConfirmed.ts
+++ clients/web/app/src/hooks/useUsernameConfirmed.ts
@@ -1,12 +1,38 @@
-import { useMyUserId, useSpaceId, useUserLookupStore } from 'use-towns-client'
+import { useEffect } from 'react'
+import { useMyUserId, useSpaceId, useTownsContext, useUserLookupStore } from 'use-towns-client'
 
 export const useUsernameConfirmed = () => {
     const userId = useMyUserId()
     const spaceId = useSpaceId()
+    const { casablancaClient } = useTownsContext()
 
     const usernameConfirmed = useUserLookupStore(({ spaceUsers }) =>
         spaceId && userId ? spaceUsers[spaceId]?.[userId]?.usernameConfirmed : undefined,
     )
+
+    // ----------------- logging -----------------
+
+    const user = useUserLookupStore(({ spaceUsers }) =>
+        spaceId && userId ? spaceUsers[spaceId]?.[userId] : undefined,
+    )
+
+    const usernameConfirmedFromStream =
+        spaceId && userId
+            ? casablancaClient?.streams.get(spaceId)?.view.getUserMetadata()?.userInfo(userId)
+            : undefined
+
+    useEffect(() => {
+        console.log('useUsernameConfirmed', {
+            spaceId,
+            userId,
+            usernameConfirmed,
+            usernameConfirmedFromStream: usernameConfirmedFromStream?.usernameConfirmed,
+            userFromLookup: user,
+            userFromStream: usernameConfirmedFromStream,
+        })
+    }, [spaceId, user, userId, usernameConfirmed, usernameConfirmedFromStream])
+
+    // ----------------- logging -----------------
 
     return (
         // prevent setting username while user isn't yet loaded
