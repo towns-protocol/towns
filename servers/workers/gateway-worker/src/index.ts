9ad7df88fc17ebf976e609d975c4da2df4d29801
Brian Meek <brian@hntlabs.com>
2024-07-09 15:32:26 -0700

    Rollback userfeedback change (#7373)
    Signed-off-by: Brian Meek <brian@hntlabs.com>

--- servers/workers/gateway-worker/src/index.ts
+++ servers/workers/gateway-worker/src/index.ts
@@ -8,32 +8,43 @@ import {
 } from 'worker-common'
 import { handleRequest } from './handler'
 
+/**
+ * Welcome to Cloudflare Workers! This is your first worker.
+ *
+ * - Run `wrangler dev src/index.ts` in your terminal to start a development server
+ * - Open a browser tab at http://localhost:8787/ to see your worker in action
+ * - Run `wrangler publish src/index.ts --name my-worker` to publish your worker
+ *
+ * Learn more at https://developers.cloudflare.com/workers/
+ */
+
+// These initial Types are based on bindings that don't exist in the project yet,
+// you can follow the links to learn how to implement them.
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
 export interface Env extends AuthEnv {
+    // Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
+    // MY_KV_NAMESPACE: KVNamespace
+    //
+    // Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
+    // MY_DURABLE_OBJECT: DurableObjectNamespace
+    //
+    // Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
+    // MY_BUCKET: R2Bucket
     API_TOKEN: string
     CF_API: string
     ACCOUNT_ID: string
     ENVIRONMENT: Environment
-    LINEAR_API_KEY: string
-    LINEAR_TEAM_ID: string
-    LINEAR_GRAPHQL_URL: string
-    LINEAR_USER_FEEDBACK_PROJECT_ID: string
-    USER_FEEDBACK_TOPIC_ARN: string
-    AWS_ACCESS_KEY_ID: string
-    AWS_SECRET_ACCESS_KEY: string
 }
 
+// have to use module syntax to gain access to env which contains secret variables for local dev
 export default {
     fetch(request: Request, env: Env, ctx: ExecutionContext) {
-        return worker.fetch(request, env, ctx)
+        return worker.fetch(request, env)
     },
 }
 
 export const worker = {
-    async fetch(
-        request: FetchEvent['request'],
-        env: Env,
-        ctx: ExecutionContext,
-    ): Promise<Response> {
+    async fetch(request: FetchEvent['request'], env: Env): Promise<Response> {
         if (isOptionsRequest(request)) {
             return getOptionsResponse(request, env.ENVIRONMENT)
         }
@@ -44,10 +55,9 @@ export const worker = {
                 headers: withCorsHeaders(request, env.ENVIRONMENT),
             })
         }
-
         console.log(`request: ${JSON.stringify(request)}`)
         const corsHeaders = withCorsHeaders(request, env.ENVIRONMENT)
-        const resp = await handleRequest(request, env, ctx) // Pass the context here
+        const resp = await handleRequest(request, env)
         const clone = new Response(resp.body, resp)
         for (const [key, value] of Object.entries(corsHeaders)) {
             clone.headers.set(key, value)
