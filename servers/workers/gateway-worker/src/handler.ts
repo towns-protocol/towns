908ccb9354005bbf8c46273376aa6d54fb8b3ddc
Brian Meek <brian@hntlabs.com>
2024-07-09 15:10:23 -0700

    Brian/userfeedback2 (#7372)
    Signed-off-by: Brian Meek <brian@hntlabs.com>

--- servers/workers/gateway-worker/src/handler.ts
+++ servers/workers/gateway-worker/src/handler.ts
@@ -288,7 +288,7 @@ router.get('/space/:id/identity', async (request: WorkerRequest, env) => {
     return new Response(JSON.parse(JSON.stringify(value)), { status: 200 })
 })
 
-router.post('/user-feedback', async (request: WorkerRequest, env, ctx) => {
+router.post('/user-feedback', async (request: WorkerRequest, env: Env, ctx) => {
     const uploads = (async () => {
         const formData = await request.formData()
         const requestBody = {
@@ -354,8 +354,12 @@ router.post('/user-feedback', async (request: WorkerRequest, env, ctx) => {
     })()
 
     // Use the waitUntil method to ensure background work continues after the response
-    ctx.waitUntil(uploads)
-    console.log('User feedback queued for processing')
+    if (ctx && typeof ctx.waitUntil === 'function') {
+        ctx.waitUntil(uploads)
+        console.log('User feedback queued for processing')
+    } else {
+        console.error('Context (ctx) is undefined or does not have waitUntil method')
+    }
 
     // Send the response immediately
     return new Response('ok', { status: 200 })
@@ -365,4 +369,5 @@ router.post('/')
 
 router.get('*', () => new Response('Not Found', { status: 404 }))
 
-export const handleRequest = (request: WorkerRequest, env: Env) => router.handle(request, env)
+export const handleRequest = (request: WorkerRequest, env: Env, ctx: ExecutionContext) =>
+    router.handle(request, env, ctx)
--- servers/workers/gateway-worker/src/index.ts
+++ servers/workers/gateway-worker/src/index.ts
@@ -8,43 +8,32 @@ import {
 } from 'worker-common'
 import { handleRequest } from './handler'
 
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `wrangler dev src/index.ts` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `wrangler publish src/index.ts --name my-worker` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
-// These initial Types are based on bindings that don't exist in the project yet,
-// you can follow the links to learn how to implement them.
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
 export interface Env extends AuthEnv {
-    // Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-    // MY_KV_NAMESPACE: KVNamespace
-    //
-    // Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-    // MY_DURABLE_OBJECT: DurableObjectNamespace
-    //
-    // Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-    // MY_BUCKET: R2Bucket
     API_TOKEN: string
     CF_API: string
     ACCOUNT_ID: string
     ENVIRONMENT: Environment
+    LINEAR_API_KEY: string
+    LINEAR_TEAM_ID: string
+    LINEAR_GRAPHQL_URL: string
+    LINEAR_USER_FEEDBACK_PROJECT_ID: string
+    USER_FEEDBACK_TOPIC_ARN: string
+    AWS_ACCESS_KEY_ID: string
+    AWS_SECRET_ACCESS_KEY: string
 }
 
-// have to use module syntax to gain access to env which contains secret variables for local dev
 export default {
     fetch(request: Request, env: Env, ctx: ExecutionContext) {
-        return worker.fetch(request, env)
+        return worker.fetch(request, env, ctx)
     },
 }
 
 export const worker = {
-    async fetch(request: FetchEvent['request'], env: Env): Promise<Response> {
+    async fetch(
+        request: FetchEvent['request'],
+        env: Env,
+        ctx: ExecutionContext,
+    ): Promise<Response> {
         if (isOptionsRequest(request)) {
             return getOptionsResponse(request, env.ENVIRONMENT)
         }
@@ -55,9 +44,10 @@ export const worker = {
                 headers: withCorsHeaders(request, env.ENVIRONMENT),
             })
         }
+
         console.log(`request: ${JSON.stringify(request)}`)
         const corsHeaders = withCorsHeaders(request, env.ENVIRONMENT)
-        const resp = await handleRequest(request, env)
+        const resp = await handleRequest(request, env, ctx) // Pass the context here
         const clone = new Response(resp.body, resp)
         for (const [key, value] of Object.entries(corsHeaders)) {
             clone.headers.set(key, value)
