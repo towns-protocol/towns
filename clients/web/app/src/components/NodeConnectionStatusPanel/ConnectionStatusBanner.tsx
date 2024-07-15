113693098a3c3a90c9042430428146438cf84c50
Theo Tillberg <ttillberg@gmail.com>
2024-07-15 06:19:08 +0200

    app: fix status color while animating (hnt-6527) (#7399)

--- clients/web/app/src/components/NodeConnectionStatusPanel/ConnectionStatusBanner.tsx
+++ clients/web/app/src/components/NodeConnectionStatusPanel/ConnectionStatusBanner.tsx
@@ -7,20 +7,33 @@ type Props = {
 }
 
 export const ConnectionStatusBanner = ({ status }: Props) => {
+    const color =
+        status === 'disconnected' || status === 'syncing'
+            ? 'negative'
+            : status === 'synced'
+            ? 'positive'
+            : undefined
+
     return status === 'synced' ? (
         <Banner>
             <Icon type="check" color="positive" size="square_xs" insetLeft="xxs" />
-            <Text truncate>Connected to node and message streams synced.</Text>
+            <Text truncate color={color}>
+                Connected to node and message streams synced.
+            </Text>
         </Banner>
     ) : status === 'syncing' ? (
         <Banner>
             <Icon type="alert" color="negative" size="square_xs" insetLeft="xxs" />
-            <Text truncate>Retrieving keys and decrypting messages</Text>
+            <Text truncate color={color}>
+                Retrieving keys and decrypting messages
+            </Text>
         </Banner>
     ) : (
         <Banner>
             <Icon type="alert" color="negative" size="square_xs" insetLeft="xxs" />
-            <Text truncate>Disconnected</Text>
+            <Text truncate color={color}>
+                Disconnected
+            </Text>
         </Banner>
     )
 }
