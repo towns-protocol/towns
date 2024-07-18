9c0a345b5138d804f6e574365781508e932ac73f
Kerem Kazan <kerem.kazan@gmail.com>
2024-07-18 12:33:05 -0400

    notification service prisma logs (#7436)

--- servers/notification-service/src/application/prisma.ts
+++ servers/notification-service/src/application/prisma.ts
@@ -1,3 +1,6 @@
 import { PrismaClient } from '@prisma/client'
 
-export const database = new PrismaClient()
+export const database = new PrismaClient({
+    // TODO: make this configurable via env vars
+    log: ['query', 'info', 'warn', 'error'],
+})
