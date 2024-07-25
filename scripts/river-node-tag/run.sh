7355fbf77bca7b4df0cb410b8f9a8e1f153103dc
Kerem Kazan <kerem.kazan@gmail.com>
2024-07-25 15:06:23 -0400

    added the autocommit config

--- scripts/river-node-tag/run.sh
+++ scripts/river-node-tag/run.sh
@@ -12,18 +12,26 @@ RIVER_NODE_TAG_FILE_PATH="scripts/river-node-tag/tag.txt"
 
 function main() {
     if [ "$1" == "set" ]; then
-        set_tag $2
+      set_tag $2
     elif [ "$1" == "get" ]; then
-        get_tag
+      get_tag
     else
-        echo "Usage: $0 [set <tag> | get]" >&2
-        exit 1
+      echo "Usage: $0 [set <tag> | get]" >&2
+      exit 1
     fi
 }
 
 function set_tag() {
   echo "Setting river-node tag to $1" >&2
   echo "$1" > $RIVER_NODE_TAG_FILE_PATH
+
+  # if the AUTOCOMMIT env var is set, commit the change
+  if [ "$AUTOCOMMIT" == "true" ]; then
+    echo "Autocommit is enabled. Committing the change." >&2
+
+    git add scripts/river-node-tag
+    git commit -m "Update river node tag to $1"
+  fi
 }
 
 function get_tag() {
