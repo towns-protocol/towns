970c3f2dbecfb59c8d5f4fbcc80c7b68972e457f
Evan Salzbrenner <evan.salzbrenner@gmail.com>
2024-08-06 10:34:47 -0700

    4337: force amd builds in docker on local 4337 (#7604)
    I forgot I ran into this same exact issue, at some point, that this guy
    did
    https://discord.com/channels/874596133148696576/942772249662996520/1237294294214377502
    ![Screenshot 2024-08-05 at 12 01
    06 PM](https://github.com/user-attachments/assets/ddb14928-b8dd-4f57-9f10-62cb70563e26)
    
    stackup's devnet docker images just randomly fail - I'm on an M3
    
    No info on how he solved it, no help from stackup team
    
    I had these amd images on my machine already built and had been using
    them for the last month.
    
    I deleted them today and ran back into this
    
    No docker expert so if anyone's run into something like this and knows a
    fix lmk

--- scripts/start-4337.sh
+++ scripts/start-4337.sh
@@ -3,4 +3,9 @@ set -euo pipefail
 cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")"
 
 cd ../servers/4337
+
+# Set the default platform if not provided
+# You can try setting this to linux/arm64 on mac, but I'm not having luck with it
+DOCKER_PLATFORM=${DOCKER_PLATFORM:-linux/amd64}
+
 docker compose up
