93d4b1171343a22dfd0f49236feec61c5118c694
Miguel Gomes <miguelgomes13@live.com>
2024-09-10 11:53:21 -0300

    app: dropdown to select river bearer token expiry time (#7941)
    <img width="447" alt="image"
    src="https://github.com/user-attachments/assets/0e9767ab-aafc-4c2f-a81f-50125ec1b444">
    <img width="452" alt="image"
    src="https://github.com/user-attachments/assets/f932504e-42c2-432f-a1dd-f0e30f62c5fd">

--- clients/web/app/src/components/BearerTokenPanel/BearerTokenPanel.tsx
+++ clients/web/app/src/components/BearerTokenPanel/BearerTokenPanel.tsx
@@ -1,44 +1,142 @@
 import React, { useCallback, useState } from 'react'
 import { useGetEmbeddedSigner } from '@towns/privy'
 import { makeBearerToken } from '@river-build/sdk'
+import { z } from 'zod'
 import { Panel } from '@components/Panel/Panel'
-import { Button, Paragraph } from '@ui'
+import { Box, Button, Dropdown, Paragraph } from '@ui'
 import { PrivyWrapper } from 'privy/PrivyProvider'
 import { ClipboardCopy } from '@components/ClipboardCopy/ClipboardCopy'
 import { ButtonSpinner } from 'ui/components/Spinner/ButtonSpinner'
 
+const EXPIRY_OPTIONS = [
+    {
+        label: '1 minute',
+        value: '1m',
+    },
+    {
+        label: '15 minutes',
+        value: '15m',
+    },
+    {
+        label: '30 minutes',
+        value: '30m',
+    },
+    {
+        label: '1 hour',
+        value: '1h',
+    },
+    {
+        label: '7 days',
+        value: '7d',
+    },
+    {
+        label: '1 day',
+        value: '1d',
+    },
+    {
+        label: '30 days',
+        value: '30d',
+    },
+]
+
+type ExpiryFormat = {
+    days?: number
+    hours?: number
+    minutes?: number
+    seconds?: number
+}
+
+const expiryUnits = {
+    d: 'days',
+    h: 'hours',
+    m: 'minutes',
+    s: 'seconds',
+} satisfies Record<string, keyof ExpiryFormat>
+
+const expiryStringSchema = z
+    .string()
+    .transform((str) => {
+        const regex = new RegExp(`^([0-9]+)(${Object.keys(expiryUnits).join('|')})$`)
+        const match = str.match(regex)
+        return match
+    })
+    .transform((match) => {
+        if (!match) {
+            return null
+        }
+        const number = parseInt(match[1])
+        const unit = match[2]
+        return {
+            [expiryUnits[unit as keyof typeof expiryUnits]]: number,
+        }
+    })
+
+const fixPlural = (value: number, singularUnit: string, pluralUnit: string) => {
+    return value === 1 ? singularUnit : pluralUnit
+}
+
+const expiryToString = (expiry: ExpiryFormat) =>
+    Object.entries(expiry).map(
+        ([key, value]) => `${value} ${fixPlural(value, key.replace(/s$/, ''), key)}`,
+    )
 const BearerTokenPanel = () => {
     const { isPrivyReady, getSigner } = useGetEmbeddedSigner()
     const [bearerToken, setBearerToken] = useState<string | null>(null)
+    const [expiry, setExpiry] = useState<{
+        days?: number
+        hours?: number
+        minutes?: number
+        seconds?: number
+    } | null>(null)
     const [isLoading, setLoading] = useState(false)
-    const generateBearerToken = useCallback(async () => {
-        setLoading(true)
-        const signer = await getSigner()
-        if (!signer) {
-            return
-        }
-        const token = await makeBearerToken(signer, { days: 7 })
-        setLoading(false)
-        return token
-    }, [getSigner])
+
+    const generateBearerToken = useCallback(
+        async (expiry: ExpiryFormat) => {
+            setLoading(true)
+            const signer = await getSigner()
+            if (!signer) {
+                return
+            }
+            const token = await makeBearerToken(signer, expiry)
+            setLoading(false)
+            return token
+        },
+        [getSigner],
+    )
 
     return (
-        <Panel label="Bearer Token">
+        <Panel label="Bearer Token" gap="lg">
             <Paragraph size="sm" color="gray2">
                 You can use it to authenticate your account in River.
             </Paragraph>
-            <Button
-                disabled={!isPrivyReady}
-                onClick={async () => {
-                    const token = await generateBearerToken()
-                    setBearerToken(token ?? null)
-                }}
-            >
-                Generate your auth token
-            </Button>
+
+            <Box gap>
+                <Dropdown
+                    label="Expiry Time"
+                    defaultValue="7d"
+                    options={EXPIRY_OPTIONS}
+                    onChange={(value) => {
+                        const parsedExpiry = expiryStringSchema.parse(value)
+                        setBearerToken(null)
+                        setExpiry(parsedExpiry)
+                    }}
+                />
+                <Button
+                    disabled={!isPrivyReady}
+                    onClick={async () => {
+                        if (!expiry) {
+                            return
+                        }
+                        const token = await generateBearerToken(expiry)
+                        setBearerToken(token ?? null)
+                    }}
+                >
+                    Generate your Bearer Token
+                </Button>
+            </Box>
 
             {isLoading && <ButtonSpinner />}
-            {bearerToken && (
+            {bearerToken && expiry && (
                 <>
                     <ClipboardCopy
                         clipboardContent={bearerToken}
@@ -46,7 +144,7 @@ const BearerTokenPanel = () => {
                         color="default"
                     />
                     <Paragraph color="gray2" size="sm" fontWeight="medium">
-                        Expires in 7 days.
+                        Expires in {expiryToString(expiry)}
                     </Paragraph>
                 </>
             )}
