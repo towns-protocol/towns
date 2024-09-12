a22d76b9b945f502223bb5cce5dadea635f1d12c
Miguel Gomes <miguelgomes13@live.com>
2024-09-12 01:09:51 -0300

    app: remove unused files (#7915)
    [Knip](https://knip.dev/) showed the files that weren't being used in
    the codebase.
    I'm keeping the unused but modified recently (< 6 months) files, since
    we may want to bring those back.
    
    Let me know how you **feel** about those kinds of PR, since
    `client/web/lib` has a few unused files and definitions too
    
    Probably the best way to review is by using the file tree in the "Files
    changed" tab
    
    super fun to see the old stuff from the codebase :')

--- clients/web/app/src/components/BackgroundGrid/BackgroundGrid.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-import { Box } from '@ui'
-import { bgStyles } from './BackgroundGrid.css'
-
-export const BackgroundGrid = () => <Box absoluteFill className={bgStyles} />
--- clients/web/app/src/components/ContextBar/index.tsx
+++
@@ -1,1 +0,0 @@
-export { ContextBar } from './ContextBar'
