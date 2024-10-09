9c1002b3a757c2b98ea463602febca21a7409863
Theo Tillberg <ttillberg@gmail.com>
2024-10-09 17:40:04 +0200

    app: update townhall link (towns-12805) (#8184)

--- clients/web/app/src/data/links.ts
+++ clients/web/app/src/data/links.ts
@@ -1,5 +1,4 @@
-export const TOWN_HALL_LINK =
-    'https://app.towns.com/t/10f3741ceefb5be552506c81a66df3323586cbde4b0000000000000000000000/'
+export const TOWN_HALL_LINK = 'https://app.towns.com/t/0x7d6936b6dbe77cda7d93fece27cdbb4daadd1ac7/'
 
 export const PRICING_LEARN_MORE_LINK =
     'https://townhall.towns.com/Town-Hall-9e5c8120218d489392e8a72aef8c0326?p=2edea6a280cb46279762d9dca2b7be41&pm=c'
