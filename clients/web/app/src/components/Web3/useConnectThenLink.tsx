c2486be99d467472c431578cede54b53630bfde4
Evan Salzbrenner <evan.salzbrenner@gmail.com>
2024-10-23 17:29:44 -0700

    privy@1.92.2 (#8304)

--- clients/web/app/src/components/Web3/useConnectThenLink.tsx
+++ clients/web/app/src/components/Web3/useConnectThenLink.tsx
@@ -22,39 +22,52 @@ export function useConnectThenLink({
 
     const { connectWallet } = useConnectWallet({
         onSuccess: async (wallet) => {
-            Analytics.getInstance().track('connected wallet', {
-                walletName: wallet.meta.name,
-            })
+            // could be either a ethereum or solana wallet
+            if ('chainId' in wallet && 'getEthersProvider' in wallet) {
+                Analytics.getInstance().track('connected wallet', {
+                    walletName: wallet.meta.name,
+                })
 
-            const rootSigner = await getSigner()
-            if (!rootSigner) {
-                createPrivyNotAuthenticatedNotification()
-                return
-            }
-
-            const chainString = baseChain.id.toString()
-
-            if (!wallet.chainId.includes(chainString)) {
-                try {
-                    await wallet.switchChain(baseChain.id)
-                } catch (e) {
-                    console.error(
-                        '[useConnectThenLink] error switching chain for connected wallet',
-                        e,
-                    )
+                const rootSigner = await getSigner()
+                if (!rootSigner) {
+                    createPrivyNotAuthenticatedNotification()
+                    return
                 }
-            }
-            // switching the chain doesn't immediately update in `wallet` (ms later it does)
-            // so we need to check the network
-            const networkChainId = (await (await wallet.getEthersProvider()).getNetwork()).chainId
 
-            if (networkChainId === baseChain.id) {
-                onLinkWallet(rootSigner, (await wallet.getEthersProvider()).getSigner())
+                const chainString = baseChain.id.toString()
+
+                if (!wallet.chainId.includes(chainString)) {
+                    try {
+                        await wallet.switchChain(baseChain.id)
+                    } catch (e) {
+                        console.error(
+                            '[useConnectThenLink] error switching chain for connected wallet',
+                            e,
+                        )
+                    }
+                }
+
+                const provider = await wallet.getEthersProvider()
+                // switching the chain doesn't immediately update in `wallet` (ms later it does)
+                // so we need to check the network
+                const networkChainId = (await provider.getNetwork()).chainId
+
+                if (networkChainId === baseChain.id) {
+                    onLinkWallet(rootSigner, provider.getSigner())
+                } else {
+                    popupToast(({ toast }) => (
+                        <StandardToast.Error
+                            toast={toast}
+                            message={`Please make sure your wallet supports and is connected to the ${baseChain.name} network.`}
+                        />
+                    ))
+                }
             } else {
+                console.log('[useConnectThenLink] unknown wallet type', wallet)
                 popupToast(({ toast }) => (
                     <StandardToast.Error
                         toast={toast}
-                        message={`Please make sure your wallet supports and is connected to the ${baseChain.name} network.`}
+                        message="We could not connect this wallet. Please make sure it is an EVM compatible wallet."
                     />
                 ))
             }
