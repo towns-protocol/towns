64862b95d9d54edc056e9fc3e39eb92b0a7307f3
texuf <austin@hntlabs.com>
2025-01-09 15:31:39 -0800

    comments and formatting

--- clients/web/app/src/workers/data_transforms.ts
+++ clients/web/app/src/workers/data_transforms.ts
@@ -1,7 +1,7 @@
 import { EncryptedData, StreamEvent } from '@river-build/proto'
 import { convert } from 'html-to-text'
-import { PlaintextDetails } from './decryptionFn'
 import { bin_toHexString } from '@river-build/dlog'
+import { PlaintextDetails } from './decryptionFn'
 
 export function getEncryptedData(data: StreamEvent): EncryptedData | undefined {
     switch (data.payload.case) {
@@ -32,14 +32,20 @@ export function getEncryptedData(data: StreamEvent): EncryptedData | undefined {
     }
 }
 
-export function getPlaintextDetailsForNonEncryptedEvents(data: StreamEvent): PlaintextDetails | undefined {
+/// for non-encrypted events, we need to extract the plaintext details from the event
+/// for example, tips are blockchain transactions that are not encrypted, sent as member payloads
+export function getPlaintextDetailsForNonEncryptedEvents(
+    data: StreamEvent,
+): PlaintextDetails | undefined {
     switch (data.payload.case) {
         case 'memberPayload':
             switch (data.payload.value.content.case) {
                 case 'memberBlockchainTransaction':
                     switch (data.payload.value.content.value.transaction?.content.case) {
                         case 'tip': {
-                            const messageId = data.payload.value.content.value.transaction.content.value.event?.messageId
+                            const messageId =
+                                data.payload.value.content.value.transaction.content.value.event
+                                    ?.messageId
                             return {
                                 body: undefined,
                                 mentions: undefined,
@@ -48,8 +54,16 @@ export function getPlaintextDetailsForNonEncryptedEvents(data: StreamEvent): Pla
                                 refEventId: messageId ? bin_toHexString(messageId) : undefined,
                             }
                         }
+                        default:
+                            // ignoring other transaction content types
+                            return undefined
                     }
+                default:
+                    // ignoring other member content types
+                    return undefined
+            }
         default:
+            // ignoring other event types
             return undefined
     }
 }
