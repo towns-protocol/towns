31b3af6945c42fce3a6f243bb60074210bbdbc4c
texuf <austinellis+git@gmail.com>
2025-01-23 21:02:45 -0800

    TOWNS-17460 Renable ladybug on public towns page (#8799)
    I broke this when i added the user account addresses to the bug report

--- clients/web/app/src/components/AppBugReport/AppBugReportOverlay.tsx
+++ clients/web/app/src/components/AppBugReport/AppBugReportOverlay.tsx
@@ -1,10 +1,12 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import React, { useCallback } from 'react'
+import { TownsContextProvider } from 'use-towns-client'
 import { Box, IconButton, Stack, Text, ZLayerProvider } from '@ui'
 import { useDevice } from 'hooks/useDevice'
 import { ModalContainer } from '@components/Modals/ModalContainer'
 import { ErrorReportForm } from '../ErrorReport/ErrorReport'
 import { useAppOverlayBugReport } from './useAppOverlayBugReport'
+import { useEnvironment } from '../../hooks/useEnvironmnet'
 
 const queryClient = new QueryClient({
     defaultOptions: {
@@ -19,6 +21,7 @@ const queryClient = new QueryClient({
  * context within overlays.
  */
 export const AppBugReportOverlay = () => {
+    const environment = useEnvironment()
     const { isTouch } = useDevice()
     const [isVisible, setVisible] = useAppOverlayBugReport((s) => [s.visible, s.setVisible])
 
@@ -29,41 +32,53 @@ export const AppBugReportOverlay = () => {
     return (
         <ZLayerProvider>
             {isVisible && (
-                <QueryClientProvider client={queryClient}>
-                    <ModalContainer padding="none" touchTitle="Bug Report" onHide={onHideBugReport}>
-                        {!isTouch && (
-                            <Stack
-                                gap
-                                grow
-                                horizontal
-                                alignItems="center"
-                                padding="md"
-                                height="x6"
-                                background="level2"
-                            >
-                                <Box width="x4" />
-                                <Box grow centerContent>
-                                    <Text
-                                        size="lg"
-                                        fontWeight="strong"
-                                        color="default"
-                                        data-testid="bug-report-header"
-                                    >
-                                        Bug Report
-                                    </Text>
+                <TownsContextProvider
+                    environmentId={environment.id}
+                    baseChain={environment.baseChain}
+                    baseConfig={environment.baseChainConfig}
+                    riverChain={environment.riverChain}
+                    riverConfig={environment.riverChainConfig}
+                >
+                    <QueryClientProvider client={queryClient}>
+                        <ModalContainer
+                            padding="none"
+                            touchTitle="Bug Report"
+                            onHide={onHideBugReport}
+                        >
+                            {!isTouch && (
+                                <Stack
+                                    gap
+                                    grow
+                                    horizontal
+                                    alignItems="center"
+                                    padding="md"
+                                    height="x6"
+                                    background="level2"
+                                >
+                                    <Box width="x4" />
+                                    <Box grow centerContent>
+                                        <Text
+                                            size="lg"
+                                            fontWeight="strong"
+                                            color="default"
+                                            data-testid="bug-report-header"
+                                        >
+                                            Bug Report
+                                        </Text>
+                                    </Box>
+                                    <Box width="x4" alignItems="end">
+                                        <IconButton icon="close" onClick={onHideBugReport} />
+                                    </Box>
+                                </Stack>
+                            )}
+                            <Box grow scroll position="relative">
+                                <Box padding shrink overflow="auto" maxHeight="100%">
+                                    <ErrorReportForm onHide={onHideBugReport} />
                                 </Box>
-                                <Box width="x4" alignItems="end">
-                                    <IconButton icon="close" onClick={onHideBugReport} />
-                                </Box>
-                            </Stack>
-                        )}
-                        <Box grow scroll position="relative">
-                            <Box padding shrink overflow="auto" maxHeight="100%">
-                                <ErrorReportForm onHide={onHideBugReport} />
                             </Box>
-                        </Box>
-                    </ModalContainer>
-                </QueryClientProvider>
+                        </ModalContainer>
+                    </QueryClientProvider>
+                </TownsContextProvider>
             )}
         </ZLayerProvider>
     )
