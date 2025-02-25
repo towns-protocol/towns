0c3e55b24968f939cadab561d7fdb0d9b76f4a5c
Miguel Nascimento <miguelgomes13@live.com>
2025-02-25 18:44:01 -0300

    wip client w/ extend

--- packages/bot/src/bot.ts
+++ packages/bot/src/bot.ts
@@ -1,28 +1,31 @@
 import { PlainMessage } from '@bufbuild/protobuf'
 
 import {
-    Client,
-    Entitlements,
-    makeBaseProvider,
-    makeRiverProvider,
-    makeSignerContext,
-    makeStreamRpcClient,
-    RiverDbManager,
-    type RiverConfig,
-    type SignerContext,
-    type StreamRpcClient,
+    getRefEventIdFromChannelMessage,
+    isChannelStreamId,
+    isDMChannelStreamId,
+    isGDMChannelStreamId,
+    makeEvent,
+    make_ChannelPayload_Message,
+    make_DMChannelPayload_Message,
+    make_GDMChannelPayload_Message,
+    streamIdAsBytes,
+    createTownsClient,
+    type ClientV2,
+    type makeRiverConfig,
 } from '@river-build/sdk'
 import { Hono, type Context } from 'hono'
 import { serve } from '@hono/node-server'
-import { ethers } from 'ethers'
 import EventEmitter from 'node:events'
 import TypedEmitter from 'typed-emitter'
 import {
-    ChannelMessage_Post_Content_Text,
     type ChannelMessage_Post_Attachment,
     type ChannelMessage_Post_Mention,
+    ChannelMessage,
+    type Envelope,
 } from '@river-build/proto'
-import { RiverRegistry, SpaceDapp } from '@river-build/web3'
+import { bin_toHexString } from '@river-build/dlog'
+import type { GroupEncryptionAlgorithmId } from '@river-build/encryption'
 
 type BasePayload = {
     userId: string
@@ -30,7 +33,7 @@ type BasePayload = {
     eventId: string
 }
 
-type Actions = {
+type BotActions = {
     sendReaction: (
         channelId: string,
         messageId: string,
@@ -51,54 +54,37 @@ type Actions = {
         messageId: string,
         message: string,
     ) => Promise<{ eventId: string }>
-    sendDm: Actions['sendMessage']
-    sendTip: (
-        userId: string,
-        eventId: string,
-        // TODO: tip params
-    ) => void // amount in wei
+    redactEvent: (channelId: string, refEventId: string) => Promise<{ eventId: string }>
+    sendDm: BotActions['sendMessage']
+    // TODO: sendTip
 }
 
 type BotEvents = {
-    message: (handler: Actions, opts: BasePayload & { message: string }) => void
-    redact: (handler: Actions, opts: BasePayload & { refEventId: string }) => void
-    messageDelete: (handler: Actions, opts: BasePayload & { eventId: string }) => void
-    botMention: (handler: Actions, opts: BasePayload & { message: string }) => void
-    reply: (handler: Actions, opts: BasePayload & { message: string }) => void
+    message: (handler: BotActions, opts: BasePayload & { message: string }) => void
+    redact: (handler: BotActions, opts: BasePayload & { refEventId: string }) => void
+    messageDelete: (handler: BotActions, opts: BasePayload & { eventId: string }) => void
+    botMention: (handler: BotActions, opts: BasePayload & { message: string }) => void
+    reply: (handler: BotActions, opts: BasePayload & { message: string }) => void
     reaction: (
-        handler: Actions,
+        handler: BotActions,
         opts: BasePayload & { reaction: string; messageId: string; userId: string },
     ) => void
-    eventRevoke: (handler: Actions, opts: BasePayload & { eventId: string }) => void
-    tip: (handler: Actions, opts: BasePayload & { amount: bigint }) => void
-    channelJoin: (handler: Actions, opts: BasePayload) => void
-    channelLeave: (handler: Actions, opts: BasePayload) => void
-    streamMessage: (handler: Actions, opts: BasePayload & { message: string }) => void
-}
-
-const buildBotWallet = async (privateKey: string) => {
-    const signer = new ethers.Wallet(privateKey)
-    const delegateWallet = ethers.Wallet.createRandom()
-    const signerContext = await makeSignerContext(signer, delegateWallet)
-    return signerContext
+    eventRevoke: (handler: BotActions, opts: BasePayload & { eventId: string }) => void
+    tip: (handler: BotActions, opts: BasePayload & { amount: bigint }) => void
+    channelJoin: (handler: BotActions, opts: BasePayload) => void
+    channelLeave: (handler: BotActions, opts: BasePayload) => void
+    streamMessage: (handler: BotActions, opts: BasePayload & { message: string }) => void
 }
 
 class Bot extends (EventEmitter as new () => TypedEmitter<BotEvents>) {
     private readonly server: Hono
-    private readonly rpcClient: Client
+    private readonly client: ClientV2<BotActions>
     botId: string
     // private readonly webhookClient: WebhookClient
-    constructor(signerContext: SignerContext, rpcClient: StreamRpcClient, config: RiverConfig) {
+    constructor(clientV2: ClientV2<BotActions>) {
         super()
-        const botId = 'bot'
-        const cryptoStore = RiverDbManager.getCryptoDb(botId)
-        const baseProvider = makeBaseProvider(config)
-        const spaceDapp = new SpaceDapp(config.base.chainConfig, baseProvider)
-        const entitlementsDelegate = new Entitlements(config, spaceDapp)
-
-        this.botId = botId
-        // TODO: disable persistence in client
-        this.rpcClient = new Client(signerContext, rpcClient, cryptoStore, entitlementsDelegate)
+        this.client = clientV2
+        this.botId = clientV2.userId
         this.server = new Hono()
         this.server.get('/webhook', (c) => {
             return c.text('')
@@ -106,66 +92,66 @@ class Bot extends (EventEmitter as new () => TypedEmitter<BotEvents>) {
     }
 
     async sendMessage(channelId: string, message: string) {
-        return this.rpcClient.sendMessage(channelId, message)
+        return this.client.sendMessage(channelId, message)
     }
 
     async sendReaction(channelId: string, refEventId: string, reaction: string) {
-        return this.rpcClient.sendChannelMessage_Reaction(channelId, { refEventId, reaction })
+        return this.client.sendReaction(channelId, refEventId, reaction)
     }
 
     async redactEvent(channelId: string, refEventId: string) {
-        return this.rpcClient.sendChannelMessage_Redaction(channelId, { refEventId })
+        return this.client.redactEvent(channelId, refEventId)
     }
 
-    onMessage(fn: (handler: Actions, opts: BasePayload & { message: string }) => void) {
+    onMessage(fn: (handler: BotActions, opts: BasePayload & { message: string }) => void) {
         this.on('message', fn)
     }
 
-    onRedact(fn: (handler: Actions, opts: BasePayload & { refEventId: string }) => void) {
+    onRedact(fn: (handler: BotActions, opts: BasePayload & { refEventId: string }) => void) {
         this.on('redact', fn)
     }
 
-    onMessageDelete(fn: (handler: Actions, opts: BasePayload & { eventId: string }) => void) {
+    onMessageDelete(fn: (handler: BotActions, opts: BasePayload & { eventId: string }) => void) {
         this.on('messageDelete', fn)
     }
 
-    onBotMention(fn: (handler: Actions, opts: BasePayload & { message: string }) => void) {
+    onBotMention(fn: (handler: BotActions, opts: BasePayload & { message: string }) => void) {
         this.on('botMention', fn)
     }
 
-    onReply(fn: (handler: Actions, opts: BasePayload & { message: string }) => void) {
+    onReply(fn: (handler: BotActions, opts: BasePayload & { message: string }) => void) {
         this.on('reply', fn)
     }
 
     onReaction(
         fn: (
-            handler: Actions,
+            handler: BotActions,
             opts: BasePayload & { reaction: string; messageId: string; userId: string },
         ) => void,
     ) {
         this.on('reaction', fn)
     }
 
-    onEventRevoke(fn: (handler: Actions, opts: BasePayload & { eventId: string }) => void) {
+    onEventRevoke(fn: (handler: BotActions, opts: BasePayload & { eventId: string }) => void) {
         this.on('eventRevoke', fn)
     }
 
-    onTip(fn: (handler: Actions, opts: BasePayload & { amount: bigint }) => void) {
+    onTip(fn: (handler: BotActions, opts: BasePayload & { amount: bigint }) => void) {
         this.on('tip', fn)
     }
 
-    onChannelJoin(fn: (handler: Actions, opts: BasePayload) => void) {
+    onChannelJoin(fn: (handler: BotActions, opts: BasePayload) => void) {
         this.on('channelJoin', fn)
     }
 
-    onChannelLeave(fn: (handler: Actions, opts: BasePayload) => void) {
+    onChannelLeave(fn: (handler: BotActions, opts: BasePayload) => void) {
         this.on('channelLeave', fn)
     }
 
-    onStreamMessage(fn: (handler: Actions, opts: BasePayload & { message: string }) => void) {
+    onStreamMessage(fn: (handler: BotActions, opts: BasePayload & { message: string }) => void) {
         this.on('streamMessage', fn)
     }
-    // onSlashCommand(command: Commands, fn: (client: Actions, opts: BasePayload) => void) {
+    // onSlashCommand(command: Commands, fn: (client: BotActions, opts: BasePayload) => void) {
     //     this.cb.onSlashCommand.set(command, fn)
     // }
 
@@ -178,59 +164,145 @@ class Bot extends (EventEmitter as new () => TypedEmitter<BotEvents>) {
         // - accepts a protobuf payload
         // - checks that the payload is signed by the bot-registry-server
         // - processes payload, returns BotWebookResponse
-        this.emit('streamMessage', this.clientToActions(this.rpcClient), {
-            userId: 'bot',
-            channelId: 'bot',
-            eventId: 'bot',
-            message: 'bot',
-        })
-    }
-    private clientToActions(client: Client): Actions {
-        return {
-            sendReaction: (channelId, messageId, reaction) =>
-                client.sendChannelMessage_Reaction(channelId, { refEventId: messageId, reaction }),
-            // TODO: hook replyTo
-            sendMessage: (channelId, message, opts) =>
-                client.sendChannelMessage_Text(channelId, {
-                    threadId: opts?.threadId,
-                    threadPreview: opts?.threadId ? '🙉' : undefined,
-                    replyId: opts?.replyId,
-                    replyPreview: opts?.replyId ? '🙈' : undefined,
-                    content: {
-                        body: message,
-                        mentions: opts?.mentions ?? [],
-                        attachments: opts?.attachments ?? [],
-                    },
-                }),
-            editMessage: (channelId, messageId, message) =>
-                client.sendChannelMessage_Edit_Text(channelId, messageId, {
-                    content: new ChannelMessage_Post_Content_Text({ body: message }),
-                }),
-            sendDm: (channelId, message, opts) =>
-                client.sendChannelMessage_Text(channelId, {
-                    threadId: opts?.threadId,
-                    threadPreview: opts?.threadId ? '🙉' : undefined,
-                    replyId: opts?.replyId,
-                    replyPreview: opts?.replyId ? '🙈' : undefined,
-                    content: {
-                        body: message,
-                        mentions: opts?.mentions ?? [],
-                        attachments: opts?.attachments ?? [],
-                    },
-                }),
-            // TODO: polish this one, since it interacts on-chain.
-            // should this call the transfer and submit the receipt?
-            // or should it only submit the receipt?
-            sendTip: (userId, eventId) => {},
-        }
+        // this.emit('streamMessage', this.client, {
+        //     userId: 'bot',
+        //     channelId: 'bot',
+        //     eventId: 'bot',
+        //     message: 'bot',
+        // })
     }
 }
 
-export const makeTownsBot = async (privateKey: string, config: RiverConfig) => {
-    const signerContext = await buildBotWallet(privateKey)
-    const riverProvider = makeRiverProvider(config)
-    const riverRegistryDapp = new RiverRegistry(config.river.chainConfig, riverProvider)
-    const urls = await riverRegistryDapp.getOperationalNodeUrls()
-    const rpcClient = makeStreamRpcClient(urls, riverRegistryDapp.getOperationalNodeUrls)
-    return new Bot(signerContext, rpcClient, config)
+export const makeTownsBot = async (
+    privateKey: string,
+    env: Parameters<typeof makeRiverConfig>[0],
+) => {
+    const client = await createTownsClient({
+        privateKey,
+        env,
+    }).then((x) => x.extend(botBotActions))
+    return new Bot(client)
+}
+
+const botBotActions = (client: ClientV2): BotActions => {
+    const sendMessageEvent = async ({
+        streamId,
+        payload,
+    }: {
+        streamId: string
+        payload: ChannelMessage
+    }) => {
+        const stream = await client.getStream(streamId)
+        const { hash: prevMiniblockHash } = await client.rpc.getLastMiniblockHash({
+            streamId: streamIdAsBytes(streamId),
+        })
+        // TODO: check entitlements. (should it be a extension?)
+        // if (isChannelStreamId(streamId)) {
+        // }
+        // TODO: tags
+        const tags = undefined
+        const encryptionAlgorithm = stream.snapshot.members?.encryptionAlgorithm?.algorithm
+
+        const message = await client.crypto.encryptGroupEvent(
+            streamId,
+            payload.toBinary(),
+            (encryptionAlgorithm as GroupEncryptionAlgorithmId) ||
+                client.defaultGroupEncryptionAlgorithm,
+        )
+        message.refEventId = getRefEventIdFromChannelMessage(payload)
+
+        let event: Envelope
+        if (isChannelStreamId(streamId)) {
+            event = await makeEvent(
+                client.signer,
+                make_ChannelPayload_Message(message),
+                prevMiniblockHash,
+                tags,
+            )
+        } else if (isDMChannelStreamId(streamId)) {
+            event = await makeEvent(
+                client.signer,
+                make_DMChannelPayload_Message(message),
+                prevMiniblockHash,
+                tags,
+            )
+        } else if (isGDMChannelStreamId(streamId)) {
+            event = await makeEvent(
+                client.signer,
+                make_GDMChannelPayload_Message(message),
+                prevMiniblockHash,
+                tags,
+            )
+        } else {
+            throw new Error(`Invalid stream ID type: ${streamId}`)
+        }
+        const eventId = bin_toHexString(event.hash)
+        const { error } = await client.rpc.addEvent({
+            streamId: streamIdAsBytes(streamId),
+            event,
+        })
+        return {
+            error,
+            eventId,
+            prevMiniblockHash,
+        }
+    }
+
+    const sendMessage: BotActions['sendMessage'] = async (streamId, message, opts) => {
+        const payload = new ChannelMessage({
+            payload: {
+                case: 'post',
+                value: {
+                    threadId: opts?.threadId,
+                    replyId: opts?.replyId,
+                    replyPreview: opts?.replyId ? '🙈' : undefined,
+                    threadPreview: opts?.threadId ? '🙉' : undefined,
+                    content: {
+                        case: 'text',
+                        value: {
+                            body: message,
+                            attachments: opts?.attachments || [],
+                            mentions: opts?.mentions || [],
+                        },
+                    },
+                },
+            },
+        })
+        return sendMessageEvent({ streamId, payload })
+    }
+    const editMessage: BotActions['editMessage'] = async (streamId, messageId, message) => {
+        const payload = new ChannelMessage({
+            payload: {
+                case: 'edit',
+                value: {
+                    refEventId: messageId,
+                    post: {
+                        content: { case: 'text', value: { body: message } },
+                    },
+                },
+            },
+        })
+        return sendMessageEvent({ streamId, payload })
+    }
+    const sendDm = sendMessage
+    const sendReaction: BotActions['sendReaction'] = async (streamId, messageId, reaction) => {
+        const payload = new ChannelMessage({
+            payload: { case: 'reaction', value: { refEventId: messageId, reaction } },
+        })
+        return sendMessageEvent({ streamId, payload })
+    }
+
+    const redactEvent: BotActions['redactEvent'] = async (streamId, messageId) => {
+        const payload = new ChannelMessage({
+            payload: { case: 'redaction', value: { refEventId: messageId } },
+        })
+        return sendMessageEvent({ streamId, payload })
+    }
+    return {
+        sendMessage,
+        editMessage,
+        sendDm,
+        sendReaction,
+        redactEvent,
+    }
 }
