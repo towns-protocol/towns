d9b35db10567b00bece027b7ba52f11d4f422991
Serge Khorun <serge@hntlabs.com>
2025-03-02 16:04:50 -0800

    Refactor QuorumPool to remove sync.Cond and use channels for task management.

--- core/node/events/quorum_pool.go
+++ core/node/events/quorum_pool.go
@@ -3,7 +3,6 @@ package events
 import (
 	"context"
 	"errors"
-	"sync"
 	"time"
 
 	"github.com/ethereum/go-ethereum/common"
@@ -75,18 +74,8 @@ type QuorumPool struct {
 
 	ctx context.Context
 
-	// totalTasks keeps track of how many remote tasks are submitted.
-	totalTasks int
-
-	// resultAvailable is called each time the result of a local or remote action was available.
-	// it guards totalSuccess and errors and is used to check if quorum is reached.
-	resultAvailable *sync.Cond
-
-	// totalSuccess keeps track how many local or remote tasks succeeded.
-	totalSuccess int
-
-	// errors captures all errors from local or remote tasks.
-	errors []error
+	tasks       []func()
+	taskResults chan error
 }
 
 // NewQuorumPool creates a new quorum pool.
@@ -96,21 +85,19 @@ func NewQuorumPool(ctx context.Context, opts *QuorumPoolOpts) *QuorumPool {
 	}
 
 	return &QuorumPool{
-		opts:            *opts,
-		ctx:             ctx,
-		resultAvailable: sync.NewCond(&sync.Mutex{}),
+		opts: *opts,
+		ctx:  ctx,
 	}
 }
 
 // AddTask executes f concurrently and captures the result for which the caller must wait.
 func (q *QuorumPool) AddTask(f func(ctx context.Context) error) {
-	q.totalTasks++
-
-	go func() {
+	q.tasks = append(q.tasks, func() {
 		ctx, cancel := q.opts.ContextMaker(q.ctx)
 		defer cancel()
-		q.onTaskFinished(common.Address{}, f(ctx))
-	}()
+		result := f(ctx)
+		q.onTaskFinished(common.Address{}, result)
+	})
 }
 
 // AddNodeTasks executes f on the given nodes concurrently and captures the results for which the caller must wait.
@@ -118,28 +105,17 @@ func (q *QuorumPool) AddNodeTasks(
 	nodes []common.Address,
 	f func(ctx context.Context, node common.Address) error,
 ) {
-	q.totalTasks += len(nodes)
-
 	for _, node := range nodes {
-		go func() {
+		q.tasks = append(q.tasks, func() {
 			ctx, cancel := q.opts.ContextMaker(q.ctx)
 			defer cancel()
-			q.onTaskFinished(node, f(ctx, node))
-		}()
+			result := f(ctx, node)
+			q.onTaskFinished(node, result)
+		})
 	}
 }
 
 func (q *QuorumPool) onTaskFinished(remote common.Address, err error) {
-	q.resultAvailable.L.Lock()
-	if err == nil {
-		q.totalSuccess++
-	} else {
-		q.errors = append(q.errors, err)
-	}
-
-	q.resultAvailable.Signal()
-	q.resultAvailable.L.Unlock()
-
 	// Cancel error is expected here: Wait() returns once quorum is achieved
 	// and some remotes are still in progress.
 	// Eventually Wait caller is going to cancel the context.
@@ -151,68 +127,95 @@ func (q *QuorumPool) onTaskFinished(remote common.Address, err error) {
 		tags = append(tags, q.opts.Tags...)
 		logging.FromCtx(q.ctx).Warnw("QuorumPool: Replica Error", tags...)
 	}
+
+	q.taskResults <- err
 }
 
-// Wait returns nil in case quorum is achieved, error otherwise.
-// It must be called after all local and remote tasks are submitted.
-func (q *QuorumPool) Wait() error {
-	quorumNum := TotalQuorumNum(q.totalTasks)
-	waitStarted := time.Now()
+type quorumState struct {
+	taskCount    int
+	quorumNum    int
+	successCount int
+	taskErrors   []error
+}
 
-	q.resultAvailable.L.Lock()
-	defer q.resultAvailable.L.Unlock()
-
-	for {
-		if q.opts.ExternalQuorumCheck == nil {
-			if q.totalSuccess >= quorumNum { // quorum achieved
-				if !q.opts.WaitForExtraSuccess {
-					return nil
-				}
-				return q.waitForExtraSuccessLocked(waitStarted)
-			}
-
-			maxAllowedErrors := q.totalTasks - quorumNum
-			if len(q.errors) > maxAllowedErrors { // not able to achieve quorum anymore
-				baseErrors := q.errors
-				q.errors = nil
-
-				return RiverErrorWithBases(Err_QUORUM_FAILED, "quorum failed", baseErrors,
-					"totalTasks", q.totalTasks,
-					"quorumNum", quorumNum,
-					"failed", len(baseErrors),
-					"succeeded", q.totalSuccess)
-			}
-		} else {
-			// determined if quorum is reached is done through external callback
-			if q.opts.ExternalQuorumCheck() {
-				return nil
-			}
-		}
-
-		// if no more results are expected and quorum hasn't been reached return error
-		if q.totalSuccess+len(q.errors) >= q.totalTasks {
-			baseErrors := q.errors
-			q.errors = nil
-
-			return RiverErrorWithBases(Err_QUORUM_FAILED, "quorum failed: no more results expected", baseErrors,
-				"totalTasks", q.totalTasks,
-				"quorumNum", quorumNum,
-				"failed", len(baseErrors),
-				"succeeded", q.totalSuccess)
-		}
-
-		// wait for more task results
-		q.resultAvailable.Wait()
+func (s *quorumState) update(err error) {
+	if err != nil {
+		s.taskErrors = append(s.taskErrors, err)
+	} else {
+		s.successCount++
 	}
 }
 
-func (q *QuorumPool) waitForExtraSuccessLocked(waitStarted time.Time) error {
-	// extraWaitTimestamp := time.Now()
-	// alreadyWaited := extraWaitTimestamp.Sub(waitStarted)
+func (s *quorumState) checkDoneNoExternal() (bool, error) {
+	// Is quorum achieved?
+	if s.successCount >= s.quorumNum {
+		return true, nil
+	}
 
-	// contextDeadline, ok := q.ctx.Deadline()
-	return nil
+	// Can still achieve quorum?
+	maxAllowedErrors := s.taskCount - s.quorumNum
+	if len(s.taskErrors) > maxAllowedErrors {
+		return true, RiverErrorWithBases(Err_QUORUM_FAILED, "quorum failed", s.taskErrors,
+			"totalTasks", s.taskCount,
+			"quorumNum", s.quorumNum,
+			"failed", len(s.taskErrors),
+			"succeeded", s.successCount)
+	}
 
+	return false, nil
+}
+
+func (s *quorumState) checkDoneExternal(check func() bool) (bool, error) {
+	// Is quorum achieved?
+	if check() {
+		return true, nil
+	}
+
+	// if no more results are expected and quorum hasn't been reached return error
+	if s.successCount+len(s.taskErrors) >= s.taskCount {
+		return true, RiverErrorWithBases(Err_QUORUM_FAILED, "quorum failed: no more results expected", s.taskErrors,
+			"totalTasks", s.taskCount,
+			"quorumNum", s.quorumNum,
+			"failed", len(s.taskErrors),
+			"succeeded", s.successCount)
+	}
+
+	return false, nil
+}
+
+func (s *quorumState) checkDone(check func() bool) (bool, error) {
+	if check == nil {
+		return s.checkDoneNoExternal()
+	} else {
+		return s.checkDoneExternal(check)
+	}
+}
+
+// Wait returns nil in case quorum is achieved, error otherwise.
+// It must be called after all local and remote tasks are added.
+func (q *QuorumPool) Wait() error {
+	state := &quorumState{
+		taskCount: len(q.tasks),
+		quorumNum: TotalQuorumNum(len(q.tasks)),
+	}
+	q.taskResults = make(chan error, state.taskCount)
+
+	for _, task := range q.tasks {
+		go task()
+	}
+
+	for {
+		select {
+		case err := <-q.taskResults:
+			state.update(err)
+			done, err := state.checkDone(q.opts.ExternalQuorumCheck)
+			if done {
+				return err
+			}
+		case <-q.ctx.Done():
+			return q.ctx.Err()
+		}
+	}
 }
 
 func TotalQuorumNum(totalNumNodes int) int {
