7adf5dd320b2f50d00b9f11ec8bd22b9c36ff70b
Erik Olsson <valross2@gmail.com>
2025-03-03 15:17:18 +0100

    style token transfers (#9078)

--- clients/web/app/src/components/MessageTimeIineItem/items/TokenTransfer.tsx
+++ clients/web/app/src/components/MessageTimeIineItem/items/TokenTransfer.tsx
@@ -1,15 +1,60 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import { TokenTransferEvent } from '@river-build/sdk'
-import { Box } from '@ui'
+import { bin_toHexString, bin_toString } from '@river-build/dlog'
+import { Box, Stack, Text } from '@ui'
+import { useCoinData } from '@components/TradingChart/useCoinData'
+import { formatUnits } from 'hooks/useBalance'
 
 type Props = {
     event: TokenTransferEvent
 }
 export const TokenTransfer = (props: Props) => {
     const { event } = props
+    const coinDataParams = useMemo(() => {
+        const chainId = event.transaction.receipt
+            ? event.transaction.receipt.chainId.toString()
+            : 'solana-mainnet'
+        const address =
+            chainId === 'solana-mainnet'
+                ? bin_toString(event.transfer.address)
+                : bin_toHexString(event.transfer.address)
+        return { address, chain: chainId }
+    }, [event])
+
+    const { data: coinData } = useCoinData(coinDataParams)
+
+    const text = useMemo(() => {
+        const formattedAmount = formatUnits(
+            BigInt(event.transfer.amount),
+            coinData?.token.decimals ?? 9,
+        )
+        if (event.transfer.isBuy) {
+            return `Bought ${formattedAmount} ${coinData?.token.symbol}`
+        }
+        return `Sold ${formattedAmount} ${coinData?.token.symbol}`
+    }, [event, coinData])
+
     return (
-        <Box paddingY="sm" color={event.transfer.isBuy ? 'greenBlue' : 'error'}>
-            {event.transfer.amount}
-        </Box>
+        <Stack horizontal>
+            <Box
+                shrink
+                grow={false}
+                justifyContent="start"
+                background={event.transfer.isBuy ? 'positiveSubtle' : 'negativeSubtle'}
+                paddingX="sm"
+                alignItems="start"
+                rounded="xs"
+            >
+                <Text
+                    // using separate vpadding here to avoid text clipping
+                    paddingBottom="sm"
+                    paddingTop="sm"
+                    color={event.transfer.isBuy ? 'greenBlue' : 'error'}
+                >
+                    {text}
+                </Text>
+            </Box>
+            <Box grow />
+        </Stack>
     )
 }
