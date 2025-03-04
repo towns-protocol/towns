3d066f0354bb68f099bb55d71c9da9baf61bef4d
Serge Khorun <serge@hntlabs.com>
2025-03-04 15:21:59 -0800

    "Remove unused SkipXlTest function from testutils/skip.go"

--- core/node/testutils/skip.go
+++ core/node/testutils/skip.go
@@ -10,9 +10,3 @@ func SkipFlakyTest(t *testing.T, msg string) {
 		t.Skip("Skipping flaky test", t.Name(), "(RIVER_TEST_ENABLE_FLAKY to enable)", msg)
 	}
 }
-
-func SkipXlTest(t *testing.T) {
-	if os.Getenv("RIVER_TEST_ENABLE_XL") == "" {
-		t.Skip("Skipping xl test", t.Name(), "(RIVER_TEST_ENABLE_XL to enable)")
-	}
-}
