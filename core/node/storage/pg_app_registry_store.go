2c7ab086209b1474ac84304df2364bcad6a19b3a
Crystal Lemire <crystal@hntlabs.com>
2025-03-04 18:36:04 -0800

    Cache implemented, skeleton code put in elsewhere.

--- core/node/storage/pg_app_registry_store.go
+++ core/node/storage/pg_app_registry_store.go
@@ -619,6 +619,10 @@ func (s *PostgresAppRegistryStore) enqueueUnsendableMessages(
 			unsendableAppIds = append(unsendableAppIds, appId)
 		}
 	}
+	unsendableAppIdStrings := make([]string, len(unsendableAppIds))
+	for i, appId := range unsendableAppIds {
+		unsendableAppIdStrings[i] = hex.EncodeToString(appId[:])
+	}
 
 	rows, err = tx.Query(
 		ctx,
