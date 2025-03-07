a7dce74a618d35af5f22b09a846cf6fb1c65141a
Crystal Lemire <crystal@hntlabs.com>
2025-03-07 14:09:05 -0800

    Self review, cleanup.

--- core/node/app_registry/service.go
+++ core/node/app_registry/service.go
@@ -234,7 +234,6 @@ waitLoop:
 			if err != nil {
 				continue
 			}
-			log.Debugw("Found user metadata stream record", "streamId", userMetadataStreamId, "appId", appId)
 			nodes := nodes.NewStreamNodesWithLock(stream.Nodes, common.Address{})
 			streamResponse, err := utils.PeerNodeRequestWithRetries(
 				ctx,
