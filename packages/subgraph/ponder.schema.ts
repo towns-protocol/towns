63f5934e6e2beb630599b93514f35242d7e24c77
g <5714678+giuseppecrj@users.noreply.github.com>
2025-03-09 16:34:29 -0400

    updates

--- packages/subgraph/ponder.schema.ts
+++ packages/subgraph/ponder.schema.ts
@@ -1,6 +1,6 @@
 import { onchainTable } from 'ponder'
 
-export const Space = onchainTable('spaces', (t) => ({
+export const space = onchainTable('spaces', (t) => ({
     id: t.hex().primaryKey(),
     owner: t.hex(),
     tokenId: t.bigint(),
--- packages/subgraph/src/index.ts
+++ packages/subgraph/src/index.ts
@@ -2,7 +2,7 @@ import { ponder } from 'ponder:registry'
 import schema from 'ponder:schema'
 
 ponder.on('CreateSpace:SpaceCreated', async ({ event, context }) => {
-    await context.db.insert(schema.Space).values({
+    await context.db.insert(schema.space).values({
         id: event.args.space,
         owner: event.args.owner,
         tokenId: event.args.tokenId,
