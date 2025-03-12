7c8c0852eebda77ae2af3f78233f7a3feb69e381
Theo Tillberg <ttillberg@gmail.com>
2025-03-12 20:47:05 +0100

    app: bring back combined wallet (towns-19296) (#9187)
    <img width="366" alt="image"
    src="https://github.com/user-attachments/assets/42b8414e-7270-480f-ae1b-c29e1fd50b55"
    />

--- clients/web/app/src/components/WalletButton/WalletButton.tsx
+++ clients/web/app/src/components/WalletButton/WalletButton.tsx
@@ -1,15 +1,64 @@
-import React, { useCallback } from 'react'
+import React, { useCallback, useMemo } from 'react'
 
-import { Box, Icon, Stack, Text } from '@ui'
-import { usePanelActions } from 'routes/layouts/hooks/usePanelActions'
-import { CHANNEL_INFO_PARAMS } from 'routes'
-import { Analytics } from 'hooks/useAnalytics'
-import { useTradingWalletAddresses } from '@components/Web3/Trading/useTradingWalletAddresses'
 import { useBalanceOnChain } from '@components/Web3/Trading/useBalanceOnChain'
-import { formatUnitsToFixedLength } from 'hooks/useBalance'
 import { useSolanaBalance } from '@components/Web3/Trading/useSolanaBalance'
+import { useTradingWalletAddresses } from '@components/Web3/Trading/useTradingWalletAddresses'
+import { Box, Icon, Stack, Text } from '@ui'
+import { Analytics } from 'hooks/useAnalytics'
+import { formatUnitsToFixedLength } from 'hooks/useBalance'
+import { CHANNEL_INFO_PARAMS } from 'routes'
+import { usePanelActions } from 'routes/layouts/hooks/usePanelActions'
+
+import { calculateTotalHoldingValueCents, formatCents } from '@components/Web3/Trading/tradingUtils'
+import { useTradingWallet } from '@components/Web3/Trading/useTradingWallet'
 
 export const WalletButton = () => {
+    const { closePanel, openPanel, isPanelOpen } = usePanelActions()
+    const isActive = isPanelOpen(CHANNEL_INFO_PARAMS.TRADING_WALLET)
+
+    const { data: chainWalletAssets } = useTradingWallet()
+    const totalHoldingValueCents = useMemo(() => {
+        return calculateTotalHoldingValueCents(chainWalletAssets)
+    }, [chainWalletAssets])
+
+    const onWalletClick = useCallback(() => {
+        if (isPanelOpen(CHANNEL_INFO_PARAMS.TRADING_WALLET)) {
+            closePanel()
+        } else {
+            Analytics.getInstance().track('clicked trading wallet')
+            openPanel(CHANNEL_INFO_PARAMS.TRADING_WALLET)
+        }
+    }, [closePanel, isPanelOpen, openPanel])
+
+    return (
+        <Stack
+            horizontal
+            hoverable
+            centerContent
+            cursor="pointer"
+            tooltip={isActive ? undefined : 'Towns Wallet'}
+            tooltipOptions={{ placement: 'horizontal' }}
+            padding="line"
+            background="level2"
+            alignSelf="center"
+            rounded="sm"
+            minWidth="x4"
+            height="x4"
+            data-testid="towns-wallet-button"
+            gap="sm"
+            onClick={onWalletClick}
+        >
+            <Icon size="square_sm" type="wallet" color={isActive ? 'default' : 'gray1'} />
+            {totalHoldingValueCents > 0 && (
+                <Text color="default" fontSize="sm" fontWeight="medium">
+                    {formatCents(totalHoldingValueCents)}
+                </Text>
+            )}
+        </Stack>
+    )
+}
+
+export const EthSolWalletButton = () => {
     const { closePanel, openPanel, isPanelOpen } = usePanelActions()
     const isActive = isPanelOpen(CHANNEL_INFO_PARAMS.TRADING_WALLET)
 
