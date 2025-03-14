0688c876444b862f0e775f70f7b829737f68ae73
Theo Tillberg <ttillberg@gmail.com>
2025-03-14 13:24:45 +0100

    app: percent input needs to be valid bigint (#9213)
    also better wysiwyg, round when blurring

--- clients/web/app/src/components/Web3/Trading/ui/PercentInputRadio.tsx
+++ clients/web/app/src/components/Web3/Trading/ui/PercentInputRadio.tsx
@@ -2,28 +2,50 @@ import React, { useCallback, useRef } from 'react'
 
 import { Box, Text, TextField } from '@ui'
 
+const validateValue = (value: string) => {
+    const parsed = Number(value)
+    return isNaN(parsed) ? '0' : Math.min(Math.max(Math.round(parsed), 0), 100).toString()
+}
+
+const percent = (
+    <Text color="peach" fontWeight="medium">
+        %
+    </Text>
+)
+
 export const PercentInputRadio = (props: { onChange: (value: number) => void }) => {
     const textRef = useRef<HTMLInputElement>(null)
     const onSelect = useCallback(() => {
         textRef.current?.focus()
     }, [])
+
+    const onBlur = useCallback((e: React.FocusEvent<HTMLInputElement>) => {
+        e.target.value = validateValue(e.target.value)
+    }, [])
+
+    const onChange = useCallback(
+        (e: React.ChangeEvent<HTMLInputElement>) => {
+            props.onChange(Number(e.target.value))
+        },
+        [props],
+    )
+
     return (
         <Box horizontal color="peach" onClick={onSelect}>
             <TextField
                 min={0}
                 max={100}
+                step={1}
                 ref={textRef}
                 type="number"
                 rounded="md"
                 height="x4"
                 background="level2"
                 placeholder="Custom"
-                before={
-                    <Text color="peach" fontWeight="medium">
-                        %
-                    </Text>
-                }
-                onChange={(e) => props.onChange(Number(e.target.value))}
+                before={percent}
+                color="peach"
+                onBlur={onBlur}
+                onChange={onChange}
             />
         </Box>
     )
