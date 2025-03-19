3f837f5553c676fd0f589b5a3becdbe566ed1554
Shuhui Luo <107524008+shuhuiluo@users.noreply.github.com>
2025-03-19 15:36:45 -0400

    refactor(contract): modularize `AlphaHelper` and enhance `InteractDia… (#2631)
    …mondCut`
    
    Refactored `AlphaHelper` to include modular utilities for core facet
    management, removal logic, and diamond cut execution. Simplified
    `InteractDiamondCut` by adopting these abstractions, enhancing clarity
    and reducing redundancy. Updated loop practices and deployment
    configurations to ensure consistency with the new structure.

--- contracts/scripts/interactions/helpers/AlphaHelper.sol
+++ contracts/scripts/interactions/helpers/AlphaHelper.sol
@@ -16,7 +16,7 @@ import {DiamondHelper} from "@river-build/diamond/scripts/common/helpers/Diamond
 // contracts
 import {Interaction} from "contracts/scripts/common/Interaction.s.sol";
 
-// note: struct fields must be in alphabetical order for the json parser to work
+/// @dev note: struct fields must be in alphabetical order for the json parser to work
 // see: https://book.getfoundry.sh/cheatcodes/parse-json
 struct DiamondFacetData {
   string chainName;
@@ -32,109 +32,134 @@ struct FacetData {
 }
 
 abstract contract AlphaHelper is Interaction, DiamondHelper, IDiamondLoupeBase {
-  function removeRemoteFacets(address deployer, address diamond) internal {
-    Facet[] memory facets = IDiamondLoupe(diamond).facets();
+  /// @notice Get addresses of core facets that should never be removed
+  /// @param diamond The diamond contract address
+  /// @return coreFacets An array of core facet addresses
+  function getCoreFacetAddresses(
+    address diamond
+  ) internal view returns (address[] memory coreFacets) {
+    coreFacets = new address[](5);
 
-    address diamondCut = IDiamondLoupe(diamond).facetAddress(
+    coreFacets[0] = IDiamondLoupe(diamond).facetAddress(
       IDiamondCut.diamondCut.selector
     );
-    address diamondLoupe = IDiamondLoupe(diamond).facetAddress(
+    coreFacets[1] = IDiamondLoupe(diamond).facetAddress(
       IDiamondLoupe.facets.selector
     );
-    address introspection = IDiamondLoupe(diamond).facetAddress(
+    coreFacets[2] = IDiamondLoupe(diamond).facetAddress(
       IERC165.supportsInterface.selector
     );
-    address ownable = IDiamondLoupe(diamond).facetAddress(
-      IERC173.owner.selector
-    );
-    address ownablePending = IDiamondLoupe(diamond).facetAddress(
+    coreFacets[3] = IDiamondLoupe(diamond).facetAddress(IERC173.owner.selector);
+    coreFacets[4] = IDiamondLoupe(diamond).facetAddress(
       IOwnablePending.currentOwner.selector
     );
+  }
+
+  /// @notice Check if an address is a core facet that should not be removed
+  /// @param facetAddress The facet address to check
+  /// @param coreFacets Array of core facet addresses
+  /// @return True if the address is a core facet
+  function isCoreFacet(
+    address facetAddress,
+    address[] memory coreFacets
+  ) internal pure returns (bool) {
+    for (uint256 i = 0; i < coreFacets.length; i++) {
+      if (facetAddress == coreFacets[i]) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  /// @notice Execute a diamond cut to remove facets
+  /// @param deployer The address that will execute the diamond cut
+  /// @param diamond The diamond contract address
+  function executeDiamondCut(address deployer, address diamond) internal {
+    vm.broadcast(deployer);
+    IDiamondCut(diamond).diamondCut(baseFacets(), address(0), "");
+    clearCuts();
+  }
+
+  /// @notice Remove all non-core facets from a diamond
+  /// @param deployer The address that will execute the diamond cut
+  /// @param diamond The diamond contract address
+  function removeRemoteFacets(address deployer, address diamond) internal {
+    Facet[] memory facets = IDiamondLoupe(diamond).facets();
+    address[] memory coreFacets = getCoreFacetAddresses(diamond);
 
     for (uint256 i; i < facets.length; i++) {
-      if (
-        facets[i].facet == diamondCut ||
-        facets[i].facet == diamondLoupe ||
-        facets[i].facet == introspection ||
-        facets[i].facet == ownable ||
-        facets[i].facet == ownablePending
-      ) {
-        info("Skipping facet: %s", facets[i].facet);
+      address facet = facets[i].facet;
+
+      if (isCoreFacet(facet, coreFacets)) {
+        info("Skipping core facet: %s", facet);
         continue;
       }
 
       addCut(
         FacetCut({
-          facetAddress: facets[i].facet,
+          facetAddress: facet,
           action: FacetCutAction.Remove,
           functionSelectors: facets[i].selectors
         })
       );
     }
 
-    vm.broadcast(deployer);
-    IDiamondCut(diamond).diamondCut(baseFacets(), address(0), "");
-
-    clearCuts();
+    executeDiamondCut(deployer, diamond);
   }
 
+  /// @notice Remove specific facets from a diamond by address
+  /// @param deployer The address that will execute the diamond cut
+  /// @param diamond The diamond contract address
+  /// @param facetAddresses Array of facet addresses to remove
   function removeRemoteFacetsByAddresses(
     address deployer,
     address diamond,
     address[] memory facetAddresses
   ) internal {
-    Facet[] memory facets = IDiamondLoupe(diamond).facets();
+    addCutsToRemove(diamond, facetAddresses);
+    executeDiamondCut(deployer, diamond);
+  }
 
-    address diamondCut = IDiamondLoupe(diamond).facetAddress(
-      IDiamondCut.diamondCut.selector
-    );
-    address diamondLoupe = IDiamondLoupe(diamond).facetAddress(
-      IDiamondLoupe.facets.selector
-    );
-    address introspection = IDiamondLoupe(diamond).facetAddress(
-      IERC165.supportsInterface.selector
-    );
-    address ownable = IDiamondLoupe(diamond).facetAddress(
-      IERC173.owner.selector
-    );
-    address ownablePending = IDiamondLoupe(diamond).facetAddress(
-      IOwnablePending.currentOwner.selector
-    );
+  /// @notice Add the facet cuts to remove from a diamond
+  /// @param diamond The diamond contract address
+  /// @param facetAddresses Array of facet addresses to remove
+  function addCutsToRemove(
+    address diamond,
+    address[] memory facetAddresses
+  ) internal {
+    Facet[] memory facets = IDiamondLoupe(diamond).facets();
+    address[] memory coreFacets = getCoreFacetAddresses(diamond);
 
     for (uint256 i; i < facets.length; i++) {
-      if (
-        facets[i].facet == diamondCut ||
-        facets[i].facet == diamondLoupe ||
-        facets[i].facet == introspection ||
-        facets[i].facet == ownable ||
-        facets[i].facet == ownablePending
-      ) {
-        info("Skipping facet: %s", facets[i].facet);
+      address facet = facets[i].facet;
+
+      if (isCoreFacet(facet, coreFacets)) {
         continue;
       }
 
-      bool shouldRemove = false;
-      for (uint256 j; j < facetAddresses.length; j++) {
-        if (facets[i].facet == facetAddresses[j]) {
-          shouldRemove = true;
-          break;
-        }
-      }
-
-      if (shouldRemove) {
+      if (shouldRemoveFacet(facet, facetAddresses)) {
         addCut(
           FacetCut({
-            facetAddress: facets[i].facet,
+            facetAddress: facet,
             action: FacetCutAction.Remove,
             functionSelectors: facets[i].selectors
           })
         );
       }
     }
+  }
 
-    vm.broadcast(deployer);
-    IDiamondCut(diamond).diamondCut(baseFacets(), address(0), "");
-
-    clearCuts();
+  /// @notice Check if a facet should be removed based on provided addresses
+  /// @param facet The facet address to check
+  /// @param facetAddresses Array of facet addresses to remove
+  /// @return True if the facet should be removed
+  function shouldRemoveFacet(
+    address facet,
+    address[] memory facetAddresses
+  ) internal pure returns (bool) {
+    for (uint256 j = 0; j < facetAddresses.length; j++) {
+      if (facet == facetAddresses[j]) return true;
+    }
+    return false;
   }
 }
