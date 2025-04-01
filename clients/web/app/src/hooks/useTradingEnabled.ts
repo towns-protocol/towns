98907dc02223953f858f1576e676c3c23b6aa86c
Erik Olsson <valross2@gmail.com>
2025-04-01 20:49:39 +0200

    enable trading for everyone on omega (#9348)

--- clients/web/app/src/hooks/useTradingEnabled.ts
+++ clients/web/app/src/hooks/useTradingEnabled.ts
@@ -1,49 +1,10 @@
-import { useMemo } from 'react'
-import { SpaceAddressFromSpaceId, useChannelContext, useTownsContext } from 'use-towns-client'
 import { env } from '../utils/environment'
 
-const useAllowedIds = () => {
-    const allowedIds = useMemo(() => {
-        const ids = env.VITE_TRADING_TOWNS ?? ''
-        return new Set(
-            ids
-                .replaceAll(' ', '')
-                .split(',')
-                .map((id) => id.toLowerCase()),
-        )
-    }, [])
-    return { allowedIds }
-}
-
 export const useShowWallet = () => {
-    const { spaces } = useTownsContext()
-    const { allowedIds } = useAllowedIds()
-    const showWallet = useMemo(() => {
-        if (env.VITE_RIVER_ENV !== 'omega') {
-            return false
-        }
-        const spaceIds = new Set(
-            spaces.map((space) => SpaceAddressFromSpaceId(space.id).toLowerCase()),
-        )
-        for (const id of allowedIds) {
-            if (spaceIds.has(id)) {
-                return true
-            }
-        }
-        return false
-    }, [spaces, allowedIds])
-    return { showWallet: showWallet || env.DEV }
+    return { showWallet: env.VITE_RIVER_ENV === 'omega' || env.DEV }
 }
 
 export const useIsTradingEnabledInCurrentSpace = () => {
-    const { spaceId } = useChannelContext()
-    const { allowedIds } = useAllowedIds()
-    const isTradingEnabled = useMemo(() => {
-        if (!spaceId) {
-            return false
-        }
-        const spaceAddress = SpaceAddressFromSpaceId(spaceId).toLowerCase()
-        return allowedIds.has(spaceAddress)
-    }, [spaceId, allowedIds])
+    const isTradingEnabled = env.VITE_RIVER_ENV === 'omega'
     return { isTradingEnabled: isTradingEnabled || env.DEV }
 }
