2de1c1cce6c6550ca695816f43f0563a87447e6c
rbhnt <roman@hntlabs.com>
2025-04-04 11:14:48 +0100

    Fixed migration

--- core/node/storage/migrations/000008_snapshot.up.sql
+++ core/node/storage/migrations/000008_snapshot.up.sql
@@ -13,15 +13,15 @@ DO $$
                 suffix = LPAD(TO_HEX(i), 2, '0');
 
                 -- Media streams
-                EXECUTE 'ALTER TABLE miniblocks_m' || suffix || ' ADD COLUMN snapshot BYTEA;';
+                EXECUTE 'ALTER TABLE miniblocks_m' || suffix || ' ADD COLUMN IF NOT EXISTS snapshot BYTEA;';
                 EXECUTE 'ALTER TABLE miniblocks_m' || suffix || ' ALTER COLUMN snapshot SET STORAGE EXTERNAL;';
-                EXECUTE 'ALTER TABLE miniblock_candidates_m' || suffix || ' ADD COLUMN snapshot BYTEA;';
+                EXECUTE 'ALTER TABLE miniblock_candidates_m' || suffix || ' ADD COLUMN IF NOT EXISTS snapshot BYTEA;';
                 EXECUTE 'ALTER TABLE miniblock_candidates_m' || suffix || ' ALTER COLUMN snapshot SET STORAGE EXTERNAL;';
 
                 -- Regular streams
-                EXECUTE 'ALTER TABLE miniblocks_r' || suffix || ' ADD COLUMN snapshot BYTEA;';
+                EXECUTE 'ALTER TABLE miniblocks_r' || suffix || ' ADD COLUMN IF NOT EXISTS snapshot BYTEA;';
                 EXECUTE 'ALTER TABLE miniblocks_r' || suffix || ' ALTER COLUMN snapshot SET STORAGE EXTERNAL;';
-                EXECUTE 'ALTER TABLE miniblock_candidates_r' || suffix || ' ADD COLUMN snapshot BYTEA;';
+                EXECUTE 'ALTER TABLE miniblock_candidates_r' || suffix || ' ADD COLUMN IF NOT EXISTS snapshot BYTEA;';
                 EXECUTE 'ALTER TABLE miniblock_candidates_r' || suffix || ' ALTER COLUMN snapshot SET STORAGE EXTERNAL;';
             END LOOP;
     END;
