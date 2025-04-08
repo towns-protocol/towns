2269df1b9f2582bbbc93a30abf622e58f00653e5
g <5714678+giuseppecrj@users.noreply.github.com>
2025-04-08 13:46:09 -0400

    rever changes to run

--- core/env/local/common/run.sh
+++ core/env/local/common/run.sh
@@ -4,4 +4,4 @@ cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")"
 
 # This file is copied into the run_files/${RUN_ENV} directory and then is executed from there.
 cd 00
-../bin/river_node --config ../common.yaml --config ../packages/contracts.env --config ../config.yaml  "$@"
+../bin/river_node --config ../common.yaml --config ../contracts.env --config ../config.yaml  "$@"
