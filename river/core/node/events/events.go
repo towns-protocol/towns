e608ca24a24a124605704486ba9464625868ea2c
bas-vk <bas-vk@users.noreply.github.com>
2025-04-10 08:48:45 +0200

    Merge river at 425fa74 (#9411)
    This merges the latest changes from the river repository at commit
    425fa74.
    
    commit 3f807a1e38223ae6374ba5ecc29f7099b4bab79f
    Author: Bas van Kervel <basvankervel@gmail.com>
    Date:   Thu Apr 10 08:32:37 2025 +0200
    
        Update river node tag to 425fa74
    
    commit 7c16758b097190b4068479779c36a5946b3d7b6b
    Author: Bas van Kervel <basvankervel@gmail.com>
    Date:   Thu Apr 10 08:30:44 2025 +0200
    
    ./pull_river.sh --prefix=river https://github.com/towns-protocol/towns
    main --squash
        
        On branch river_subtree_merge_425fa74
        Changes to be committed:
          (use "git restore --staged <file>..." to unstage)
                modified:   river/.github/CODEOWNERS
                modified:   river/core/node/app_registry/event_processor.go
                modified:   river/core/node/app_registry/testutil.go
                modified:   river/core/node/events/events.go
                modified:   river/core/node/events/stream.go
                modified:   river/core/node/events/stream_cache.go
                modified:   river/core/node/rpc/app_registry_test.go
                modified:   river/core/node/rpc/forwarder.go
                modified:   river/core/node/rpc/stream_replacement_test.go
                modified:   river/packages/contracts/foundry.toml
    modified:
    river/packages/contracts/scripts/deployments/diamonds/DeploySpaceFactory.s.sol
    new file:
    river/packages/contracts/scripts/deployments/facets/DeployFeatureManager.s.sol
    new file:
    river/packages/contracts/src/factory/facets/feature/FeatureConditionLib.sol
    new file:
    river/packages/contracts/src/factory/facets/feature/FeatureManagerFacet.sol
    new file:
    river/packages/contracts/src/factory/facets/feature/FeatureManagerLib.sol
    new file:
    river/packages/contracts/src/factory/facets/feature/IFeatureManagerFacet.sol
    modified:
    river/packages/contracts/src/spaces/facets/tipping/TippingBase.sol
    new file:
    river/packages/contracts/test/base/registry/FeatureManager.t.sol
    new file: river/packages/contracts/test/mocks/MockERC20Votes.sol
                modified:   river/packages/stress/esbuild.config.mjs
                modified:   river/packages/stress/package.json
                modified:   river/packages/stress/src/demo.ts
                new file:   river/packages/stress/src/notifications.ts

--- river/core/node/events/events.go
+++ river/core/node/events/events.go
@@ -342,13 +342,13 @@ func Make_ChannelPayload_Message(content string) *StreamEvent_ChannelPayload {
 	}
 }
 
-func Make_ChannelPayload_Message_WithSession(content string, sessionId string) *StreamEvent_ChannelPayload {
+func Make_ChannelPayload_Message_WithSessionBytes(content string, sessionIdBytes []byte) *StreamEvent_ChannelPayload {
 	return &StreamEvent_ChannelPayload{
 		ChannelPayload: &ChannelPayload{
 			Content: &ChannelPayload_Message{
 				Message: &EncryptedData{
-					Ciphertext: content,
-					SessionId:  sessionId,
+					Ciphertext:     content,
+					SessionIdBytes: sessionIdBytes,
 				},
 			},
 		},
--- river/core/node/events/stream.go
+++ river/core/node/events/stream.go
@@ -491,12 +491,13 @@ func (s *Stream) initFromGenesisLocked(
 	return nil
 }
 
-// GetViewIfLocal returns stream view if stream is local, nil if stream is not local,
+// GetViewIfLocalEx returns stream view if stream is local, nil if stream is not local,
 // and error if stream is local and failed to load.
 // If local storage is not initialized, it will wait for it to be initialized.
-// GetViewIfLocal is thread-safe.
-func (s *Stream) GetViewIfLocal(ctx context.Context) (*StreamView, error) {
-	view, isLocal := s.tryGetView()
+// If allowNoQuorum is true, it will return the view even if the local node is not in quorum.
+// GetViewIfLocalEx is thread-safe.
+func (s *Stream) GetViewIfLocalEx(ctx context.Context, allowNoQuorum bool) (*StreamView, error) {
+	view, isLocal := s.tryGetView(allowNoQuorum)
 	if !isLocal {
 		return nil, nil
 	}
@@ -516,6 +517,14 @@ func (s *Stream) GetViewIfLocal(ctx context.Context) (*StreamView, error) {
 	return view, nil
 }
 
+// GetViewIfLocal returns stream view if stream is local, nil if stream is not local,
+// and error if stream is local and failed to load.
+// If local storage is not initialized, it will wait for it to be initialized.
+// GetViewIfLocal is thread-safe.
+func (s *Stream) GetViewIfLocal(ctx context.Context) (*StreamView, error) {
+	return s.GetViewIfLocalEx(ctx, false)
+}
+
 // GetView returns stream view if stream is local, and error if stream is not local or failed to load.
 // If local storage is not initialized, it will wait for it to be initialized.
 // GetView is thread-safe.
@@ -532,11 +541,22 @@ func (s *Stream) GetView(ctx context.Context) (*StreamView, error) {
 
 // tryGetView returns StreamView if it's already loaded, or nil if it's not.
 // The second return value is true if the view is local.
+// If allowNoQuorum is true, it will return the view even if the local node is not in quorum.
 // tryGetView is thread-safe.
-func (s *Stream) tryGetView() (*StreamView, bool) {
+func (s *Stream) tryGetView(allowNoQuorum bool) (*StreamView, bool) {
 	s.mu.RLock()
 	defer s.mu.RUnlock()
-	isLocal := s.nodesLocked.IsLocalInQuorum() && s.local != nil
+	if s.local == nil {
+		return nil, false
+	}
+
+	isLocal := false
+	if !allowNoQuorum {
+		isLocal = s.nodesLocked.IsLocalInQuorum()
+	} else {
+		isLocal = s.nodesLocked.IsLocal()
+	}
+
 	if isLocal && s.getViewLocked() != nil {
 		s.maybeScrubLocked()
 		return s.getViewLocked(), true
