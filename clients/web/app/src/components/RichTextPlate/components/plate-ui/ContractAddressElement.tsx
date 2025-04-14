8b88f06e2efd8052431edab5b4048912204fbb30
Theo Tillberg <ttillberg@gmail.com>
2025-04-14 11:21:21 +0200

    add clipboard

--- clients/web/app/src/components/RichTextPlate/components/plate-ui/ContractAddressElement.tsx
+++ clients/web/app/src/components/RichTextPlate/components/plate-ui/ContractAddressElement.tsx
@@ -6,6 +6,8 @@ import { atoms } from 'ui/styles/atoms.css'
 import { ButtonSpinner } from 'ui/components/Spinner/ButtonSpinner'
 import { pillShimmerClass } from 'ui/styles/globals/shimmer.css'
 import { LoadingElementDataContext } from '@components/RichTextPlate/LoadingElementDataContext'
+import useCopyToClipboard from 'hooks/useCopyToClipboard'
+import { SECOND_MS } from 'data/constants'
 import { TContractAddressElement } from './autocomplete/types'
 
 export const ContractAddressElementWithoutPlate = ({
@@ -16,6 +18,13 @@ export const ContractAddressElementWithoutPlate = ({
 
     const { loadingAddresses } = useContext(LoadingElementDataContext)
     const isLoading = loadingAddresses.has(address)
+    const [, copy] = useCopyToClipboard()
+    const [copied, setCopied] = useState(false)
+    const onCopy = async () => {
+        await copy(address)
+        setCopied(true)
+        setTimeout(() => setCopied(false), SECOND_MS * 1)
+    }
     return (
         <Box
             horizontal
@@ -42,8 +51,17 @@ export const ContractAddressElementWithoutPlate = ({
             {isLoading ? (
                 <ButtonSpinner height="x1" />
             ) : (
-                <Box paddingTop="xxs">
-                    <Icon type="copy" display="inline-block" size="square_xxs" />
+                <Box
+                    paddingTop="xxs"
+                    cursor="pointer"
+                    color={copied ? 'positive' : 'gray2'}
+                    onClick={onCopy}
+                >
+                    {!copied ? (
+                        <Icon type="copy" display="inline-block" size="square_xxs" />
+                    ) : (
+                        <Icon type="check" display="inline-block" size="square_xxs" />
+                    )}
                 </Box>
             )}
             {children}
