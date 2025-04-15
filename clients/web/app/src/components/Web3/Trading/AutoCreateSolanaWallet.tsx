446ca9f809a6b2a74e39232e3182b68b9df6b467
Erik Olsson <valross2@gmail.com>
2025-04-15 09:20:23 +0200

    avoid logging solana creation err to datadog (#9436)

--- clients/web/app/src/components/Web3/Trading/AutoCreateSolanaWallet.tsx
+++ clients/web/app/src/components/Web3/Trading/AutoCreateSolanaWallet.tsx
@@ -2,17 +2,17 @@ import { useEffect } from 'react'
 import { useSolanaWallets } from '@privy-io/react-auth'
 
 export const AutoCreateSolanaWallet = () => {
-    const { wallets, createWallet } = useSolanaWallets()
+    const { wallets, createWallet, ready } = useSolanaWallets()
     useEffect(() => {
-        if (wallets.length === 0) {
+        if (wallets.length === 0 && ready) {
             createWallet()
                 .then(() => {
                     console.log('Created Solana Wallet')
                 })
                 .catch((error) => {
-                    console.error('Failed to create Solana Wallet', error)
+                    console.warn('Failed to create Solana Wallet', error)
                 })
         }
-    }, [wallets.length, createWallet])
+    }, [wallets.length, createWallet, ready])
     return null
 }
