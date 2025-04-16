73cc9c4135145d62be5dd4ae437d8bb08b602839
rbhnt <roman@hntlabs.com>
2025-04-16 09:50:23 +0100

    Linter

--- packages/sdk/src/signerContext.ts
+++ packages/sdk/src/signerContext.ts
@@ -46,7 +46,11 @@ export const checkDelegateSig = (params: {
         bin_equal(recoveredCreatorAddress, creatorAddress),
         'delegateSig does not match creatorAddress',
         Err.BAD_DELEGATE_SIG,
-        { creatorAddress: creatorAddress, recoveredCreatorAddress: recoveredCreatorAddress, params: params }
+        {
+            creatorAddress: creatorAddress,
+            recoveredCreatorAddress: recoveredCreatorAddress,
+            params: params,
+        },
     )
 }
 
