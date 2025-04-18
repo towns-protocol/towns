4085be5afb4717d81bc408c6558720321dc5139d
Evan Salzbrenner <evan.salzbrenner@gmail.com>
2025-04-18 10:47:56 -0700

    userops: use-deploy-base-contracts for ci (#9467)

--- scripts/deploy-config-base-only.sh
+++ scripts/deploy-config-base-only.sh
@@ -1,41 +1,8 @@
 #!/bin/bash
 set -euo pipefail
 
-export RIVER_ENV=local_multi
-
 pushd river/packages/contracts
-    make build
-
-    set -a
-    . .env.localhost
-    set +a
-
-    # see deploy-contracts.sh
-    # Space Architect
-    make clear-anvil-deployments context=$RIVER_ENV
-    make deploy-any-local context=$RIVER_ENV rpc=base_anvil type=diamonds contract=DeployBaseRegistry
-    make deploy-any-local context=$RIVER_ENV rpc=base_anvil type=utils contract=DeployProxyBatchDelegation
-    make deploy-any-local context=$RIVER_ENV rpc=base_anvil type=utils contract=DeployTownsBase
-    make deploy-any-local context=$RIVER_ENV rpc=base_anvil type=diamonds contract=DeploySpace
-    make deploy-any-local context=$RIVER_ENV rpc=base_anvil type=diamonds contract=DeploySpaceOwner
-    make deploy-facet-local context=$RIVER_ENV rpc=base_anvil contract=UserEntitlement
-    make deploy-any-local context=$RIVER_ENV rpc=base_anvil type=utils contract=DeployTieredLogPricingV2
-    make deploy-any-local context=$RIVER_ENV rpc=base_anvil type=utils contract=DeployTieredLogPricingV3
-    make deploy-facet-local context=$RIVER_ENV rpc=base_anvil contract=FixedPricing
-    make deploy-any-local context=$RIVER_ENV rpc=base_anvil type=diamonds contract=DeploySpaceFactory
-    make deploy-any-local context=$RIVER_ENV rpc=base_anvil type=diamonds contract=DeployRiverAirdrop
-    make interact-any-local context=$RIVER_ENV rpc=base_anvil contract=InteractPostDeploy
-    make interact-any-local context=$RIVER_ENV rpc=base_anvil contract=InteractSetDefaultUriLocalhost
-    make interact-any-local context=$RIVER_ENV rpc=base_anvil contract=InteractClaimCondition
-
-    # Utils
-    make deploy-any-local context=$RIVER_ENV rpc=base_anvil type=utils contract=DeployMember
-    make deploy-any-local context=$RIVER_ENV rpc=base_anvil type=utils contract=DeployMockNFT
-    make deploy-any-local context=$RIVER_ENV rpc=base_anvil type=utils contract=DeployEntitlementGatedExample
-
-    # Create destination directory if it doesn't exist
     mkdir -p ../generated/deployments/${RIVER_ENV}
-
     cp -r deployments/${RIVER_ENV} ../generated/deployments
 popd
 
