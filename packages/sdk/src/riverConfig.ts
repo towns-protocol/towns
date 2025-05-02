3051b7a2752ebb93ead05b364428e9ae79607643
Miguel Gomes <miguelgomes13@live.com>
2025-05-02 13:45:19 -0300

    add `getStreamMetadataUrl` in sdk (#2942)
    So we can use it in other places such as the bot framework.

--- packages/sdk/src/riverConfig.ts
+++ packages/sdk/src/riverConfig.ts
@@ -128,3 +128,22 @@ export function makeRiverConfig(inEnvironmentId?: string) {
     }
     return config
 }
+
+export const getStreamMetadataUrl = (environmentId: string) => {
+    switch (environmentId) {
+        case 'alpha':
+            return 'https://alpha.river.delivery'
+        case 'gamma':
+            return 'https://gamma.river.delivery'
+        case 'omega':
+            return 'https://river.delivery'
+        case 'delta':
+            return 'https://delta.river.delivery'
+        case 'local_multi':
+            return 'http://localhost:3002'
+        case 'local_multi_ne':
+            return 'http://localhost:3003'
+        default:
+            throw new Error(`No stream metadata url for environmentId ${environmentId}`)
+    }
+}
