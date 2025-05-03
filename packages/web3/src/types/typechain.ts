4a84532086398ced22885b5154366a74f0965f17
Evan Salzbrenner <15717901+salzbrenner@users.noreply.github.com>
2025-05-03 05:52:00 -0700

    typings feedback

--- packages/web3/src/types/typechain.ts
+++ packages/web3/src/types/typechain.ts
@@ -1,5 +1,4 @@
 import { Signer, providers } from 'ethers'
 
 export type Connect<I> = (address: string, signerOrProvider: Signer | providers.Provider) => I
-export type ConnectReturnType<C> = ReturnType<Connect<C>>
-export type ContractType<C extends Connect<unknown>> = ReturnType<ConnectReturnType<C>>
+export type ContractType<C extends Connect<unknown>> = C extends Connect<infer R> ? R : never
