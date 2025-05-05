58245c9ed7b884eeb065477b939db5155e502a03
Evan Salzbrenner <15717901+salzbrenner@users.noreply.github.com>
2025-05-05 13:50:45 -0700

    INodeRegistry__factory -> NodeRegistry__factory

--- packages/web3/src/river-registry/INodeRegistryShim.ts
+++ packages/web3/src/river-registry/INodeRegistryShim.ts
@@ -1,12 +1,12 @@
 import { ethers } from 'ethers'
 import { BaseContractShim } from '../BaseContractShim'
-import { INodeRegistry__factory } from '@towns-protocol/generated/dev/typings/factories/INodeRegistry__factory'
+import { NodeRegistry__factory } from '@towns-protocol/generated/dev/typings/factories/NodeRegistry__factory'
 import { ContractType } from '../types/typechain'
 
-export class INodeRegistryShim extends BaseContractShim<
-    ContractType<typeof INodeRegistry__factory.connect>
-> {
+const { abi, connect } = NodeRegistry__factory
+
+export class INodeRegistryShim extends BaseContractShim<typeof connect> {
     constructor(address: string, provider: ethers.providers.Provider) {
-        super(address, provider, INodeRegistry__factory.connect.bind(INodeRegistry__factory))
+        super(address, provider, connect, abi)
     }
 }
