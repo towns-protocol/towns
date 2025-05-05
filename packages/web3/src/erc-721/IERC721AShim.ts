3bfbd007ede6d50c6e3418539341ea978cd959b1
Evan Salzbrenner <15717901+salzbrenner@users.noreply.github.com>
2025-05-05 13:39:57 -0700

    IERC721AShim

--- packages/web3/src/erc-721/IERC721AShim.ts
+++ packages/web3/src/erc-721/IERC721AShim.ts
@@ -1,10 +1,11 @@
 import { ethers } from 'ethers'
 import { IERC721A__factory } from '@towns-protocol/generated/dev/typings/factories/IERC721A__factory'
-import { ContractType } from '../types/typechain'
 import { BaseContractShim } from '../BaseContractShim'
 
-export class IERC721AShim extends BaseContractShim<ContractType<typeof IERC721A__factory.connect>> {
+const { abi, connect } = IERC721A__factory
+
+export class IERC721AShim extends BaseContractShim<typeof connect> {
     constructor(address: string, provider: ethers.providers.Provider) {
-        super(address, provider, IERC721A__factory.connect.bind(IERC721A__factory))
+        super(address, provider, connect, abi)
     }
 }
