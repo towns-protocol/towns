510bc5024ac26d83837837ae4f059fc8e411c85d
Evan Salzbrenner <15717901+salzbrenner@users.noreply.github.com>
2025-05-05 13:39:38 -0700

    IRiverPointsShim

--- packages/web3/src/airdrop/IRiverPointsShim.ts
+++ packages/web3/src/airdrop/IRiverPointsShim.ts
@@ -1,12 +1,11 @@
 import { ethers } from 'ethers'
 import { BaseContractShim } from '../BaseContractShim'
-import { ContractType } from '../types/typechain'
 import { ITownsPoints__factory } from '@towns-protocol/generated/dev/typings/factories/ITownsPoints__factory'
 
-export class IRiverPointsShim extends BaseContractShim<
-    ContractType<typeof ITownsPoints__factory.connect>
-> {
+const { abi, connect } = ITownsPoints__factory
+
+export class IRiverPointsShim extends BaseContractShim<typeof connect> {
     constructor(address: string, provider: ethers.providers.Provider) {
-        super(address, provider, ITownsPoints__factory.connect.bind(ITownsPoints__factory))
+        super(address, provider, connect, abi)
     }
 }
