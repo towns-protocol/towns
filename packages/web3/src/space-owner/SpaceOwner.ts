341d71c655e717a02293c584fc7879bb507d6134
Evan Salzbrenner <15717901+salzbrenner@users.noreply.github.com>
2025-05-06 11:59:00 -0700

    web3: cache owner(), getSpaceInfo(), and getEntitlements()

--- packages/web3/src/space-owner/SpaceOwner.ts
+++ packages/web3/src/space-owner/SpaceOwner.ts
@@ -3,17 +3,103 @@ import { ethers } from 'ethers'
 import { ISpaceOwnerBase } from '@towns-protocol/generated/dev/typings/SpaceOwner'
 import { BaseContractShim } from '../BaseContractShim'
 import { SpaceOwner__factory } from '@towns-protocol/generated/dev/typings/factories/SpaceOwner__factory'
+import { Keyable } from '../cache/Keyable'
+import { SimpleCache } from '../cache/SimpleCache'
+import { TransactionOpts } from '../types/ContractTypes'
+import { Space } from '../space/Space'
+import { wrapTransaction } from '../space-dapp/wrapTransaction'
+
+export class GetSpaceInfo implements Keyable {
+    spaceId: string
+    constructor(spaceId: string) {
+        this.spaceId = spaceId
+    }
+    toKey(): string {
+        return `getSpaceInfo:${this.spaceId}`
+    }
+}
 
 export type { ISpaceOwnerBase }
 
 const { abi, connect } = SpaceOwner__factory
 
 export class SpaceOwner extends BaseContractShim<typeof connect> {
+    private readonly spaceInfoCache: SimpleCache<GetSpaceInfo, ISpaceOwnerBase.SpaceStructOutput>
+
     constructor(address: string, provider: ethers.providers.Provider) {
         super(address, provider, connect, abi)
+
+        this.spaceInfoCache = new SimpleCache({
+            ttlSeconds: 15 * 60,
+        })
+    }
+
+    public async getSpaceInfo(spaceId: string): Promise<ISpaceOwnerBase.SpaceStructOutput> {
+        return this.spaceInfoCache.executeUsingCache(new GetSpaceInfo(spaceId), async () =>
+            this.read.getSpaceInfo(spaceId),
+        )
     }
 
     public async getNumTotalSpaces(): Promise<ethers.BigNumber> {
         return this.read.totalSupply()
+    }
+
+    public async prepareUpdateSpaceInfo(args: {
+        spaceId: string
+        spaceAddress: string
+        name: string
+        uri: string
+        shortDescription: string
+        longDescription: string
+    }): Promise<{
+        args: {
+            spaceId: string
+            spaceAddress: string
+            name: string
+            uri: string
+            shortDescription: string
+            longDescription: string
+        }
+        encoded: string
+    }> {
+        this.spaceInfoCache.remove(new GetSpaceInfo(args.spaceId))
+        return {
+            args,
+            encoded: this.encodeFunctionData('updateSpaceInfo', [
+                args.spaceAddress,
+                args.name,
+                args.uri,
+                args.shortDescription,
+                args.longDescription,
+            ]),
+        }
+    }
+
+    public async updateSpaceInfo(args: {
+        spaceId: string
+        space: Space
+        name: string
+        uri: string
+        shortDescription: string
+        longDescription: string
+        signer: ethers.Signer
+        txnOpts?: TransactionOpts
+    }) {
+        const { space, ...rest } = args
+        const request = await this.prepareUpdateSpaceInfo({
+            ...rest,
+            spaceAddress: space.Address,
+        })
+        return wrapTransaction(
+            () =>
+                this.write(args.signer).updateSpaceInfo(
+                    request.args.spaceAddress,
+                    request.args.name,
+                    request.args.uri,
+                    request.args.shortDescription,
+                    request.args.longDescription,
+                ),
+            args.txnOpts,
+        )
     }
 }
--- packages/web3/src/space/IEntitlementsShim.ts
+++ packages/web3/src/space/IEntitlementsShim.ts
@@ -2,13 +2,42 @@ import { EntitlementsManager__factory } from '@towns-protocol/generated/dev/typi
 import { ethers } from 'ethers'
 import { IEntitlementsManagerBase } from '@towns-protocol/generated/dev/typings/IEntitlementsManager'
 import { BaseContractShim } from '../BaseContractShim'
+import { Keyable } from '../cache/Keyable'
+import { SimpleCache } from '../cache/SimpleCache'
 
 const { abi, connect } = EntitlementsManager__factory
 
 export type { IEntitlementsManagerBase as IEntitlementsBase }
 
+export class GetEntitlements implements Keyable {
+    spaceId: string
+    constructor(spaceId: string) {
+        this.spaceId = spaceId
+    }
+    toKey(): string {
+        return `getEntitlements:${this.spaceId}`
+    }
+}
+
 export class IEntitlementsShim extends BaseContractShim<typeof connect> {
+    private readonly getEntitlementsCache: SimpleCache<
+        GetEntitlements,
+        IEntitlementsManagerBase.EntitlementStructOutput[]
+    >
+
     constructor(address: string, provider: ethers.providers.Provider) {
         super(address, provider, connect, abi)
+
+        this.getEntitlementsCache = new SimpleCache({
+            ttlSeconds: 15 * 60,
+        })
+    }
+
+    public async getEntitlements(
+        spaceId: string,
+    ): Promise<IEntitlementsManagerBase.EntitlementStructOutput[]> {
+        return this.getEntitlementsCache.executeUsingCache(new GetEntitlements(spaceId), async () =>
+            this.read.getEntitlements(),
+        )
     }
 }
