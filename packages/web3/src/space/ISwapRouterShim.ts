541bf610c9e0e763739ff4d0651f864b9b9339ea
Shuhui Luo <107524008+shuhuiluo@users.noreply.github.com>
2025-05-06 22:50:14 -0400

    fix(web3): update `ISwapRouterShim` to use factory-based implementation
    Refactored `ISwapRouterShim` to leverage `ISwapRouter__factory` for ABI and connection logic, simplifying the implementation.

--- packages/web3/src/space/ISwapRouterShim.ts
+++ packages/web3/src/space/ISwapRouterShim.ts
@@ -1,15 +1,11 @@
-import {
-    ISwapRouter as LocalhostContract,
-    ISwapRouterInterface as LocalhostInterface,
-} from '@towns-protocol/generated/dev/typings/ISwapRouter'
-
-import { ethers } from 'ethers'
+import { ISwapRouter__factory } from '@towns-protocol/generated/dev/typings/factories/ISwapRouter__factory'
 import { BaseContractShim } from '../BaseContractShim'
+import { ethers } from 'ethers'
 
-import LocalhostAbi from '@towns-protocol/generated/dev/abis/ISwapRouter.abi.json' assert { type: 'json' }
+const { abi, connect } = ISwapRouter__factory
 
-export class ISwapRouterShim extends BaseContractShim<LocalhostContract, LocalhostInterface> {
-    constructor(address: string, provider: ethers.providers.Provider | undefined) {
-        super(address, provider, LocalhostAbi)
+export class ISwapRouterShim extends BaseContractShim<typeof connect> {
+    constructor(address: string, provider: ethers.providers.Provider) {
+        super(address, provider, connect, abi)
     }
 }
