7058e73ee3ac069a64efa951c7292d4c94730f53
Tomass <155266802+zeroprooff@users.noreply.github.com>
2025-05-08 15:29:03 +0200

    Update river_registry_contract.go

--- core/node/registries/river_registry_contract.go
+++ core/node/registries/river_registry_contract.go
@@ -23,9 +23,6 @@ import (
 	. "github.com/towns-protocol/towns/core/node/shared"
 )
 
-var streamRegistryABI, _ = river.StreamRegistryV1MetaData.GetAbi()
-
-// RiverRegistryContract is the convenience wrapper for the IRiverRegistryV1 interface (abigen exports it as RiverRegistryV1)
 // RiverRegistryContract is the convinience wrapper for the IRiverRegistryV1 interface (abigen exports it as RiverRegistryV1)
 type RiverRegistryContract struct {
 	OperatorRegistry *river.OperatorRegistryV1
