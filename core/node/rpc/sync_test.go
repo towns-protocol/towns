8c6973b8fcdf35d38a020a42bd69d662e9b0d74f
rbhnt <roman@hntlabs.com>
2025-05-12 12:24:23 +0100

    Added test

--- core/node/rpc/sync_test.go
+++ core/node/rpc/sync_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"reflect"
+	"slices"
 	"strings"
 	"testing"
 	"time"
@@ -245,6 +246,31 @@ func (sc *syncClients) expectNUpdates(
 
 				streamID, _ := StreamIdFromBytes(update.GetNextSyncCookie().GetStreamId())
 				uniqueStreamsWithUpdates[streamID] = struct{}{}
+			case <-timer.C:
+				t.Fatalf("Timeout waiting for update on client %d", i)
+				return
+			}
+		}
+	}
+}
+
+func (sc *syncClients) expectNStreamsDown(
+	t *testing.T,
+	streams []StreamId,
+	timeout time.Duration,
+) {
+	t.Helper()
+	timer := time.NewTimer(timeout)
+	defer timer.Stop()
+
+	for i, client := range sc.clients {
+		for range len(streams) {
+			select {
+			case update := <-client.downC:
+				if !slices.Contains(streams, update) {
+					t.Fatalf("Unexpected stream down on client %d: %v", i, update)
+					return
+				}
 			case <-timer.C:
 				t.Fatalf("Timeout waiting for update on client %d", i)
 				return
@@ -532,4 +558,79 @@ func TestSyncWithManyStreams(t *testing.T) {
 	// finish testing
 	syncClients.cancelAll(t, ctx)
 	syncClients.checkDone(t)
+}
+
+// TestRemoteNodeFailsDuringSync ensures that when a remote node fails during sync, the sync session is closed and
+// the client receives a stream down event for all streams on the failed node.
+func TestRemoteNodeFailsDuringSync(t *testing.T) {
+	numNodes := 3
+	tt := newServiceTester(t, serviceTesterOpts{numNodes: numNodes, start: true, replicationFactor: 2})
+	ctx := tt.ctx
+	require := tt.require
+
+	syncClients := makeSyncClients(tt, 1)
+	syncClient0 := syncClients.clients[0].client
+
+	wallet, err := crypto.NewWallet(ctx)
+	require.NoError(err)
+	resuser, _, err := createUser(ctx, wallet, syncClient0, nil)
+	require.NoError(err)
+	require.NotNil(resuser)
+
+	_, _, err = createUserMetadataStream(ctx, wallet, syncClient0, nil)
+	require.NoError(err)
+
+	// create space with 500 channels and add 1 event to each channel
+	spaceId := testutils.FakeStreamId(STREAM_SPACE_BIN)
+	_, _, err = createSpace(ctx, wallet, syncClient0, spaceId, &protocol.StreamSettings{})
+	require.NoError(err)
+
+	var channelCookies []*protocol.SyncCookie
+	nodeToStreams := make(map[common.Address][]StreamId)
+	for range 5 {
+		channelId := testutils.FakeStreamId(STREAM_CHANNEL_BIN)
+		channel, channelHash, err := createChannel(
+			ctx,
+			wallet,
+			syncClient0,
+			spaceId,
+			channelId,
+			&protocol.StreamSettings{DisableMiniblockCreation: true},
+		)
+		require.NoError(err)
+		require.NotNil(channel)
+		b0ref, err := makeMiniblock(ctx, syncClient0, channelId, false, -1)
+		require.NoError(err)
+		require.Equal(int64(0), b0ref.Num)
+		addMessageToChannel(ctx, syncClient0, wallet, "hello", StreamId(channel.StreamId), channelHash, require)
+		channelCookies = append(channelCookies, channel)
+
+		node := common.BytesToAddress(channel.NodeAddress)
+		nodeToStreams[node] = append(nodeToStreams[node], StreamId(channel.StreamId))
+	}
+
+	now := time.Now()
+	syncClients.startSyncMany(t, ctx, channelCookies)
+	syncClients.expectNUpdates(
+		t,
+		len(channelCookies),
+		30*time.Second,
+		&updateOpts{events: 1, eventType: "ChannelPayload"},
+	)
+	testfmt.Printf(
+		t,
+		"Received first update for all %d streams in init sync session took: %s",
+		len(channelCookies),
+		time.Since(now),
+	)
+
+	// simulate node failure
+	unavailableNodeAddress := tt.nodes[1].address
+	go tt.CloseNode(1)
+
+	syncClients.expectNStreamsDown(
+		t,
+		nodeToStreams[unavailableNodeAddress],
+		time.Minute,
+	)
 }
