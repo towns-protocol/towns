3b2f60b7e4abf326d8fd1b116a4127b675815fec
Erik Olsson <valross2@gmail.com>
2025-05-14 22:00:02 +0200

    add swapRouter address in IStaticContractsInfo

--- packages/web3/src/utils/IStaticContractsInfo.ts
+++ packages/web3/src/utils/IStaticContractsInfo.ts
@@ -9,6 +9,7 @@ export interface BaseChainConfig {
         spaceOwner: Address
         baseRegistry: Address
         riverAirdrop?: Address
+        swapRouter?: Address
         utils: {
             mockNFT?: Address // mockErc721aAddress
             member?: Address // testGatingTokenAddress - For tesing token gating scenarios
