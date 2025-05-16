e5115efb43cb61af0af593e290b8a0c315469729
rbhnt <roman@hntlabs.com>
2025-05-16 22:51:44 +0100

    Enable stream trimming for user setting stream

--- core/node/crypto/config_test.go
+++ core/node/crypto/config_test.go
@@ -146,6 +146,7 @@ func TestSetOnChain(t *testing.T) {
 	btc.SetConfigValue(t, ctx, StreamSnapshotIntervalInMiniblocksConfigKey, ABIEncodeUint64(1000))
 	btc.SetConfigValue(t, ctx, StreamDefaultStreamTrimmingMiniblocksToKeepConfigKey, ABIEncodeUint64(1000))
 	btc.SetConfigValue(t, ctx, StreamSpaceStreamTrimmingMiniblocksToKeepConfigKey, ABIEncodeUint64(999))
+	btc.SetConfigValue(t, ctx, StreamUserSettingStreamTrimmingMiniblocksToKeepConfigKey, ABIEncodeUint64(888))
 
 	s := btc.OnChainConfig.Get()
 	assert.EqualValues(3, s.ReplicationFactor)
@@ -158,6 +159,7 @@ func TestSetOnChain(t *testing.T) {
 	assert.Equal(uint64(1000), s.StreamSnapshotIntervalInMiniblocks)
 	assert.Equal(uint64(1000), s.StreamTrimmingMiniblocksToKeep.Default)
 	assert.Equal(uint64(999), s.StreamTrimmingMiniblocksToKeep.Space)
+	assert.Equal(uint64(888), s.StreamTrimmingMiniblocksToKeep.UserSetting)
 
 	btc.SetConfigValue(t, ctx, StreamReplicationFactorConfigKey, []byte("invalid value is ignored"))
 	assert.EqualValues(3, btc.OnChainConfig.Get().ReplicationFactor)
