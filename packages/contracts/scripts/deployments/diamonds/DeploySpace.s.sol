567485f6bfab1993e0283b0420b3a9b2e6b8c6d8
Shuhui Luo <107524008+shuhuiluo@users.noreply.github.com>
2025-05-17 02:12:15 -0400

    Refactor facet deployment logic in `DeploySpace` script
    Reorder facet deployment to clarify the flow and remove redundant deployment for the `AppAccount` facet. Improves code readability and ensures consistent deployment behavior.

--- packages/contracts/scripts/deployments/diamonds/DeploySpace.s.sol
+++ packages/contracts/scripts/deployments/diamonds/DeploySpace.s.sol
@@ -106,12 +106,12 @@ contract DeploySpace is IDiamondInitHelper, DiamondHelper, Deployer {
         facetHelper.add("MembershipMetadata");
         facetHelper.add("EntitlementDataQueryable");
         facetHelper.add("EntitlementsManager");
+        facetHelper.add("Roles");
+        facetHelper.add("Channels");
 
         // Deploy the first batch of facets
         facetHelper.deployBatch(deployer);
 
-        facetHelper.add("Roles");
-        facetHelper.add("Channels");
         facetHelper.add("TokenPausableFacet");
         facetHelper.add("PrepayFacet");
         facetHelper.add("ReferralsFacet");
@@ -245,7 +245,6 @@ contract DeploySpace is IDiamondInitHelper, DiamondHelper, Deployer {
             } else if (facetName.eq("Treasury")) {
                 addCut(DeployTreasury.makeCut(facet, IDiamond.FacetCutAction.Add));
             } else if (facetName.eq("AppAccount")) {
-                facet = facetHelper.deploy("AppAccount", deployer);
                 addCut(DeployAppAccount.makeCut(facet, IDiamond.FacetCutAction.Add));
             }
         }
