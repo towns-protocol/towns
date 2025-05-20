cd95aff50a138697ad127db37665c9c5f5ef7817
Theo Tillberg <ttillberg@gmail.com>
2025-05-21 01:28:22 +0200

    fix signature

--- packages/web3/src/space-dapp/SpaceDapp.ts
+++ packages/web3/src/space-dapp/SpaceDapp.ts
@@ -1941,7 +1941,7 @@ export class SpaceDapp {
         operatorAddress: `0x${string}`
         signer: ethers.Signer
         txnOpts?: TransactionOpts
-        overrideExecution?: (callData: string) => Promise<T>
+        overrideExecution?: ((calldata: string) => Promise<T>) | undefined
     }) {
         const { spaceId, operatorAddress, signer, txnOpts, overrideExecution } = args
         const space = this.getSpace(spaceId)
