84e48178fbfccb16f44bc87e6c9a6a62a914d8a1
Shuhui Luo <107524008+shuhuiluo@users.noreply.github.com>
2025-05-22 13:46:15 -0600

    refactor(contract): update operator status checks and rename related functions (#3139)
    The updates remove the on-chain operator registration from the base
    chain configuration process, shifting this logic into the
    post-deployment script. The post-deployment script now handles operator
    registration and approval. In the rewards distribution contract,
    operator validation is broadened to include both "Approved" and "Active"
    statuses.

--- packages/contracts/scripts/interactions/InteractPostDeploy.s.sol
+++ packages/contracts/scripts/interactions/InteractPostDeploy.s.sol
@@ -2,17 +2,16 @@
 pragma solidity ^0.8.23;
 
 // interfaces
-
-import {IImplementationRegistry} from "./../../src/factory/facets/registry/IImplementationRegistry.sol";
-
+import {IImplementationRegistry} from "../../src/factory/facets/registry/IImplementationRegistry.sol";
 import {SpaceDelegationFacet} from "src/base/registry/facets/delegation/SpaceDelegationFacet.sol";
 import {IMainnetDelegation} from "src/base/registry/facets/mainnet/IMainnetDelegation.sol";
 import {ISpaceOwner} from "src/spaces/facets/owner/ISpaceOwner.sol";
+import {INodeOperator} from "src/base/registry/facets/operator/INodeOperator.sol";
 
 // libraries
+import {NodeOperatorStatus} from "src/base/registry/facets/operator/NodeOperatorStorage.sol";
 
 // contracts
-
 import {MAX_CLAIMABLE_SUPPLY} from "./InteractClaimCondition.s.sol";
 import {Interaction} from "scripts/common/Interaction.s.sol";
 import {MockTowns} from "test/mocks/MockTowns.sol";
@@ -24,6 +23,9 @@ import {DeployTownsBase} from "scripts/deployments/utils/DeployTownsBase.s.sol";
 contract InteractPostDeploy is Interaction {
     DeployProxyBatchDelegation deployProxyDelegation = new DeployProxyBatchDelegation();
     DeployTownsBase deployTownsBase = new DeployTownsBase();
+
+    address internal constant OPERATOR = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266;
+    address internal constant STAKER = 0x70997970C51812dc3A010C7d01b50e0d17dc79C8;
 
     function __interact(address deployer) internal override {
         vm.pauseGasMetering();
@@ -37,14 +39,18 @@ contract InteractPostDeploy is Interaction {
 
         // this is for anvil deployment only
         vm.startBroadcast(deployer);
-        // this is for anvil deployment only
         MockTowns(townsBase).localMint(riverAirdrop, MAX_CLAIMABLE_SUPPLY);
+        MockTowns(townsBase).localMint(STAKER, MAX_CLAIMABLE_SUPPLY);
         ISpaceOwner(spaceOwner).setFactory(spaceFactory);
         IImplementationRegistry(spaceFactory).addImplementation(baseRegistry);
         IImplementationRegistry(spaceFactory).addImplementation(riverAirdrop);
         IImplementationRegistry(spaceFactory).addImplementation(appRegistry);
         SpaceDelegationFacet(baseRegistry).setRiverToken(townsBase);
         IMainnetDelegation(baseRegistry).setProxyDelegation(proxyDelegation);
+
+        INodeOperator operatorFacet = INodeOperator(baseRegistry);
+        operatorFacet.registerOperator(OPERATOR);
+        operatorFacet.setOperatorStatus(OPERATOR, NodeOperatorStatus.Approved);
         vm.stopBroadcast();
     }
 }
