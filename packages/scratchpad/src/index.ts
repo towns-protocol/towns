cc2964a49964cdee0a9592781795df185657d124
Miguel Nascimento <miguelgomes13@live.com>
2025-05-25 17:46:08 -0300

    scratchpad package to test

--- packages/scratchpad/src/index.ts
+++ packages/scratchpad/src/index.ts
@@ -1,10 +1,6 @@
-/* eslint-disable no-console */
-import 'fake-indexeddb/auto'
 import { makeRiverConfig, makeSignerContextFromBearerToken, SyncAgent } from '@towns-protocol/sdk'
 
-const signerContext = await makeSignerContextFromBearerToken(
-    '0a423078393038376538383763393964346363636362383963663762336336363863383963616235616365383866333164613364346134333833656463356662383938301241e983cb1279b19d745f7774c5995818f3a005a9ec89fc235975b3b4d2ea7662e50c2a97c71015c9e760540d159c75ced829f8ee67d071ef904dfeeeef54e504251b18b9a1d6e7f232',
-)
+const signerContext = await makeSignerContextFromBearerToken('<bearer-token>')
 const syncAgent = new SyncAgent({ riverConfig: makeRiverConfig('omega'), context: signerContext })
 await syncAgent.start()
 
