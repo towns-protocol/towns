eb65494f668bb7bb79f9cd322035cd3a18b59afe
Bas van Kervel <basvankervel@gmail.com>
2025-05-27 17:26:30 +0200

    node: implement new strategy for choosing stream nodes

--- core/node/crypto/config.go
+++ core/node/crypto/config.go
@@ -57,6 +57,10 @@ const (
 	StreamSpaceStreamTrimmingMiniblocksToKeepConfigKey       = "stream.streamTrimmingMiniblocksToKeep.10"
 	StreamUserSettingStreamTrimmingMiniblocksToKeepConfigKey = "stream.streamTrimmingMiniblocksToKeep.a5"
 	StreamEnableNewSnapshotFormatConfigKey                   = "stream.enableNewSnapshotFormat"
+
+	// StreamDistributionCandidatesCountKey is the key for the number of candidate nodes to pick the best nodes from
+	// when choosing stream nodes.
+	StreamDistributionCandidatesCountKey = "stream.distribution.candidatescount"
 )
 
 var (
@@ -136,6 +140,8 @@ type OnChainSettings struct {
 	// StreamTrimmingMiniblocksToKeep is the number of miniblocks to keep before the last snapshot.
 	// Defined with the default value and per stream type.
 	StreamTrimmingMiniblocksToKeep StreamTrimmingMiniblocksToKeepSettings `mapstructure:",squash"`
+	// StreamDistribution holds settings for the stream distribution algorithm.
+	StreamDistribution StreamDistribution `mapstructure:",squash"`
 }
 
 type XChainSettings struct {
@@ -196,6 +202,12 @@ func (m StreamTrimmingMiniblocksToKeepSettings) ForType(streamType byte) uint64 
 	default:
 		return m.Default
 	}
+}
+
+// StreamDistribution holds settings for the stream distribution algorithm.
+type StreamDistribution struct {
+	// CandidatesCount is the number of candidate nodes to pick the best nodes from.
+	CandidatesCount uint64 `mapstructure:"stream.distribution.candidatescount"`
 }
 
 func DefaultOnChainSettings() *OnChainSettings {
