d7bae0152f94e52142cfdb144a7625966ba057af
rbhnt <roman@hntlabs.com>
2025-05-28 15:38:19 +0100

    Update packages/contracts/scripts/interactions/InteractEnableNewSnapshotFormat.sol
    Co-authored-by: graphite-app[bot] <96075541+graphite-app[bot]@users.noreply.github.com>

--- packages/contracts/scripts/interactions/InteractEnableNewSnapshotFormat.sol
+++ packages/contracts/scripts/interactions/InteractEnableNewSnapshotFormat.sol
@@ -10,17 +10,17 @@ import {IRiverConfig} from "src/river/registry/facets/config/IRiverConfig.sol";
 import {Interaction} from "scripts/common/Interaction.s.sol";
 
 contract InteractEnableNewSnapshotFormat is Interaction {
-  function __interact(address deployer) internal override {
-    address riverRegistry = getDeployment("riverRegistry");
+function __interact(address deployer) internal override {
+  address riverRegistry = getDeployment("riverRegistry");
 
-    uint64 value = 1;
+  uint64 value = 1;
 
-    vm.startBroadcast(deployer);
-    IRiverConfig(riverRegistry).setConfiguration(
-      keccak256("stream.enableNewSnapshotFormat"),
-      0,
-      abi.encode(value)
-    );
-    vm.stopBroadcast();
-  }
+  vm.startBroadcast(deployer);
+  IRiverConfig(riverRegistry).setConfiguration(
+    keccak256("stream.enableNewSnapshotFormat"),
+    0,
+    abi.encode(value)
+  );
+  vm.stopBroadcast();
+}
 }
