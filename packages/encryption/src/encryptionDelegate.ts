a8a7491738946505e4654a6b18743275c511b431
Miguel Nascimento <miguelgomes13@live.com>
2025-05-29 15:49:51 -0300

    bump towns-protocol/olm to 3.2.27

--- packages/encryption/src/encryptionDelegate.ts
+++ packages/encryption/src/encryptionDelegate.ts
@@ -1,4 +1,4 @@
-import Olm from '@towns-protocol/olm'
+import Olm, { type OlmImpl } from '@towns-protocol/olm'
 import {
     Account,
     InboundGroupSession,
@@ -10,81 +10,72 @@ import {
     Utility,
 } from './encryptionTypes'
 
-type OlmLib = typeof Olm
-
 export class EncryptionDelegate {
-    private delegate: OlmLib | undefined
-    private _initialized = false
-
-    public get initialized(): boolean {
-        return this._initialized
-    }
+    private delegate: OlmImpl | undefined
 
     // eslint-disable-next-line @typescript-eslint/no-empty-function
     constructor() {}
 
     public async init(): Promise<void> {
         // initializes Olm library. This should run before using any Olm classes.
-        if (this._initialized) {
+        if (this.delegate) {
             return
         }
 
-        await Olm.initAsync()
-        this.delegate = Olm
-        this._initialized = typeof this.delegate.get_library_version === 'function'
+        this.delegate = await Olm.initAsync()
     }
 
     public createAccount(): Account {
-        if (!this._initialized || !this.delegate) {
+        if (!this.delegate) {
             throw new Error('olm not initialized')
         }
         return new this.delegate.Account()
     }
 
     public createSession(): Session {
-        if (!this._initialized || !this.delegate) {
+        if (!this.delegate) {
             throw new Error('olm not initialized')
         }
         return new this.delegate.Session()
     }
 
     public createInboundGroupSession(): InboundGroupSession {
-        if (!this._initialized || !this.delegate) {
+        if (!this.delegate) {
             throw new Error('olm not initialized')
         }
         return new this.delegate.InboundGroupSession()
     }
 
     public createOutboundGroupSession(): OutboundGroupSession {
-        if (!this._initialized || !this.delegate) {
+        if (!this.delegate) {
             throw new Error('olm not initialized')
         }
         return new this.delegate.OutboundGroupSession()
     }
 
     public createPkEncryption(): PkEncryption {
-        if (!this._initialized || !this.delegate) {
+        if (!this.delegate) {
             throw new Error('olm not initialized')
         }
         return new this.delegate.PkEncryption()
     }
 
     public createPkDecryption(): PkDecryption {
-        if (!this._initialized || !this.delegate) {
+        if (!this.delegate) {
             throw new Error('olm not initialized')
         }
         return new this.delegate.PkDecryption()
     }
 
     public createPkSigning(): PkSigning {
-        if (!this._initialized || !this.delegate) {
+        if (!this.delegate) {
             throw new Error('olm not initialized')
         }
         return new this.delegate.PkSigning()
     }
 
     public createUtility(): Utility {
-        if (!this._initialized || !this.delegate) {
+        if (!this.delegate) {
             throw new Error('olm not initialized')
         }
         return new this.delegate.Utility()
