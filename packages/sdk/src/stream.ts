dab96b2979c196b441622b03ff2b41b955e53ac2
texuf <texuf.eth@gmail.com>
2025-05-29 17:23:46 -0700

    Remove the extra IStreamStateView interface (#3191)
    i ended up going a different way with this and no longer need the
    abstraction.

--- packages/sdk/src/stream.ts
+++ packages/sdk/src/stream.ts
@@ -2,7 +2,7 @@ import { ChannelMessage, MembershipOp, Snapshot, SyncCookie } from '@towns-proto
 import { DLogger } from '@towns-protocol/dlog'
 import EventEmitter from 'events'
 import TypedEmitter from 'typed-emitter'
-import { IStreamStateView, StreamStateView } from './streamStateView'
+import { StreamStateView } from './streamStateView'
 import {
     LocalEventStatus,
     ParsedEvent,
@@ -19,7 +19,7 @@ export class Stream extends (EventEmitter as new () => TypedEmitter<StreamEvents
     readonly logEmitFromStream: DLogger
     readonly userId: string
     _view: StreamStateView
-    get view(): IStreamStateView {
+    get view(): StreamStateView {
         return this._view
     }
     private stopped = false
