3798da83df37b7adee4e3218e9ee98fd5a95428a
John Terzis <john@hntlabs.com>
2025-06-16 13:46:07 -0700

    Fix: Subgraph enable cache across envs (#3346)
    Enables cache such that if subgraph pod restarts (happens often),
    subgraph will quickly attempt to rebuild instance on new pid from cache
    versus re-indexing from start block, which takes >12 hours in omega.

--- packages/subgraph/ponder.config.gamma.ts
+++ packages/subgraph/ponder.config.gamma.ts
@@ -51,12 +51,12 @@ export default createConfig({
         anvil: {
             id: 31337,
             rpc: http(process.env.PONDER_RPC_URL_1),
-            disableCache: true,
+            disableCache: false,
         },
         gamma: {
             id: 84532,
             rpc: http(process.env.PONDER_RPC_URL_1),
-            disableCache: true,
+            disableCache: false,
         },
     },
     contracts: {
