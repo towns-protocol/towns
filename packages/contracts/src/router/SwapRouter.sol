56d00d5d01590cfd08235d6d06742c7cc261e4a7
Shuhui Luo <107524008+shuhuiluo@users.noreply.github.com>
2025-06-23 14:27:38 -0400

    fmt

--- packages/contracts/src/router/SwapRouter.sol
+++ packages/contracts/src/router/SwapRouter.sol
@@ -175,7 +175,10 @@ contract SwapRouter is PausableBase, ReentrancyGuardTransient, ISwapRouter, Face
     /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
 
     /// @inheritdoc ISwapRouter
-    function getPermit2Nonce(address owner, uint256 startNonce) external view returns (uint256 nonce) {
+    function getPermit2Nonce(
+        address owner,
+        uint256 startNonce
+    ) external view returns (uint256 nonce) {
         unchecked {
             uint256 bitPos = startNonce & 0xff;
 
