36f1bcd1d22c87f4526b2d363167c983a0df5e19
g <5714678+giuseppecrj@users.noreply.github.com>
2025-06-24 15:28:00 -0400

    feat(ABI): add new events and errors for app management across multiple facets
    ### Changes
    - Introduced `AppBanned`, `AppCreated`, and `AppInstalled` events to enhance app management tracking.
    - Added corresponding error types: `UnauthorizedApp`, `UnauthorizedCall`, `UnauthorizedCancel`, `UnauthorizedRenounce`, `UnauthorizedSelector`, and `UnauthorizedTarget` for better permission handling.
    - Updated ABI and TypeScript typings for `Channels`, `EntitlementsManager`, `MembershipFacet`, `PrepayFacet`, and `Roles` to reflect these changes.
    
    ### Checklist
    - [x] ABI files updated
    - [x] TypeScript typings updated
    - [x] Changes adhere to the repository's contribution guidelines

--- packages/generated/dev/typings/factories/PrepayFacet__factory.ts
+++ packages/generated/dev/typings/factories/PrepayFacet__factory.ts
@@ -61,6 +61,189 @@ const _abi = [
   },
   {
     type: "event",
+    name: "AppBanned",
+    inputs: [
+      {
+        name: "app",
+        type: "address",
+        indexed: true,
+        internalType: "address",
+      },
+      {
+        name: "uid",
+        type: "bytes32",
+        indexed: false,
+        internalType: "bytes32",
+      },
+    ],
+    anonymous: false,
+  },
+  {
+    type: "event",
+    name: "AppCreated",
+    inputs: [
+      {
+        name: "app",
+        type: "address",
+        indexed: true,
+        internalType: "address",
+      },
+      {
+        name: "uid",
+        type: "bytes32",
+        indexed: false,
+        internalType: "bytes32",
+      },
+    ],
+    anonymous: false,
+  },
+  {
+    type: "event",
+    name: "AppInstalled",
+    inputs: [
+      {
+        name: "app",
+        type: "address",
+        indexed: true,
+        internalType: "address",
+      },
+      {
+        name: "account",
+        type: "address",
+        indexed: true,
+        internalType: "address",
+      },
+      {
+        name: "appId",
+        type: "bytes32",
+        indexed: true,
+        internalType: "bytes32",
+      },
+    ],
+    anonymous: false,
+  },
+  {
+    type: "event",
+    name: "AppRegistered",
+    inputs: [
+      {
+        name: "app",
+        type: "address",
+        indexed: true,
+        internalType: "address",
+      },
+      {
+        name: "uid",
+        type: "bytes32",
+        indexed: false,
+        internalType: "bytes32",
+      },
+    ],
+    anonymous: false,
+  },
+  {
+    type: "event",
+    name: "AppRenewed",
+    inputs: [
+      {
+        name: "app",
+        type: "address",
+        indexed: true,
+        internalType: "address",
+      },
+      {
+        name: "account",
+        type: "address",
+        indexed: true,
+        internalType: "address",
+      },
+      {
+        name: "appId",
+        type: "bytes32",
+        indexed: true,
+        internalType: "bytes32",
+      },
+    ],
+    anonymous: false,
+  },
+  {
+    type: "event",
+    name: "AppSchemaSet",
+    inputs: [
+      {
+        name: "uid",
+        type: "bytes32",
+        indexed: false,
+        internalType: "bytes32",
+      },
+    ],
+    anonymous: false,
+  },
+  {
+    type: "event",
+    name: "AppUninstalled",
+    inputs: [
+      {
+        name: "app",
+        type: "address",
+        indexed: true,
+        internalType: "address",
+      },
+      {
+        name: "account",
+        type: "address",
+        indexed: true,
+        internalType: "address",
+      },
+      {
+        name: "appId",
+        type: "bytes32",
+        indexed: true,
+        internalType: "bytes32",
+      },
+    ],
+    anonymous: false,
+  },
+  {
+    type: "event",
+    name: "AppUnregistered",
+    inputs: [
+      {
+        name: "app",
+        type: "address",
+        indexed: true,
+        internalType: "address",
+      },
+      {
+        name: "uid",
+        type: "bytes32",
+        indexed: false,
+        internalType: "bytes32",
+      },
+    ],
+    anonymous: false,
+  },
+  {
+    type: "event",
+    name: "AppUpdated",
+    inputs: [
+      {
+        name: "app",
+        type: "address",
+        indexed: true,
+        internalType: "address",
+      },
+      {
+        name: "uid",
+        type: "bytes32",
+        indexed: false,
+        internalType: "bytes32",
+      },
+    ],
+    anonymous: false,
+  },
+  {
+    type: "event",
     name: "Approval",
     inputs: [
       {
@@ -161,6 +344,144 @@ const _abi = [
   },
   {
     type: "event",
+    name: "GroupAccessGranted",
+    inputs: [
+      {
+        name: "groupId",
+        type: "bytes32",
+        indexed: true,
+        internalType: "bytes32",
+      },
+      {
+        name: "account",
+        type: "address",
+        indexed: true,
+        internalType: "address",
+      },
+      {
+        name: "delay",
+        type: "uint32",
+        indexed: false,
+        internalType: "uint32",
+      },
+      {
+        name: "since",
+        type: "uint48",
+        indexed: false,
+        internalType: "uint48",
+      },
+      {
+        name: "newMember",
+        type: "bool",
+        indexed: false,
+        internalType: "bool",
+      },
+    ],
+    anonymous: false,
+  },
+  {
+    type: "event",
+    name: "GroupAccessRevoked",
+    inputs: [
+      {
+        name: "groupId",
+        type: "bytes32",
+        indexed: true,
+        internalType: "bytes32",
+      },
+      {
+        name: "account",
+        type: "address",
+        indexed: true,
+        internalType: "address",
+      },
+      {
+        name: "revoked",
+        type: "bool",
+        indexed: false,
+        internalType: "bool",
+      },
+    ],
+    anonymous: false,
+  },
+  {
+    type: "event",
+    name: "GroupExpirationSet",
+    inputs: [
+      {
+        name: "groupId",
+        type: "bytes32",
+        indexed: true,
+        internalType: "bytes32",
+      },
+      {
+        name: "expiration",
+        type: "uint48",
+        indexed: false,
+        internalType: "uint48",
+      },
+    ],
+    anonymous: false,
+  },
+  {
+    type: "event",
+    name: "GroupGrantDelaySet",
+    inputs: [
+      {
+        name: "groupId",
+        type: "bytes32",
+        indexed: true,
+        internalType: "bytes32",
+      },
+      {
+        name: "delay",
+        type: "uint32",
+        indexed: false,
+        internalType: "uint32",
+      },
+    ],
+    anonymous: false,
+  },
+  {
+    type: "event",
+    name: "GroupGuardianSet",
+    inputs: [
+      {
+        name: "groupId",
+        type: "bytes32",
+        indexed: true,
+        internalType: "bytes32",
+      },
+      {
+        name: "guardian",
+        type: "bytes32",
+        indexed: false,
+        internalType: "bytes32",
+      },
+    ],
+    anonymous: false,
+  },
+  {
+    type: "event",
+    name: "GroupStatusSet",
+    inputs: [
+      {
+        name: "groupId",
+        type: "bytes32",
+        indexed: true,
+        internalType: "bytes32",
+      },
+      {
+        name: "active",
+        type: "bool",
+        indexed: false,
+        internalType: "bool",
+      },
+    ],
+    anonymous: false,
+  },
+  {
+    type: "event",
     name: "Initialized",
     inputs: [
       {
@@ -194,6 +515,69 @@ const _abi = [
         type: "bytes4",
         indexed: true,
         internalType: "bytes4",
+      },
+    ],
+    anonymous: false,
+  },
+  {
+    type: "event",
+    name: "OperationCanceled",
+    inputs: [
+      {
+        name: "operationId",
+        type: "bytes32",
+        indexed: true,
+        internalType: "bytes32",
+      },
+      {
+        name: "nonce",
+        type: "uint32",
+        indexed: false,
+        internalType: "uint32",
+      },
+    ],
+    anonymous: false,
+  },
+  {
+    type: "event",
+    name: "OperationExecuted",
+    inputs: [
+      {
+        name: "operationId",
+        type: "bytes32",
+        indexed: true,
+        internalType: "bytes32",
+      },
+      {
+        name: "nonce",
+        type: "uint32",
+        indexed: false,
+        internalType: "uint32",
+      },
+    ],
+    anonymous: false,
+  },
+  {
+    type: "event",
+    name: "OperationScheduled",
+    inputs: [
+      {
+        name: "operationId",
+        type: "bytes32",
+        indexed: true,
+        internalType: "bytes32",
+      },
+      {
+        name: "timepoint",
+        type: "uint48",
+        indexed: false,
+        internalType: "uint48",
+      },
+      {
+        name: "nonce",
+        type: "uint32",
+        indexed: false,
+        internalType: "uint32",
       },
     ],
     anonymous: false,
@@ -239,6 +623,100 @@ const _abi = [
         type: "uint256",
         indexed: false,
         internalType: "uint256",
+      },
+    ],
+    anonymous: false,
+  },
+  {
+    type: "event",
+    name: "TargetDisabledSet",
+    inputs: [
+      {
+        name: "target",
+        type: "address",
+        indexed: true,
+        internalType: "address",
+      },
+      {
+        name: "disabled",
+        type: "bool",
+        indexed: false,
+        internalType: "bool",
+      },
+    ],
+    anonymous: false,
+  },
+  {
+    type: "event",
+    name: "TargetFunctionDelaySet",
+    inputs: [
+      {
+        name: "target",
+        type: "address",
+        indexed: true,
+        internalType: "address",
+      },
+      {
+        name: "newDelay",
+        type: "uint32",
+        indexed: false,
+        internalType: "uint32",
+      },
+      {
+        name: "minSetback",
+        type: "uint32",
+        indexed: false,
+        internalType: "uint32",
+      },
+    ],
+    anonymous: false,
+  },
+  {
+    type: "event",
+    name: "TargetFunctionDisabledSet",
+    inputs: [
+      {
+        name: "target",
+        type: "address",
+        indexed: true,
+        internalType: "address",
+      },
+      {
+        name: "selector",
+        type: "bytes4",
+        indexed: true,
+        internalType: "bytes4",
+      },
+      {
+        name: "disabled",
+        type: "bool",
+        indexed: false,
+        internalType: "bool",
+      },
+    ],
+    anonymous: false,
+  },
+  {
+    type: "event",
+    name: "TargetFunctionGroupSet",
+    inputs: [
+      {
+        name: "target",
+        type: "address",
+        indexed: true,
+        internalType: "address",
+      },
+      {
+        name: "selector",
+        type: "bytes4",
+        indexed: true,
+        internalType: "bytes4",
+      },
+      {
+        name: "groupId",
+        type: "bytes32",
+        indexed: true,
+        internalType: "bytes32",
       },
     ],
     anonymous: false,
@@ -302,6 +780,41 @@ const _abi = [
   },
   {
     type: "error",
+    name: "AlreadyScheduled",
+    inputs: [],
+  },
+  {
+    type: "error",
+    name: "AppAlreadyInstalled",
+    inputs: [],
+  },
+  {
+    type: "error",
+    name: "AppAlreadyRegistered",
+    inputs: [],
+  },
+  {
+    type: "error",
+    name: "AppDoesNotImplementInterface",
+    inputs: [],
+  },
+  {
+    type: "error",
+    name: "AppNotInstalled",
+    inputs: [],
+  },
+  {
+    type: "error",
+    name: "AppNotRegistered",
+    inputs: [],
+  },
+  {
+    type: "error",
+    name: "AppRevoked",
+    inputs: [],
+  },
+  {
+    type: "error",
     name: "ApprovalCallerNotOwnerNorApproved",
     inputs: [],
   },
@@ -313,6 +826,11 @@ const _abi = [
   {
     type: "error",
     name: "BalanceQueryForZeroAddress",
+    inputs: [],
+  },
+  {
+    type: "error",
+    name: "BannedApp",
     inputs: [],
   },
   {
@@ -360,6 +878,21 @@ const _abi = [
   },
   {
     type: "error",
+    name: "CallerAlreadyRegistered",
+    inputs: [],
+  },
+  {
+    type: "error",
+    name: "CallerNotRegistered",
+    inputs: [],
+  },
+  {
+    type: "error",
+    name: "ClientAlreadyRegistered",
+    inputs: [],
+  },
+  {
+    type: "error",
     name: "Entitlement__InvalidValue",
     inputs: [],
   },
@@ -380,6 +913,41 @@ const _abi = [
   },
   {
     type: "error",
+    name: "ExecutionAlreadyRegistered",
+    inputs: [],
+  },
+  {
+    type: "error",
+    name: "ExecutionFunctionAlreadySet",
+    inputs: [],
+  },
+  {
+    type: "error",
+    name: "ExecutionHookAlreadySet",
+    inputs: [],
+  },
+  {
+    type: "error",
+    name: "ExecutionNotFound",
+    inputs: [],
+  },
+  {
+    type: "error",
+    name: "ExecutionNotRegistered",
+    inputs: [],
+  },
+  {
+    type: "error",
+    name: "ExecutorCallFailed",
+    inputs: [],
+  },
+  {
+    type: "error",
+    name: "Expired",
+    inputs: [],
+  },
+  {
+    type: "error",
     name: "Initializable_InInitializingState",
     inputs: [],
   },
@@ -390,12 +958,78 @@ const _abi = [
   },
   {
     type: "error",
+    name: "InsufficientPayment",
+    inputs: [],
+  },
+  {
+    type: "error",
     name: "Introspection_AlreadySupported",
     inputs: [],
   },
   {
     type: "error",
     name: "Introspection_NotSupported",
+    inputs: [],
+  },
+  {
+    type: "error",
+    name: "InvalidAddressInput",
+    inputs: [],
+  },
+  {
+    type: "error",
+    name: "InvalidAppAddress",
+    inputs: [
+      {
+        name: "app",
+        type: "address",
+        internalType: "address",
+      },
+    ],
+  },
+  {
+    type: "error",
+    name: "InvalidAppId",
+    inputs: [],
+  },
+  {
+    type: "error",
+    name: "InvalidAppName",
+    inputs: [],
+  },
+  {
+    type: "error",
+    name: "InvalidArrayInput",
+    inputs: [],
+  },
+  {
+    type: "error",
+    name: "InvalidCaller",
+    inputs: [],
+  },
+  {
+    type: "error",
+    name: "InvalidDataLength",
+    inputs: [],
+  },
+  {
+    type: "error",
+    name: "InvalidDuration",
+    inputs: [],
+  },
+  {
+    type: "error",
+    name: "InvalidExpiration",
+    inputs: [],
+  },
+  {
+    type: "error",
+    name: "InvalidManifest",
+    inputs: [],
+  },
+  {
+    type: "error",
+    name: "InvalidPrice",
     inputs: [],
   },
   {
@@ -411,6 +1045,41 @@ const _abi = [
   {
     type: "error",
     name: "MintZeroQuantity",
+    inputs: [],
+  },
+  {
+    type: "error",
+    name: "ModuleInstallCallbackFailed",
+    inputs: [],
+  },
+  {
+    type: "error",
+    name: "NotAllowed",
+    inputs: [],
+  },
+  {
+    type: "error",
+    name: "NotAppOwner",
+    inputs: [],
+  },
+  {
+    type: "error",
+    name: "NotEnoughEth",
+    inputs: [],
+  },
+  {
+    type: "error",
+    name: "NotReady",
+    inputs: [],
+  },
+  {
+    type: "error",
+    name: "NotScheduled",
+    inputs: [],
+  },
+  {
+    type: "error",
+    name: "NullModule",
     inputs: [],
   },
   {
@@ -497,6 +1166,42 @@ const _abi = [
   {
     type: "error",
     name: "URIQueryForNonexistentToken",
+    inputs: [],
+  },
+  {
+    type: "error",
+    name: "UnauthorizedApp",
+    inputs: [
+      {
+        name: "app",
+        type: "address",
+        internalType: "address",
+      },
+    ],
+  },
+  {
+    type: "error",
+    name: "UnauthorizedCall",
+    inputs: [],
+  },
+  {
+    type: "error",
+    name: "UnauthorizedCancel",
+    inputs: [],
+  },
+  {
+    type: "error",
+    name: "UnauthorizedRenounce",
+    inputs: [],
+  },
+  {
+    type: "error",
+    name: "UnauthorizedSelector",
+    inputs: [],
+  },
+  {
+    type: "error",
+    name: "UnauthorizedTarget",
     inputs: [],
   },
 ] as const;
