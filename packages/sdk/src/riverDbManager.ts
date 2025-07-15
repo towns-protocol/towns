d81d6689eeb2dc6c6bd5540f312d5dad7cb8cdca
Miguel Nascimento <miguelgomes13@live.com>
2025-07-14 21:18:51 -0300

    remove riverdbmanager class

--- packages/sdk/src/riverDbManager.ts
+++
@@ -1,7 +0,0 @@
-import { createCryptoStore, CryptoStore } from '@towns-protocol/encryption'
-
-export class RiverDbManager {
-    public static getCryptoDb(userId: string, dbName?: string): CryptoStore {
-        return createCryptoStore(dbName ?? `database-${userId}`, userId)
-    }
-}
