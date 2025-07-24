0010b331ed7d49579c580080ee754d993657e908
rbhnt <roman@hntlabs.com>
2025-07-24 11:49:13 +0100

    Fixed data race

--- core/node/rpc/sync/client/syncer_set_test.go
+++ core/node/rpc/sync/client/syncer_set_test.go
@@ -262,14 +262,12 @@ func TestGetOrCreateSyncer_SyncerLifecycle(t *testing.T) {
 	cancel()
 
 	// Wait for syncer to be removed (give it some time to clean up)
-	time.Sleep(100 * time.Millisecond)
+	time.Sleep(500 * time.Millisecond)
 
 	// Verify syncer is removed after Run completes
 	syncerEntity, found := syncerSet.syncers.Load(localAddr)
 	assert.True(t, found)
-	syncerEntity.Lock()
 	assert.Nil(t, syncerEntity.StreamsSyncer)
-	syncerEntity.Unlock()
 
 	// Cleanup
 	streamCache.AssertExpectations(t)
