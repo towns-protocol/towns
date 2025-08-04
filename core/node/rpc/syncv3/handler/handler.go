bff2ee50f409298b509fcd5d1bb1365b23341e1d
begmaroman <begmaroman@gmail.com>
2025-08-04 12:53:59 +0100

    Interfaces for sync v3

--- core/node/rpc/syncv3/handler/handler.go
+++ core/node/rpc/syncv3/handler/handler.go
@@ -3,6 +3,8 @@ package handler
 import (
 	"context"
 
+	"github.com/puzpuzpuz/xsync/v4"
+
 	. "github.com/towns-protocol/towns/core/node/protocol"
 	"github.com/towns-protocol/towns/core/node/rpc/syncv3/eventbus"
 	. "github.com/towns-protocol/towns/core/node/shared"
@@ -39,9 +41,31 @@ type (
 		Ping(ctx context.Context)
 	}
 
+	// Receiver is a final receiver of the stream update message, i.e. client.
+	// It is not thread safe so the race detector will throw an error if multiple goroutines
+	// try to call Send at the same time.
+	Receiver interface {
+		Send(*SyncStreamsResponse) error
+	}
+
+	// SyncStreamHandlerRegistry holds a mapping from syncId that is shared with the client and
+	// its associated SyncStreamHandler that is responsible to send stream updates to the client.
+	SyncStreamHandlerRegistry interface {
+		// Get sync stream handler by sync id.
+		Get(syncID string) (SyncStreamHandler, bool)
+
+		// New create a new sync stream handler.
+		New(ctx context.Context, syncID string, receiver Receiver) SyncStreamHandler
+	}
+
 	syncStreamHandlerImpl struct {
 		syncID           string
 		receiver         Receiver
+		streamUpdatesBus eventbus.StreamSubscriptionManager
+	}
+
+	syncStreamHandlerRegistryImpl struct {
+		handlers         xsync.Map[string, *syncStreamHandlerImpl]
 		streamUpdatesBus eventbus.StreamSubscriptionManager
 	}
 )
@@ -49,6 +73,8 @@ type (
 var (
 	_ SyncStreamHandler         = (*syncStreamHandlerImpl)(nil)
 	_ eventbus.StreamSubscriber = (*syncStreamHandlerImpl)(nil)
+
+	_ SyncStreamHandlerRegistry = (*syncStreamHandlerRegistryImpl)(nil)
 )
 
 func (s *syncStreamHandlerImpl) SyncID() string {
@@ -108,4 +134,20 @@ func (s *syncStreamHandlerImpl) OnUpdate(update *SyncStreamsResponse) {
 	// add update to internal queue
 
 	panic("implement me")
+}
+
+func (s *syncStreamHandlerRegistryImpl) Get(syncID string) (SyncStreamHandler, bool) {
+	return s.handlers.Load(syncID)
+}
+
+func (s *syncStreamHandlerRegistryImpl) New(ctx context.Context, syncID string, receiver Receiver) SyncStreamHandler {
+	handler := &syncStreamHandlerImpl{
+		syncID:           syncID,
+		receiver:         receiver,
+		streamUpdatesBus: s.streamUpdatesBus,
+	}
+
+	s.handlers.Store(handler.SyncID(), handler)
+
+	return handler
 }
--- core/node/rpc/syncv3/syncer/syncer.go
+++ core/node/rpc/syncv3/syncer/syncer.go
@@ -4,7 +4,7 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 
 	. "github.com/towns-protocol/towns/core/node/protocol"
-	"github.com/towns-protocol/towns/core/node/shared"
+	. "github.com/towns-protocol/towns/core/node/shared"
 )
 
 type (
@@ -27,7 +27,7 @@ type (
 	// 2. Remote emitters that emit events for remote streams.
 	StreamUpdateEmitter interface {
 		// StreamID returns the StreamID of the stream that this emitter emits events for.
-		StreamID() shared.StreamId
+		StreamID() StreamId
 
 		// Node returns the address of the node that the emitter subscribes to for stream updates.
 		// For local streams this is the local node address.
@@ -46,5 +46,30 @@ type (
 		// Backfill backfills the given stream by the given cookie.
 		// syncIDs is the chain of sync IDs that the backfill request should be sent to.
 		Backfill(cookie *SyncCookie, syncIDs []string) error
+	}
+
+	Registry interface {
+		// Subscribe subscribes to the given stream updates.
+		//
+		// If the given stream ID is not found, it sends the stream down message to the subscriber
+		// with the reason (message field in proto).
+		Subscribe(streamID StreamId, subscriber StreamSubscriber)
+
+		// Unsubscribe unsubscribes from the given stream updates.
+		//
+		// If StreamSubscriber has received the stream down message, it should call the given Unsubscribe function.
+		// If no more subscribers are left for the stream, the emitter should be closed.
+		//
+		// Note: initially, the event bus is the only subscriber for the stream.
+		Unsubscribe(streamID StreamId, subscriber StreamSubscriber)
+
+		// Backfill sends a request to appropriate syncer to backfill a specific sync operation by the given
+		// chain of sync identifiers.
+		// TODO: If a syncer for the given stream ID is not found, it returns an error or the sync down message.
+		Backfill(cookie *SyncCookie, syncIDs []string) error
+	}
+
+	registryImpl struct {
+		syncers map[StreamId]StreamUpdateEmitter
 	}
 )
