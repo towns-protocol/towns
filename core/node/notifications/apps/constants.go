8a7e19b4f03b36a2c5c146d1fe5fb5828622cc9e
Yoni Londner <jontra@gmail.com>
2025-08-07 14:49:06 +0300

    added files

---
+++ core/node/notifications/apps/constants.go
@@ -0,0 +1,11 @@
+package apps
+
+// App identifiers for multi-app notification support
+const (
+	// Towns is the identifier for the Towns app
+	Towns = "towns"
+	// Sendit is the identifier for the Sendit app
+	Sendit = "sendit"
+	// Default is the default app for backward compatibility
+	Default = Towns
+)
