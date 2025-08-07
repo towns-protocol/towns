5b7977e3d97e4e7f9bc48af89781ec4d5b0d5efe
Yoni Londner <jontra@gmail.com>
2025-08-07 15:29:12 +0300

    removed unneeded index

--- core/node/storage/notification_migrations/000003_add_app_id.up.sql
+++ core/node/storage/notification_migrations/000003_add_app_id.up.sql
@@ -7,9 +7,6 @@ ALTER TABLE webpushsubscriptions DROP CONSTRAINT webpushsubscriptions_pkey;
 -- Add new composite primary key including app_id
 ALTER TABLE webpushsubscriptions ADD PRIMARY KEY (key_auth, key_p256dh, app_id);
 
--- Add index on app_id for efficient filtering
-CREATE INDEX WEB_SUB_APP_ID_IDX ON webpushsubscriptions USING hash (app_id);
-
 -- Add app_id column to apnpushsubscriptions table
 ALTER TABLE apnpushsubscriptions ADD COLUMN app_id SMALLINT NOT NULL DEFAULT 1; -- 1 = NOTIFICATION_APP_TOWNS
 
@@ -18,6 +15,3 @@ ALTER TABLE apnpushsubscriptions DROP CONSTRAINT apnpushsubscriptions_pkey;
 
 -- Add new composite primary key including app_id
 ALTER TABLE apnpushsubscriptions ADD PRIMARY KEY (device_token, app_id);
-
--- Add index on app_id for efficient filtering
-CREATE INDEX APN_SUB_APP_ID_IDX ON apnpushsubscriptions USING hash (app_id);
