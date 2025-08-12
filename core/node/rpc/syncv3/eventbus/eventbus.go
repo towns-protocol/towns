f82edb1dbba2661e86a71e3300da8efe5b724743
begmaroman <begmaroman@gmail.com>
2025-08-12 14:53:27 -0400

    Sync v3: event bus implementation

--- core/node/rpc/syncv3/eventbus/eventbus.go
+++ core/node/rpc/syncv3/eventbus/eventbus.go
@@ -3,7 +3,9 @@ package eventbus
 import (
 	"context"
 	"slices"
+	"sync"
 
+	"github.com/towns-protocol/towns/core/node/logging"
 	. "github.com/towns-protocol/towns/core/node/protocol"
 	"github.com/towns-protocol/towns/core/node/rpc/syncv3/syncer"
 	. "github.com/towns-protocol/towns/core/node/shared"
@@ -18,6 +20,9 @@ var (
 type (
 	// StreamSubscriber subscribes to stream updates.
 	StreamSubscriber interface {
+		// SyncID returns subscription (sync op basically) identifier.
+		SyncID() string
+
 		// OnUpdate is called when there is a stream update available.
 		// Each subscriber receives its own copy of the update.
 		//
@@ -59,36 +64,49 @@ type (
 		Backfill(cookie *SyncCookie, syncIDs ...string) error
 	}
 
+	eventBusMessageStreamUpdate struct {
+		msg     *SyncStreamsResponse
+		version int32
+	}
+
+	eventBusMessageSub struct {
+		streamID   StreamId
+		subscriber StreamSubscriber
+	}
+
+	eventBusMessageUnsub struct {
+		streamID   StreamId
+		subscriber StreamSubscriber
+	}
+
+	eventBusMessageBackfill struct {
+		cookie  *SyncCookie
+		syncIDs []string
+	}
+
 	// eventBusMessage is put on the internal event bus queue for processing in eventBusImpl.run.
 	eventBusMessage struct {
-		// a stream update
-		update *SyncStreamsResponse
-		// a stream subscribe command
-		sub *struct {
-			streamID   StreamId
-			subscriber StreamSubscriber
-		}
-		// stream update
-		unsub *struct {
-			streamID   StreamId
-			subscriber StreamSubscriber
-		}
-		// stream backfill
-		backfill *struct {
-			cookie  *SyncCookie
-			syncIDs []string
-		}
+		update   *eventBusMessageStreamUpdate
+		sub      *eventBusMessageSub
+		unsub    *eventBusMessageUnsub
+		backfill *eventBusMessageBackfill
 	}
 
 	// eventBusImpl is a concrete implementation of the StreamUpdateEmitter and StreamSubscriber interfaces.
 	// It is responsible for handling stream updates from syncer.StreamUpdateEmitter and distributes updates
 	// to subscribers. As such it keeps track of which subscribers are subscribed on updates on which streams.
+	//
+	// TODO: Periodically unsubscribe from streams that have no subscribers.
 	eventBusImpl struct {
+		// log is the event bus named logger
+		log *logging.Log
 		// TODO: discuss if we want to have an unbounded queue here?
 		// Processing items on the queue should be fast enough to always keep up with the added items.
 		queue *dynmsgbuf.DynamicBuffer[*eventBusMessage]
 		// registry is the syncer registry.
 		registry syncer.Registry
+		// subscribers is the list of subscribers grouped by stream ID and syncer version.
+		subscribers map[StreamId]map[int32][]StreamSubscriber
 	}
 )
 
@@ -96,10 +114,15 @@ type (
 //
 // It creates an internal queue for stream updates and a background goroutine that reads updates from this queue
 // and distributes them to subscribers.
-func New(ctx context.Context, registry syncer.Registry) *eventBusImpl {
+func New(
+	ctx context.Context,
+	registry syncer.Registry,
+) *eventBusImpl {
 	e := &eventBusImpl{
-		queue:    dynmsgbuf.NewDynamicBuffer[*eventBusMessage](),
-		registry: registry,
+		log:         logging.FromCtx(ctx).Named("syncv3.eventbus"),
+		queue:       dynmsgbuf.NewDynamicBuffer[*eventBusMessage](),
+		registry:    registry,
+		subscribers: make(map[StreamId]map[int32][]StreamSubscriber),
 	}
 	go e.run(ctx)
 	return e
@@ -112,43 +135,72 @@ func New(ctx context.Context, registry syncer.Registry) *eventBusImpl {
 //
 // There is a background goroutine that reads updates from this queue and forwards the update to subscribers
 // on the stream. If the update is a SyncOp_SYNC_DOWN update, all subscribers are automatically unsubscribed
-// from the stream.
+// from the stream. A stream down message could be an indicator for a client to re-subscribe on a given stream.
+//
+// TODO: Add retry mechanism?
 func (e *eventBusImpl) OnStreamEvent(update *SyncStreamsResponse, version int32) {
-	// TODO: if the queue is full, we should retry few times, if it still fails, we unsubscribe from the given stream
-	//  updates and send the stream down message to all subscribers.
-	_ = e.queue.AddMessage(&eventBusMessage{update: update})
+	err := e.queue.AddMessage(&eventBusMessage{update: &eventBusMessageStreamUpdate{msg: update, version: version}})
+	if err == nil {
+		// All good, just return
+		return
+	}
+
+	streamID, parseErr := StreamIdFromBytes(update.StreamID())
+	if parseErr != nil {
+		e.log.Errorw("failed to unsubscribe: failed to parse stream id",
+			"streamID", update.StreamID(), "error", err, "parseErr", parseErr)
+		return
+	}
+
+	e.log.Errorw("failed to add stream update message to the queue", "streamID", streamID, "error", err)
+
+	// Unsubscribe event bus from receiving updates of the given stream.
+	e.registry.Unsubscribe(streamID)
+
+	subscribers, ok := e.subscribers[streamID]
+	if !ok {
+		// No subscribers for the given stream, do nothing
+		return
+	}
+
+	// Send sync down message directly to clients to avoid common queue
+	var wg sync.WaitGroup
+	wg.Add(len(subscribers))
+	for _, sub := range subscribers {
+		go func(sub StreamSubscriber) {
+			sub.OnUpdate(&SyncStreamsResponse{SyncOp: SyncOp_SYNC_DOWN, StreamId: streamID[:]})
+			wg.Done()
+		}(sub)
+	}
+	wg.Wait()
+
+	// Remove the given stream from the list
+	delete(e.subscribers, streamID)
 }
 
 // Subscribe adds the given subscriber to the stream updates.
 func (e *eventBusImpl) Subscribe(streamID StreamId, subscriber StreamSubscriber) error {
-	// TODO implement me
 	// 1. Check if the stream exists, if not return Err_NOT_FOUND
 	// (TODO: determine how to determine if a stream exists to prevent locking subscribe too long, or pushing a command
 
-	// 2. add command to e.queue that adds the subscriber to the stream's subscriber list in eventBusImpl.run
-	err := e.queue.AddMessage(&eventBusMessage{
-		sub: &struct {
-			streamID   StreamId
-			subscriber StreamSubscriber
-		}{
+	// 2. Add command to e.queue that adds the subscriber to the stream's subscriber list in eventBusImpl.run.
+	//
+	// An error might indicate that either the queue is full and the error must be returned to the caller to indicate
+	// that subscribing failed. Or the message is added to the queue, and the subscriber is guaranteed to
+	// receive stream updates. It will receive SyncOp_SYNC_DOWN when something bad happens and the subscriber
+	// is automatically unsubscribed.
+	return e.queue.AddMessage(&eventBusMessage{
+		sub: &eventBusMessageSub{
 			streamID:   streamID,
 			subscriber: subscriber,
 		},
 	})
-
-	// Either the queue is full and the error must be returned to the caller to indicate that subscribing failed.
-	// Or the message is added to the queue, and the subscriber is guaranteed to receive stream updates.
-	// It will receive SyncOp_SYNC_DOWN when something bad happens and the subscriber is automatically unsubscribed.
-	return err
 }
 
 // Unsubscribe removes the given subscriber from the stream updates.
 func (e *eventBusImpl) Unsubscribe(streamID StreamId, subscriber StreamSubscriber) error {
 	return e.queue.AddMessage(&eventBusMessage{
-		unsub: &struct {
-			streamID   StreamId
-			subscriber StreamSubscriber
-		}{
+		unsub: &eventBusMessageUnsub{
 			streamID:   streamID,
 			subscriber: subscriber,
 		},
@@ -158,49 +210,192 @@ func (e *eventBusImpl) Unsubscribe(streamID StreamId, subscriber StreamSubscribe
 // Backfill requests a backfill message for the given cookie and sync IDs.
 func (e *eventBusImpl) Backfill(cookie *SyncCookie, syncIDs ...string) error {
 	return e.queue.AddMessage(&eventBusMessage{
-		backfill: &struct {
-			cookie  *SyncCookie
-			syncIDs []string
-		}{
+		backfill: &eventBusMessageBackfill{
 			cookie:  cookie,
 			syncIDs: syncIDs,
 		},
 	})
 }
 
+// run starts processing commands from the queue.
 func (e *eventBusImpl) run(ctx context.Context) {
-	var (
-		msgs        []*eventBusMessage
-		subscribers = make(map[StreamId][]StreamSubscriber)
-	)
+	var msgs []*eventBusMessage
 	for {
-		<-e.queue.Wait()
+		var open bool
+		select {
+		case <-ctx.Done():
+			if err := ctx.Err(); err != nil {
+				e.log.Errorw("failed to process commands from the queue, context cancelled", "error", err)
+			} else {
+				e.log.Infow("closing commands processor")
+			}
+			return
+		case _, open = <-e.queue.Wait():
+		}
+
 		msgs = e.queue.GetBatch(msgs)
 
+		// nil msgs indicates the buffer is closed.
+		if msgs == nil {
+			// TODO: Log error and skip the rest of commands
+			return
+		}
+
+		// Process messages from the current batch one by one.
 		for _, msg := range msgs {
 			if msg.update != nil {
-				// TODO: implement me
-				// if msg is stream update:
-				// 1. loop over all subscribers for update.StreamId
-				// 2. create a copy of the update and unset syncID because each subscriber has its own unique syncID
-				//    (all other fields can be set by reference/pointer to prevent needless copying)
-				// 3. if update.SyncOp is SyncOp_SYNC_DOWN, unsubscribe the subscriber from the stream
-				// 4. call subscriber.OnUpdate with the copied stream update
+				e.processStreamUpdateCommand(msg.update.msg, msg.update.version)
 			} else if msg.sub != nil {
-				currentSubscribers := subscribers[msg.sub.streamID]
-				if !slices.Contains(currentSubscribers, msg.sub.subscriber) {
-					subscribers[msg.sub.streamID] = append(currentSubscribers, msg.sub.subscriber)
-				}
+				e.processSubscribeCommand(msg.sub)
 			} else if msg.unsub != nil {
-				for i, subscriber := range subscribers[msg.unsub.streamID] {
-					if subscriber == msg.unsub.subscriber {
-						slices.Delete(subscribers[msg.unsub.streamID], i, i+1)
-						break
-					}
-				}
+				e.processUnsubscribeCommand(msg.unsub)
 			} else if msg.backfill != nil {
-				// TODO: Implement me
+				e.processBackfillCommand(msg.backfill)
 			}
 		}
+
+		if !open {
+			// TODO: Log error and skip the rest of commands
+			// If the queue is closed, we stop processing commands.
+			return
+		}
 	}
+}
+
+// processStreamUpdateCommand processes the given stream update command.
+func (e *eventBusImpl) processStreamUpdateCommand(msg *SyncStreamsResponse, version int32) {
+	if msg == nil {
+		return
+	}
+
+	streamID, err := StreamIdFromBytes(msg.StreamID())
+	if err != nil {
+		e.log.Errorw("failed to parse stream id from the stream update message",
+			"streamID", msg.StreamID(), "error", err)
+		return
+	}
+
+	// If the given message has target sync IDs, it means that this is a backfill message and should be forwarded
+	// to the target subscriber only.
+	if len(msg.GetTargetSyncIds()) > 0 {
+		var target StreamSubscriber
+		for _, sub := range e.subscribers[streamID] {
+			if sub.SyncID() == msg.GetTargetSyncIds()[0] {
+				target = sub
+				break
+			}
+		}
+
+		if target == nil {
+			e.log.Debugw("no subscriber found for the given backfill message",
+				"streamID", streamID, "syncIDs", msg.GetTargetSyncIds())
+			return
+		}
+
+		target.OnUpdate(msg)
+
+		return
+	}
+
+	subscribers, ok := e.subscribers[streamID]
+	if !ok {
+		// No subscribers for the given stream, do nothing
+		return
+	}
+
+	var wg sync.WaitGroup
+	wg.Add(len(subscribers))
+	for _, sub := range subscribers {
+		go func(sub StreamSubscriber) {
+			// Create a copy of the update and unset syncID because each subscriber has its own unique syncID.
+			// All other fields can be set by reference/pointer to prevent needless copying.
+			sub.OnUpdate(&SyncStreamsResponse{
+				SyncOp:   msg.GetSyncOp(),
+				Stream:   msg.GetStream(),
+				StreamId: msg.GetStreamId(),
+			})
+			wg.Done()
+		}(sub)
+	}
+	wg.Wait()
+
+	// Remove the stream from registries if the sync down message is received.
+	if msg.GetSyncOp() == SyncOp_SYNC_DOWN {
+		delete(e.subscribers, streamID)
+	}
+}
+
+// processSubscribeCommand processes the given subscribe command.
+func (e *eventBusImpl) processSubscribeCommand(msg *eventBusMessageSub) {
+	if msg == nil {
+		return
+	}
+
+	currentSubscribers, ok := e.subscribers[msg.streamID]
+	if !ok {
+		// Event bus is not subscribed on updates of the given stream. Subscribe first.
+		e.subscribers[msg.streamID] = []StreamSubscriber{msg.subscriber}
+		e.registry.Subscribe(msg.streamID)
+	} else {
+		if !slices.Contains(currentSubscribers, msg.subscriber) {
+			e.subscribers[msg.streamID] = append(currentSubscribers, msg.subscriber)
+		}
+	}
+}
+
+// processUnsubscribeCommand processes the given unsubscribe command.
+func (e *eventBusImpl) processUnsubscribeCommand(msg *eventBusMessageUnsub) {
+	if msg == nil {
+		return
+	}
+
+	e.subscribers[msg.streamID] = slices.DeleteFunc(
+		e.subscribers[msg.streamID],
+		func(subscriber StreamSubscriber) bool {
+			return subscriber == msg.subscriber
+		},
+	)
+}
+
+// processBackfillCommand processes the given backfill command.
+func (e *eventBusImpl) processBackfillCommand(msg *eventBusMessageBackfill) {
+	if msg == nil {
+		return
+	}
+
+	err := e.registry.Backfill(msg.cookie, msg.syncIDs)
+	if err == nil {
+		return
+	}
+
+	streamID, err := StreamIdFromBytes(msg.cookie.GetStreamId())
+	if err != nil {
+		e.log.Errorw("failed to parse stream ID from cookie in backfill message",
+			"streamID", msg.cookie.GetStreamId(), "error", err)
+		return
+	}
+
+	e.log.Errorw("failed to process backfill request",
+		"streamID", streamID, "syncIDs", msg.syncIDs, "error", err)
+
+	var target StreamSubscriber
+	e.subscribers[streamID] = slices.DeleteFunc(
+		e.subscribers[streamID],
+		func(subscriber StreamSubscriber) bool {
+			if subscriber.SyncID() != msg.syncIDs[0] {
+				return false
+			}
+			target = subscriber
+			return true
+		},
+	)
+
+	if target == nil {
+		e.log.Debugw("no subscriber found for the given failed backfill request",
+			"streamID", streamID, "syncIDs", msg.syncIDs)
+		return
+	}
+
+	// Send sync down message back to the subscriber as an indicator of backfilling failure
+	target.OnUpdate(&SyncStreamsResponse{SyncOp: SyncOp_SYNC_DOWN, StreamId: streamID[:]})
 }
