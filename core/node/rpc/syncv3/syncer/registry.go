a05773ea6566500d8928d999c7381b163633ef5b
begmaroman <begmaroman@gmail.com>
2025-08-12 14:18:34 -0400

    Sync v3: syncer registry implementation

--- core/node/rpc/syncv3/syncer/registry.go
+++ core/node/rpc/syncv3/syncer/registry.go
@@ -1,8 +1,15 @@
 package syncer
 
 import (
+	"context"
 	"sync"
+	"time"
 
+	"github.com/ethereum/go-ethereum/common"
+
+	. "github.com/towns-protocol/towns/core/node/base"
+	"github.com/towns-protocol/towns/core/node/logging"
+	"github.com/towns-protocol/towns/core/node/nodes"
 	. "github.com/towns-protocol/towns/core/node/protocol"
 	. "github.com/towns-protocol/towns/core/node/shared"
 )
@@ -11,29 +18,161 @@ var (
 	_ Registry = (*registryImpl)(nil)
 )
 
+// registryImpl is an implementation of the Registry interface.
 type registryImpl struct {
+	// ctx is the global node context.
+	ctx context.Context
+	// log is the logger for the registry.
+	log *logging.Log
+	// localAddr is the address of the current node.
+	localAddr common.Address
+	// streamCache is the global stream cache.
+	streamCache StreamCache
+	// nodeRegistry is the global node registry.
+	nodeRegistry nodes.NodeRegistry
+	// subscriber is the StreamSubscriber that receives updates from all syncers.
+	subscriber StreamSubscriber
+	// syncersLock is a mutex to protect the syncers map.
 	syncersLock sync.Mutex
-	syncers     map[StreamId]StreamUpdateEmitter
+	// syncers is a map of stream IDs to their corresponding StreamUpdateEmitter instances.
+	syncers map[StreamId]StreamUpdateEmitter
 }
 
 // NewRegistry creates a new instance of the Registry.
-func NewRegistry() *registryImpl {
+func NewRegistry(
+	ctx context.Context,
+	localAddr common.Address,
+	streamCache StreamCache,
+	nodeRegistry nodes.NodeRegistry,
+	subscriber StreamSubscriber,
+) *registryImpl {
 	return &registryImpl{
-		syncers: make(map[StreamId]StreamUpdateEmitter),
+		ctx:          ctx,
+		log:          logging.FromCtx(ctx).Named("syncv3.Registry"),
+		localAddr:    localAddr,
+		streamCache:  streamCache,
+		nodeRegistry: nodeRegistry,
+		subscriber:   subscriber,
+		syncers:      make(map[StreamId]StreamUpdateEmitter),
 	}
 }
 
-func (r *registryImpl) Subscribe(streamID StreamId, subscriber StreamSubscriber) {
-	// TODO: Implement me
-	panic("implement me")
+// Subscribe subscribes a StreamSubscriber to the updates of the given streamID.
+func (r *registryImpl) Subscribe(streamID StreamId) {
+	r.syncersLock.Lock()
+	defer r.syncersLock.Unlock()
+
+	if _, ok := r.syncers[streamID]; !ok {
+		var err error
+		if _, err = r.createEmitterNoLock(streamID); err != nil {
+			r.log.Errorw("failed to create stream emitter", "streamId", streamID, "error", err)
+			r.sendStreamDown(streamID)
+			return
+		}
+	}
 }
 
-func (r *registryImpl) Unsubscribe(streamID StreamId, subscriber StreamSubscriber) {
-	// TODO: Implement me
-	panic("implement me")
+// Unsubscribe unsubscribes a StreamSubscriber from the updates of the given streamID.
+func (r *registryImpl) Unsubscribe(streamID StreamId) {
+	r.syncersLock.Lock()
+	defer r.syncersLock.Unlock()
+
+	if emitter, ok := r.syncers[streamID]; ok {
+		emitter.Unsubscribe(subscriber)
+	}
 }
 
+// Backfill sends a backfill request for the given syncIDs to the corresponding StreamUpdateEmitter.
 func (r *registryImpl) Backfill(cookie *SyncCookie, syncIDs []string) error {
-	// TODO: Implement me
-	panic("implement me")
+	r.syncersLock.Lock()
+	defer r.syncersLock.Unlock()
+
+	streamID, err := StreamIdFromBytes(cookie.GetStreamId())
+	if err != nil {
+		return AsRiverError(err).
+			Tag("streamID", cookie.GetStreamId()).Func("registryImpl.Backfill")
+	}
+
+	emitter, ok := r.syncers[streamID]
+	if !ok {
+		emitter, err = r.createEmitterNoLock(streamID)
+		if err != nil {
+			return AsRiverError(err).
+				Tag("streamID", streamID).Func("registryImpl.Backfill")
+		}
+	}
+
+	if !emitter.Backfill(cookie, syncIDs) {
+		emitter, err = r.createEmitterNoLock(streamID)
+		if err != nil {
+			return AsRiverError(err).
+				Tag("streamID", streamID).Func("registryImpl.Backfill")
+		}
+
+		if !emitter.Backfill(cookie, syncIDs) {
+			return RiverError(Err_NOT_FOUND, "failed to create a new stream updates emitter").
+				Tag("streamID", streamID).Func("registryImpl.Backfill")
+		}
+	}
+
+	return nil
+}
+
+// createEmitterNoLock creates a new StreamUpdateEmitter for the given streamID without acquiring the lock.
+func (r *registryImpl) createEmitterNoLock(streamID StreamId) (StreamUpdateEmitter, error) {
+	ctxWithTimeout, ctxWithCancel := context.WithTimeout(r.ctx, 20*time.Second)
+	defer ctxWithCancel()
+
+	// Get stream remote nodes first
+	stream, err := r.streamCache.GetStreamNoWait(ctxWithTimeout, streamID)
+	if err != nil {
+		return nil, AsRiverError(err).Func("registryImpl.createEmitter")
+	}
+
+	// Get current sticky peer address for the stream.
+	// The sticky peer could be advanced by the emitter if a remote node is failed or unavailable.
+	addr := stream.GetStickyPeer()
+
+	// Create a new emitter based on whether the address is local or remote.
+	var streamUpdateEmitter StreamUpdateEmitter
+	if addr == r.localAddr {
+		streamUpdateEmitter = NewLocalStreamUpdateEmitter(
+			r.ctx,
+			addr,
+			r.streamCache,
+			streamID,
+			r.subscriber,
+			1,
+		)
+	} else {
+		streamUpdateEmitter = NewRemoteStreamUpdateEmitter(
+			r.ctx,
+			addr,
+			r.nodeRegistry,
+			streamID,
+			r.subscriber,
+			1,
+		)
+	}
+
+	// Store the given emitter in the registry without acquiring the lock.
+	// This function is called with the lock already held, so we can safely add it to the map.
+	r.syncers[streamID] = streamUpdateEmitter
+
+	return streamUpdateEmitter, nil
+}
+
+// deleteEmitter returns a function that deletes the emitter for the given streamID from the registry.
+// The returned function is passed as a callback and called when the emitter is closed or no longer needed.
+func (r *registryImpl) deleteEmitter(streamID StreamId) func() {
+	return func() {
+		r.syncersLock.Lock()
+		delete(r.syncers, streamID)
+		r.syncersLock.Unlock()
+	}
+}
+
+// sendStreamDown sends a stream down message to the given subscriber for the specified streamID.
+func (r *registryImpl) sendStreamDown(streamID StreamId) {
+	r.subscriber.OnStreamEvent(&SyncStreamsResponse{SyncOp: SyncOp_SYNC_DOWN, StreamId: streamID[:]}, 0)
 }
