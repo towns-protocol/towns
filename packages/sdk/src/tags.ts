12ad50788eb5d1a0fd1b08f8de9275d38ef569db
texuf <texuf.eth@gmail.com>
2025-08-22 22:15:12 -0700

    Fix irregular tipping include from sdk

--- packages/sdk/src/tags.ts
+++ packages/sdk/src/tags.ts
@@ -9,7 +9,7 @@ import {
 import { StreamStateView } from './streamStateView'
 import { addressFromUserId } from './id'
 import { bin_fromHexString, bin_toHexString, check } from '@towns-protocol/dlog'
-import { TipEventObject } from '@towns-protocol/generated/dev/typings/ITipping'
+import { TipEventObject } from '@towns-protocol/web3'
 import { isDefined } from './check'
 import { bytesToHex } from 'ethereum-cryptography/utils'
 import { RiverTimelineEvent } from './views/models/timelineTypes'
