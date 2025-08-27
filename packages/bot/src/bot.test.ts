ec54aa9ce11565c4764108dcdc7df08b414f24c2
Miguel Nascimento <miguelgomes13@live.com>
2025-08-27 17:59:28 -0300

    feat(bot): add `adminRemoveEvent`

--- packages/bot/src/bot.test.ts
+++ packages/bot/src/bot.test.ts
@@ -115,7 +115,7 @@ describe('Bot', { sequential: true }, () => {
         const tx = await appRegistryDapp.createApp(
             bob.signer,
             BOT_USERNAME,
-            [...Object.values(Permission)], // all permissions
+            [...Object.values(Permission).filter((p) => p !== Permission.React)], // all permissions
             botClientAddress,
             ethers.utils.parseEther('0.01').toBigInt(),
             31536000n,
