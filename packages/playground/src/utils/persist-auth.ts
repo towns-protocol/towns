a3fe2228adebe7875aa00b6f08b2d9cdfa387063
texuf <texuf.eth@gmail.com>
2025-08-27 15:53:11 -0700

    wip envs - currently breaking up makeWeb3Deployment

--- packages/playground/src/utils/persist-auth.ts
+++ packages/playground/src/utils/persist-auth.ts
@@ -7,27 +7,25 @@ export const storeAuth = (signerContext: SignerContext, riverConfig: RiverConfig
         signerPrivateKey: signerContext.signerPrivateKey(),
     }
     const signerContextString = superjson.stringify(fixedContext)
-    const riverConfigString = superjson.stringify(riverConfig)
     window.localStorage.setItem('river-signer', signerContextString)
-    window.localStorage.setItem('river-last-config', riverConfigString)
+    window.localStorage.setItem('river-last-env', riverConfig.environmentId)
 }
 
 export const loadAuth = () => {
     const signerContextString = window.localStorage.getItem('river-signer')
-    const riverConfigString = window.localStorage.getItem('river-last-config')
+    const riverConfigString = window.localStorage.getItem('river-last-env')
     if (!signerContextString || !riverConfigString) {
-        return
+        return undefined
     }
     const signerContext = superjson.parse<Record<string, string>>(signerContextString)
-    const riverConfig = superjson.parse<RiverConfig>(riverConfigString)
     const fixedContext = {
         ...signerContext,
         signerPrivateKey: () => signerContext.signerPrivateKey,
     } as SignerContext
-    return { signerContext: fixedContext, riverConfig }
+    return { signerContext: fixedContext, riverEnvironmentId: riverConfigString }
 }
 
 export const deleteAuth = () => {
     window.localStorage.removeItem('river-signer')
-    window.localStorage.removeItem('river-last-config')
+    window.localStorage.removeItem('river-last-env')
 }
