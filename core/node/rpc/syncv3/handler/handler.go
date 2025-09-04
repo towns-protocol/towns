a4317b2d3bd1233d76ad1367cd128423f9e51201
Roman Behma <13855864+begmaroman@users.noreply.github.com>
2025-09-04 18:31:20 +0100

    Sync v3: sync operation handler (#3876)

--- core/node/rpc/syncv3/handler/handler.go
+++ core/node/rpc/syncv3/handler/handler.go
@@ -3,10 +3,19 @@ package handler
 import (
 	"context"
 	"sync"
+	"time"
 
+	. "github.com/towns-protocol/towns/core/node/base"
+	"github.com/towns-protocol/towns/core/node/logging"
 	. "github.com/towns-protocol/towns/core/node/protocol"
 	"github.com/towns-protocol/towns/core/node/rpc/syncv3/eventbus"
 	. "github.com/towns-protocol/towns/core/node/shared"
+	"github.com/towns-protocol/towns/core/node/utils/dynmsgbuf"
+)
+
+const (
+	// defaultCommandReplyTimeout is the default timeout for command replies in the sync operation.
+	defaultCommandReplyTimeout = 30 * time.Second
 )
 
 var (
@@ -32,9 +41,7 @@ type (
 		// update from the given sync position is sent to the client (backfill).
 		//
 		// For each stream that is removed, the operation will unsubscribe from further stream updates.
-		//
-		// If stream does not exist, a client will receive a stream down message with the reason (not found).
-		Modify(req *ModifySyncRequest) *ModifySyncResponse
+		Modify(req *ModifySyncRequest) (*ModifySyncResponse, error)
 
 		// Cancel the stream sync operation.
 		// The given operation must be removed from the SyncStreamHandlerRegistry.
@@ -44,7 +51,11 @@ type (
 		Cancel(ctx context.Context) error
 
 		// Ping is used to keep the sync operation alive and make sure it is operational.
-		Ping(ctx context.Context)
+		Ping(ctx context.Context, nonce string)
+
+		// DebugDropStream is a debug method to drop a specific stream from the sync operation.
+		// Sends a SyncOp_SYNC_DOWN message to the receiver and unsubscribes from the stream updates.
+		DebugDropStream(ctx context.Context, streamId StreamId) error
 	}
 
 	// Receiver is a final receiver of the stream update message, i.e. client.
@@ -62,22 +73,38 @@ type (
 		Get(syncID string) (SyncStreamHandler, bool)
 
 		// New create a new sync stream handler.
-		New(ctx context.Context, syncID string, receiver Receiver) SyncStreamHandler
+		New(ctx context.Context, syncID string, receiver Receiver) (SyncStreamHandler, error)
 	}
 
 	// syncStreamHandlerImpl is a concrete implementation of the SyncStreamHandler interface.
+	//
+	// TODO: Remove sync operation from event bas after its cancellation.
 	syncStreamHandlerImpl struct {
-		syncID           string
-		receiver         Receiver
-		streamUpdatesBus eventbus.StreamSubscriptionManager
+		// ctx is the context of the sync operation.
+		ctx context.Context
+		// cancel is the cancel function for the operation context.
+		cancel context.CancelCauseFunc
+		// log is the logger for the operation.
+		log *logging.Log
+		// syncID is the unique identifier for the sync operation.
+		syncID string
+		// receiver is the final receiver of the stream update message, i.e. client.
+		receiver Receiver
+		// eventBus is the event bus that is used to subscribe for stream updates.
+		eventBus eventbus.StreamSubscriptionManager
+		// streamUpdates is the stream updates queue.
+		// When a stream update is received, it should be sent to the queue so the updates processor can handle them.
+		streamUpdates *dynmsgbuf.DynamicBuffer[*SyncStreamsResponse]
 	}
 
 	// syncStreamHandlerRegistryImpl is a concrete implementation of the Registry interface.
 	syncStreamHandlerRegistryImpl struct {
+		// handlersLock is a mutex that protects access to the handlers map.
 		handlersLock sync.Mutex
-		handlers     map[string]*syncStreamHandlerImpl
-
-		streamUpdatesBus eventbus.StreamSubscriptionManager
+		// handlers is a map of sync stream handlers by their sync ID.
+		handlers map[string]*syncStreamHandlerImpl
+		// eventBus is the event bus that is used to subscribe for stream updates.
+		eventBus eventbus.StreamSubscriptionManager
 	}
 )
 
@@ -85,85 +112,248 @@ func (s *syncStreamHandlerImpl) SyncID() string {
 	return s.syncID
 }
 
-func (s *syncStreamHandlerImpl) Modify(req *ModifySyncRequest) *ModifySyncResponse {
-	// TODO: implement me
+func (s *syncStreamHandlerImpl) Modify(req *ModifySyncRequest) (*ModifySyncResponse, error) {
+	// Perform an initial validation of the request before processing further.
+	if err := validateModifySync(req); err != nil {
+		return nil, err
+	}
 
 	var res ModifySyncResponse
-	for _, stream := range req.AddStreams {
-		if err := s.streamUpdatesBus.Subscribe(StreamId(stream.GetStreamId()), s); err != nil {
+
+	for _, cookie := range req.GetAddStreams() {
+		if err := s.eventBus.Subscribe(cookie, s); err != nil {
+			rvrErr := AsRiverError(err)
 			res.Adds = append(res.Adds, &SyncStreamOpStatus{
-				StreamId:    stream.GetStreamId(),
-				Code:        1, // TODO??
-				Message:     err.Error(),
-				NodeAddress: nil, // TODO??
+				StreamId: cookie.GetStreamId(),
+				Code:     int32(rvrErr.Code),
+				Message:  rvrErr.GetMessage(),
 			})
 		}
 	}
 
-	for _, streamID := range req.RemoveStreams {
-		s.streamUpdatesBus.Unsubscribe(StreamId(streamID), s)
+	for _, streamID := range req.GetRemoveStreams() {
+		if err := s.eventBus.Unsubscribe(StreamId(streamID), s); err != nil {
+			rvrErr := AsRiverError(err)
+			res.Removals = append(res.Removals, &SyncStreamOpStatus{
+				StreamId: streamID[:],
+				Code:     int32(rvrErr.Code),
+				Message:  rvrErr.GetMessage(),
+			})
+		}
 	}
 
-	for _, cookie := range req.BackfillStreams.Streams {
-		if err := s.streamUpdatesBus.Backfill(cookie, s.syncID, req.BackfillStreams.GetSyncId()); err != nil {
+	for _, cookie := range req.GetBackfillStreams().GetStreams() {
+		if err := s.eventBus.Backfill(cookie, s.syncID, req.BackfillStreams.GetSyncId()); err != nil {
+			rvrErr := AsRiverError(err)
 			res.Backfills = append(res.Backfills, &SyncStreamOpStatus{
-				StreamId:    cookie.GetStreamId(),
-				Code:        1, // TODO??
-				Message:     err.Error(),
-				NodeAddress: nil, // TODO??
+				StreamId: cookie.GetStreamId(),
+				Code:     int32(rvrErr.Code),
+				Message:  rvrErr.GetMessage(),
 			})
 		}
 	}
 
-	return &res
+	return &res, nil
 }
 
 func (s *syncStreamHandlerImpl) Cancel(ctx context.Context) error {
-	// TODO implement me
-	panic("implement me")
+	select {
+	case <-s.ctx.Done():
+		return s.ctx.Err()
+	default:
+	}
+
+	if err := s.streamUpdates.AddMessage(&SyncStreamsResponse{
+		SyncOp: SyncOp_SYNC_CLOSE,
+	}); err != nil {
+		return AsRiverError(err).
+			Func("syncStreamHandlerImpl.Cancel").
+			Message("failed to add close message to the stream updates queue")
+	}
+
+	// Operation is expected to be cancelled by the updates processor.
+	select {
+	case <-time.After(defaultCommandReplyTimeout):
+		return RiverError(Err_DEADLINE_EXCEEDED, "sync operation could not be cancelled in time")
+	case <-ctx.Done():
+		return AsRiverError(ctx.Err(), Err_CANCELED).Message("request context cancelled")
+	case <-s.ctx.Done():
+		return nil
+	}
 }
 
-func (s *syncStreamHandlerImpl) Ping(ctx context.Context) {
-	// TODO implement me
-	panic("implement me")
+func (s *syncStreamHandlerImpl) Ping(_ context.Context, nonce string) {
+	select {
+	case <-s.ctx.Done():
+		return
+	default:
+	}
+
+	if err := s.streamUpdates.AddMessage(&SyncStreamsResponse{
+		SyncOp:    SyncOp_SYNC_PONG,
+		PongNonce: nonce,
+	}); err != nil {
+		s.log.Errorw("failed to add ping message to the stream updates queue", "error", err)
+	}
+}
+
+func (s *syncStreamHandlerImpl) DebugDropStream(ctx context.Context, streamId StreamId) error {
+	select {
+	case <-s.ctx.Done():
+		return s.ctx.Err()
+	default:
+	}
+
+	if err := s.eventBus.Unsubscribe(streamId, s); err != nil {
+		return err
+	}
+
+	return s.streamUpdates.AddMessage(&SyncStreamsResponse{
+		SyncOp:   SyncOp_SYNC_DOWN,
+		StreamId: streamId[:],
+	})
 }
 
 // OnUpdate implements the eventbus.StreamSubscriber interface.
+// It is expected to receive SyncOp_SYNC_UPDATE and SyncOp_SYNC_DOWN messages here.
 func (s *syncStreamHandlerImpl) OnUpdate(update *SyncStreamsResponse) {
-	// TODO implement me
+	select {
+	case <-s.ctx.Done():
+		return
+	default:
+	}
 
-	// set syncID as exchanged with client
-	update.SyncId = s.syncID
+	// Add update to internal queue
+	if err := s.streamUpdates.AddMessage(update); err != nil {
+		s.cancel(err)
+	}
+}
 
-	// add update to internal queue
+// startUpdatesProcessor starts the updates processor that sends stream updates to the receiver.
+func (s *syncStreamHandlerImpl) startUpdatesProcessor() {
+	var msgs []*SyncStreamsResponse
+	for {
+		select {
+		case <-s.ctx.Done():
+			return
+		case _, open := <-s.streamUpdates.Wait():
+			msgs = s.streamUpdates.GetBatch(msgs)
 
-	panic("implement me")
+			// nil msgs indicates the buffer is closed.
+			if msgs == nil {
+				_ = s.receiver.Send(&SyncStreamsResponse{
+					SyncId: s.syncID,
+					SyncOp: SyncOp_SYNC_CLOSE,
+				})
+				s.cancel(nil)
+				return
+			}
+
+			// Process each message in the batch.
+			// Messages must be processed in the order they were received.
+			for _, msg := range msgs {
+				if stop := s.processMessage(msg); stop {
+					return
+				}
+			}
+
+			// If the client sent a close message, stop sending messages to client from the buffer.
+			// In theory should not happen, but just in case.
+			if !open {
+				_ = s.receiver.Send(&SyncStreamsResponse{
+					SyncId: s.syncID,
+					SyncOp: SyncOp_SYNC_CLOSE,
+				})
+				s.cancel(nil)
+				return
+			}
+		}
+	}
+}
+
+// processMessage processes a single message from the stream updates queue.
+// Returns true if the processor should stop processing messages.
+func (s *syncStreamHandlerImpl) processMessage(msg *SyncStreamsResponse) bool {
+	// Check context before AND after preparing the message
+	select {
+	case <-s.ctx.Done():
+		return true
+	default:
+	}
+
+	select {
+	case <-s.ctx.Done():
+		return true
+	default:
+		msg.SyncId = s.syncID
+		if err := s.receiver.Send(msg); err != nil {
+			s.cancel(err)
+			return true
+		}
+	}
+
+	// Special cases depending on the message type that should be applied after sending the message.
+	if msg.GetSyncOp() == SyncOp_SYNC_CLOSE {
+		// Close the operation and return from the stream updates processor.
+		s.cancel(nil)
+		return true
+	}
+
+	return false
 }
 
 // NewRegistry creates a new instance of the Registry.
-func NewRegistry() Registry {
+func NewRegistry(eventBus eventbus.StreamSubscriptionManager) Registry {
 	return &syncStreamHandlerRegistryImpl{
 		handlers: make(map[string]*syncStreamHandlerImpl),
+		eventBus: eventBus,
 	}
 }
 
 // Get retrieves a sync stream handler by its sync ID.
 func (s *syncStreamHandlerRegistryImpl) Get(syncID string) (SyncStreamHandler, bool) {
+	s.handlersLock.Lock()
 	handler, ok := s.handlers[syncID]
+	s.handlersLock.Unlock()
 	return handler, ok
 }
 
 // New creates a new sync stream handler and registers it in the registry.
-func (s *syncStreamHandlerRegistryImpl) New(ctx context.Context, syncID string, receiver Receiver) SyncStreamHandler {
-	handler := &syncStreamHandlerImpl{
-		syncID:           syncID,
-		receiver:         receiver,
-		streamUpdatesBus: s.streamUpdatesBus,
+func (s *syncStreamHandlerRegistryImpl) New(
+	ctx context.Context,
+	syncID string,
+	receiver Receiver,
+) (SyncStreamHandler, error) {
+	s.handlersLock.Lock()
+	defer s.handlersLock.Unlock()
+
+	if _, exists := s.handlers[syncID]; exists {
+		return nil, RiverError(Err_ALREADY_EXISTS, "sync operation with the given ID already exists")
 	}
 
-	s.handlersLock.Lock()
-	s.handlers[handler.SyncID()] = handler
-	s.handlersLock.Unlock()
+	ctx, cancel := context.WithCancelCause(ctx)
 
-	return handler
+	handler := &syncStreamHandlerImpl{
+		ctx:           ctx,
+		cancel:        cancel,
+		log:           logging.FromCtx(ctx).Named("syncv3.handler").With("syncID", syncID),
+		syncID:        syncID,
+		receiver:      receiver,
+		eventBus:      s.eventBus,
+		streamUpdates: dynmsgbuf.NewDynamicBuffer[*SyncStreamsResponse](),
+	}
+
+	go handler.startUpdatesProcessor()
+
+	s.handlers[syncID] = handler
+
+	// The first message must be a SyncOp_SYNC_NEW message to notify the receiver about the new sync operation with ID.
+	if err := receiver.Send(&SyncStreamsResponse{
+		SyncId: syncID,
+		SyncOp: SyncOp_SYNC_NEW,
+	}); err != nil {
+		return nil, err
+	}
+
+	return handler, nil
 }
