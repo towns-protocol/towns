dba7aa59d64bcad519f21249e93cf20f39290912
texuf <texuf.eth@gmail.com>
2025-09-10 12:57:47 -0700

    Update envUtils to check for vite configs in the right place

--- packages/dlog/src/envUtils.ts
+++ packages/dlog/src/envUtils.ts
@@ -1,3 +1,9 @@
+declare global {
+    interface ImportMeta {
+        readonly env: Record<string, string>
+    }
+}
+
 export function safeEnv(keys: string[]): string | undefined {
     if (typeof process !== 'object') {
         return undefined
@@ -8,9 +14,18 @@ export function safeEnv(keys: string[]): string | undefined {
             return process.env[key]
         }
         // look for the key in process.env.VITE_ for vite apps
-        const viteKey = `VITE_${key}`
-        if (process.env[viteKey]) {
-            return process.env[viteKey]
+        if (typeof import.meta === 'object' && 'env' in import.meta) {
+            // first check if the key is directly set in the import.meta.env
+            // for server side rendering it could be passed in,
+            // or perhaps somehow you specified the vite in the key alread for some reason
+            if (import.meta.env[key]) {
+                return import.meta.env[key]
+            }
+            // otherwise check for the vite prefix, this is the only way to pass env variables to the web client
+            const viteKey = `VITE_${key}`
+            if (import.meta.env[viteKey]) {
+                return import.meta.env[viteKey]
+            }
         }
     }
     return undefined
