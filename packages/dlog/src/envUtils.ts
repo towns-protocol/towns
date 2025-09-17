0de398a1daa2507df9ad267287a09a84a6b46904
Theo Tillberg <ttillberg@gmail.com>
2025-09-17 11:45:38 +0200

    fix(dlog): import.meta causing issues outside modules
    also use "key in x" to prevent skip if value explicitly is set to "false" (probably redundant since it's all strings)

--- packages/dlog/src/envUtils.ts
+++ packages/dlog/src/envUtils.ts
@@ -8,22 +8,13 @@ export function safeEnv(keys: string[]): string | undefined {
     for (const key of keys) {
         // look for the key in process.env
         if (typeof process === 'object' && 'env' in process) {
-            if (process.env[key]) {
+            if (key in process.env) {
                 return process.env[key]
             }
-        }
-        // look for the key in process.env.VITE_ for vite apps
-        if (typeof import.meta === 'object' && 'env' in import.meta) {
-            // first check if the key is directly set in the import.meta.env
-            // for server side rendering it could be passed in,
-            // or perhaps somehow you specified the vite in the key alread for some reason
-            if (import.meta.env[key]) {
-                return import.meta.env[key]
-            }
-            // otherwise check for the vite prefix, this is the only way to pass env variables to the web client
+            // check for the vite prefix, this is the only way to pass env variables to the web client
             const viteKey = `VITE_${key}`
-            if (import.meta.env[viteKey]) {
-                return import.meta.env[viteKey]
+            if (viteKey in process.env) {
+                return process.env[viteKey]
             }
         }
     }
