2afd34833e0a3909ece431c28837824bbf7726d8
Bas van Kervel <basvankervel@gmail.com>
2025-09-19 09:34:15 +0200

    node: put generated bindings for app registry/account in own package

--- scripts/gen-river-node-bindings.sh
+++ scripts/gen-river-node-bindings.sh
@@ -56,8 +56,10 @@ generate_go base base XChain xchain
 generate_go base base IDiamond diamond
 generate_go base base IDelegateRegistryV1 IDelegateRegistryV1
 generate_go base base IMembership membership
-generate_go base base IAppRegistry app_registry
-generate_go base base IAppAccount app_account
+# put bot interfaces in unique packages because they share structures which will
+# otherwise lead to duplicate type definitions in generated go code.
+generate_go base/app_registry base_app_registry IAppRegistry app_registry
+generate_go base/app_account base_app_account IAppAccount app_account
 
 # Full Base (and other) contracts for deployment from tests
 generate_go base/deploy deploy MockCrossChainEntitlement mock_cross_chain_entitlement
