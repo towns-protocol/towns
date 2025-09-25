403b7567bfb99213784e1b95425d9b3ccb6ce3b3
Serge Khorun <serge@hntlabs.com>
2025-09-25 16:27:40 -0700

    fmt

--- core/node/events/stream_cache_test.go
+++ core/node/events/stream_cache_test.go
@@ -562,7 +562,11 @@ func TestMiniblockRegistrationWithPendingLocalCandidate(t *testing.T) {
 	// because the candidate was not promoted because the node never witnessed the set stream event.
 	riverChainBlockNum, err := instance.params.RiverChain.Client.BlockNumber(ctx)
 	require.NoError(err)
-	getStream, err := instance.params.Registry.StreamRegistry.GetStream(ctx, blockchain.BlockNumber(riverChainBlockNum), spaceStreamId)
+	getStream, err := instance.params.Registry.StreamRegistry.GetStream(
+		ctx,
+		blockchain.BlockNumber(riverChainBlockNum),
+		spaceStreamId,
+	)
 	require.NoError(err)
 
 	require.Equal(getStream.LastMbNum(), candidate.Ref.Num)
