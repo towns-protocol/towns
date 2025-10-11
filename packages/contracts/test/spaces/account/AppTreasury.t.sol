228b2745e961dfdb805ba3dc4f8c74bf6870033c
g <5714678+giuseppecrj@users.noreply.github.com>
2025-10-10 21:20:49 -0400

    feat(treasury): implement AppTreasury facet for fund management
    - Added AppTreasuryFacet for managing fund requests and stream configurations.
    - Introduced AppTreasuryBase and AppTreasuryStorage for handling treasury logic and state.
    - Implemented functions for requesting funds, configuring streams, and managing vouchers.
    - Added circuit breaker functionality to control fund withdrawals.
    - Updated DeploySpace to include AppTreasuryFacet in the deployment process.
    - Enhanced permissions with new ConfigureTreasury role for treasury operations.
    - Added tests for AppTreasury functionality and integration with MockSimpleApp.

---
+++ packages/contracts/test/spaces/account/AppTreasury.t.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.29;
+
+// interfaces
+import {AppAccountBaseTest} from "test/spaces/account/AppAccountBase.t.sol";
+
+// libraries
+
+// contracts
+import {MockSimpleApp} from "test/mocks/MockSimpleApp.sol";
+import {MockERC20} from "test/mocks/MockERC20.sol";
+import {console} from "forge-std/console.sol";
+
+contract AppTreasuryTest is AppAccountBaseTest {
+    function test_requestFunds() external {
+        address owner = _randomAddress();
+        address client = _randomAddress();
+
+        MockSimpleApp mockSimpleApp = _deployMockSimpleApp(owner);
+        MockERC20 mockERC20 = new MockERC20("Test Token", "TEST");
+
+        vm.label(address(mockSimpleApp), "MockSimpleApp");
+        vm.label(address(mockERC20), "Test Token");
+
+        _registerAppAs(owner, mockSimpleApp, client);
+        _installAppAs(founder, mockSimpleApp);
+
+        mockERC20.mint(address(appAccount), 10 ether);
+
+        vm.prank(founder);
+        appTreasury.configureStream(address(mockSimpleApp), address(mockERC20), 1 ether);
+
+        uint256 streamBalance = appTreasury.getStreamBalance(
+            address(mockSimpleApp),
+            address(mockERC20)
+        );
+        assertEq(streamBalance, 0);
+
+        vm.warp(block.timestamp + 1 seconds);
+
+        streamBalance = appTreasury.getStreamBalance(address(mockSimpleApp), address(mockERC20));
+        assertEq(streamBalance, 1 ether);
+
+        vm.prank(client);
+        appAccount.execute({
+            target: address(mockSimpleApp),
+            value: 0,
+            data: abi.encodeCall(mockSimpleApp.swap, (address(mockERC20)))
+        });
+    }
+}
