b9379621e2e9e0307599df17e2a94eb80ebc2bbe
g <5714678+giuseppecrj@users.noreply.github.com>
2025-10-17 15:57:09 -0400

    feat: introduce AppInstallerFacet for app management in spaces
    ### Description
    
    Added the AppInstallerFacet to facilitate the installation, uninstallation, updating, and renewing of apps within spaces. This new facet enhances the app management capabilities of the protocol.
    
    ### Changes
    
    - Created `AppInstallerFacet` contract with functions for installing, uninstalling, updating, and renewing apps.
    - Implemented deployment logic for the new facet in `DeployAppInstallerFacet`.
    - Updated `DeployAppRegistry` to include the new AppInstallerFacet in the deployment process.
    - Refactored existing AppRegistryFacet to remove app management functions now handled by AppInstallerFacet.
    - Updated tests to ensure proper functionality of the new app management features.
    
    ### Checklist
    
    - [x] Tests added where required
    - [x] Documentation updated where applicable
    - [x] Changes adhere to the repository's contribution guidelines

---
+++ packages/contracts/scripts/deployments/facets/DeployAppInstallerFacet.s.sol
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.23;
+
+//interfaces
+import {IDiamond} from "@towns-protocol/diamond/src/IDiamond.sol";
+
+//libraries
+
+//contracts
+import {LibDeploy} from "@towns-protocol/diamond/src/utils/LibDeploy.sol";
+import {AppInstallerFacet} from "src/apps/facets/installer/AppInstallerFacet.sol";
+import {DynamicArrayLib} from "solady/utils/DynamicArrayLib.sol";
+
+library DeployAppInstallerFacet {
+    using DynamicArrayLib for DynamicArrayLib.DynamicArray;
+
+    function selectors() internal pure returns (bytes4[] memory res) {
+        DynamicArrayLib.DynamicArray memory arr = DynamicArrayLib.p().reserve(4);
+        arr.p(AppInstallerFacet.installApp.selector);
+        arr.p(AppInstallerFacet.uninstallApp.selector);
+        arr.p(AppInstallerFacet.updateApp.selector);
+        arr.p(AppInstallerFacet.renewApp.selector);
+        bytes32[] memory selectors_ = arr.asBytes32Array();
+        assembly ("memory-safe") {
+            res := selectors_
+        }
+    }
+
+    function makeCut(
+        address facetAddress,
+        IDiamond.FacetCutAction action
+    ) internal pure returns (IDiamond.FacetCut memory) {
+        return
+            IDiamond.FacetCut({
+                action: action,
+                facetAddress: facetAddress,
+                functionSelectors: selectors()
+            });
+    }
+
+    function makeInitData() internal pure returns (bytes memory) {
+        return abi.encodeCall(AppInstallerFacet.__AppInstaller_init, ());
+    }
+
+    function deploy() internal returns (address) {
+        return LibDeploy.deployCode("AppInstallerFacet.sol", "");
+    }
+}
