5a1056432c1f6c56e62e55add6c9ebb0e9776944
begmaroman <begmaroman@gmail.com>
2025-10-22 22:58:02 +0100

    Linter

--- packages/contracts/scripts/interactions/helpers/RiverConfigValues.sol
+++ packages/contracts/scripts/interactions/helpers/RiverConfigValues.sol
@@ -19,5 +19,6 @@ library RiverConfigValues {
     bytes32 public constant MB_RECENCY_CHECK = keccak256("stream.recencyconstraints.ageseconds");
     bytes32 public constant STREAM_HISTORY_MINIBLOCKS_USER_SETTINGS =
         keccak256("stream.historyminiblocks.a5");
-    bytes32 public constant STREAM_TRIM_ACTIVATION_FACTOR = keccak256("stream.trimactivationfactor");
+    bytes32 public constant STREAM_TRIM_ACTIVATION_FACTOR =
+        keccak256("stream.trimactivationfactor");
 }
