bebdd42392fd2c8f2f84d91734c8e6924e0792ce
Roman Behma <13855864+begmaroman@users.noreply.github.com>
2025-11-18 19:30:46 +0000

    Set stream history miniblocks onchain setting for space stream (#4552)

--- packages/contracts/scripts/interactions/helpers/RiverConfigValues.sol
+++ packages/contracts/scripts/interactions/helpers/RiverConfigValues.sol
@@ -19,6 +19,8 @@ library RiverConfigValues {
     bytes32 public constant MB_RECENCY_CHECK = keccak256("stream.recencyconstraints.ageseconds");
     bytes32 public constant STREAM_HISTORY_MINIBLOCKS_USER_SETTINGS =
         keccak256("stream.historyminiblocks.a5");
+    bytes32 public constant STREAM_HISTORY_MINIBLOCKS_SPACE =
+        keccak256("stream.historyminiblocks.10");
     bytes32 public constant STREAM_TRIM_ACTIVATION_FACTOR =
         keccak256("stream.trimactivationfactor");
 }
