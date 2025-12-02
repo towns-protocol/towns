9377c81f739cc8c7c3c492552e4d404ef016a9a3
Bas van Kervel <basvankervel@gmail.com>
2025-12-02 09:15:27 +0100

    panic on deadlock after 10 min

--- core/cmd/root_cmd.go
+++ core/cmd/root_cmd.go
@@ -103,10 +103,10 @@ func initConfigAndLog() {
 
 func init() {
 	deadlock.Opts.WriteLocked(func() {
-		deadlock.Opts.DeadlockTimeout = time.Minute
-		deadlock.Opts.MaxMapSize = 1024 * 256
-		deadlock.Opts.PrintAllCurrentGoroutines = false
-		deadlock.Opts.OnPotentialDeadlock = func() {} // Do not panic and continue execution
+		deadlock.Opts.DeadlockTimeout = 10 * time.Minute
+		deadlock.Opts.MaxMapSize = 25 * 1024 * 256
+		deadlock.Opts.PrintAllCurrentGoroutines = true
+		//deadlock.Opts.OnPotentialDeadlock = func() {} // Do not panic and continue execution
 		// TODO: make params configurable (move this section after config initialization) and wire zap logger to get better integration with log processors
 	})
 
