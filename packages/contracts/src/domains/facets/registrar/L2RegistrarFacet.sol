eeeb1e577325db85622a1f2b76686c8e1fb2a9c4
g <5714678+giuseppecrj@users.noreply.github.com>
2025-12-31 13:01:22 -0500

    initial setup

---
+++ packages/contracts/src/domains/facets/registrar/L2RegistrarFacet.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.29;
+
+// interfaces
+
+// libraries
+
+// contracts
+import {Facet} from "@towns-protocol/diamond/src/facets/Facet.sol";
+
+contract L2RegistrarFacet is Facet {}
